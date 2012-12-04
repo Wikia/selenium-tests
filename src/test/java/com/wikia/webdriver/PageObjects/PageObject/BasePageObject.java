f9e2ec91bbb05e6f002f9484e74dd9a967aada7d
kkarolk <karol@wikia-inc.com>
2012-12-04 15:38:01 +0100

    cosmetics

--- src/test/java/com/wikia/webdriver/PageObjects/PageObject/BasePageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjects/PageObject/BasePageObject.java
@@ -1,7 +1,5 @@
 package com.wikia.webdriver.PageObjects.PageObject;
 
-import static org.testng.AssertJUnit.fail;
-
 import java.util.Date;
 import java.util.List;
 
@@ -9,7 +7,6 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -19,8 +16,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import com.google.common.base.Throwables;
 import com.wikia.webdriver.Common.Core.CommonExpectedConditions;
 import com.wikia.webdriver.Common.Core.CommonFunctions;
 import com.wikia.webdriver.Common.Core.Global;
