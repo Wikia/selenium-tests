87134e6601f495bf2ea09bce922d947928668739
kkarolk <karol@wikia-inc.com>
2013-02-05 16:23:50 +0100

    unused imports removed from TestCaseTemplate.java

--- src/test/java/com/wikia/webdriver/TestCases/TestCaseTemplate.java
+++ src/test/java/com/wikia/webdriver/TestCases/TestCaseTemplate.java
@@ -3,8 +3,6 @@ package com.wikia.webdriver.TestCases;
 import org.testng.annotations.Test;
 
 import com.wikia.webdriver.Common.Templates.TestTemplate;
-import com.wikia.webdriver.PageObjectsFactory.PageObject.CreateNewWiki.CreateNewWikiLogInPageObject;
-import com.wikia.webdriver.PageObjectsFactory.PageObject.CreateNewWiki.CreateNewWikiPageObjectStep1;
 
 public class TestCaseTemplate extends TestTemplate
 {
