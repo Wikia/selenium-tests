44fbef4d7b074f23604c84c83a807d0fa616f083
kkarolk <karol@wikia-inc.com>
2013-05-13 12:48:00 +0200

    Missing page objects for special pages added

---
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/Preferences/EditingPageObject.java
@@ -0,0 +1,11 @@
+package com.wikia.webdriver.PageObjectsFactory.PageObject.Special.Preferences;
+
+import org.openqa.selenium.WebDriver;
+
+public class EditingPageObject extends PreferencesPageObject {
+
+	public EditingPageObject(WebDriver driver) {
+		super(driver);
+	}
+
+}
---
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/Preferences/EmailPageObject.java
@@ -0,0 +1,11 @@
+package com.wikia.webdriver.PageObjectsFactory.PageObject.Special.Preferences;
+
+import org.openqa.selenium.WebDriver;
+
+public class EmailPageObject extends PreferencesPageObject{
+
+	public EmailPageObject(WebDriver driver) {
+		super(driver);
+	}
+
+}
