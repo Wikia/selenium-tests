777124c69527eed7b1cf99f4ef21b1ab0e068e10
Jonathan Robson <jnrbsn@gmail.com>
2013-05-15 19:27:37 +0200

    DAR-153 Added openWallPage() method.

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Wall/WallPageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Wall/WallPageObject.java
@@ -1,13 +1,29 @@
 package com.wikia.webdriver.PageObjectsFactory.PageObject.Wall;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import com.wikia.webdriver.Common.Core.Global;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.BasePageObject;
 
 public class WallPageObject extends BasePageObject {
 
+	@FindBy(css="textarea#WallMessageTitle")
+	private WebElement wallMessageTitle;
+
 	public WallPageObject(WebDriver driver) {
 		super(driver);
+	}
+
+	/**
+	 * Opens the message wall for a user.
+	 *
+	 * @param userName
+	 */
+	public void openWallPage(String userName) {
+		getUrl(Global.DOMAIN+"wiki/Message_Wall:"+userName);
+		waitForElementByElement(wallMessageTitle);
 	}
 
 }
