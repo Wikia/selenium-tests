c4d99ee62be533e2efde966837f8e7060dd9d3d4
Jonathan Robson <jnrbsn@gmail.com>
2013-05-15 17:42:37 +0200

    Added empty WallPageObject class.

---
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Wall/WallPageObject.java
@@ -0,0 +1,13 @@
+package com.wikia.webdriver.PageObjectsFactory.PageObject.Wall;
+
+import org.openqa.selenium.WebDriver;
+
+import com.wikia.webdriver.PageObjectsFactory.PageObject.BasePageObject;
+
+public class WallPageObject extends BasePageObject {
+
+	public WallPageObject(WebDriver driver) {
+		super(driver);
+	}
+
+}
