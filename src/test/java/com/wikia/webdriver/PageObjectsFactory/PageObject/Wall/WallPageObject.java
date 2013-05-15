f65cfd31b43312620a51c1269aac6b198a6d44a3
Jonathan Robson <jnrbsn@gmail.com>
2013-05-15 19:47:48 +0200

    DAR-153 Added logging for openWallPage() method.

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Wall/WallPageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Wall/WallPageObject.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import com.wikia.webdriver.Common.Core.Global;
+import com.wikia.webdriver.Common.Logging.PageObjectLogging;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.BasePageObject;
 
 public class WallPageObject extends BasePageObject {
@@ -24,6 +25,7 @@ public class WallPageObject extends BasePageObject {
 	public void openWallPage(String userName) {
 		getUrl(Global.DOMAIN+"wiki/Message_Wall:"+userName);
 		waitForElementByElement(wallMessageTitle);
+		PageObjectLogging.log("openWallPage", "message wall page opened", true);
 	}
 
 }
