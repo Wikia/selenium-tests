51660b536ff5ba6ec157c2f60698ce1566caf666
mech <mech@wikia-inc.com>
2013-05-16 12:37:16 +0200

    Adding a dummy test for wall notifications

---
+++ src/test/java/com/wikia/webdriver/TestCases/NotificationsTests/WallNotificationsTests.java
@@ -0,0 +1,22 @@
+package com.wikia.webdriver.TestCases.NotificationsTests;
+
+import org.testng.annotations.Test;
+
+import com.wikia.webdriver.Common.Core.CommonFunctions;
+import com.wikia.webdriver.Common.Properties.Properties;
+import com.wikia.webdriver.Common.Templates.TestTemplate;
+import com.wikia.webdriver.PageObjectsFactory.ComponentObject.Notifications.NotificationsComponentObject;
+
+public class WallNotificationsTests extends TestTemplate {
+	
+	@Test(groups= {"NotificationsTests", "Wall"} )
+	public void wallNotificationsTests_001_wallOwnerReceivesANotification() {
+		CommonFunctions.logOut(driver);
+		CommonFunctions.logIn(Properties.userName, Properties.password, driver);
+		
+		NotificationsComponentObject notifications = new NotificationsComponentObject(driver);
+		notifications.showNotifications();
+		notifications.clickNotifications();
+		System.out.println("MECH " + notifications.getNumberOfUnderNotifications());
+	}
+}
