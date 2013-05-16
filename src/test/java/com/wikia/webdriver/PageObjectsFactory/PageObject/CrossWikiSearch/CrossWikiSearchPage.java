7275bd7c6c944edc60478b1937bb2fbe74375e1f
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-05-16 15:49:23 +0200

    Fix indent and wrong class name

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/CrossWikiSearch/CrossWikiSearchPage.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/CrossWikiSearch/CrossWikiSearchPage.java
@@ -18,108 +18,112 @@ import java.util.List;
  */
 public class CrossWikiSearchPage extends BasePageObject {
 
-    @FindBy(css="#search-v2-input")
-    protected WebElement searchInput;
-    @FindBy(css="#search-v2-button")
-    protected WebElement searchButton;
-    @FindBy(css=".Results")
-    protected WebElement resultsContainer;
-    @FindBy(css=".Results > :nth-child(1)")
-    protected WebElement firstResult;
-    @FindBy(css=".Results > :nth-child(1) h1 > a")
-    protected WebElement firstResultLink;
-    @FindBy(css=".Results > :nth-child(1) > .result-description > :nth-child(2)")
-    protected WebElement firstResultVertical;
-    @FindBy(css=".Results > :nth-child(1) .wiki-statistics.subtle")
-    protected WebElement firstResultStatistics;
-    @FindBy(css=".Results > :nth-child(1) .wiki-statistics.subtle > :nth-child(1)")
-    protected WebElement firstResultStatisticsPageCount;
-    @FindBy(css=".Results > :nth-child(1) .wiki-statistics.subtle > :nth-child(2)")
-    protected WebElement firstResultStatisticsPageImages;
-    @FindBy(css=".Results > :nth-child(1) .wiki-statistics.subtle > :nth-child(3)")
-    protected WebElement firstResultStatisticsPageVideos;
+	@FindBy(css="#search-v2-input")
+	protected WebElement searchInput;
+	@FindBy(css="#search-v2-button")
+	protected WebElement searchButton;
+	@FindBy(css=".Results")
+	protected WebElement resultsContainer;
+	@FindBy(css=".Results > :nth-child(1)")
+	protected WebElement firstResult;
+	@FindBy(css=".Results > :nth-child(1) h1 > a")
+	protected WebElement firstResultLink;
+	@FindBy(css=".Results > :nth-child(1) > .result-description > :nth-child(2)")
+	protected WebElement firstResultVertical;
+	@FindBy(css=".Results > :nth-child(1) .wiki-statistics.subtle")
+	protected WebElement firstResultStatistics;
+	@FindBy(css=".Results > :nth-child(1) .wiki-statistics.subtle > :nth-child(1)")
+	protected WebElement firstResultStatisticsPageCount;
+	@FindBy(css=".Results > :nth-child(1) .wiki-statistics.subtle > :nth-child(2)")
+	protected WebElement firstResultStatisticsPageImages;
+	@FindBy(css=".Results > :nth-child(1) .wiki-statistics.subtle > :nth-child(3)")
+	protected WebElement firstResultStatisticsPageVideos;
 
-    @FindBy(css=".paginator-next.button.secondary")
-    protected WebElement paginatorNextButton;
-    @FindBy(css=".paginator-prev.button.secondary")
-    protected WebElement paginatorPrevButton;
+	@FindBy(css=".paginator-next.button.secondary")
+	protected WebElement paginatorNextButton;
+	@FindBy(css=".paginator-prev.button.secondary")
+	protected WebElement paginatorPrevButton;
 
-    protected By results = By.cssSelector(".Results .result");
-    protected By resultLinks = By.cssSelector(".Results .result > a");
+	protected By results = By.cssSelector(".Results .result");
+	protected By resultLinks = By.cssSelector(".Results .result > a");
 
-    public CrossWikiSearchPage(WebDriver driver) {
-        super(driver);
-    }
+	public CrossWikiSearchPage(WebDriver driver) {
+		super(driver);
+	}
 
 
-    public void verifyFirstResultTitle(String wikiName) {
-        waitForTextToBePresentInElementByElement(firstResultLink, wikiName);
-    }
+	public void verifyFirstResultTitle(String wikiName) {
+		waitForTextToBePresentInElementByElement(firstResultLink, wikiName);
+	}
 
-    public void verifyFirstResultVertical(String vertical) {
-        waitForTextToBePresentInElementByElement(firstResultVertical, vertical);
-        Assertion.assertFalse(firstResultVertical.getText().isEmpty(), "Vertical (Hub) string is empty.");
-    }
+	public void verifyFirstResultVertical(String vertical) {
+		waitForTextToBePresentInElementByElement(firstResultVertical, vertical);
+		Assertion.assertFalse(firstResultVertical.getText().isEmpty(), "Vertical (Hub) string is empty.");
+	}
 
-    public void verifyFirstResultDescription() {
-        waitForElementByElement(firstResultVertical);
-        Assertion.assertFalse(firstResult.getText().isEmpty(), "There is no article description.");
-    }
+	public void verifyFirstResultDescription() {
+		waitForElementByElement(firstResultVertical);
+		Assertion.assertFalse(firstResult.getText().isEmpty(), "There is no article description.");
+	}
 
-    public void verifyFirstResultPageCount() {
-        waitForElementByElement(firstResultStatisticsPageCount);
-        Assertion.assertFalse(firstResultStatisticsPageCount.getText().isEmpty(), "Page count string is empty.");
-    }
+	public void verifyFirstResultPageCount() {
+		waitForElementByElement(firstResultStatisticsPageCount);
+		Assertion.assertFalse(firstResultStatisticsPageCount.getText().isEmpty(), "Page count string is empty.");
+	}
 
-    public void verifyFirstResultPageImages() {
-        waitForElementByElement(firstResultStatisticsPageImages);
-        Assertion.assertFalse(firstResultStatisticsPageImages.getText().isEmpty(), "Images count is empty.");
-    }
+	public void verifyFirstResultPageImages() {
+		waitForElementByElement(firstResultStatisticsPageImages);
+		Assertion.assertFalse(firstResultStatisticsPageImages.getText().isEmpty(), "Images count is empty.");
+	}
 
-    public void verifyFirstResultPageVideos() {
-        waitForElementByElement(firstResultStatisticsPageVideos);
-        Assertion.assertFalse(firstResultStatisticsPageVideos.getText().isEmpty(), "Results count is empty.");
-    }
+	public void verifyFirstResultPageVideos() {
+		waitForElementByElement(firstResultStatisticsPageVideos);
+		Assertion.assertFalse(firstResultStatisticsPageVideos.getText().isEmpty(), "Results count is empty.");
+	}
 
-    public void verifyResultsCount( int resultsPerPage ) {
-        waitForElementByElement(resultsContainer);
-        List<WebElement> elements = driver.findElements(resultLinks);
-        Assertion.assertEquals( elements.size(), resultsPerPage, "Wrong number of results per page.");
-    }
+	 /**
+	 * Verify that result count is same as expected
+	 * @param expectedResultsPerPage
+	 */
+	public void verifyResultsCount( int expectedResultsPerPage ) {
+		waitForElementByElement(resultsContainer);
+		List<WebElement> elements = driver.findElements(resultLinks);
+		Assertion.assertEquals( elements.size(), expectedResultsPerPage, "Wrong number of results per page.");
+	}
 
-    public void verifyResultsPosForPage(int pageNumber, int resultsPerPage) {
-        waitForElementByElement(resultsContainer);
-        List<WebElement> elements = driver.findElements(resultLinks);
-        int curNo = pageNumber * resultsPerPage + 1;
-        for(WebElement link: elements) {
-            String dataPos = link.getAttribute("data-pos");
-            int pos = Integer.parseInt(dataPos);
-            Assertion.assertEquals( pos, curNo, "Wrong data-pos. Verify paging.");
-            curNo++;
-        }
-    }
+	public void verifyResultsPosForPage(int pageNumber, int resultsPerPage) {
+		waitForElementByElement(resultsContainer);
+		List<WebElement> elements = driver.findElements(resultLinks);
+		int curNo = pageNumber * resultsPerPage + 1;
+		for(WebElement link: elements) {
+			String dataPos = link.getAttribute("data-pos");
+			int pos = Integer.parseInt(dataPos);
+			Assertion.assertEquals( pos, curNo, "Wrong data-pos. Verify paging.");
+			curNo++;
+		}
+	}
 
-    public WikiArticleHomePage openResult(int no) {
-        WebElement webElement = getResultWikiNameLink(no);
-        clickAndWait(webElement);
-        return new WikiArticleHomePage(driver);
-    }
+	public WikiArticleHomePage openResult(int no) {
+		WebElement webElement = getResultWikiNameLink(no);
+		clickAndWait(webElement);
+		return new WikiArticleHomePage(driver);
+	}
 
-    public CrossWikiSearchPage prevPage() {
-        clickAndWait(paginatorPrevButton);
-        PageObjectLogging.log("prevPage", "Moving to prev page of search results.",
-                true, driver);
-        return new CrossWikiSearchPage(driver);
-    }
+	public CrossWikiSearchPage prevPage() {
+		clickAndWait(paginatorPrevButton);
+		PageObjectLogging.log("prevPage", "Moving to prev page of search results.",
+				true, driver);
+		return new CrossWikiSearchPage(driver);
+	}
 
-    public CrossWikiSearchPage nextPage() {
-        clickAndWait(paginatorNextButton);
-        PageObjectLogging.log("nextPage", "Moving to next page of search results.",
-                true, driver);
-        return new CrossWikiSearchPage(driver);
-    }
+	public CrossWikiSearchPage nextPage() {
+		clickAndWait(paginatorNextButton);
+		PageObjectLogging.log("nextPage", "Moving to next page of search results.",
+				true, driver);
+		return new CrossWikiSearchPage(driver);
+	}
 
-    protected WebElement getResultWikiNameLink(int no) {
-        return driver.findElements(resultLinks).get(no);
-    }
+	protected WebElement getResultWikiNameLink(int no) {
+		return driver.findElements(resultLinks).get(no);
+	}
 }
--- src/test/java/com/wikia/webdriver/TestCases/SearchTests/CrossWikiSearchTests.java
+++ src/test/java/com/wikia/webdriver/TestCases/SearchTests/CrossWikiSearchTests.java
@@ -1,9 +1,12 @@
 package com.wikia.webdriver.TestCases.SearchTests;
 
+import com.wikia.webdriver.Common.ContentPatterns.PageContent;
 import com.wikia.webdriver.Common.DataProvider.CrossWikiSearchProvider;
+import com.wikia.webdriver.Common.DataProvider.SearchDataProvider;
 import com.wikia.webdriver.Common.Templates.TestTemplate;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.CrossWikiSearch.CrossWikiSearchPage;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.HomePageObject;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.Special.SpecialSearchPageObject;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.WikiPage.WikiArticleHomePage;
 import org.testng.annotations.Test;
 
@@ -13,66 +16,76 @@ import org.testng.annotations.Test;
  * Time: 11:22
  */
 public class CrossWikiSearchTests extends TestTemplate {
-    private static final int resultsPerPage = 7;
-    private static final String searchPhrase = "muppets";
+	private static final int resultsPerPage = 7;
+	private static final String searchPhrase = "muppets";
 
-    @Test(dataProviderClass = CrossWikiSearchProvider.class,
-            dataProvider = "getExactMatchQueries",
-            groups = {"CrossWikiSearchTests", "CrossWikiSearchTests_ExactMatch" })
-    public void testExactMatch(String query, String wikiName, String vertical) {
-        HomePageObject home = new HomePageObject(driver);
-        home.openHomePage();
-        CrossWikiSearchPage searchPage = home.searchFor(query);
-        searchPage.verifyFirstResultTitle(wikiName);
-        searchPage.verifyFirstResultVertical(vertical);
-        searchPage.verifyFirstResultDescription();
-        searchPage.verifyFirstResultPageCount();
-        searchPage.verifyFirstResultPageImages();
-        searchPage.verifyFirstResultPageVideos();
-    }
+	@Test(dataProviderClass = CrossWikiSearchProvider.class,
+			dataProvider = "getExactMatchQueries",
+			groups = {"CrossWikiSearchTests", "CrossWikiSearchTests_ExactMatch" })
+	public void testExactMatch(String query, String wikiName, String vertical) {
+		HomePageObject home = new HomePageObject(driver);
+		home.openHomePage();
+		CrossWikiSearchPage searchPage = home.searchFor(query);
+		searchPage.verifyFirstResultTitle(wikiName);
+	searchPage.verifyFirstResultVertical(vertical);
+		searchPage.verifyFirstResultDescription();
+		searchPage.verifyFirstResultPageCount();
+		searchPage.verifyFirstResultPageImages();
+		searchPage.verifyFirstResultPageVideos();
+	}
 
-    @Test(groups= {"CrossWikiSearchTests_Pagination_001"
-                 , "CrossWikiSearchTests"
-                 , "CrossWikiSearchTests_Pagination"} )
-    public void crossWikiSearchTests_Pagination_001() {
-        HomePageObject home = new HomePageObject(driver);
-        home.openHomePage();
-        CrossWikiSearchPage searchPage = home.searchFor(searchPhrase);
+	@Test(groups= {"CrossWikiSearchTests_Pagination_001"
+				 , "CrossWikiSearchTests"
+				 , "CrossWikiSearchTests_Pagination"} )
+	public void crossWikiSearchTests_Pagination_001() {
+		HomePageObject home = new HomePageObject(driver);
+		home.openHomePage();
+		CrossWikiSearchPage searchPage = home.searchFor(searchPhrase);
 
-        // verify results pos parameter for first page
-        searchPage.verifyResultsPosForPage(0, resultsPerPage);
-        searchPage.verifyResultsCount(resultsPerPage);
-        searchPage.nextPage();
-        // verify results pos parameter for second page
-        searchPage.verifyResultsPosForPage(1, resultsPerPage);
-        searchPage.verifyResultsCount(resultsPerPage);
-        searchPage.prevPage();
-        // verify results pos parameter for first page
-        searchPage.verifyResultsPosForPage(0, resultsPerPage);
-        searchPage.verifyResultsCount(resultsPerPage);
-    }
+		// verify results pos parameter for first page
+		searchPage.verifyResultsPosForPage(0, resultsPerPage);
+		searchPage.verifyResultsCount(resultsPerPage);
+		searchPage.nextPage();
+		// verify results pos parameter for second page
+		searchPage.verifyResultsPosForPage(1, resultsPerPage);
+		searchPage.verifyResultsCount(resultsPerPage);
+		searchPage.prevPage();
+		// verify results pos parameter for first page
+		searchPage.verifyResultsPosForPage(0, resultsPerPage);
+		searchPage.verifyResultsCount(resultsPerPage);
+	}
 
-    @Test(groups= {"CrossWikiSearchTests_ResultClick_001"
-                 , "CrossWikiSearchTests"
-                 , "CrossWikiSearchTests_ResultClick"} )
-    public void CrossWikiSearchTests_ResultClick_001() {
-        HomePageObject home = new HomePageObject(driver);
-        home.openHomePage();
-        CrossWikiSearchPage searchPage = home.searchFor(searchPhrase);
+	@Test(groups= {"CrossWikiSearchTests_ResultClick_001"
+				 , "CrossWikiSearchTests"
+				 , "CrossWikiSearchTests_ResultClick"} )
+	public void CrossWikiSearchTests_ResultClick_001() {
+		HomePageObject home = new HomePageObject(driver);
+		home.openHomePage();
+		CrossWikiSearchPage searchPage = home.searchFor(searchPhrase);
 
-        WikiArticleHomePage wikiArticleHomePage = searchPage.openResult(0);
-        wikiArticleHomePage.verifyThisIsWikiHomePage();
-    }
+		WikiArticleHomePage wikiArticleHomePage = searchPage.openResult(0);
+		wikiArticleHomePage.verifyThisIsWikiHomePage();
+	}
 
-    @Test(groups= {"CrossWikiSearchTests_ResultClick_002"
-                 , "CrossWikiSearchTests"
-                 , "CrossWikiSearchTests_ResultClick"} )
-    public void CrossWikiSearchTests_ResultClick_002() {
-        HomePageObject home = new HomePageObject(driver);
-        home.openHomePage();
-        CrossWikiSearchPage searchPage = home.searchFor(searchPhrase);
+	@Test(groups= {"CrossWikiSearchTests_ResultClick_002"
+				 , "CrossWikiSearchTests"
+				 , "CrossWikiSearchTests_ResultClick"} )
+	public void CrossWikiSearchTests_ResultClick_002() {
+		HomePageObject home = new HomePageObject(driver);
+		home.openHomePage();
+		CrossWikiSearchPage searchPage = home.searchFor(searchPhrase);
 
-        WikiArticleHomePage wikiArticleHomePage = searchPage.openResult(1);
-        wikiArticleHomePage.verifyThisIsWikiHomePage();
-    }
+		WikiArticleHomePage wikiArticleHomePage = searchPage.openResult(1);
+		wikiArticleHomePage.verifyThisIsWikiHomePage();
+	}
+
+	@Test(dataProviderClass=SearchDataProvider.class,
+			dataProvider="getCrossWikiTermsAndUrls",
+			groups = {"CrossWikiSearch_001_wikimatch", "", "Search"})
+	public void CrossWikiSearch_001_wikimatch( String searchTerm, String expectedUrl ) {
+		SpecialSearchPageObject search = new SpecialSearchPageObject(driver);
+		search.goToSearchPage(PageContent.wikiaGlobalUrl);
+		SpecialSearchPageObject searched = search.searchFor(searchTerm);
+		searched.verifyMatchResultUrl(expectedUrl);
+	}
 }
