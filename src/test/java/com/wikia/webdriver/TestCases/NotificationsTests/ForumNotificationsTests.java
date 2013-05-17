f2aaf4d5a11648b3146b724de020e104488146c8
michal.nowierski@wikia-inc.com <michal.nowierski@wikia-inc.com>
2013-05-17 15:49:01 +0200

    adding Two users leave four replies test

--- src/test/java/com/wikia/webdriver/TestCases/NotificationsTests/ForumNotificationsTests.java
+++ src/test/java/com/wikia/webdriver/TestCases/NotificationsTests/ForumNotificationsTests.java
@@ -319,4 +319,76 @@ public class ForumNotificationsTests extends TestTemplate {
 		String anchor = anchoredLink.substring(anchoredLink.indexOf("#"));
 		Assertion.assertEquals("#2", anchor);
 	}
+	/**
+	 * User A posts a thread, user B leaves a 1 reply to this thread and then
+	 * User C leaves 1 reply. User A verifies if the anchor is correct
+	 */
+	@Test(groups = { "ForumNotificationsTests_007", "ForumNotificationsTests",
+			"Forum", "NotificationsTests" })
+	public void forumNotificationsTests_007_notificationsRepliesAnchor_TwoUsersLeaveFourReplies() {
+		// user 1 creates a thread
+		CommonFunctions.logOut(driver);
+		ForumPageObject forumMainPage = new ForumPageObject(driver);
+		CommonFunctions.logIn(Properties.userName, Properties.password, driver);
+		title = PageContent.forumTitlePrefix + forumMainPage.getTimeStamp();
+		message = PageContent.forumMessage + forumMainPage.getTimeStamp();
+		forumMainPage.openForumMainPage();
+		ForumBoardPageObject forumBoard = forumMainPage.openForumBoard(1);
+		ForumThreadPageObject forumThread = forumBoard.startDiscussion(title,
+				message, false);
+		forumThread.verifyDiscussionTitleAndMessage(title, message);
+		CommonFunctions.logOut(driver);
+		CommonFunctions.logIn(Properties.userName2, Properties.password2,
+				driver);
+		forumMainPage.openForumMainPage();
+		forumBoard = forumMainPage.openForumBoard(1);
+		forumThread = forumBoard.openDiscussion(title);
+		// user 2 leaves 1 replies on user 1 thread and logs out
+		forumThread.reply(message);
+		forumThread.verifyReplyMessage(1, message);
+		CommonFunctions.logOut(driver);
+		forumMainPage = new ForumPageObject(driver);
+		CommonFunctions.logIn(Properties.userNameStaff,
+				Properties.passwordStaff, driver);
+		forumMainPage.openForumMainPage();
+		forumBoard = forumMainPage.openForumBoard(1);
+		forumThread = forumBoard.openDiscussion(title);
+		// user 3 leaves 1 replies on user 1 thread and logs out
+		forumThread.reply(message);
+		forumThread.verifyReplyMessage(1, message);
+		CommonFunctions.logOut(driver);
+		CommonFunctions.logIn(Properties.userName2, Properties.password2,
+				driver);
+		forumMainPage.openForumMainPage();
+		forumBoard = forumMainPage.openForumBoard(1);
+		forumThread = forumBoard.openDiscussion(title);
+		// user 2 leaves 1 replies on user 1 thread and logs out
+		forumThread.reply(message);
+		forumThread.verifyReplyMessage(1, message);
+		CommonFunctions.logOut(driver);
+		forumMainPage = new ForumPageObject(driver);
+		CommonFunctions.logIn(Properties.userNameStaff,
+				Properties.passwordStaff, driver);
+		forumMainPage.openForumMainPage();
+		forumBoard = forumMainPage.openForumBoard(1);
+		forumThread = forumBoard.openDiscussion(title);
+		// user 3 leaves 1 replies on user 1 thread and logs out
+		forumThread.reply(message);
+		forumThread.verifyReplyMessage(1, message);
+		CommonFunctions.logOut(driver);
+		// user 1 verifies his notifications
+		forumMainPage = new ForumPageObject(driver);
+		CommonFunctions.logIn(Properties.userName, Properties.password, driver);
+		title = PageContent.forumTitlePrefix + forumMainPage.getTimeStamp();
+		message = PageContent.forumMessage + forumMainPage.getTimeStamp();
+		forumMainPage.openForumMainPage();
+		forumBoard = forumMainPage.openForumBoard(1);
+		NotificationsComponentObject notifications = new NotificationsComponentObject(
+				driver);
+		notifications.showNotifications();
+		notifications.clickNotifications();
+		String anchoredLink = notifications.getNotificationLink(1);
+		String anchor = anchoredLink.substring(anchoredLink.indexOf("#"));
+		Assertion.assertEquals("#2", anchor);
+	}
 }
