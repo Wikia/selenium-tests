3c3748ed74d6626d13e2a83eda24a230cc7ac081
RodriGomez <rodrigo@wikia-inc.com>
2013-05-21 16:19:39 +0200

    correct indenting for video providers added

--- src/test/java/com/wikia/webdriver/TestCases/VideoTests/VetProvidersTests.java
+++ src/test/java/com/wikia/webdriver/TestCases/VideoTests/VetProvidersTests.java
@@ -27,38 +27,54 @@ public class VetProvidersTests extends TestTemplate{
 			String pageName;
 	
 			@DataProvider
-			private static final Object[][] provideVideo()
-			{
-				return new Object[][] 
-				{
-//				Premium Provider Links
-						
-//                  	screenplay from video.wikia
-				    {"http://video.wikia.com/wiki/File:The_Muppets_(2011)_-_Featurette_Behind_The_Scenes_-_Ok_Go_Video", "The Muppets (2011) - Featurette Behind The Scenes - Ok Go Video"},
-//				    	realgravity form video.wikia
-				    {"http://video.wikia.com/wiki/File:Good-Looking_Gamer_Girlfriend_Episode_16:_Snow!_by_xRpMx13_(Modern_Warfare_3_Gameplay/Commentary)", "Good-Looking Gamer Girlfriend Episode 16: Snow! by xRpMx13 (Modern Warfare 3 Gameplay/Commentary)"},
-//Failing		    {"http://www.anyclip.com/movies/apollo-13/"},
-//Failing		    {"http://uk.ign.com/videos/2013/05/18/the-last-of-us-is-beautiful-immersive-scary"},
-//Failing			{"http://movieclips.com/AfsQY-21-grams-movie-jack-returns-home/"},
-//Failing		    {"http://www.ooyala.com/#ooid=dwM3FmOTqFWfsRDguLM7wijlltNJAJil"},				    
-					{"http://www.twitch.tv/girlsgonegaming", "Girlsgonegaming playing Minecraft"},
-				    
-//				    Non-Premium Provider Links
-				    
-				    {"http://blip.tv/q-tv/s-gus-unger-hamilton-and-gwil-sainsbury-in-studio-q-6587602", "Q with Jian Ghomeshi - ∆'s Gus Unger-Hamilton and Gwil Sainsbury in Studio Q"},
-				    {"http://www.dailymotion.com/video/x101vdw_robert-pattison-y-kristen-stewart-se-dan-un-tiempo_people#.UZovsrWnqXw", "Robert Pattison y Kristen Stewart se dan un tiempo"},
-//Failing		    {"http://on.aol.com/playlist/soccer-legend-david-beckham-retires-152618?icid=OnHomepageC2Wide_Trending_Tag"},
-				    {"http://www.gamestar.de/videos/action,9/batman-arkham-origins,70131.html", "Batman Arkham Origins - Erster Teaser-Trailer Batman vs. Deathstroke"},
-//Failing				    {"http://www.gametrailers.com/videos/g504tl/next-xbox-xbox-360-memories-part-ii", ""},
-				    {"http://www.hulu.com/watch/489169", "The Unnatural (Bob's Burgers)"},
-				    {"http://www.metacafe.com/watch/10534054/fast_furious_6_favourite_stunts/", "Fast Furious 6 Favourite Stunts"},
-				    {"http://www.myvideo.de/watch/9112478/Snowblind", "Snowblind"},
-//Failing		    {"http://www.sevenload.com/videos/72-stunden-paris-50d84cfe4faad3551900000f"},
-//Failing		    {"http://www.southparkstudios.com/clips/360885/oh-my-god-its-gordon-ramsay"},
-				    {"http://vimeo.com/channels/staffpicks/58024671", "Melt Yourself Down - Fix My Life"},
-				    {"http://www.youtube.com/watch?v=d9r5_DDMMjY", "Top 5 Plays of the Night May 19th"},    
-				    {"http://www.viddler.com/v/27dbe690", "Lawn mower beer train gets pulled over"},
-		
+			private static final Object[][] provideVideo() {
+				return new Object[][] {
+						// Premium Provider Links
+
+						// screenplay from video.wikia
+						{
+							"http://video.wikia.com/wiki/File:The_Muppets_(2011)_-_Featurette_Behind_The_Scenes_-_Ok_Go_Video",
+						"The Muppets (2011) - Featurette Behind The Scenes - Ok Go Video" },
+						// realgravity form video.wikia
+						{
+							"http://video.wikia.com/wiki/File:Good-Looking_Gamer_Girlfriend_Episode_16:_Snow!_by_xRpMx13_(Modern_Warfare_3_Gameplay/Commentary)",
+						"Good-Looking Gamer Girlfriend Episode 16: Snow! by xRpMx13 (Modern Warfare 3 Gameplay/Commentary)" },
+						// {"http://www.anyclip.com/movies/apollo-13/"},
+						// {"http://uk.ign.com/videos/2013/05/18/the-last-of-us-is-beautiful-immersive-scary"},
+						// {"http://movieclips.com/AfsQY-21-grams-movie-jack-returns-home/"},
+						// {"http://www.ooyala.com/#ooid=dwM3FmOTqFWfsRDguLM7wijlltNJAJil"},
+						{ "http://www.twitch.tv/girlsgonegaming",
+						"Girlsgonegaming playing Minecraft" },
+
+						// Non-Premium Provider Links
+
+						{
+							"http://blip.tv/q-tv/s-gus-unger-hamilton-and-gwil-sainsbury-in-studio-q-6587602",
+						"Q with Jian Ghomeshi - ∆'s Gus Unger-Hamilton and Gwil Sainsbury in Studio Q" },
+						{
+							"http://www.dailymotion.com/video/x101vdw_robert-pattison-y-kristen-stewart-se-dan-un-tiempo_people#.UZovsrWnqXw",
+						"Robert Pattison y Kristen Stewart se dan un tiempo" },
+						// {"http://on.aol.com/playlist/soccer-legend-david-beckham-retires-152618?icid=OnHomepageC2Wide_Trending_Tag"},
+						{
+							"http://www.gamestar.de/videos/action,9/batman-arkham-origins,70131.html",
+						"Batman Arkham Origins - Erster Teaser-Trailer Batman vs. Deathstroke" },
+						// {"http://www.gametrailers.com/videos/g504tl/next-xbox-xbox-360-memories-part-ii",
+						// ""},
+						{ "http://www.hulu.com/watch/489169",
+						"The Unnatural (Bob's Burgers)" },
+						{
+							"http://www.metacafe.com/watch/10534054/fast_furious_6_favourite_stunts/",
+						"Fast Furious 6 Favourite Stunts" },
+						{ "http://www.myvideo.de/watch/9112478/Snowblind", "Snowblind" },
+						// {"http://www.sevenload.com/videos/72-stunden-paris-50d84cfe4faad3551900000f"},
+						// {"http://www.southparkstudios.com/clips/360885/oh-my-god-its-gordon-ramsay"},
+						{ "http://vimeo.com/channels/staffpicks/58024671",
+						"Melt Yourself Down - Fix My Life" },
+						{ "http://www.youtube.com/watch?v=d9r5_DDMMjY",
+						"Top 5 Plays of the Night May 19th" },
+						{ "http://www.viddler.com/v/27dbe690",
+						"Lawn mower beer train gets pulled over" },
+
 				};
 			}
 			
