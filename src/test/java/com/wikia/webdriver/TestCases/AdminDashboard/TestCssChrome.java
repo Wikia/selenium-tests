05eb0d26bda987f86f0f8f6f46e6d4f17e1b84ff
unknown <lukaszk@wikia-inc.com>
2013-05-29 19:15:27 +0200

    DAR-289: Prepare Selenium test

--- src/test/java/com/wikia/webdriver/TestCases/AdminDashboard/TestCssChrome.java
+++ src/test/java/com/wikia/webdriver/TestCases/AdminDashboard/TestCssChrome.java
@@ -1,11 +1,44 @@
 package com.wikia.webdriver.TestCases.AdminDashboard;
 
-/**
- * Created with IntelliJ IDEA.
- * User: lukasz
- * Date: 29.05.13
- * Time: 15:13
- * To change this template use File | Settings | File Templates.
- */
-public class TestCssChrome {
+import com.wikia.webdriver.Common.Core.CommonFunctions;
+import com.wikia.webdriver.Common.Properties.Properties;
+import com.wikia.webdriver.Common.Templates.TestTemplate;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.AdminDashboard.AdminDashboardPageObject;
+import org.testng.annotations.Test;
+
+public class TestCssChrome extends TestTemplate {
+	@Test(groups = {"AdminDashboard001", "AdminDashboard"})
+	public void goToCssChrome() {
+		AdminDashboardPageObject pageObject = new AdminDashboardPageObject(driver);
+
+		CommonFunctions.logIn(Properties.userNameStaff, Properties.passwordStaff, driver);
+
+		pageObject.openAdminDashboard();
+
+		pageObject.clickCssChromeIcon();
+
+		CommonFunctions.logOut(driver);
+	}
+
+	@Test(groups = {"AdminDashboard002", "AdminDashboard"})
+	public void openMediawikiWikiaCss() {
+		AdminDashboardPageObject pageObject = new AdminDashboardPageObject(driver);
+
+		CommonFunctions.logIn(Properties.userNameStaff, Properties.passwordStaff, driver);
+
+		pageObject.openMediawikiWikiaCss();
+
+		CommonFunctions.logOut(driver);
+	}
+
+	@Test(groups = {"AdminDashboard003", "AdminDashboard"})
+	public void openMediawikiWikiaCssAsAnon() {
+		AdminDashboardPageObject pageObject = new AdminDashboardPageObject(driver);
+
+		pageObject.openMediawikiWikiaCss();
+
+		//TODO check edit button & edit page
+
+		CommonFunctions.logOut(driver);
+	}
 }
