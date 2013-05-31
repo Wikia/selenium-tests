6530fccc1d0465078f0ce8a62224c5a4f4c493f4
michal.nowierski@wikia-inc.com <michal.nowierski@wikia-inc.com>
2013-05-31 15:18:02 +0200

    [DAR-294]
    adding test 02

--- src/test/java/com/wikia/webdriver/TestCases/AdminDashboardTests/editingLocalCssTests.java
+++ src/test/java/com/wikia/webdriver/TestCases/AdminDashboardTests/editingLocalCssTests.java
@@ -20,16 +20,29 @@ public class editingLocalCssTests extends TestTemplate{
 	
 	private String mediaWikiCss = "MediaWiki:Wikia.css";
 	private String specialCSS = "Special:CSS";
+	
 	/**
 	 *	https://wikia-inc.atlassian.net/browse/DAR-293 
 	 */
 	@Test(groups = { "editingLocalCss_001", "editingLocalCss", "AdminDashboard" })
-	public void editingLocalCss_001() {
+	public void editingLocalCss_001_UserWithAdminRightsTriesToEditWikiaCss() {
 		WikiBasePageObject wiki = new WikiBasePageObject(driver, Global.DOMAIN);
 		wiki.openWikiPage();
 		CommonFunctions.logInCookie(Properties.userNameStaff, Properties.passwordStaff);
 		wiki.openArticle(this.mediaWikiCss);
 		wiki.clickEditButton("");
-		wiki.verifyURL(Global.DOMAIN+"wiki/"+specialCSS);
+		wiki.verifyURL(Global.DOMAIN+"wiki/"+this.specialCSS);
+	}
+	
+	/**
+	 *	https://wikia-inc.atlassian.net/browse/DAR-294
+	 */
+	@Test(groups = { "editingLocalCss_002", "editingLocalCss", "AdminDashboard" })
+	public void editingLocalCss_002_UserWithoudAdminRightsHasNoEditOption() {
+		WikiBasePageObject wiki = new WikiBasePageObject(driver, Global.DOMAIN);
+		wiki.openWikiPage();
+		CommonFunctions.logInCookie(Properties.userName, Properties.password);
+		wiki.openArticle(this.mediaWikiCss);
+		wiki.verifyEditButtonNotPresent();
 	}
 }
