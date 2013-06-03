9162c9fd64e835e2c36e90e44942eefbcad0c884
michal.nowierski@wikia-inc.com <michal.nowierski@wikia-inc.com>
2013-06-03 18:51:12 +0200

    Karol's request execution: removing two tests containing long intervals (it was confirmed that those tests are no longer necessary)

--- src/test/java/com/wikia/webdriver/TestCases/NotificationsTests/ForumNotificationsTests.java
+++ src/test/java/com/wikia/webdriver/TestCases/NotificationsTests/ForumNotificationsTests.java
@@ -159,120 +159,14 @@ public class ForumNotificationsTests extends TestTemplate {
 		String anchor = anchoredLink.substring(anchoredLink.indexOf("#"));
 		Assertion.assertEquals("#2", anchor);
 	}
-
-	/**
-	 * User A posts a thread, user B leaves a 2 replies to this thread with 60
-	 * sec interval betwen one an onother. Then user A verifies if the anchor is
-	 * correct
-	 */
-	@Test(groups = { "ForumNotificationsTests_004", "ForumNotificationsTests",
-			"NotificationsTests" })
-	public void forumNotificationsTests_004_notificationsRepliesAnchor_userSlowlyLeaves2replies60SecInterval() {
-		// user 1 creates a thread
-		CommonFunctions.logOut(driver);
-		ForumPageObject forumMainPage = new ForumPageObject(driver);
-		CommonFunctions.logIn(Properties.userName, Properties.password, driver);
-		title = PageContent.forumTitlePrefix + forumMainPage.getTimeStamp();
-		message = PageContent.forumMessage + forumMainPage.getTimeStamp();
-		forumMainPage.openForumMainPage();
-		ForumBoardPageObject forumBoard = forumMainPage.openForumBoard(1);
-		ForumThreadPageObject forumThread = forumBoard.startDiscussion(title,
-				message, false);
-		forumThread.verifyDiscussionTitleAndMessage(title, message);
-		CommonFunctions.logOut(driver);
-		CommonFunctions.logIn(Properties.userName2, Properties.password2,
-				driver);
-		forumMainPage.openForumMainPage();
-		forumBoard = forumMainPage.openForumBoard(1);
-		forumThread = forumBoard.openDiscussion(title);
-		// user 2 leaves 1 replies on user 1 thread and sleeps for 60 seconds,
-		forumThread.reply(message);
-		forumThread.verifyReplyMessage(1, message);
-		try {
-			Thread.sleep(60000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		forumThread.reply(message);
-		forumThread.verifyReplyMessage(1, message);
-		CommonFunctions.logOut(driver);
-		// user 1 verifies his notifications
-		forumMainPage = new ForumPageObject(driver);
-		CommonFunctions.logIn(Properties.userName, Properties.password, driver);
-		title = PageContent.forumTitlePrefix + forumMainPage.getTimeStamp();
-		message = PageContent.forumMessage + forumMainPage.getTimeStamp();
-		forumMainPage.openForumMainPage();
-		forumBoard = forumMainPage.openForumBoard(1);
-
-		NotificationsComponentObject notifications = new NotificationsComponentObject(
-				driver);
-		notifications.showNotifications();
-		notifications.clickNotifications();
-		String anchoredLink = notifications.getNotificationLink(1);
-		String anchor = anchoredLink.substring(anchoredLink.indexOf("#"));
-		Assertion.assertEquals("#2", anchor);
-	}
-
-	/**
-	 * User A posts a thread, user B leaves a 2 replies to this thread with 120
-	 * sec interval between one an another. Then user A verifies if the anchor
-	 * is correct
-	 */
-	@Test(groups = { "ForumNotificationsTests_005", "ForumNotificationsTests",
-			"NotificationsTests" })
-	public void forumNotificationsTests_005_notificationsRepliesAnchor_userSlowlyLeaves2replies120SecInterval() {
-		// user 1 creates a thread
-		CommonFunctions.logOut(driver);
-		ForumPageObject forumMainPage = new ForumPageObject(driver);
-		CommonFunctions.logIn(Properties.userName, Properties.password, driver);
-		title = PageContent.forumTitlePrefix + forumMainPage.getTimeStamp();
-		message = PageContent.forumMessage + forumMainPage.getTimeStamp();
-		forumMainPage.openForumMainPage();
-		ForumBoardPageObject forumBoard = forumMainPage.openForumBoard(1);
-		ForumThreadPageObject forumThread = forumBoard.startDiscussion(title,
-				message, false);
-		forumThread.verifyDiscussionTitleAndMessage(title, message);
-		CommonFunctions.logOut(driver);
-		CommonFunctions.logIn(Properties.userName2, Properties.password2,
-				driver);
-		forumMainPage.openForumMainPage();
-		forumBoard = forumMainPage.openForumBoard(1);
-		forumThread = forumBoard.openDiscussion(title);
-		// user 2 leaves 1 replies on user 1 thread and sleeps for 120 seconds,
-		forumThread.reply(message);
-		forumThread.verifyReplyMessage(1, message);
-		try {
-			Thread.sleep(120000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		forumThread.reply(message);
-		forumThread.verifyReplyMessage(1, message);
-		CommonFunctions.logOut(driver);
-		// user 1 verifies his notifications
-		forumMainPage = new ForumPageObject(driver);
-		CommonFunctions.logIn(Properties.userName, Properties.password, driver);
-		title = PageContent.forumTitlePrefix + forumMainPage.getTimeStamp();
-		message = PageContent.forumMessage + forumMainPage.getTimeStamp();
-		forumMainPage.openForumMainPage();
-		forumBoard = forumMainPage.openForumBoard(1);
-
-		NotificationsComponentObject notifications = new NotificationsComponentObject(
-				driver);
-		notifications.showNotifications();
-		notifications.clickNotifications();
-		String anchoredLink = notifications.getNotificationLink(1);
-		String anchor = anchoredLink.substring(anchoredLink.indexOf("#"));
-		Assertion.assertEquals("#2", anchor);
-	}
 	
 	/**
 	 * User A posts a thread, user B leaves a 1 reply to this thread and then
 	 * User C leaves 1 reply. User A verifies if the anchor is correct
 	 */
-	@Test(groups = { "ForumNotificationsTests_006", "ForumNotificationsTests",
+	@Test(groups = { "ForumNotificationsTests_004", "ForumNotificationsTests",
 			"NotificationsTests" })
-	public void forumNotificationsTests_006_notificationsRepliesAnchor_TwoUsersLeaveTwoReplies() {
+	public void forumNotificationsTests_004_notificationsRepliesAnchor_TwoUsersLeaveTwoReplies() {
 		// user 1 creates a thread
 		CommonFunctions.logOut(driver);
 		ForumPageObject forumMainPage = new ForumPageObject(driver);
@@ -324,9 +218,9 @@ public class ForumNotificationsTests extends TestTemplate {
 	 * User A posts a thread, user B leaves a 1 reply to this thread and then
 	 * User C leaves 1 reply. user B leave yet annother reply. User A verifies if the anchor is correct
 	 */
-	@Test(groups = { "ForumNotificationsTests_007", "ForumNotificationsTests",
+	@Test(groups = { "ForumNotificationsTests_005", "ForumNotificationsTests",
 			"NotificationsTests" })
-	public void forumNotificationsTests_007_notificationsRepliesAnchor_TwoUsersLeaveThreeReplies() {
+	public void forumNotificationsTests_005_notificationsRepliesAnchor_TwoUsersLeaveThreeReplies() {
 		// user 1 creates a thread
 		CommonFunctions.logOut(driver);
 		ForumPageObject forumMainPage = new ForumPageObject(driver);
@@ -389,9 +283,9 @@ public class ForumNotificationsTests extends TestTemplate {
 	 * User A posts a thread, user B leaves a 1 reply in turns with user C to this thread and then
 	 * Four replies are left. User A verifies if the anchor is correct
 	 */
-	@Test(groups = { "ForumNotificationsTests_008", "ForumNotificationsTests",
+	@Test(groups = { "ForumNotificationsTests_006", "ForumNotificationsTests",
 			"NotificationsTests" })
-	public void forumNotificationsTests_008_notificationsRepliesAnchor_TwoUsersLeaveFourReplies() {
+	public void forumNotificationsTests_006_notificationsRepliesAnchor_TwoUsersLeaveFourReplies() {
 		// user 1 creates a thread
 		CommonFunctions.logOut(driver);
 		ForumPageObject forumMainPage = new ForumPageObject(driver);
@@ -461,9 +355,9 @@ public class ForumNotificationsTests extends TestTemplate {
 	 * User A posts a thread, user B leaves a 1 reply in turns with user C to this thread and then
 	 * Six replies are left. User A verifies if the anchor is correct
 	 */
-	@Test(groups = { "ForumNotificationsTests_009", "ForumNotificationsTests",
+	@Test(groups = { "ForumNotificationsTests_007", "ForumNotificationsTests",
 			"NotificationsTests" })
-	public void forumNotificationsTests_009_notificationsRepliesAnchor_TwoUsersLeaveSixReplies() {
+	public void forumNotificationsTests_007_notificationsRepliesAnchor_TwoUsersLeaveSixReplies() {
 		// user 1 creates a thread
 		CommonFunctions.logOut(driver);
 		ForumPageObject forumMainPage = new ForumPageObject(driver);
