4208947a3b6c3c3860299fd6a4a0bf437955b8f1
unknown <lukaszk@wikia-inc.com>
2013-06-17 15:32:56 -0700

    DAR-285: Removed whitespaces

--- src/test/java/com/wikia/webdriver/Common/ContentPatterns/URLsContent.java
+++ src/test/java/com/wikia/webdriver/Common/ContentPatterns/URLsContent.java
@@ -35,8 +35,8 @@ public class URLsContent {
 	public static final String specialPhalanxTest = "wiki/Special:Phalanx/test";
 	public static final String specialCreatePage = "wiki/Special:CreatePage";
 	public static final String specialPreferences = "wiki/Special:Preferences";
-    public static final String specialAdminDashboard = "wiki/Special:AdminDashboard";
-    public static final String specialCss = "wiki/Special:Css";
+	public static final String specialAdminDashboard = "wiki/Special:AdminDashboard";
+	public static final String specialCss = "wiki/Special:Css";
 	public static final String specialCSS = "Special:CSS";
 
 	//Mediawiki Urls
