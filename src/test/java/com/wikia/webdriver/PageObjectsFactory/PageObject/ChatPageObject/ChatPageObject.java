39bf170bd203a23be5caf36d168a8a82ef359889
bognix <bogna@wikia-inc.com>
2013-07-03 17:46:50 +0200

    Clean up in ChatPageObject

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/ChatPageObject/ChatPageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/ChatPageObject/ChatPageObject.java
@@ -14,78 +14,54 @@ import org.openqa.selenium.support.PageFactory;
 public class ChatPageObject extends BasePageObject
 {
 
-	/*
-	 * https://internal.wikia-inc.com/wiki/Chat/UAT
-	 * 
-	 * 
-	 */
 	@FindBy(css="textarea[name='message']")
 	private WebElement messageWritingArea;
-	
 	@FindBy(css="form.Write")
 	private WebElement messageForm;
-	
 	@FindBy(css="form[class='Write blocked']")
 	private WebElement messageWriteAreaBlocked;//when user is disconnected
-	
 	@FindBy(xpath="//div[@class='Chat']//li[contains(text(), 'Welcome to the ')]")
 	private WebElement welcomeMessage;
-	
 	@FindBy(css="div.Rail")
 	private WebElement sideBar;
-	
 	@FindBy(css="h1[class=public wordmark''] img")
 	private WebElement wordmark;
-	
 	@FindBy(css="div.User span.username")
 	private WebElement userName;
-
 	@FindBy(css="#Chat_4 .inline-alert")
 	private WebElement chatInlineAlert;
-
 	@FindBy(css="div.User img")
 	private WebElement userAvatar;
-	
 	@FindBy(css="li.private")
 	private WebElement privateMassageButton;
-	
 	@FindBy(css="li.private-allow")
 	private WebElement allowPrivateMassageButton;
-	
 	@FindBy(css="li.private-block")
 	private WebElement blockPrivateMassageButton;
-	
 	@FindBy(css="div#Rail img.wordmark")
 	private WebElement mainChatButton;
-	
 	@FindBy(css="h1[class='public wordmark selected']")
 	private WebElement mainChatSelection;
-	
 	@FindBy(css="ul.PrivateChatList span.splotch")
 	private WebElement privateMessageNotificator;
-	
 	@FindBy(css="li.User.selected.blocked")
 	private WebElement userDisconnectedButton;
-	
 	@FindBy(css="li.give-chat-mod span.label")
 	private WebElement giveChatModStatusButton;
-	
-	@FindBy(xpath="//li[@class='inline-alert' and contains(text(), 'Welcome to the')]")
-	private WebElement userIsOnChatIndicator1;
-
-	@FindBy(xpath="//li[@class='inline-alert' and contains(text(), 'Wiki chat')]")
-	private WebElement userIsOnChatIndicator2;
+	@FindBy(css=".continued.inline-alert")
+	private WebElement chatInlineAlertContinued;
 
 	By userContextMenu = By.cssSelector("ul.regular-actions li");
 	By adminContextMenu = By.cssSelector("ul.admin-actions li");
-	By privateMessageHeader = By.xpath("//h1[@class='private' and contains(text(), 'Private Messages')]");
+	By privateMessageHeader = By.cssSelector("#Rail h1.private");
+	By privateChatHeader = By.cssSelector("#ChatHeader h1.private");
 
 	public ChatPageObject(WebDriver driver)
 	{
 		super(driver);
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	/**
 	 * @author Karol Kujawiak
 	 * opens chat page, should be launched when user is logged in
@@ -116,13 +92,13 @@ public class ChatPageObject extends BasePageObject
 		waitForElementByElement(userAvatar);
 		PageObjectLogging.log("verifyChatPage", "Chat page verified", true, driver);
 	}
-	
+
 	public void verifyMessageOnChat(String message)
 	{
 		waitForElementByBy(By.xpath("//span[@class='message' and contains(text(), '"+message+"')]"));
 		PageObjectLogging.log("writeOnChat", "Message: "+message+" is visible on chat board", true, driver);
 	}
-	
+
 	/**
 	 * @author Karol
 	 * @param userName
@@ -130,17 +106,16 @@ public class ChatPageObject extends BasePageObject
 	 */
 	public void verifyUserJoinToChat(String userName)
 	{
-		PageObjectLogging.log("verifyUserJoinToChat", "verifying if user joined chat", true, driver);
-		waitForElementByXPath("//div[@class='Chat']/ul/li[contains(text(), '"+userName+" has joined the chat.')]");
+		waitForElementByElement(chatInlineAlertContinued);
 		PageObjectLogging.log("verifyUserJoinToChat", userName+" has joined the chat.", true, driver);
 	}
-	
+
 	public void verifyUserIsVisibleOnContactsList(String userName)
 	{
 		waitForElementByXPath("//li[@id='user-"+userName+"']");
 		PageObjectLogging.log("verifyUserIsVisibleOnContactsList", userName+" is visible on contacts list", true, driver);
 	}
-	
+
 	/**
 	 * @author Karol Kujawiak
 	 * verifies if there is private message header in right hand sidebar
@@ -149,9 +124,12 @@ public class ChatPageObject extends BasePageObject
 	public void verifyPrivateMessageHeader()
 	{
 		waitForElementByBy(privateMessageHeader);
-		PageObjectLogging.log("verifyPrivateMessageHeader", "private message header is visible", true, driver);
+		PageObjectLogging.log(
+			"verifyPrivateMessageHeader",
+			"private message header is visible", true, driver
+		);
 	}
-	
+
 	public void verifyPrivateMessageNotification()
 	{
 		waitForElementByElement(privateMessageNotificator);
@@ -163,21 +141,19 @@ public class ChatPageObject extends BasePageObject
 		waitForElementByXPath("//span[@class='splotch' and contains(text(), '"+notificationNumber+"')]");
 		PageObjectLogging.log("verifyPrivateMessageNotification", "private message notification number "+notificationNumber+" is visible", true, driver);
 	}
-	
+
 	public void verifyPrivateMessageIsHighLighted(String user)
 	{
-		//li[@class='User selected' and @id='priv-user-QATestsUser']
-//		waitForElementByXPath("//li[@class='User selected' and @id='priv-user-"+user+"']");
 		waitForElementByXPath("//li[contains(@class, 'User') and contains(@class, 'selected') and @id='priv-user-"+user+"']");
 		PageObjectLogging.log("verifyPrivateMessageIsHighLighted", "private message section is highlighted", true, driver);
 	}
-	
+
 	public void verifyPrivateChatTitle(String userName)
 	{
-		waitForElementByXPath("//h1[@class='private' and contains(text(), 'Private chat with "+userName+"')]");
+		waitForElementByBy(privateChatHeader);
 		PageObjectLogging.log("verifyPrivateChatTitle", "private chat title is correct", true, driver);
 	}
-	
+
 	public void verifyMainChatIsHighLighted()
 	{
 		waitForElementByElement(mainChatSelection);
@@ -217,29 +193,7 @@ public class ChatPageObject extends BasePageObject
 		Assertion.assertEquals("contribs", list.get(1).getAttribute("class"));
 		Assertion.assertEquals("private-allow", list.get(2).getAttribute("class"));	
 	}
-	
-	/**
-	 * @author Karol Kujawiak
-	 * verifies blocked user drop-down content, should be executed after clickOnDifferentUser() execution
-	 */
-	public void verifyBlockedUserDropdown()
-	{
-		List<WebElement> list = getDropDownListOfElements();
-		Assertion.assertNumber(2, list.size(), "Checking number of elements in the drop-down");
-		for (int i=0; i<list.size(); i++)
-		{
-			PageObjectLogging.log("verifyBlockedUserDropdown", i+" item in drop-down is "+ list.get(i).getAttribute("class"), true);	
-		}
-		Assertion.assertEquals("message-wall", list.get(0).getAttribute("class"));
-		Assertion.assertEquals("contribs", list.get(1).getAttribute("class"));	
-	}
-	
-	public void verifyBlockedUserMessage(String blockingUserName, String blockedUserName)
-	{
-		waitForElementByXPath("//li[@class='inline-alert' and contains(text(), '"+blockingUserName+" has blocked "+blockedUserName+".')]");
-		PageObjectLogging.log("verifyBlockedUserMessage", "user is blocked message is visible: "+blockingUserName+" has blocked "+blockedUserName, true, driver);
-	}
-	
+
 	/**
 	 * @author Karol Kujawiak
 	 * verifies private message user drop-down content, should be executed after clickPrivateMessageUser() execution
@@ -283,29 +237,7 @@ public class ChatPageObject extends BasePageObject
 		Assertion.assertEquals("kick", list.get(1).getAttribute("class"));
 		Assertion.assertEquals("ban", list.get(2).getAttribute("class"));
 	}
-	
-	public void verifyChatModUserDropdown()
-	{
-		List<WebElement> list = getDropDownListOfElements();
-		Assertion.assertNumber(3, list.size(), "Checking number of elements in the drop-down");
-		for (int i=0; i<list.size(); i++)
-		{
-			PageObjectLogging.log("verifyAdminUserDropdown", i+" item in drop-down is "+ list.get(i).getAttribute("class"), true);
-		}
-		Assertion.assertEquals("message-wall", list.get(0).getAttribute("class"));
-		Assertion.assertEquals("contribs", list.get(1).getAttribute("class"));
-		Assertion.assertEquals("private", list.get(2).getAttribute("class"));
-		
-		list = getAdminDropDownListOfElements();
-		Assertion.assertNumber(2, list.size(), "Checking number of elements in the drop-down");
-		for (int i=0; i<list.size(); i++)
-		{
-			PageObjectLogging.log("verifyAdminUserDropdown", i+" item in drop-down is "+ list.get(i).getAttribute("class"), true);
-		}
-		Assertion.assertEquals("kick", list.get(0).getAttribute("class"));
-		Assertion.assertEquals("ban", list.get(1).getAttribute("class"));
-	}
-	
+
 	public void verifyUserIsGreyedOut()
 	{
 		waitForElementByElement(userDisconnectedButton);
@@ -375,25 +307,17 @@ public class ChatPageObject extends BasePageObject
 
 	public void clickOnMainChat(WebDriver driver)
 	{
-//		Point p = mainChatButton.getLocation();
-//		CommonFunctions.MoveCursorToElement(p, driver);
-//		CommonFunctions.ClickElement();
 		executeScript("document.querySelectorAll('.public.wordmark img.wordmark')[0].click()", driver);
 		PageObjectLogging.log("clickOnMainChat", "main chat is clicked", true, driver);
 	}
-	
 
-	
 	public void clickOnPrivateChat(String user, WebDriver driver)
 	{
-//		By privateChatUserButton = By.xpath("//li[@id='priv-user-"+user+"']");
-//		Point p = driver.findElement(privateChatUserButton).getLocation();
-//		CommonFunctions.MoveCursorToElement(p, driver);
-//		CommonFunctions.ClickElement();
 		executeScript("document.querySelectorAll('#priv-user-"+user+"')[0].click()", driver);
 		verifyPrivateMessageIsHighLighted(user);
 		PageObjectLogging.log("clickOnPrivateChat", "private chat is clicked", true, driver);
 	}
+
 	/**
 	 * @author Karol Kujawiak
 	 * @param userName
