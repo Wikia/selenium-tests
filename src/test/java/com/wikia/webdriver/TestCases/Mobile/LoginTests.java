365ddbdec526a4319cb7597f2053823bad549afd
kkarolk <karol@wikia-inc.com>
2013-07-09 11:46:37 +0200

    [DEBUG] mobile login tests

--- src/test/java/com/wikia/webdriver/TestCases/Mobile/LoginTests.java
+++ src/test/java/com/wikia/webdriver/TestCases/Mobile/LoginTests.java
@@ -12,51 +12,7 @@ public class LoginTests extends TestTemplate{
 	String url;
 
 	@Test(groups={"mobile", "login_mobile"})
-	public void Login1(){//String userName, String password){
-		MobileBasePageObject mobile = new MobileBasePageObject(driver);
-		mobile.openHome();
-		mobile.openRandomPage();
-		url = driver.getCurrentUrl();
-		mobile.login(Properties.userName, Properties.password);
-		mobile.verifyURLcontains(url);
-		CommonFunctions.logOutMobile(driver);
-	}
-
-	@Test(groups={"mobile", "login_mobile"})
-	public void Login2(){//String userName, String password){
-		MobileBasePageObject mobile = new MobileBasePageObject(driver);
-		mobile.openHome();
-		mobile.openRandomPage();
-		url = driver.getCurrentUrl();
-		mobile.login(Properties.userName, Properties.password);
-		mobile.verifyURLcontains(url);
-		CommonFunctions.logOutMobile(driver);
-	}
-
-	@Test(groups={"mobile", "login_mobile"})
-	public void Login3(){//String userName, String password){
-		MobileBasePageObject mobile = new MobileBasePageObject(driver);
-		mobile.openHome();
-		mobile.openRandomPage();
-		url = driver.getCurrentUrl();
-		mobile.login(Properties.userName, Properties.password);
-		mobile.verifyURLcontains(url);
-		CommonFunctions.logOutMobile(driver);
-	}
-
-	@Test(groups={"mobile", "login_mobile"})
-	public void Login4(){//String userName, String password){
-		MobileBasePageObject mobile = new MobileBasePageObject(driver);
-		mobile.openHome();
-		mobile.openRandomPage();
-		url = driver.getCurrentUrl();
-		mobile.login(Properties.userName, Properties.password);
-		mobile.verifyURLcontains(url);
-		CommonFunctions.logOutMobile(driver);
-	}
-
-	@Test(groups={"mobile", "login_mobile"})
-	public void Login5(){//String userName, String password){
+	public void Login(){//String userName, String password){
 		MobileBasePageObject mobile = new MobileBasePageObject(driver);
 		mobile.openHome();
 		mobile.openRandomPage();
