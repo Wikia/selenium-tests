3a2163fd52187f6db78dea46fbb60a978fa8c826
andrzejd <andrzej.dolinski@wikia-inc.com>
2013-07-09 09:59:51 +0200

    Signup as too young user QAART-152

--- src/test/java/com/wikia/webdriver/TestCases/SignUpTests/SignUpValidationTests.java
+++ src/test/java/com/wikia/webdriver/TestCases/SignUpTests/SignUpValidationTests.java
@@ -7,15 +7,15 @@ import org.testng.annotations.Test;
 
 public class SignUpValidationTests extends TestTemplate {
 
-    @Test(groups = {"SignUp_validation2", "SignUp_validation"})
-    public void SignUp_validation2()
-    {
-	SignUpPageObject signUpPage = new SignUpPageObject(driver);
-	signUpPage.openSignUpPage();
-	signUpPage.typeInUserName(signUpPage.getTimeStamp());
-	signUpPage.typeInEmail(Properties.emailQaart1);
-	signUpPage.typeInPassword(signUpPage.getTimeStamp());
-	signUpPage.selectTooYoungBirthDate();
-	signUpPage.waitForTooYoungErrorMsg();
-    }
+	@Test(groups = {"SignUp_validation2", "SignUp_validation"})
+	public void SignUp_validation2()
+	{
+		SignUpPageObject signUpPage = new SignUpPageObject(driver);
+		signUpPage.openSignUpPage();
+		signUpPage.typeInUserName(signUpPage.getTimeStamp());
+		signUpPage.typeInEmail(Properties.emailQaart1);
+		signUpPage.typeInPassword(signUpPage.getTimeStamp());
+		signUpPage.selectTooYoungBirthDate();
+		signUpPage.waitForTooYoungErrorMsg();
+	}
 }
