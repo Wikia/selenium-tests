baf74bfbb6b2c0c05b4c04726687957e4de57e01
mech <mech@wikia-inc.com>
2013-07-12 16:30:09 +0200

    DAR-1055 Test FB button on Special:Connect - adding new files

---
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/SpecialFBConnectPageObject.java
@@ -0,0 +1,28 @@
+package com.wikia.webdriver.PageObjectsFactory.PageObject.Special;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.wikia.webdriver.Common.Logging.PageObjectLogging;
+
+public class SpecialFBConnectPageObject extends SpecialPageObject {
+
+	@FindBy(css="#WikiaArticle iframe[src^=\"http://www.facebook.com/plugins/login_button\"]") 
+	private WebElement fbConnectButton;
+
+	
+	public SpecialFBConnectPageObject(WebDriver driver) {
+		super(driver);		
+	}
+	
+	/**
+	 * verify that the Facebook button was properly loaded on the special page
+	 */
+	public void verifyFacebookButtonAppeared() {
+		waitForElementByElement(fbConnectButton);
+		PageObjectLogging.log("verifyFacebookButtonAppeared", "Verify that facebook button appeared", true, driver);
+		
+	}
+
+}
