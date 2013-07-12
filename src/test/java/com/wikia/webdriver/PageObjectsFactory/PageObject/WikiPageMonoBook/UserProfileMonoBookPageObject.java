b20dc81adbefc8587e7d167e9a453de71a619ec3
kkarolk <karol@wikia-inc.com>
2013-07-12 09:49:11 +0200

    Revert "QAART-164 removed monobook from the project"
    This reverts commit 67d33a67d64d5fd5e5103b36a03724bd68434297.

---
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/WikiPageMonoBook/UserProfileMonoBookPageObject.java
@@ -0,0 +1,327 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wikia.webdriver.PageObjectsFactory.PageObject.WikiPageMonoBook;
+
+import com.wikia.webdriver.Common.ContentPatterns.PageContent;
+import com.wikia.webdriver.Common.ContentPatterns.URLsContent;
+import com.wikia.webdriver.Common.Core.Assertion;
+import com.wikia.webdriver.Common.Core.Global;
+import com.wikia.webdriver.Common.Core.MailFunctions;
+import com.wikia.webdriver.Common.Logging.PageObjectLogging;
+import com.wikia.webdriver.Common.Properties.Properties;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.BaseMonoBookPageObject;
+import java.util.HashMap;
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ *
+ * @author Bogna 'bognix' Knychala
+ */
+public class UserProfileMonoBookPageObject extends BaseMonoBookPageObject {
+
+    @FindBy (css="#pt-userpage a")
+    private WebElement userNameLink;
+    @FindBy (css="#firstHeading")
+    private WebElement header;
+    @FindBy (css="#blogPostTitle")
+    private WebElement blogPostTitleInput;
+    @FindBy (css="#wpTextbox1")
+    private WebElement contentInput;
+    @FindBy (css="#wpSave")
+    private WebElement submitBlogPost;
+    @FindBy (css="section.WikiaBlogListing")
+    private WebElement blogPostsSection;
+    @FindBy (css="#ca-move")
+    private WebElement renameLink;
+    @FindBy (css="#wpNewTitleMain")
+    private WebElement renameTitleInput;
+    @FindBy (css="#wpReason")
+    private WebElement renameReason;
+    @FindBy (css=".mw-submit > input")
+    private WebElement renameSubmit;
+    @FindBy (css = ".noarticletext")
+    private WebElement noArticleArea;
+    @FindBy (css = "#ca-edit > a")
+    private WebElement editLink;
+    @FindBy (css = "#mw-content-text")
+    private WebElement postContent;
+    @FindBy (css = "#ca-delete > a")
+    private WebElement deleteLink;
+    @FindBy (css = "#wpDeleteReasonList")
+    private WebElement deleteReasonSelect;
+    @FindBy (css = "#wpConfirmB")
+    private WebElement deleteSubmit;
+    @FindBy (css = ".error.mw-error-cannotdelete")
+    private WebElement cannotDeleteMessageContainer;
+
+
+    private String userName;
+
+    public UserProfileMonoBookPageObject(WebDriver driver, String user) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+        changeToMonoBook();
+        userName = user;
+    }
+
+    public void verifyUserProfilePage(String username) {
+        verifyUserNameLink(username);
+    }
+
+    public void verifyUserProfileWelcomePage(String username) {
+        verifyUserNameLink(username);
+        verifyUserNameInHeader(username);
+    }
+
+    public void verifyWelcomeEmail () {
+        PageObjectLogging.log("verifyWelcomeEmail ", "start of email verification", true);
+        String[] mailContent = MailFunctions.getWelcomeMailContent(MailFunctions.getFirstMailContent(Properties.email, Properties.emailPassword));
+        System.out.println("*****************************************************************************");
+        System.out.println("*****************************************************************************");
+        System.out.println("*****************************************************************************");
+        System.out.println("*****************************************************************************");
+        System.out.println("*****************************************************************************");
+        System.out.println("*****************************************************************************");
+        System.out.println("*****************************************************************************");
+        for (int i=0; i<mailContent.length; i++){
+            System.out.println(i+".  "+mailContent[i]);
+            System.out.println("\n");
+        }
+        Assertion.assertEquals("Edit your profile.", mailContent[4]);
+        Assertion.assertEquals("Learn the basics.", mailContent[10]);
+        Assertion.assertEquals("Get a quick tutorial on the basics of Wikia: how to edit a page, your user =profile, change your preferences, and more.", mailContent[12]);
+        Assertion.assertEquals("Check it out (http://community.wikia.com/wiki/Help:Wikia_Basics)", mailContent[14]);
+        Assertion.assertEquals("Explore more wikis.", mailContent[16]);
+        Assertion.assertEquals("There are thousands of wikis on Wikia, find more wikis that interest you by= heading to one of our hubs: Video Games (http://www.wikia.com/Video_Games)=, Entertainment (http://www.wikia.com/Entertainment), or Lifestyle (http://=www.wikia.com/Lifestyle).", mailContent[18]);
+        Assertion.assertEquals("Go to http://www.wikia.com", mailContent[20]);
+        Assertion.assertEquals("Want more information? Find advice, answers, and the Wikia community at Com=munity Central (http://www.community.wikia.com). Happy editing!", mailContent[22]);
+        Assertion.assertEquals("The Wikia Team", mailContent[24]);
+        PageObjectLogging.log("verifyWelcomeEmail ", "end of email verification", true);
+    }
+
+    private void verifyUserNameLink(String username) {
+        waitForElementByElement(userNameLink);
+        username = username.replace("_", " ");
+        Assertion.assertEquals(username, userNameLink.getText());
+        PageObjectLogging.log(
+            "verifyUserNameLink",
+            "username link is present in top right corner",
+            true, driver
+        );
+    }
+
+    public void addBlogPostViaSpecial() {
+        getUrl(Global.DOMAIN + URLsContent.specialAddBlogPost + "?useskin=monobook");
+        waitForTextToBePresentInElementByElement(
+            header, PageContent.createNewBlogPostMessage
+        );
+        PageObjectLogging.log(
+            "addBlogPostPageOpened",
+            "Add blog post page is opened with monobook skin",
+            true, driver
+        );
+    }
+
+    public HashMap fillBlogPostForm() {
+        String postTitle = PageContent.blogPostNamePrefix + getTimeStamp();
+        String blogPostContent = PageContent.blogContent + getTimeStamp();
+        waitForElementByElement(blogPostTitleInput);
+        blogPostTitleInput.sendKeys(postTitle);
+        waitForElementByElement(contentInput);
+        contentInput.sendKeys(blogPostContent);
+        clickAndWait(submitBlogPost);
+
+        PageObjectLogging.log(
+            "SubmitBlogPost",
+            "New blog post is submitted",
+            true, driver
+        );
+
+        HashMap content = new HashMap();
+        content.put("postTitle", postTitle);
+        content.put("postContent", blogPostContent);
+        return content;
+    }
+
+    public void openUserBlogViaUrl() {
+        String userBlog = URLsContent.userBlog.replace("%user%", userName);
+        getUrl(Global.DOMAIN + userBlog);
+        changeToMonoBook();
+
+        PageObjectLogging.log(
+            "OpenBlogPage",
+            "Blog page is opened with monobook skin",
+            true, driver
+        );
+    }
+
+    public void verifyBlogPost(HashMap content) {
+        waitForElementByElement(blogPostsSection);
+        waitForTextToBePresentInElementByElement(
+            blogPostsSection, (String) content.get("postTitle")
+        );
+        PageObjectLogging.log(
+            "BlogPostTitlePresent",
+            "New blog post title is present",
+            true, driver
+        );
+
+        waitForTextToBePresentInElementByElement(
+            blogPostsSection, (String) content.get("postContent")
+        );
+        PageObjectLogging.log(
+            "BlogPostContentPresent",
+            "New blog post content is present",
+            true, driver
+        );
+    }
+
+    public void openBlogPost(String postTitle) {
+        WebElement blogPostLink = driver.findElement(By.partialLinkText(postTitle));
+        clickAndWait(blogPostLink);
+        changeToMonoBook();
+        PageObjectLogging.log(
+            "EnterBlogPost",
+            "Enter blog post with monobook skin",
+            true, driver
+        );
+    }
+
+    public void renameBlogPost() {
+        waitForElementByElement(renameLink);
+        clickAndWait(renameLink);
+        PageObjectLogging.log(
+            "EnterRenamingForm",
+            "Enter renaming blog post form for currently selected post",
+            true, driver
+        );
+    }
+
+    public String fillRenameForm() {
+        String newName = userName + "/" + getTimeStamp();
+        changeToMonoBook();
+        renameTitleInput.clear();
+        renameTitleInput.sendKeys(newName);
+        renameReason.sendKeys(getTimeStamp());
+        clickAndWait(renameSubmit);
+
+        PageObjectLogging.log(
+            "renameFormSubmitted",
+            "Rename form is filled and submitted",
+            true, driver
+        );
+
+        return newName;
+    }
+
+    public void verifyNewName (String newName) {
+        openUserBlogViaUrl();
+        String currentUrl = driver.getCurrentUrl();
+        getUrl(currentUrl + "/" + newName);
+        waitForTextToBePresentInElementByElement(header, newName);
+        PageObjectLogging.log(
+            "verifyNewName",
+            "New blog post title is present",
+            true, driver
+        );
+
+        waitForElementNotVisibleByElement(noArticleArea);
+        PageObjectLogging.log(
+            "verifyOldContent",
+            "Old blog post content is present",
+            true, driver
+        );
+    }
+
+    public void editBlogPost() {
+        waitForElementByElement(editLink);
+        clickAndWait(editLink);
+        changeToMonoBook();
+        PageObjectLogging.log(
+            "enterEditionForm",
+            "Enter edition form and change skin to monobook",
+            true, driver
+        );
+    }
+
+    public String fillEditForm(String blogPostContent) {
+        String content = blogPostContent + getTimeStamp();
+        waitForElementByElement(contentInput);
+        contentInput.clear();
+        contentInput.sendKeys(content);
+        clickAndWait(submitBlogPost);
+        PageObjectLogging.log(
+            "EditionFormSubmited",
+            "Edition form is submited and skin is changed to monobook",
+            true, driver
+        );
+        return content;
+    }
+
+    public void verifyNewContent(String content) {
+        waitForElementByElement(postContent);
+        waitForTextToBePresentInElementByElement(postContent, content);
+        PageObjectLogging.log(
+            "newContentPresent",
+            "new blog post content is present",
+            true, driver
+        );
+    }
+
+    public void deleteBlogPost() {
+        waitForElementByElement(deleteLink);
+        clickAndWait(deleteLink);
+        changeToMonoBook();
+        PageObjectLogging.log(
+            "enterDeletionForm",
+            "Enter deletion form and change skin to monobook",
+            true, driver
+        );
+    }
+
+    public String fillDeleteForm() {
+        Select deleteSelect = new Select(deleteReasonSelect);
+        deleteSelect.selectByIndex(1);
+        clickAndWait(deleteSubmit);
+
+        String deletionUrl = getCurrentUrl();
+        PageObjectLogging.log(
+            "submitDeletionForm",
+            "blog post deletion form is submitted and skin is changed to monobook",
+            true, driver
+        );
+        return deletionUrl;
+    }
+
+    public void verifyDeleted(String deletionUrl) {
+        getUrl(deletionUrl);
+        changeToMonoBook();
+        waitForElementNotVisibleByElement(cannotDeleteMessageContainer);
+        PageObjectLogging.log(
+            "messagePresent",
+            "Message about deletion blog post is present",
+            true, driver
+        );
+    }
+
+    private void verifyUserNameInHeader(String username) {
+        waitForElementByElement(header);
+        username = username.replace("_", " ");
+        String prefixedUserName = URLsContent.userPrefix.replace("%user%", username);
+        Assertion.assertEquals(prefixedUserName, header.getText());
+        PageObjectLogging.log(
+            "verifyUserNameInHeader",
+            "username is present in header",
+            true, driver
+        );
+    }
+    
+}
---
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/WikiPageMonoBook/WikiArticleMonoBookPageObject.java
@@ -0,0 +1,350 @@
+package com.wikia.webdriver.PageObjectsFactory.PageObject.WikiPageMonoBook;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import com.wikia.webdriver.Common.ContentPatterns.PageContent;
+import com.wikia.webdriver.Common.Core.Assertion;
+import com.wikia.webdriver.Common.Core.Global;
+import com.wikia.webdriver.Common.Logging.PageObjectLogging;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.BaseMonoBookPageObject;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author lukaszk
+ */
+public class WikiArticleMonoBookPageObject extends BaseMonoBookPageObject {
+
+	@FindBy(css = "#mw-content-text")
+	private WebElement articleContentLocator;
+	@FindBy (css = "#bodyContent")
+	private WebElement bodyContent;
+	@FindBy(css = "#article-comments-ul")
+	private WebElement comments;
+	@FindBy(css = "#ca-delete > a")
+	private WebElement deleteArticleLink;
+	@FindBy(css = "#wpDeleteReasonList")
+	private WebElement deleteReasonSelect;
+	@FindBy(css = "#ForumNewMessage > .message")
+	private WebElement disussionArea;
+	@FindBy(css = ".editarea textarea:nth-of-type(2)")
+	private WebElement discussionContentElement;
+	@FindBy(css = "#RelatedForumDiscussion > .forum-content")
+	private WebElement discussionUnderArticle;
+	@FindBy(css = ".title-container textarea:nth-of-type(2)")
+	private WebElement discussionTitleElement;
+	@FindBy(css = "#wpTextbox1")
+	private WebElement editionArea;
+	@FindBy(css = "#ca-edit a")
+	private WebElement editLink;
+	@FindBy (css = "#wpNewTitleMain")
+	private WebElement newTitleLocator;
+	@FindBy(css = ".oasis-only-warning")
+	private WebElement oasisOnly;
+	@FindBy(css = "#article-comm")
+	private WebElement postCommentLocator;
+	@FindBy(css = "#article-comm-submit")
+	private WebElement postCommentSubmit;
+	@FindBy (css = "textarea[name='wpArticleReply']")
+	private WebElement replyArea;
+	@FindBy (css = ".article-comm-reply")
+	private WebElement replyButton;
+	@FindBy (css = "#ca-move > a")
+	private WebElement renameLink;
+	@FindBy(css = "#BoardList")
+	private WebElement selectBoard;
+	@FindBy(css = "#wpConfirmB")
+	private WebElement submitDelete;
+	@FindBy (css= ".buttons > .submit")
+	private WebElement submitDiscussion;
+	@FindBy(css = "#wpSave")
+	private WebElement submitEditButton;
+	@FindBy (css = ".article-comm-input-text > .buttons > input[type='submit']")
+	private WebElement submitReply;
+	@FindBy (css = ".mw-submit>input")
+	private WebElement submitRename;
+	@FindBy(css = "#RelatedForumDiscussion .forum-new-post")
+	private WebElement startDiscussion;
+	@FindBy(css = "#firstHeading")
+	private WebElement titleLocator;
+	@FindBy(css = ".mw-warning-with-logexcerpt > p")
+	private WebElement warningMessage;
+
+	private String articleTitle;
+	private final String commentsSelector = ".comments > li > .speech-bubble-message";
+
+	public WikiArticleMonoBookPageObject(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver, this);
+		articleTitle = getArticleTitle();
+	}
+
+	public Boolean checkCommentsPresent() {
+		List commentsElements = driver.findElements(By.cssSelector(commentsSelector));
+		if (commentsElements.size() > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public void clickEdit() {
+		clickAndWait(editLink);
+		PageObjectLogging.log("clickEdit", "click on Edit link", true);
+	}
+
+	public String deleteArticle() {
+		clickAndWait(deleteArticleLink);
+		PageObjectLogging.log(
+				"DeleteForm",
+				"Eneter article delete form",
+				true, driver
+		);
+		Select select = new Select(deleteReasonSelect);
+		select.selectByIndex(1);
+		PageObjectLogging.log(
+				"ReasonSelected",
+				"deletion reason selected",
+				true, driver
+		);
+		clickAndWait(submitDelete);
+		return articleTitle;
+	}
+
+	public String editContent() {
+		String articleContent = enterDefaultContent();
+		waitForElementByElement(submitEditButton);
+		clickAndWait(submitEditButton);
+		PageObjectLogging.log(
+				"submitEditForm",
+				"new content is added to form",
+				true, driver
+		);
+
+		//BodyContent element is unique for edition form with oasis skin
+		//until it is present new article is not saved to database
+		waitForElementNotVisibleByElement(bodyContent);
+		return articleContent;
+	}
+
+	public String enterDefaultContent() {
+		String content = PageContent.articleText + getTimeStamp();
+		editionArea.sendKeys(content);
+		PageObjectLogging.log(
+				"addContentToArticle",
+				"add content to article",
+				true, driver
+		);
+		return content;
+	}
+
+	public String enterEdition() {
+		waitForElementByElement(editLink);
+		clickAndWait(editLink);
+		waitForElementByElement(submitEditButton);
+		PageObjectLogging.log(
+				"editArticlePageMonobook",
+				"edit article page with monobook skin opened",
+				true, driver
+		);
+
+		waitForElementByElement(titleLocator);
+		Assertion.assertStringContains(titleLocator.getText(), articleTitle);
+		PageObjectLogging.log(
+				"editTitleContainsArticleTitle",
+				"title on edit article page contains article title",
+				true, driver
+		);
+		return articleTitle;
+	}
+
+	public HashMap fillDiscussForm() {
+		waitForElementByElement(disussionArea);
+		Select selectElement = new Select(selectBoard);
+		selectElement.selectByIndex(1);
+		String discussionTitle = "QA Title" + getTimeStamp();
+		String discussionContent = "QA Content" + getTimeStamp();
+		discussionTitleElement.sendKeys(discussionTitle);
+		discussionContentElement.sendKeys(discussionContent);
+
+		PageObjectLogging.log(
+				"fillDiscussionForm",
+				"mandatory fields in discussion form are filled",
+				true, driver
+		);
+
+		waitForElementClickableByElement(submitDiscussion);
+		clickAndWait(submitDiscussion);
+
+		HashMap content = new HashMap();
+		content.put("discussionTitle", discussionTitle);
+		content.put("discussionContent", discussionContent);
+		return content;
+	}
+
+	private String getArticleTitle() {
+		waitForElementByElement(titleLocator);
+		String title = titleLocator.getText();
+		return title;
+	}
+
+	public void openArticle(String articleName) {
+		String articleTitleWithUnderscore = articleName.replace(" ", "_");
+		String url = Global.DOMAIN + "wiki/" + articleTitleWithUnderscore;
+		getUrl(url);
+	}
+
+	public String postComment() {
+		String comment = PageContent.commentText + getTimeStamp();
+		postCommentLocator.sendKeys(comment);
+		clickAndWait(postCommentSubmit);
+		return comment;
+	}
+
+	public String replyComment() {
+		waitForElementByElement(replyButton);
+		clickAndWait(replyButton);
+		waitForElementByElement(replyArea);
+		String reply =  getTimeStamp();
+		replyArea.sendKeys(reply);
+		clickAndWait(submitReply);
+		return reply;
+	}
+
+	public String renameArticle() {
+		clickAndWait(renameLink);
+		String newName = PageContent.articleNamePrefix + getTimeStamp();
+		newTitleLocator.clear();
+		newTitleLocator.sendKeys(newName);
+
+		clickAndWait(submitRename);
+		return newName;
+	}
+
+	public void startDiscussion() {
+		waitForElementByElement(startDiscussion);
+		startDiscussion.click();
+		PageObjectLogging.log(
+				"discussAboutArticle",
+				"discuss about article page with monobook skin opened",
+				true, driver
+		);
+
+		waitForElementByElement(titleLocator);
+		Assertion.assertStringContains(titleLocator.getText(), articleTitle);
+		PageObjectLogging.log(
+				"discussionTitleContainsArticleTitle",
+				"title on discuss about article page contains article title",
+				true, driver
+		);
+	}
+
+	public void verifyCommentPresent(String comment) {
+
+		waitForTextToBePresentInElementByElement(
+				comments,comment
+		);
+
+		PageObjectLogging.log(
+				"CommentFound",
+				"new comment found under the article",
+				true, driver
+		);
+	}
+
+	public void verifyDeleted(String articleName) {
+		openArticle(articleName);
+		waitForElementByElement(warningMessage);
+		waitForTextToBePresentInElementByElement(
+				warningMessage, PageContent.articleDeletedMessage
+		);
+		PageObjectLogging.log(
+				"MessageAboutDeletion",
+				"Message about deletion is present",
+				true, driver
+		);
+	}
+
+	public void verifyDiscussion(HashMap content) {
+		openArticle(articleTitle);
+		waitForTextToBePresentInElementByElement(
+				discussionUnderArticle, (String) content.get("discussionTitle")
+		);
+		PageObjectLogging.log(
+				"DiscussionTitleFound",
+				"new discussion title found under the article",
+				true, driver
+		);
+
+		waitForTextToBePresentInElementByElement(
+				discussionUnderArticle, (String) content.get("discussionContent")
+		);
+		PageObjectLogging.log(
+				"DiscussionContentFound",
+				"new discussion content found under the article",
+				true, driver
+		);
+	}
+
+	public void verifyEditionArea() {
+		waitForElementByElement(editionArea);
+		PageObjectLogging.log("verifyEditArea", "verify that edition area is present", true);
+	}
+
+	public void verifyNewArticle(String articleName) {
+		waitForElementByElement(titleLocator);
+
+		articleName = articleName.replace("_", " ");
+		waitForTextToBePresentInElementByElement(titleLocator, articleName);
+		PageObjectLogging.log(
+				"NewArticleTitle",
+				"New article title is present",
+				true, driver
+		);
+	}
+
+	public void verifyNewContent(String content) {
+		waitForElementByElement(articleContentLocator);
+
+		waitForTextToBePresentInElementByElement(
+				articleContentLocator, content
+		);
+
+		PageObjectLogging.log(
+				"ContentFound",
+				"new content found in the article",
+				true, driver
+		);
+	}
+
+	public void verifyOasisOnly() {
+		waitForElementByElement(oasisOnly);
+		PageObjectLogging.log("verifyOasisOnly", "Oasis only warning is present", true);
+	}
+
+	public void verifyReplyPresent(String reply) {
+		waitForTextToBePresentInElementByElement(
+				comments, reply
+		);
+		PageObjectLogging.log(
+				"ReplyFound",
+				"reply found under the comment",
+				true, driver
+		);
+	}
+
+	public void verifyRenamed(String articleName) {
+		openArticle(articleName);
+		Assertion.assertEquals(titleLocator.getText(), articleName);
+		PageObjectLogging.log(
+				"NewArticleNamePresent",
+				"Article has new name",
+				true, driver
+		);
+	}
+}
