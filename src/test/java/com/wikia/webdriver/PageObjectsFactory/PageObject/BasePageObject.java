2ccb74e3beb4bab496261ef0598273dec7990521
michal.nowierski@wikia-inc.com <michal.nowierski@wikia-inc.com>
2013-07-19 16:08:42 +0200

    [QAART-167] adding the functionality

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/BasePageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/BasePageObject.java
@@ -4,15 +4,22 @@ import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import com.wikia.webdriver.Common.ContentPatterns.URLsContent;
-import com.wikia.webdriver.Common.ContentPatterns.XSSContent;
-import org.openqa.selenium.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.wikia.webdriver.Common.ContentPatterns.URLsContent;
+import com.wikia.webdriver.Common.ContentPatterns.XSSContent;
 import com.wikia.webdriver.Common.Core.Assertion;
 import com.wikia.webdriver.Common.Core.CommonExpectedConditions;
 import com.wikia.webdriver.Common.Core.Global;
@@ -181,7 +188,7 @@ public class BasePageObject{
 		}
 
 		PageObjectLogging.log("getUrl",
-				"page loaded for less then 30 seconds after click", true);
+				"page loaded for less then 30 seconds after click", true, true);
 	}
 
 	public void refreshPage() {
