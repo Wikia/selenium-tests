ac293871b2fa46a10813e9087e3c36a659750592
bognix <bogna@wikia-inc.com>
2013-10-16 15:52:16 +0200

    ADEN-591 IDG on german corp pages

--- src/test/java/com/wikia/webdriver/Common/Templates/NewTestTemplate.java
+++ src/test/java/com/wikia/webdriver/Common/Templates/NewTestTemplate.java
@@ -1,16 +1,11 @@
 package com.wikia.webdriver.Common.Templates;
 
 import java.lang.reflect.Method;
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class NewTestTemplate extends NewTestTemplateCore {
-
-	public NewTestTemplate() {
-		super();
-	}
 
 	@BeforeClass(alwaysRun = true)
 	public void beforeClass() {
@@ -20,6 +15,7 @@ public class NewTestTemplate extends NewTestTemplateCore {
 	@BeforeMethod(alwaysRun = true)
 	public void start(Method method, Object[] data) {
 		startBrowser();
+		logOut();
 	}
 
 	@AfterMethod(alwaysRun = true)
--- src/test/java/com/wikia/webdriver/TestCases/AdsTests/TestAdsOnCorporatePages.java
+++ src/test/java/com/wikia/webdriver/TestCases/AdsTests/TestAdsOnCorporatePages.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.wikia.webdriver.TestCases.AdsTests;
 
 import com.wikia.webdriver.Common.Core.GeoEdge.GeoEdgeProxy;
@@ -50,15 +46,6 @@ public class TestAdsOnCorporatePages extends AdsTestTemplate {
 		groups={"Ads_Corporate_Page_002", "GB"}
 	)
 	public void TestCorporatePage_GB() throws Exception {
-		AdsBaseObject wikiPage = new AdsBaseObject(driver, testedPage);
-		wikiPage.verifyNoLiftiumAdsOnPage();
-	}
-
-	@GeoEdgeProxy(country="DE")
-	@Test (
-		groups={"Ads_Corporate_Page_003", "DE"}
-	)
-	public void TestCorporatePage_DE() throws Exception {
 		AdsBaseObject wikiPage = new AdsBaseObject(driver, testedPage);
 		wikiPage.verifyNoLiftiumAdsOnPage();
 	}
