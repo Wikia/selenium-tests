c4b5f505cfd902ec25117108ba657b46562ebaa2
justnpT <Michal.Nowierski@wikia-inc.com>
2013-10-22 21:31:29 +0200

    QAART-180 fixing engine

--- src/test/java/com/wikia/webdriver/Common/Logging/PageObjectLogging.java
+++ src/test/java/com/wikia/webdriver/Common/Logging/PageObjectLogging.java
@@ -3,12 +3,16 @@ package com.wikia.webdriver.Common.Logging;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -29,7 +33,7 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 
 	private By lastFindBy;
 	private WebDriver driver;
-	private StringWriter htmlWriter;
+	private static StringWriter htmlWriter;
 	private static Html builder;
 
 	private static long imageCounter;
@@ -42,43 +46,44 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 	private static String logPath = reportPath + logFileName;
 
 //head
-	String httpEquiv = "content type";
-	String content = "text/html";
-	String charset = "UTF-8";
+	private String httpEquiv = "content type";
+	private String content = "text/html";
+	private String charset = "UTF-8";
 //styles
-	String tableStyle = "table {margin:0 auto;}";
-	String tdStyle = "td {border-top: 1px solid grey;}";
-	String tdFirstChildStyle = "td:first-child {width:200px;}";
-	String tdSecondChildStyle = "td:nth-child(2) {width:660px;}";
-	String tdThirdChildStyle = "td:nth-child(3) {width:100px;}";
-	String trSucces= "tr.success {color:black;background-color:#CCFFCC;}";
-	String trError = "tr.error {color:black;background-color:#FFCCCC;}";
-	String trStep = "tr.step {color:white;background:grey}";
-	String nl = "\n";
+	private String tableStyle = "table {margin:0 auto;}";
+	private String tdStyle = "td {border-top: 1px solid grey;}";
+	private String tdFirstChildStyle = "td:first-child {width:200px;}";
+	private String tdSecondChildStyle = "td:nth-child(2) {width:660px;}";
+	private String tdThirdChildStyle = "td:nth-child(3) {width:100px;}";
+	private String trSuccess= "tr.success {color:black;background-color:#CCFFCC;}";
+	private String trError = "tr.error {color:black;background-color:#FFCCCC;}";
+	private String trStep = "tr.step {color:white;background:grey}";
+	private String nl = "\n";
+	private static String nbsp = "&nbsp;";
 //scripts
 	//jquery
-	String scriptType = "text/javascript";
-	String jQyeryScriptSource = "http://code.jquery.com/jquery-1.8.2.min.js";
+	private String scriptType = "text/javascript";
+	private String jQyeryScriptSource = "http://code.jquery.com/jquery-1.8.2.min.js";
 	//custom wikia qa scripts
-	String wikiaScriptSource = "./src/test/resources/script.js";
+	private String wikiaScriptSource = "./src/test/resources/script.js";
 	//test information paragraphs
-	String pDate = "asd";
-	String pBrowser = "sdf";
-	String pOS = "dfg";
-	String pScreenRes = "fgh";
-	String pTestEnv = "ghj";
-	String pTestEnv2 = "hjk";
-	String pTestVersion = "jkl";
+	private String pDate = "asd";
+	private String pBrowser = "sdf";
+	private String pOS = "dfg";
+	private String pScreenRes = "fgh";
+	private String pTestEnv = "ghj";
+	private String pTestEnv2 = "hjk";
+	private String pTestVersion = "jkl";
 //buttons
 	//hide low level action button
-	String hllButtonID = "hideLowLevel";
-	String hllButtonText = "hide low level actions";
+	private String hllButtonID = "hideLowLevel";
+	private String hllButtonText = "hide low level actions";
 	//show low level action button
-	String sllButtonID = "showLowLevel";
-	String sllButtonText = "show low level actions";
+	private String sllButtonID = "showLowLevel";
+	private String sllButtonText = "show low level actions";
 //onStart table
-	String testClassName = "asd";
-	String testName = "sdf";
+	private String testClassName = "asd";
+	private String testName = "sdf";
 
 
 	public static void log(String command, String description, boolean success) {
@@ -121,25 +126,34 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 		this.builder = new Html(htmlWriter);
 
 		builder.html();
-			builder.style().text(nl+tableStyle+nl+tdFirstChildStyle+nl+tdSecondChildStyle+nl+tdThirdChildStyle+nl+trSucces+nl+trError+nl+trStep).end();
+			builder.style().raw(nl+tableStyle+nl+tdFirstChildStyle+nl+tdSecondChildStyle+nl+tdThirdChildStyle+nl+trSuccess+nl+trError+nl+trStep).end();
 			builder.head();
 				builder.meta().httpEquiv(httpEquiv).content(content).charset(charset);
 				//TODO: check if you can extract below style to previous styles
-				builder.style().text(nl+tdStyle).end();
+				builder.style().raw(nl+tdStyle).end();
 			builder.end();
 			builder.body();
 				builder.script().type(scriptType).src(jQyeryScriptSource).end();
-				builder.p().text(pDate).end();
-				builder.p().text(pBrowser).end();
-				builder.p().text(pOS).end();
-				builder.p().text(pScreenRes).end();
-				builder.p().text(pTestEnv).end();
-				builder.p().text(pTestEnv2).end();
-				builder.p().text(pTestVersion).end();
+				builder.p().raw(pDate).end();
+				builder.p().raw(pBrowser).end();
+				builder.p().raw(pOS).end();
+				builder.p().raw(pScreenRes).end();
+				builder.p().raw(pTestEnv).end();
+				builder.p().raw(pTestEnv2).end();
+				builder.p().raw(pTestVersion).end();
 				builder.div().id("toc").end();
-				builder.button().id(hllButtonID).text(hllButtonText).end();
-				builder.button().id(sllButtonID).text(sllButtonText).end();
-				builder.script().src(wikiaScriptSource).end();
+				builder.button().id(hllButtonID).raw(hllButtonText).end();
+				builder.button().id(sllButtonID).raw(sllButtonText).end();
+				FileInputStream input;
+				try {
+					input = new FileInputStream(wikiaScriptSource);
+					String content = IOUtils.toString(input);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				builder.script().raw(content).end();
 //		CommonUtils.appendTextToFile(logPath, builder.toString());
 //		try{
 //			FileInputStream input = new FileInputStream("./src/test/resources/script.txt");
@@ -156,14 +170,14 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 		this.testName = result.getName().toString();
 		this.testClassName = result.getTestClass().getName().toString();
 
-		builder.h1().text("Class: ");
-			builder.em().text(testClassName+"." +testName).end();
+		builder.h1().raw("Class: ");
+			builder.em().raw(testClassName+"." +testName).end();
 		builder.end();
-		builder.table(); //open table (will not be closed till onTestSucces)
+		builder.table(); //open table (will not be closed till onTestSuccess)
 		builder.tr().classAttr("step");
-			builder.td().text("&nbsp").end();
-			builder.td().h1().em().text(testName).end().end().end();
-			builder.td().br().text("&nbsp").end(); //TODO: sprawdz czy dziala &nbsp zamiast &nbsp; Jesli tak wywal nbsp do zmiennej
+			builder.td().raw(nbsp).end();
+			builder.td().h1().em().raw(testName).end().end().end();
+			builder.td().br().raw(nbsp).end();
 		builder.end();
 
 		System.out.println(testClassName + " " + testName);
@@ -180,9 +194,9 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 		}
 
 		builder.tr().classAttr(className);
-			builder.td().text(description).end();
-			builder.td().text(command).end();
-			builder.td().br().text("&nbsp").end();
+			builder.td().raw(command).end();
+			builder.td().raw(description).end();
+			builder.td().br().raw(nbsp).end();
 		builder.end();
 
 //		CommonUtils.appendTextToFile(logPath, builder.toString());
@@ -198,11 +212,11 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 		String className = success ? "success" : "error";
 
 		builder.tr().classAttr(className+"lowLevelAction");
-			builder.td().text(description).end();
-			builder.td().text(command).end();
+			builder.td().raw(description).end();
+			builder.td().raw(command).end();
 			builder.td();
-				builder.br().a().href("screenshots/screenshot"+imageCounter+".png").text("screenshot").end();
-				builder.br().a().href("screenshots/screenshot"+imageCounter+".html").text("HTML Source").end();
+				builder.br().a().href("screenshots/screenshot"+imageCounter+".png").raw("screenshot").end();
+				builder.br().a().href("screenshots/screenshot"+imageCounter+".html").raw("HTML Source").end();
 			builder.end();
 		builder.end();
 		logJSError(driver);
@@ -211,15 +225,15 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 	@Override
 	public void onTestSuccess(ITestResult result) { //jatl DONE
 		builder.tr().classAttr("step");
-		builder.td().text("&nbsp").end();
-			builder.td().text("STOP LOGGING METHOD");
+		builder.td().raw(nbsp).end();
+			builder.td().raw("STOP LOGGING METHOD");
 				builder.div().style("text-align:center");
 					builder.a().style("color:blue");
-						builder.b().text("BACK TO MENU").end();
+						builder.b().raw("BACK TO MENU").end();
 						builder.end();
 					builder.end();
 				builder.end();
-			builder.td().br().text("&nbsp").end();
+			builder.td().br().raw(nbsp).end();
 		builder.end();
 		builder.end(); // end </table>
 //		CommonUtils.appendTextToFile(logPath, builder.toString());
@@ -250,11 +264,11 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 					"\n" +ExceptionUtils.getStackTrace(result.getThrowable());
 
 			builder.tr().classAttr("error");
-				builder.td().text("error").end();
-				builder.td().text(exception).end();
+				builder.td().raw("error").end();
+				builder.td().raw(exception).end();
 				builder.td();
-					builder.br().a().href("screenshots/screenshot"+imageCounter+".png").text("Screenshot").end();
-					builder.br().a().href("screenshots/screenshot"+imageCounter+".html").text("HTML Source").end();
+					builder.br().a().href("screenshots/screenshot"+imageCounter+".png").raw("Screenshot").end();
+					builder.br().a().href("screenshots/screenshot"+imageCounter+".html").raw("HTML Source").end();
 					builder.end();
 				builder.end();
 			builder.end();
@@ -268,9 +282,9 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 	@Override
 	public void afterChangeValueOf(WebElement element, WebDriver driver) { //jatl DONE
 		builder.tr().classAttr("success lowLevelAction");
-			builder.td().text("ChangeValueOfField").end();
-			builder.td().text(lastFindBy.toString()).end(); //TODO: verify if lastFindBy.ToString works properly. Before it was lastFindBy alone
-			builder.td().br().text("&nbsp").end();
+			builder.td().raw("ChangeValueOfField").end();
+			builder.td().raw(lastFindBy.toString()).end();
+			builder.td().br().raw(nbsp).end();
 		builder.end();
 //		CommonUtils.appendTextToFile(logPath, builder.toString());
 
@@ -279,19 +293,19 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 	@Override
 	public void afterClickOn(WebElement element, WebDriver driver) { //jtal DONE
 		builder.tr().classAttr("success lowLevelAction");
-			builder.td().text("click").end();
-			builder.td().text(lastFindBy.toString()).end();
-			builder.td().br().text("&nbsp").end();
+			builder.td().raw("click").end();
+			builder.td().raw(lastFindBy.toString()).end();
+			builder.td().br().raw(nbsp).end();
 		builder.end();
 //		CommonUtils.appendTextToFile(logPath, builder.toString());
 	}
 
 	@Override
 	public void afterNavigateTo(String url, WebDriver driver) { //jtal DONE
-		builder.tr().classAttr("succes");
-			builder.td().text("Navigate to").end();
-			builder.td().text(url).end();
-			builder.td().br().text("&nbsp").end();
+		builder.tr().classAttr("success");
+			builder.td().raw("Navigate to").end();
+			builder.td().raw(url).end();
+			builder.td().br().raw(nbsp).end();
 		builder.end();
 //		CommonUtils.appendTextToFile(logPath, builder.toString());
 		logJSError(driver);
@@ -303,9 +317,9 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 			ArrayList<String> error = (ArrayList<String>) js.executeScript("return window.JSErrorCollector_errors.pump()");
 			if (!error.isEmpty()) {
 				builder.tr().classAttr("error");
-					builder.td().text("click").end();
-					builder.td().text(error.toString()).end(); //TODO: check if it can be toString, originally was 'error' alone
-					builder.td().br().text("&nbsp").end();
+					builder.td().raw("click").end();
+					builder.td().raw(error.toString()).end(); //TODO: check if it can be toString, originally was 'error' alone
+					builder.td().br().raw(nbsp).end();
 				builder.end();
 //				CommonUtils.appendTextToFile(logPath, builder.toString());
 			}
@@ -324,6 +338,6 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 	public void onFinish(ITestContext context) { //jatl DONE
 			builder.end();  // end </body>
 		builder.end(); // end </html>
-//		CommonUtils.appendTextToFile(logPath, "</body></html>");
+		CommonUtils.appendTextToFile(logPath, htmlWriter.getBuffer().toString());
 	}
 }
