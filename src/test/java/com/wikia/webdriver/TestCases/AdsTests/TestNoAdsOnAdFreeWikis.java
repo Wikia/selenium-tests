f1f1283a5bd8698eabee0ae916750b8eae809ae7
bognix <bogna@wikia-inc.com>
2013-11-21 14:41:25 +0100

    ADEN-724 tests for ad free wikis

---
+++ src/test/java/com/wikia/webdriver/TestCases/AdsTests/TestNoAdsOnAdFreeWikis.java
@@ -0,0 +1,70 @@
+package com.wikia.webdriver.TestCases.AdsTests;
+
+import com.wikia.webdriver.Common.Core.GeoEdge.GeoEdgeProxy;
+import com.wikia.webdriver.Common.Core.URLBuilder.UrlBuilder;
+import com.wikia.webdriver.Common.DataProvider.AdsDataProvider;
+import com.wikia.webdriver.Common.Templates.AdsTestTemplate;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.AdsBase.AdsBaseObject;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Bogna 'bognix' Knychala
+ */
+@Test (
+	groups={"NoAdsOnAdFreeWikis", "Ads"}
+)
+public class TestNoAdsOnAdFreeWikis extends AdsTestTemplate {
+
+	private String testedPage;
+
+	@Factory(
+		dataProviderClass=AdsDataProvider.class,
+		dataProvider="adFreeWikis"
+	)
+	public TestNoAdsOnAdFreeWikis(String wikiName, String path) {
+		super();
+		UrlBuilder urlBuilder = new UrlBuilder(config.getEnv());
+		testedPage = urlBuilder.getUrlForPath(wikiName, path);
+		if (config.getQS() != null) {
+			testedPage = urlBuilder.appendQueryStringToURL(testedPage, config.getQS());
+		}
+	}
+
+	@GeoEdgeProxy(country="US")
+	@Test (
+		groups={"NoAdsOnAdFreeWikis_001", "US"}
+	)
+	public void TestNoAdsForUsers_US() throws Exception {
+		AdsBaseObject wikiPage = new AdsBaseObject(driver, testedPage);
+		wikiPage.verifyNoAdsOnPage();
+	}
+
+	@GeoEdgeProxy(country="DE")
+	@Test (
+		groups={"NoAdsOnAdFreeWikis_002", "DE"}
+	)
+	public void TestNoAdsForUsers_DE() throws Exception {
+		AdsBaseObject wikiPage = new AdsBaseObject(driver, testedPage);
+		wikiPage.verifyNoAdsOnPage();
+	}
+
+	@GeoEdgeProxy(country="HR")
+	@Test (
+		groups={"NoAdsOnAdFreeWikis_003", "HR"}
+	)
+	public void TestNoAdsForUsers_HR() throws Exception {
+		AdsBaseObject wikiPage = new AdsBaseObject(driver, testedPage);
+		wikiPage.verifyNoAdsOnPage();
+	}
+
+	@GeoEdgeProxy(country="AU")
+	@Test (
+		groups={"NoAdsOnAdFreeWikis_004", "AU"}
+	)
+	public void TestNoAdsForUsers_AU() throws Exception {
+		AdsBaseObject wikiPage = new AdsBaseObject(driver, testedPage);
+		wikiPage.verifyNoAdsOnPage();
+	}
+}
