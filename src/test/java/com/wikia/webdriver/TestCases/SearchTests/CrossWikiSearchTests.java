0b3e003dc3f831bbe7d8bf8c477a46b093bfd776
justnpT <michal.nowierski@wikia-inc.com>
2014-02-27 19:59:32 +0100

    Adding open P3 annotation to CrossWikiSearchTests_009 test

--- src/test/java/com/wikia/webdriver/TestCases/SearchTests/CrossWikiSearchTests.java
+++ src/test/java/com/wikia/webdriver/TestCases/SearchTests/CrossWikiSearchTests.java
@@ -136,8 +136,15 @@ public class CrossWikiSearchTests extends NewTestTemplate {
 		search.verifyQuery(expectedWikiTitle);
 	}
 
+	/**
+	 * Navigate to http://www.wikia.com/index.php?title=Special:Search
+	 * type: GTA V
+	 * verify that GTA V wikia was found
+	 * type: GRA 5
+	 * verify that GTA V wikia was found
+	 */
 	@Test(groups = {"CrossWikiSearchTests_009", "Search", "CrossWikiSearch"})
-	public void crossWikiSearch_009_romanNumbersMatch() {
+	public void crossWikiSearch_009_romanNumbersMatch_PLA_1245() {
 		CrossWikiSearchPageObject search = new CrossWikiSearchPageObject(driver);
 		search.goToSearchPage(wikiCorporateURL);
 		search.searchFor(SearchContent.searchPhraseRomanNumber);
