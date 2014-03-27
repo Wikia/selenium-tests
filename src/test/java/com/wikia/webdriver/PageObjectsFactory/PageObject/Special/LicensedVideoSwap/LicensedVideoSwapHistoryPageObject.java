0e8d07b9c4728996a5eee96e107ecf227ed76859
Kenneth Kouot <kenneth@wikia-inc.com>
2014-03-27 11:18:21 -0700

    VID-1376 tighten up some assertions and log output

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/LicensedVideoSwap/LicensedVideoSwapHistoryPageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/LicensedVideoSwap/LicensedVideoSwapHistoryPageObject.java
@@ -24,40 +24,36 @@ public class LicensedVideoSwapHistoryPageObject extends SpecialPageObject {
 	private WebElement backToLvsBtn;
 	@FindBy(css = ".undo-link")
 	private WebElement firstUndoLink;
+	@FindBy(css = ".global-notification .msg")
+	private WebElement notification;
 
 	public LicensedVideoSwapHistoryPageObject(WebDriver driver) {
 		super(driver);
-		PageFactory.initElements(driver, this);
 	}
 
 	public LicensedVideoSwapPageObject navigateToLvsPage() {
 		backToLvsBtn.click();
-		PageObjectLogging.log("navigateToLvsPage", "verify that the lvs back button navigates to lvs page", true);
+		PageObjectLogging.log("navigateToLvsPage", "lvs back button navigates to lvs page", true);
 		return new LicensedVideoSwapPageObject(driver);
-
 	}
 
 	public void verifyOnHistoryPage() {
 		String url = driver.getCurrentUrl();
-		String[] urlParts = url.split("/");
-		Assertion.assertEquals(urlParts[urlParts.length - 1], "History");
-		PageObjectLogging.log("verifyOnHistoryPage", "verify that the url is the correct one for history page", true);
+		Assertion.assertTrue(url.contains("/History"));
+		PageObjectLogging.log("verifyOnHistoryPage", "the url is the correct one for history page", true);
 	}
 
 	public void clickUndoSwapLink() {
 		firstUndoLink.click();
 		PageObjectLogging.log("undoSwap", "undo link clicked", true);
 	}
+
 	public void verifyUndoSucceeded() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		try {
-			String notificationMsg = driver.findElement(By.cssSelector(".global-notification .msg")).getText();
-			PageObjectLogging.log("verifyUndoSucceeded", "grabbing notification text", true);
-			Assertion.assertEquals(notificationMsg, "This page has been restored.");
-		} catch (NoSuchElementException e) {
-			PageObjectLogging.log("verifyUndoSucceeded", "no notification present", true);
-			driver.quit();
-		}
+		waitForElementByElement(notification);
+		String notificationMsg = notification.getText();
+		Assertion.assertEquals(notificationMsg, "This page has been restored.");
+		PageObjectLogging.log("verifyUndoSucceeded", "able to undo a swapped video", true);
 	}
 }
 
--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/LicensedVideoSwap/LicensedVideoSwapPageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/LicensedVideoSwap/LicensedVideoSwapPageObject.java
@@ -7,7 +7,6 @@ package com.wikia.webdriver.PageObjectsFactory.PageObject.Special.LicensedVideoS
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 import com.wikia.webdriver.Common.ContentPatterns.URLsContent;
 import com.wikia.webdriver.Common.Core.Assertion;
@@ -21,20 +20,18 @@ public class LicensedVideoSwapPageObject extends SpecialPageObject {
 
 	public LicensedVideoSwapPageObject(WebDriver driver) {
 		super(driver);
-		PageFactory.initElements(driver, this);
 	}
 
 	public LicensedVideoSwapHistoryPageObject navigateToHistoryPage() {
 		lvsHistoryBtn.click();
-		PageObjectLogging.log("navigateToHistoryPage", "verify that the lvs history button navigates to right page", true);
+		PageObjectLogging.log("navigateToHistoryPage", "lvs history button navigates to right page", true);
 		return new LicensedVideoSwapHistoryPageObject(driver);
 	}
 
 	public void verifyOnLvsPage() {
 		String url = driver.getCurrentUrl();
-		String[] urlParts = url.split("/");
-		Assertion.assertEquals(urlParts[urlParts.length - 2] + '/' +urlParts[urlParts.length - 1], URLsContent.specialLicensedVideoSwap);
-		PageObjectLogging.log("verifyOnLvsPage", "verify that the url is the correct one for LVS page", true);
+		Assertion.assertTrue(url.contains(URLsContent.specialLicensedVideoSwap));
+		PageObjectLogging.log("verifyOnLvsPage", "url is the correct one for LVS page", true);
 	}
 }
 
