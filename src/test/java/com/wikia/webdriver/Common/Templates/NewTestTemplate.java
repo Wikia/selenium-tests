d02373d93f5d0288b7a57cfe9ecb8615f03c7393
Bogna Knychala <bogna.ka@gmail.com>
2014-05-13 16:00:55 +0200

    ADEN-1137 fix for GeoEdge not working on ADEN-644 branch

--- src/test/java/com/wikia/webdriver/Common/Templates/NewTestTemplate.java
+++ src/test/java/com/wikia/webdriver/Common/Templates/NewTestTemplate.java
@@ -21,15 +21,7 @@ public class NewTestTemplate extends NewTestTemplateCore {
 				method.getAnnotation(UserAgent.class).userAgent()
 			);
 		}
-	}
-
-	@BeforeMethod(alwaysRun = true)
-	public void createDriver() {
 		startBrowser();
-	}
-
-	@BeforeMethod(alwaysRun = true, dependsOnMethods = {"createDriver"})
-	public void logOutMethod() {
 		logOut();
 	}
 
