b1a476366c229e74c55dd3c0877ca219918c4070
Damian Jozwiak <damian@wikia-inc.com>
2014-05-19 12:59:12 +0200

    CON-1142 | Edithub Dasboard test cleanup

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/EditHub/DashBoardPageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/EditHub/DashBoardPageObject.java
@@ -1,24 +1,17 @@
 package com.wikia.webdriver.PageObjectsFactory.PageObject.EditHub;
 
 import com.wikia.webdriver.Common.ContentPatterns.URLsContent;
-import com.wikia.webdriver.Common.Core.Global;
 import com.wikia.webdriver.Common.Logging.PageObjectLogging;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.BasePageObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
-/**
- * Created with IntelliJ IDEA.
- * User: kvas
- * Date: 15.03.13
- * Time: 14:47
- * To change this template use File | Settings | File Templates.
- */
 public class DashBoardPageObject extends BasePageObject {
+	@FindBy(css = "#date-picker .ui-datepicker")
+	protected WebElement edithub_dashboardCalendar;
+
 	public DashBoardPageObject(WebDriver driver) {
 		super(driver);
 		PageFactory.initElements(driver, this);
@@ -30,7 +23,7 @@ public class DashBoardPageObject extends BasePageObject {
 	}
 
 	public void verifyCalendarAppears() {
-		waitForElementByCss("#date-picker .ui-datepicker");
+		waitForElementByElement(edithub_dashboardCalendar);
 		PageObjectLogging.log("verifyCalendarAppears", "Curators calendar visible", true);
 	}
 }
