ca389c72ab63419e00d720322d7f8e908d5811aa
Bogna Knychala <bogna.ka@gmail.com>
2014-05-23 15:10:16 +0200

    ADEN-1188 update automated tests for HVC

--- src/test/java/com/wikia/webdriver/TestCases/AdsTests/TestHighValueCountries.java
+++ src/test/java/com/wikia/webdriver/TestCases/AdsTests/TestHighValueCountries.java
@@ -16,27 +16,11 @@ public class TestHighValueCountries extends NewTestTemplate {
 
 	private Credentials credentials = config.getCredentials();
 	private SpecialFactoryPageObject wikiFactory;
+	private String instantGlobalsPrefix = "Wikia.InstantGlobals.";
 
 	private void logIn() {
 		SpecialUserLoginPageObject userLoginPageObject = new SpecialUserLoginPageObject(driver);
 		userLoginPageObject.loginAndVerify(credentials.userNameStaff, credentials.passwordStaff, wikiURL);
-	}
-
-	@Test(
-		dataProvider="getWikisWithNonStandardHVC", dataProviderClass=AdsDataProvider.class,
-		groups= {"HVC", "HVC_nonStandard"}
-	)
-	public void TestNonStandardHVC(String wikiName) {
-		logIn();
-		String testedWiki = urlBuilder.getUrlForWiki(wikiName);
-		wikiFactory = new SpecialFactoryPageObject(driver);
-		wikiFactory.openWikiFactoryPage(testedWiki);
-		String valueOnCommunity = wikiFactory.getVariableSetValue(
-			WikiFactoryVariables.wgHighValueCountries
-		);
-		wikiFactory.verifyWgVariableValueSameAsProvided(
-			WikiFactoryVariables.wgHighValueCountries, valueOnCommunity, testedWiki
-		);
 	}
 
 	@Test(
@@ -48,11 +32,14 @@ public class TestHighValueCountries extends NewTestTemplate {
 		String testedWiki = urlBuilder.getUrlForWiki(wikiName);
 		wikiFactory = new SpecialFactoryPageObject(driver);
 		wikiFactory.openWikiFactoryPage(testedWiki);
-		String valueOnCommunity = wikiFactory.getVariableDefaultValue(
+		Object[] variableKeysFromCommunity = wikiFactory.getVariableDefaultValueKeys(
 			WikiFactoryVariables.wgHighValueCountries
 		);
-		wikiFactory.verifyWgVariableValueSameAsProvided(
-			WikiFactoryVariables.wgHighValueCountries, valueOnCommunity, testedWiki
+		String wgHVC_JSConsole = instantGlobalsPrefix + WikiFactoryVariables.wgHighValueCountries.toString();
+		Object[] wgHVC_valueInConsole = wikiFactory.getWgVariableKeysFromPage(testedWiki, wgHVC_JSConsole);
+
+		wikiFactory.verifyWgVariableValuesTheSame(
+			wgHVC_valueInConsole, variableKeysFromCommunity
 		);
 	}
 }
