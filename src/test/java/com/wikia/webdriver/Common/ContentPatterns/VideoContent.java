d03739ee4800c274b476e6f28cc72babeaabe2dd
Garth Webb <garth@wikia-inc.com>
2014-06-04 18:27:18 -0700

    VID-1396 Implemented test

--- src/test/java/com/wikia/webdriver/Common/ContentPatterns/VideoContent.java
+++ src/test/java/com/wikia/webdriver/Common/ContentPatterns/VideoContent.java
@@ -9,6 +9,7 @@ public class VideoContent {
 	public static String youtubeVideoURL3 = "http://www.youtube.com/watch?v=LQjkDW3UPVk";
 	public static String youtubeVideoURL4 = "https://www.youtube.com/watch?v=bKHz7wOjb9w";
 	public static String youtubeVideoURL4FileName = "The_JET_Bicycle_-_The_most_dangerous_unsafe_bike_EVER";
+	public static String youtubeVideoURL5 = "https://www.youtube.com/watch?v=ewum1Uq2MtU";
 	public static String wikiaVideoQuery = "wikia";
 	public static String wikiaVideoURL = "http://mediawiki119.wikia.com/wiki/File:Jimmy_Wales_-_What_is_Wikia%3F";
 	public static String nonPremiumVideoURL = "http://www.youtube.com/watch?v=d9r5_DDMMjY";
