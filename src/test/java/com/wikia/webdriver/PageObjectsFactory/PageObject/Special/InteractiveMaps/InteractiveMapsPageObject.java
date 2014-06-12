fdf2a374d8a3ac11c8e1eda0ddcbf16a17b06774
Rodrigo Molinero Gomez <rodrigo@wikia-inc.com>
2014-06-12 16:15:09 +0200

    added page object

---
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/InteractiveMaps/InteractiveMapsPageObject.java
@@ -0,0 +1,22 @@
+package com.wikia.webdriver.PageObjectsFactory.PageObject.Special.InteractiveMaps;
+
+import org.openqa.selenium.WebDriver;
+
+import com.wikia.webdriver.PageObjectsFactory.PageObject.BasePageObject;
+
+/**
+* @author Rodrigo 'RodriGomez' Molinero
+*
+*/
+
+public class InteractiveMapsPageObject extends BasePageObject{
+
+	public InteractiveMapsPageObject(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
+
+
