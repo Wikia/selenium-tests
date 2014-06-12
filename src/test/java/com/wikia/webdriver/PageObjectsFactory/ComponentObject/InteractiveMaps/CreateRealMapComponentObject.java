dd4539826e6ee561ea1e748b9643b36a888509ce
Rodrigo Molinero Gomez <rodrigo@wikia-inc.com>
2014-06-12 15:19:07 +0200

    first component objects

---
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/ComponentObject/InteractiveMaps/CreateRealMapComponentObject.java
@@ -0,0 +1,22 @@
+package com.wikia.webdriver.PageObjectsFactory.ComponentObject.InteractiveMaps;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.wikia.webdriver.Common.Logging.PageObjectLogging;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.BasePageObject;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.Article.ArticlePageObject;
+
+/**
+ * @author Rodrigo 'RodriGomez' Molinero
+ *
+ */
+
+public class CreateRealMapComponentObject extends BasePageObject{
+
+	public CreateRealMapComponentObject(WebDriver driver) {
+		super(driver);
+	}
+
+}
