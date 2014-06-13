28ee2d383153fb1b54eb686cb57e58412e7c7ee0
Robert Chan <rchan@wikia-inc.com>
2014-06-12 17:33:15 -0700

    VE-1223 Adding the test and testsuite.xml

---
+++ src/test/java/com/wikia/webdriver/TestCases/VisualEditor/VERecaptchaTests.java
@@ -0,0 +1,50 @@
+package com.wikia.webdriver.TestCases.VisualEditor;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.wikia.webdriver.Common.ContentPatterns.PageContent;
+import com.wikia.webdriver.Common.ContentPatterns.URLsContent;
+import com.wikia.webdriver.Common.Templates.NewTestTemplateBeforeClass;
+import com.wikia.webdriver.PageObjectsFactory.ComponentObject.VisualEditorDialogs.VisualEditorSaveChangesDialog;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.WikiBasePageObject;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.Article.ArticlePageObject;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.VisualEditor.VisualEditorPageObject;
+
+/**
+ * @author Robert 'Rochan' Chan
+ * @ownership Contribution
+ *
+ * VE-1233 Testing Anon adding external link would prompt recaptcha
+ *
+ */
+
+public class VERecaptchaTests extends NewTestTemplateBeforeClass {
+
+	WikiBasePageObject base;
+
+	@BeforeMethod(alwaysRun = true)
+	public void setupd() {
+		wikiURL = urlBuilder.getUrlForWiki(URLsContent.veEnabledTestMainPage);
+		base = new WikiBasePageObject(driver);
+	}
+
+	@Test(
+		groups = {"VERecaptchaTests", "VEAddRecaptcha_001", "VEAnon"}
+	)
+	public void VEAddRecaptchaTests_001_AddExternalURL() throws InterruptedException {
+		String articleName = PageContent.articleNamePrefix + base.getTimeStamp();
+		ArticlePageObject article =
+			base.openArticleByName(wikiURL, articleName);
+		VisualEditorPageObject ve = article.openVEModeWithMainEditButton();
+		ve.verifyVEToolBarPresent();
+		ve.verifyEditorSurfacePresent();
+		ve.typeTextArea("http://www.wikia.com");
+		VisualEditorSaveChangesDialog saveDialog = ve.clickPublishButton();
+		saveDialog.verifyRecaptchaIsNotVisible();
+		saveDialog.clickSaveWithRecaptcha();
+		String firstImgSrc = saveDialog.getRecaptchaImageSrc();
+		saveDialog.clickSaveWithRecaptcha();
+		saveDialog.verifyIsNewRecaptcha(firstImgSrc);
+	}
+}
