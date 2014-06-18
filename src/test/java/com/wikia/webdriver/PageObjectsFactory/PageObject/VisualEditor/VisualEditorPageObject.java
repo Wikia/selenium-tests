1e25e659981634cfcfe976525d937fa832e6c3af
Robert Chan <rchan@wikia-inc.com>
2014-06-18 16:31:17 -0700

    VE-1210 DEBUG

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/VisualEditor/VisualEditorPageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/VisualEditor/VisualEditorPageObject.java
@@ -102,15 +102,10 @@ public class VisualEditorPageObject extends VisualEditorMenu {
 
 	public void copyAndPaste() {
 		waitForElementClickableByElement(editArea);
-//		editArea.sendKeys(Keys.chord(Keys.CONTROL, Keys.LEFT_SHIFT, Keys.ARROW_RIGHT));
 		editArea.sendKeys(Keys.chord(Keys.CONTROL, "a"));
 		editArea.sendKeys(Keys.chord(Keys.CONTROL, "c"));
 		editArea.sendKeys(Keys.chord(Keys.CONTROL, "v"));
 		editArea.sendKeys(Keys.chord(Keys.CONTROL, "v"));
-//		editArea.sendKeys(Keys.chord(Keys.COMMAND, "a"));
-//		editArea.sendKeys(Keys.chord(Keys.COMMAND, Keys.LEFT_SHIFT, Keys.ARROW_RIGHT));
-//		editArea.sendKeys(Keys.chord(Keys.COMMAND, "c"));
-//		editArea.sendKeys(Keys.chord(Keys.COMMAND, "v"));
 		PageObjectLogging.log("copyAndPaste", editArea.getText(), true, driver);
 	}
 
