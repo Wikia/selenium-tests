2d2ec5f9a09222937e0522fd05cadf7f338ddeed
Lukasz Jedrzejczak <lukasz.jedrzejczak@wikia-inc.com>
2014-07-16 16:32:43 +0200

    creating TemplateComponentObject methods and fields

---
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/ComponentObject/InteractiveMaps/TemplateComponentObject.java
@@ -0,0 +1,82 @@
+package com.wikia.webdriver.PageObjectsFactory.ComponentObject.InteractiveMaps;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.wikia.webdriver.Common.Logging.PageObjectLogging;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.BasePageObject;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.Special.InteractiveMaps.InteractiveMapsPageObject;
+import org.testng.Assert;
+
+/**
+ * @author Rodrigo 'RodriGomez' Molinero
+ * @author Lukasz Jedrzejczak
+ *
+ */
+
+public class TemplateComponentObject extends BasePageObject{
+
+	public TemplateComponentObject(WebDriver driver) {
+		super(driver);
+	}
+	
+	//UI Mapping
+	@FindBy(css = "input[name='map-title']")
+	private WebElement mapTitleField;
+	@FindBy(css = "input[name='tile-set-title']")
+	private WebElement nameTemplateField;
+	@FindBy(css = "#intMapNext")
+	private WebElement nextButton;
+	@FindBy(css = "#intMapBack")
+	private WebElement backButton;
+	@FindBy(css = ".close")
+	private WebElement closeButton;
+	@FindBy(css = ".intMapPreviewImage")
+	private WebElement templateImagePreview;
+	@FindBy(css = ".modal.medium.int-map-modal")
+	private WebElement newTemplateDialog;
+	
+	public void typeMapName(String mapName) {
+		waitForElementByElement(mapTitleField);
+		mapTitleField.sendKeys(mapName);
+		PageObjectLogging.log("typeMapName", mapName + " title for map typed in", true, driver);
+	}
+	
+	public void typeTemplateName(String templateName) {
+		waitForElementByElement(nameTemplateField);
+		nameTemplateField.sendKeys(templateName);
+		PageObjectLogging.log("typeTemplateName", templateName + " title for template typed in", true, driver);
+	}
+	
+	public CreatePinTypesComponentObject clickNext() {
+		waitForElementClickableByElement(nextButton);
+		nextButton.click();
+		PageObjectLogging.log("clickNext", "clicked next button in naming map modal", true, driver);
+		return new CreatePinTypesComponentObject(driver);
+	}
+	
+	public CreateACustomMapComponentObject clickBack() {
+		waitForElementClickableByElement(backButton);
+		backButton.click();
+		PageObjectLogging.log("clickBack", "clicked back button in naming map modal", true, driver);
+		return new CreateACustomMapComponentObject(driver);
+	}
+	
+	public InteractiveMapsPageObject closeTemplateComponentDialog() {
+		waitForElementClickableByElement(closeButton);
+		closeButton.click();
+		PageObjectLogging.log("closeTemplateComponentDialog", "Template dialog was closed", true, driver);
+		return new InteractiveMapsPageObject(driver);
+	}
+	
+	public void verifyTemplateDialog() {
+		waitForElementByElement(newTemplateDialog);
+		PageObjectLogging.log("verifyTemplateDialog", "Template dialog is visible", true);
+	}
+
+	public void verifyTemplateImagePreview(String selectedTemplateName) {
+		waitForElementByElement(templateImagePreview);
+		Assert.assertTrue(templateImagePreview.getAttribute("src").endsWith(selectedTemplateName));
+	}
+}
