04659a35a6254d095f7dc8c6e2879827d5a3c4a9
Dyktus <lukasz.nowak@wikia-inc.com>
2014-08-06 15:17:39 +0200

    Back to old code

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/InteractiveMaps/InteractiveMapPageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/InteractiveMaps/InteractiveMapPageObject.java
@@ -70,7 +70,7 @@ public class InteractiveMapPageObject extends BasePageObject{
 
 	public void verifyMapOpened() {
 		driver.switchTo().frame(mapFrame);
-		openMapAgain();
+		closeMapBeingProcessedModalIfVisible();
 		waitForElementByElement(map);
 		driver.switchTo().defaultContent();
 	}
@@ -95,12 +95,9 @@ public class InteractiveMapPageObject extends BasePageObject{
 		checkIfElementOnPage(embedMapCodeSmall);
 	}
 	
-	public void openMapAgain() {
-		if(checkIfElementOnPage(mapBeingProcessedModal)){
-			this.navigateBack(); 
-			InteractiveMapsPageObject mapBack = new InteractiveMapsPageObject(driver); 
-			mapBack.clickMapWithIndex(0);
-			driver.switchTo().frame(mapFrame);
+	public void closeMapBeingProcessedModalIfVisible(){
+		while(checkIfElementOnPage(mapBeingProcessedModal)) {
+			refreshButton.click();
 		}
 	}
 	
