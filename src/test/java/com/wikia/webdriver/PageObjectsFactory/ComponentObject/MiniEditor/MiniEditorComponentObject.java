aafd3edcf906c27dad6d3d5a5017c800d3439bf6
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2014-08-28 16:01:58 +0200

    CR changes

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/ComponentObject/MiniEditor/MiniEditorComponentObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/ComponentObject/MiniEditor/MiniEditorComponentObject.java
@@ -58,7 +58,11 @@ public class MiniEditorComponentObject extends WikiBasePageObject{
 		waitForElementByElement(messageBodyField);
 		// This was intensively investigated and sleep is the only way to make the tests more reliable.
 		// The reason is the Minieditor defectiveness
-		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		messageBodyField.clear();
 		messageBodyField.sendKeys(text);
 	}
