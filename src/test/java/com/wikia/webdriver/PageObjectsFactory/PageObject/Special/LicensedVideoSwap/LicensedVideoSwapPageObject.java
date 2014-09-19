2590834de141459266baab7cebf979f1ab0185e5
Garth Webb <garth@wikia-inc.com>
2014-09-19 15:11:15 -0700

    VID-1993 Fixed broken Selenium tests
    * Need to wait for elements to appear before testing them
    * Was not swapping video before trying to undo a swap

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/LicensedVideoSwap/LicensedVideoSwapPageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Special/LicensedVideoSwap/LicensedVideoSwapPageObject.java
@@ -12,19 +12,34 @@ import com.wikia.webdriver.Common.ContentPatterns.URLsContent;
 import com.wikia.webdriver.Common.Core.Assertion;
 import com.wikia.webdriver.Common.Logging.PageObjectLogging;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.Special.SpecialPageObject;
-import com.wikia.webdriver.PageObjectsFactory.PageObject.Special.LicensedVideoSwap.LicensedVideoSwapHistoryPageObject;
 
 public class LicensedVideoSwapPageObject extends SpecialPageObject {
 	@FindBy(css = ".lvs-history-btn")
 	private WebElement lvsHistoryBtn;
+
+	@FindBy(xpath = "//a[@title='Special:LicensedVideoSwap']" )
+	private WebElement backLink;
+
+	@FindBy(css = ".swap-button")
+	private WebElement firstSwapButton;
+
+	@FindBy(css = ".count")
+	private WebElement swapCount;
 
 	public LicensedVideoSwapPageObject(WebDriver driver) {
 		super(driver);
 	}
 
 	public LicensedVideoSwapHistoryPageObject navigateToHistoryPage() {
+		// Make sure the button has been drawn
+		this.waitForElementByElement(lvsHistoryBtn);
 		lvsHistoryBtn.click();
+
+		// Make sure the click above has happened and the browser has responded by looking for an element
+		// on the history page
+		this.waitForElementByElement(backLink);
 		PageObjectLogging.log("navigateToHistoryPage", "lvs history button navigates to right page", true);
+
 		return new LicensedVideoSwapHistoryPageObject(driver);
 	}
 
@@ -32,6 +47,20 @@ public class LicensedVideoSwapPageObject extends SpecialPageObject {
 		String url = driver.getCurrentUrl();
 		Assertion.assertTrue(url.contains(URLsContent.specialLicensedVideoSwap));
 		PageObjectLogging.log("verifyOnLvsPage", "url is the correct one for LVS page", true);
+	}
+
+	public void verifySwapVideo() {
+		// Make sure the element is on page before trying to retrieve it
+		this.waitForElementByElement(swapCount);
+		int initialCount = Integer.parseInt(swapCount.getText());
+
+		// Swap the first video
+		this.waitForElementByElement(firstSwapButton);
+		firstSwapButton.click();
+
+		this.waitForTextToBePresentInElementByElement(swapCount,  String.valueOf(initialCount-1));
+
+		PageObjectLogging.log("verifyClickSwap", "Swap button has been clicked", true);
 	}
 }
 
