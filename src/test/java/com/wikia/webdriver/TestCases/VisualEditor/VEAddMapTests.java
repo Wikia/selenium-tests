9b3bfb4cdb3985d12839320f1a92ae77aad71507
Robert Chan <rchan@wikia-inc.com>
2014-09-23 15:44:28 -0700

    VE-1351 Adding the test to verify adding a map from empty state would update the map dialog

--- src/test/java/com/wikia/webdriver/TestCases/VisualEditor/VEAddMapTests.java
+++ src/test/java/com/wikia/webdriver/TestCases/VisualEditor/VEAddMapTests.java
@@ -3,15 +3,20 @@ package com.wikia.webdriver.TestCases.VisualEditor;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.wikia.webdriver.Common.ContentPatterns.InteractiveMapsContent;
 import com.wikia.webdriver.Common.ContentPatterns.PageContent;
 import com.wikia.webdriver.Common.ContentPatterns.URLsContent;
 import com.wikia.webdriver.Common.DataProvider.VisualEditorDataProvider.InsertDialog;
 import com.wikia.webdriver.Common.Properties.Credentials;
 import com.wikia.webdriver.Common.Templates.NewTestTemplateBeforeClass;
+import com.wikia.webdriver.PageObjectsFactory.ComponentObject.InteractiveMaps.CreateAMapComponentObject;
+import com.wikia.webdriver.PageObjectsFactory.ComponentObject.InteractiveMaps.CreatePinTypesComponentObject;
+import com.wikia.webdriver.PageObjectsFactory.ComponentObject.InteractiveMaps.CreateRealMapComponentObject;
 import com.wikia.webdriver.PageObjectsFactory.ComponentObject.VisualEditorDialogs.VisualEditorAddMapDialog;
 import com.wikia.webdriver.PageObjectsFactory.ComponentObject.VisualEditorDialogs.VisualEditorSaveChangesDialog;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.WikiBasePageObject;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.Article.ArticlePageObject;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.Special.InteractiveMaps.InteractiveMapPageObject;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.VisualEditor.VisualEditorPageObject;
 
 /**
@@ -20,6 +25,7 @@ import com.wikia.webdriver.PageObjectsFactory.PageObject.VisualEditor.VisualEdit
  *
  * VE-1337 - Adding existing map onto article
  * VE-1337 - Checking empty state dialog on wiki with no maps
+ * VE-1351 - Adding map in a empty state
  *
  */
 
@@ -62,5 +68,30 @@ public class VEAddMapTests extends NewTestTemplateBeforeClass {
 		VisualEditorAddMapDialog mapDialog =
 			(VisualEditorAddMapDialog) ve.openDialogFromMenu(InsertDialog.MAP);
 		mapDialog.checkIsEmptyState();
+	}
+
+	@Test(
+		groups = {"VEAddMap", "VEAddMapTests_003", "VEEmptyMap"},
+		dependsOnGroups = {"VEAddMapTests_002"}
+	)
+	public void VEAddMapTests_003_InsertMapFromZeroState() {
+		wikiURL = urlBuilder.getUrlForWiki(URLsContent.veDisabledTestMainPage);
+		int expectedMapNum = 1;
+
+		VisualEditorPageObject ve = base.launchVisualEditorWithMainEdit(articleName, wikiURL);
+		VisualEditorAddMapDialog mapDialog =
+			(VisualEditorAddMapDialog) ve.openDialogFromMenu(InsertDialog.MAP);
+		CreateAMapComponentObject map = mapDialog.clickCreateAMapButton();
+		CreateRealMapComponentObject realMap = map.clickRealMap();
+		realMap.verifyRealMapPreviewImage();
+		realMap.typeMapName(InteractiveMapsContent.mapName);
+		CreatePinTypesComponentObject pinDialog = realMap.clickNext();
+		pinDialog.typePinTypeTitle(InteractiveMapsContent.pinTypeName, InteractiveMapsContent.pinTypeIndex);
+		InteractiveMapPageObject createdMap = pinDialog.clickSave();
+		createdMap.verifyMapOpened();
+		createdMap.verifyControButtonsAreVisible();
+		mapDialog = createdMap.switchBackToVETab();
+		//Defect VE-1557 - the next line would fail
+		mapDialog.verifyNumOfMaps(expectedMapNum);
 	}
 }
