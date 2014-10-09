98dbf4f8934d8775614a18ae3367a81bdcc83116
Robert Chan <rchan@wikia-inc.com>
2014-10-08 17:43:17 -0700

    VE-1415 Added logic to handle inline diff

--- src/test/java/com/wikia/webdriver/Common/ContentPatterns/VEContent.java
+++ src/test/java/com/wikia/webdriver/Common/ContentPatterns/VEContent.java
@@ -7,4 +7,17 @@ public class VEContent {
 	public static String templateSearchStr3 = "per";
 	public static String templateSearchStr4 = "ar";
 	public static String templateSearchStr5 = "book";
+
+	public static String templateParamLabel1 = "author";
+	public static String templateParamLabel2 = "next";
+
+	public static String templateParamValue1 = "contribution";
+	public static String templateParamValue2 = "others";
+
+	public static String templateAssignment = " = ";
+	public static String paramSeparator = "|";
+
+	public static String templateWikiText =
+		paramSeparator + templateParamLabel1 + templateAssignment + templateParamValue1 +
+		paramSeparator + templateParamLabel2 + templateAssignment + templateParamValue2;
 }
