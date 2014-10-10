ff789b66590a39f61a975ec1a270ee69bed9a4a7
Robert Chan <rchan@wikia-inc.com>
2014-10-10 11:42:18 -0700

    VE-1413 Addressing CRs

--- src/test/java/com/wikia/webdriver/Common/ContentPatterns/VEContent.java
+++ src/test/java/com/wikia/webdriver/Common/ContentPatterns/VEContent.java
@@ -2,9 +2,9 @@ package com.wikia.webdriver.Common.ContentPatterns;
 
 public class VEContent {
 
-	public static String templateSearchStr1 = "a";
-	public static String templateSearchStr2 = "ab";
-	public static String templateSearchStr3 = "per";
+	public static String templateSearch1CharNoMatch = "a";
+	public static String templateSearch2CharsNoMatch = "ab";
+	public static String templateSearch3CharsPartialMatch = "per";
 	public static String templateSearchMatchArticle = "ar";
 	public static String templateSearchExactMatch = "book";
 
