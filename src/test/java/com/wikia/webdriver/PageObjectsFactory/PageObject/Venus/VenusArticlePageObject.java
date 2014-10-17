df67f586e14c1e08455bea673c535b864421fbc9
Bogna Knychala <bogna.ka@gmail.com>
2014-10-17 17:14:16 +0200

    CON-1813 add tests for scrollable tables on venus

---
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Venus/VenusArticlePageObject.java
@@ -0,0 +1,41 @@
+package com.wikia.webdriver.PageObjectsFactory.PageObject.Venus;
+
+import com.wikia.webdriver.Common.Logging.PageObjectLogging;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.WikiBasePageObject;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class VenusArticlePageObject extends WikiBasePageObject {
+
+	@FindBy(css = ".table-scrollable-wrapper")
+	private WebElement scrollableTable;
+
+	public VenusArticlePageObject(WebDriver driver, String page) {
+		super(driver);
+		getUrl(page);
+	}
+
+	public void verifyScrollableTablePresent() {
+		if (checkIfElementOnPage(scrollableTable)) {
+			PageObjectLogging.log("ScrollableTablePresent", "Scrollable table is present on page", true);
+		} else {
+			throw new NoSuchElementException("Scrollable table not found on page");
+		}
+	}
+
+	public void verifyScrollableTableNotPresent() {
+		if (!checkIfElementOnPage(scrollableTable)) {
+			PageObjectLogging.log("ScrollableTableNotPresent", "Scrollable table isn't present on page", true);
+		} else {
+			throw new WebDriverException("Scrollable table found on page");
+		}
+	}
+
+	public void resizeWindow(Dimension resolution) {
+		driver.manage().window().setSize(resolution);
+	}
+}
---
+++ src/test/java/com/wikia/webdriver/TestCases/VenusTests/TestTablesOnVenus.java
@@ -0,0 +1,46 @@
+package com.wikia.webdriver.TestCases.VenusTests;
+
+import com.wikia.webdriver.Common.DataProvider.Venus.VenusArticleDataProvider;
+import com.wikia.webdriver.Common.Templates.NewTestTemplate;
+import com.wikia.webdriver.PageObjectsFactory.PageObject.Venus.VenusArticlePageObject;
+import org.openqa.selenium.Dimension;
+import org.testng.annotations.Test;
+
+public class TestTablesOnVenus extends NewTestTemplate {
+
+	@Test(
+			dataProvider = "getArticleWithScrollableTable",
+			dataProviderClass = VenusArticleDataProvider.class,
+			groups = {"TestTablesOnVenus_001", "TestTablesOnVenus"}
+	)
+	public void TestTablesOnVenus_001_scrollableTablePresent(String wikiName, String article) {
+		String url = urlBuilder.getUrlForPath(wikiName, article);
+		VenusArticlePageObject venusArticle = new VenusArticlePageObject(driver, url);
+		venusArticle.verifyScrollableTablePresent();
+	}
+
+	@Test(
+			dataProvider = "getArticleWithScrollableTableOnSmallResoultion",
+			dataProviderClass = VenusArticleDataProvider.class,
+			groups = {"TestTablesOnVenus_002", "TestTablesOnVenus"}
+	)
+	public void TestTablesOnVenus_002_scrollableTablePresentOnSmallResolution(String wikiName, String article, Dimension bigResolution, Dimension smallResolution) {
+		String url = urlBuilder.getUrlForPath(wikiName, article);
+		VenusArticlePageObject venusArticle = new VenusArticlePageObject(driver, url);
+		venusArticle.resizeWindow(bigResolution);
+		venusArticle.verifyScrollableTableNotPresent();
+		venusArticle.resizeWindow(smallResolution);
+		venusArticle.verifyScrollableTablePresent();
+	}
+
+	@Test(
+			dataProvider = "getArticleWithNotScrollableTable",
+			dataProviderClass = VenusArticleDataProvider.class,
+			groups = {"TestTablesOnVenus_003", "TestTablesOnVenus"}
+	)
+	public void TestTablesOnVenus_002_scrollableTableNotPresent(String wikiName, String article) {
+		String url = urlBuilder.getUrlForPath(wikiName, article);
+		VenusArticlePageObject venusArticle = new VenusArticlePageObject(driver, url);
+		venusArticle.verifyScrollableTableNotPresent();
+	}
+}
