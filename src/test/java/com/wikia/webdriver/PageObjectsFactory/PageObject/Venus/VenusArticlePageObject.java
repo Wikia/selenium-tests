778b704a39ce7386127ab79b05ffe094790d75ce
Bogna Knychala <bogna.ka@gmail.com>
2014-10-21 16:37:07 +0200

    CON-1813 make tests more awesome

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Venus/VenusArticlePageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Venus/VenusArticlePageObject.java
@@ -1,11 +1,7 @@
 package com.wikia.webdriver.PageObjectsFactory.PageObject.Venus;
 
-import com.wikia.webdriver.Common.Logging.PageObjectLogging;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.WikiBasePageObject;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -18,19 +14,7 @@ public class VenusArticlePageObject extends WikiBasePageObject {
 		super(driver);
 	}
 
-	public void verifyScrollableTablePresent() {
-		if (checkIfElementOnPage(scrollableTable)) {
-			PageObjectLogging.log("ScrollableTablePresent", "Scrollable table is present on page", true);
-		} else {
-			throw new NoSuchElementException("Scrollable table not found on page");
-		}
-	}
-
-	public void verifyScrollableTableNotPresent() {
-		if (!checkIfElementOnPage(scrollableTable)) {
-			PageObjectLogging.log("ScrollableTableNotPresent", "Scrollable table isn't present on page", true);
-		} else {
-			throw new WebDriverException("Scrollable table found on page");
-		}
+	public Boolean isScrollableTablePresent() {
+		return checkIfElementOnPage(scrollableTable);
 	}
 }
--- src/test/java/com/wikia/webdriver/TestCases/VenusTests/TestTablesOnVenus.java
+++ src/test/java/com/wikia/webdriver/TestCases/VenusTests/TestTablesOnVenus.java
@@ -1,5 +1,6 @@
 package com.wikia.webdriver.TestCases.VenusTests;
 
+import com.wikia.webdriver.Common.Core.Assertion;
 import com.wikia.webdriver.Common.Templates.NewTestTemplate;
 import com.wikia.webdriver.PageObjectsFactory.PageObject.Venus.VenusArticlePageObject;
 import org.openqa.selenium.Dimension;
@@ -7,44 +8,39 @@ import org.testng.annotations.Test;
 
 /**
  * Bogna 'bognix' Knychala
+ *
  * @ownership Consumer
  */
 public class TestTablesOnVenus extends NewTestTemplate {
 
-	private final static String wikiName = "venustest";
+	private final static String wikiName = "mediawiki119";
 	private final static Dimension bigResolution = new Dimension(1920, 900);
 	private final static Dimension smallResolution = new Dimension(768, 1024);
 
-	@Test(
-			groups = {"TestTablesOnVenus_001", "TestTablesOnVenus"}
-	)
+	@Test(groups = {"TestTablesOnVenus_001", "TestTablesOnVenus"})
 	public void TestTablesOnVenus_001_scrollableTablePresent() {
 		String url = urlBuilder.getUrlForPath(wikiName, "Tables/ScrollableTable");
 		VenusArticlePageObject venusArticle = new VenusArticlePageObject(driver);
 		venusArticle.getUrl(url);
-		venusArticle.verifyScrollableTablePresent();
+		Assertion.assertTrue(venusArticle.isScrollableTablePresent(), "Scrollable table should be present");
 	}
 
-	@Test(
-			groups = {"TestTablesOnVenus_002", "TestTablesOnVenus"}
-	)
+	@Test(groups = {"TestTablesOnVenus_002", "TestTablesOnVenus"})
 	public void TestTablesOnVenus_002_scrollableTablePresentOnSmallResolution() {
 		String url = urlBuilder.getUrlForPath(wikiName, "Tables/ScrollableTableOnSmallRes");
 		VenusArticlePageObject venusArticle = new VenusArticlePageObject(driver);
 		venusArticle.getUrl(url);
 		venusArticle.resizeWindow(bigResolution);
-		venusArticle.verifyScrollableTableNotPresent();
+		Assertion.assertFalse(venusArticle.isScrollableTablePresent(), "Scrollable table shouldn't be present");
 		venusArticle.resizeWindow(smallResolution);
-		venusArticle.verifyScrollableTablePresent();
+		Assertion.assertTrue(venusArticle.isScrollableTablePresent(), "Scrollable table should be present");
 	}
 
-	@Test(
-			groups = {"TestTablesOnVenus_003", "TestTablesOnVenus"}
-	)
+	@Test(groups = {"TestTablesOnVenus_003", "TestTablesOnVenus"})
 	public void TestTablesOnVenus_002_scrollableTableNotPresent() {
 		String url = urlBuilder.getUrlForPath(wikiName, "Tables/NotScrollableTable");
 		VenusArticlePageObject venusArticle = new VenusArticlePageObject(driver);
 		venusArticle.getUrl(url);
-		venusArticle.verifyScrollableTableNotPresent();
+		Assertion.assertFalse(venusArticle.isScrollableTablePresent(), "Scrollable table shouldn't be present");
 	}
 }
