5aa33d2ab0d42fbe4c16f6bdf77eb8ac6c770a93
V <bartosz.bentkowski@gmail.com>
2014-10-24 17:42:34 +0200

    CON-1657 | fix dup

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/GlobalNav/VenusGlobalNavPageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/GlobalNav/VenusGlobalNavPageObject.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
