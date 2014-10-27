d9beca9ad7bc8b0d040b3d1ffbf346ad6da8d9b0
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2014-10-27 17:04:53 +0100

    Deleted EventContent

--- src/test/java/com/wikia/webdriver/Common/ContentPatterns/EventsContent.java
+++
@@ -1,9 +0,0 @@
-package com.wikia.webdriver.Common.ContentPatterns;
-
-
-public class EventsContent {
-
-	public static String editPageEvent = "editor-ck*view*edit-page";
-	public static String openGalleryEvent = "editor-ck*open*gallery-tool";
-
-}
