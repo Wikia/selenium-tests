90d7832455af4edf9ef3370bf3793974c88f93b7
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2014-10-30 15:33:21 +0100

    Typo fix

--- src/test/java/com/wikia/webdriver/Common/DriverProvider/UseUnstablePageLoadStrategy.java
+++ src/test/java/com/wikia/webdriver/Common/DriverProvider/UseUnstablePageLoadStrategy.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 /**
  * Created by Ludwik Kaźmierczak on 2014-10-30.
  *
- * Annotate test method if you want to use unstable page load strategy for firefox. This causes firefox not ot wait
+ * Annotate test method if you want to use unstable page load strategy for firefox. This causes firefox not to wait
  * for full page load before executing WebDriver commands.
  */
 @Retention(value = RetentionPolicy.RUNTIME)
