faf8eb532be0da8e14760841db716e7943943c77
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2014-11-05 16:56:28 +0100

    Venus tables tests fixes

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Venus/VenusArticlePageObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/PageObject/Venus/VenusArticlePageObject.java
@@ -10,11 +10,18 @@ public class VenusArticlePageObject extends WikiBasePageObject {
 	@FindBy(css = ".table-scrollable-wrapper")
 	private WebElement scrollableTable;
 
+	@FindBy(css = ".article-table")
+	private WebElement articleTable;
+
 	public VenusArticlePageObject(WebDriver driver) {
 		super(driver);
 	}
 
 	public Boolean isScrollableTablePresent() {
 		return checkIfElementOnPage(scrollableTable);
+	}
+
+	public Boolean isTablePresent() {
+		return checkIfElementOnPage(articleTable);
 	}
 }
