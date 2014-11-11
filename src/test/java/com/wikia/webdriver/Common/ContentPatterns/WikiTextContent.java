ddc65980e73d35b8759a58de4864fb2661f4b131
Robert Chan <rchan@wikia-inc.com>
2014-11-10 16:37:17 -0800

    QAART-447 Fixed public static fields should always be constant for WikiTextContent

--- src/test/java/com/wikia/webdriver/Common/ContentPatterns/WikiTextContent.java
+++ src/test/java/com/wikia/webdriver/Common/ContentPatterns/WikiTextContent.java
@@ -2,57 +2,57 @@ package com.wikia.webdriver.Common.ContentPatterns;
 
 public class WikiTextContent {
 
-	public static String headingPrefix = "== ";
-	public static String headingPostfix = " ==";
-	public static String subHeading1Prefix = "=== ";
-	public static String subHeading1Postix = " ===";
-	public static String subHeading2Prefix = "==== ";
-	public static String subHeading2Postix = " ====";
-	public static String subHeading3Prefix = "===== ";
-	public static String subHeading3Postix = " =====";
-	public static String subHeading4Prefix = "====== ";
-	public static String subHeading4Postix = " ======";
+	public static final String HEADING_PREFIX = "== ";
+	public static final String headingPostfix = " ==";
+	public static final String SUBHEADING1_PREFIX = "=== ";
+	public static final String SUBHEADING1_POSTIX = " ===";
+	public static final String SUBHEADING2_PREFIX = "==== ";
+	public static final String SUBHEADING2_POSTIX = " ====";
+	public static final String SUBHEADING3_PREFIX = "===== ";
+	public static final String SUBHEADING3_POSTIX = " =====";
+	public static final String SUBHEADING4_PREFIX = "====== ";
+	public static final String SUBHEADING4_POSTIX = " ======";
 
-	public static String preformatted = "";
-	public static String bold = "'''";
-	public static String italic = "''";
+	public static final String PREFORMATTED = "";
+	public static final String BOLD = "'''";
+	public static final String ITALIC = "''";
 
-	public static String strikethroughPrefix = "<s>";
-	public static String strikethroughPostfix = "</s>";
-	public static String underlinePrefix = "<u>";
-	public static String underlinePostfix = "</u>";
-	public static String subscriptPrefix = "<sub>";
-	public static String subscriptPostfix = "</sub>";
-	public static String superscriptPrefix = "<sup>";
-	public static String superscriptPostfix = "</sup>";
+	public static final String STRIKETHROUGH_PREFIX = "<s>";
+	public static final String STRIKETHROUGH_POSTFIX = "</s>";
+	public static final String UNDERLINE_PREFIX = "<u>";
+	public static final String UNDERLINE_POSTFIX = "</u>";
+	public static final String SUBSCRIPT_PREFIX = "<sub>";
+	public static final String SUBSCRIPT_POSTFIX = "</sub>";
+	public static final String SUPERSCRIPT_PREFIX = "<sup>";
+	public static final String SUPERSCRIPT_POSTFIX = "</sup>";
 
-	public static String bulletList = "* ";
-	public static String nummberedList = "# ";
+	public static final String BULLET_LIST = "* ";
+	public static final String NUMMBERED_LIST = "# ";
 
-	public static String linkPrefix = "[[";
-	public static String linkPostfix = "]]";
+	public static final String LINK_PREFIX = "[[";
+	public static final String LINK_POSTFIX = "]]";
 
-	public static String text = PageContent.articleText;
+	public static final String TEXT = PageContent.ARTICLE_TEXT;
 
-	public static String paragraphText = text;
-	public static String headingText = headingPrefix + text + headingPostfix;
-	public static String subHeading1Text = subHeading1Prefix + text + subHeading1Postix;
-	public static String subHeading2Text = subHeading2Prefix + text + subHeading2Postix;
-	public static String subHeading3Text = subHeading3Prefix + text + subHeading3Postix;
-	public static String subHeading4Text = subHeading4Prefix + text + subHeading4Postix;
-	public static String preformattedText = preformatted + text;
-	public static String boldText = bold + text + bold;
-	public static String italicText = italic + text + italic;
-	public static String striketroughText = strikethroughPrefix + text + strikethroughPostfix;
-	public static String underlineText = underlinePrefix + text + underlinePostfix;
-	public static String subscriptText = subscriptPrefix + text + subscriptPostfix;
-	public static String superscriptText = superscriptPrefix + text + superscriptPostfix;
-	public static String bulletListText = bulletList + text;
-	public static String numberedListText = nummberedList + text;
+	public static final String PARAGRAPH_TEXT = TEXT;
+	public static final String HEADING_TEXT = HEADING_PREFIX + TEXT + headingPostfix;
+	public static final String SUBHEADING1_TEXT = SUBHEADING1_PREFIX + TEXT + SUBHEADING1_POSTIX;
+	public static final String SUBHEADING2_TEXT = SUBHEADING2_PREFIX + TEXT + SUBHEADING2_POSTIX;
+	public static final String SUBHEADING3_TEXT = SUBHEADING3_PREFIX + TEXT + SUBHEADING3_POSTIX;
+	public static final String SUBHEADING4_TEXT = SUBHEADING4_PREFIX + TEXT + SUBHEADING4_POSTIX;
+	public static final String PREFORMATTED_TEXT = PREFORMATTED + TEXT;
+	public static final String BOLD_TEXT = BOLD + TEXT + BOLD;
+	public static final String ITALIC_TEXT = ITALIC + TEXT + ITALIC;
+	public static final String STRIKETROUGH_TEXT = STRIKETHROUGH_PREFIX + TEXT + STRIKETHROUGH_POSTFIX;
+	public static final String UNDERLINE_TEXT = UNDERLINE_PREFIX + TEXT + UNDERLINE_POSTFIX;
+	public static final String SUBSCRIPT_TEXT = SUBSCRIPT_PREFIX + TEXT + SUBSCRIPT_POSTFIX;
+	public static final String SUPERSCRIPT_TEXT = SUPERSCRIPT_PREFIX + TEXT + SUPERSCRIPT_POSTFIX;
+	public static final String BULLET_LIST_TEXT = BULLET_LIST + TEXT;
+	public static final String NUMBERED_LIST_TEXT = NUMMBERED_LIST + TEXT;
 
-	public static String blueLinkText = linkPrefix + PageContent.internalLink + linkPostfix;
-	public static String externalLinkText = linkPrefix + PageContent.externalLink + linkPostfix;
-	public static String redLinkText = linkPrefix + PageContent.redLink + linkPostfix;
-	public static String redirectLinkText = linkPrefix + PageContent.redirectLink + linkPostfix;
+	public static final String BLUELINK_TEXT = LINK_PREFIX + PageContent.INTERNAL_LINK + LINK_POSTFIX;
+	public static final String EXTERNAL_LINK_TEXT = LINK_PREFIX + PageContent.EXTERNAL_LINK + LINK_POSTFIX;
+	public static final String REDLINK_TEXT = LINK_PREFIX + PageContent.REDLINK + LINK_POSTFIX;
+	public static final String REDIRECT_LINK_TEXT = LINK_PREFIX + PageContent.REDIRECT_LINK + LINK_POSTFIX;
 
 }
