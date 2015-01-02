4baee8b066ef65365d43938ddf18728257ff26a6
ludwikkazmierczak <ludwik.kazmierczak@gmail.com>
2015-01-02 15:50:14 +0100

    Increased timeout for dropdown to show

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/dropdowncomponentobject/DropDownComponentObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/dropdowncomponentobject/DropDownComponentObject.java
@@ -55,7 +55,7 @@ public class DropDownComponentObject extends WikiBasePageObject {
 	public DropDownComponentObject openDropDown() {
 		driver.manage().timeouts().implicitlyWait(250, TimeUnit.MILLISECONDS);
 		try {
-			new WebDriverWait(driver, 15, 3000).until(new ExpectedCondition<Boolean>() {
+			new WebDriverWait(driver, 30, 3000).until(new ExpectedCondition<Boolean>() {
 				@Override
 				public Boolean apply(WebDriver webDriver) {
 					if (!loginDropdown.isDisplayed()) {
