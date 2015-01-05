903de26508a8d468a350b68cdcf996a569877bc3
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-01-05 18:20:33 +0100

    minor tweaks

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/dropdowncomponentobject/DropDownComponentObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/dropdowncomponentobject/DropDownComponentObject.java
@@ -1,7 +1,6 @@
 package com.wikia.webdriver.pageobjectsfactory.componentobject.dropdowncomponentobject;
 
 import com.wikia.webdriver.pageobjectsfactory.pageobject.signup.SignUpPageObject;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
