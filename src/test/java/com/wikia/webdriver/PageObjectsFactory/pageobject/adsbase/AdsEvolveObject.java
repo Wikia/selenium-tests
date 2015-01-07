3ff0535c4dc8f645cbc7b13dea6b7ac689405d83
drets <dmytro.rets@wikia-inc.com>
2015-01-07 11:27:02 +0100

    ADEN-1618 Fix CR comment.

--- src/test/java/com/wikia/webdriver/PageObjectsFactory/pageobject/adsbase/AdsEvolveObject.java
+++ src/test/java/com/wikia/webdriver/PageObjectsFactory/pageobject/adsbase/AdsEvolveObject.java
@@ -1,6 +1,7 @@
 package com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase;
 
 import com.wikia.webdriver.common.contentpatterns.AdsContent;
+import com.wikia.webdriver.common.core.Assertion;
 import org.openqa.selenium.*;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
  * @ownership AdEngineering
  */
 public class AdsEvolveObject extends AdsBaseObject {
+	private static final String EVOLVE_ERROR_MESSAGE = "Verify Evolve call in ";
 	private static final String EVOLVE_SELECTOR = " script[src*=\"4403ad\"]";
 
 	public AdsEvolveObject(WebDriver driver, String page) {
@@ -23,5 +25,20 @@ public class AdsEvolveObject extends AdsBaseObject {
 			slotSelector, EVOLVE_SELECTOR
 		);
 		return !evolveScripts.isEmpty();
+	}
+
+	public void verifyEvolveCall() {
+		Assertion.assertTrue(evolveCalledInSlot(AdsContent.TOP_LB),
+			EVOLVE_ERROR_MESSAGE + AdsContent.TOP_LB);
+		Assertion.assertTrue(evolveCalledInSlot(AdsContent.MEDREC),
+			EVOLVE_ERROR_MESSAGE + AdsContent.MEDREC);
+		Assertion.assertTrue(evolveCalledInSlot(AdsContent.LEFT_SKYSCRAPPER_2),
+			EVOLVE_ERROR_MESSAGE + AdsContent.LEFT_SKYSCRAPPER_2);
+		Assertion.assertFalse(evolveCalledInSlot(AdsContent.FLOATING_MEDREC),
+			EVOLVE_ERROR_MESSAGE + AdsContent.FLOATING_MEDREC);
+		Assertion.assertFalse(evolveCalledInSlot(AdsContent.PREFOOTER_LEFT),
+			EVOLVE_ERROR_MESSAGE + AdsContent.PREFOOTER_LEFT);
+		Assertion.assertFalse(evolveCalledInSlot(AdsContent.PREFOOTER_RIGHT),
+			EVOLVE_ERROR_MESSAGE + AdsContent.PREFOOTER_RIGHT);
 	}
 }
--- src/test/java/com/wikia/webdriver/TestCases/adstests/TestEvolveAds.java
+++ src/test/java/com/wikia/webdriver/TestCases/adstests/TestEvolveAds.java
@@ -1,7 +1,5 @@
 package com.wikia.webdriver.testcases.adstests;
 
-import com.wikia.webdriver.common.contentpatterns.AdsContent;
-import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.geoedge.GeoEdgeProxy;
 import com.wikia.webdriver.common.dataprovider.ads.AdsDataProvider;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
@@ -14,8 +12,6 @@ import org.testng.annotations.Test;
  */
 public class TestEvolveAds extends NewTestTemplate {
 
-	private static final String EVOLVE_ERROR_MESSAGE = "Verify Evolve call in ";
-
 	@GeoEdgeProxy(country = "CA")
 	@Test(
 		dataProviderClass = AdsDataProvider.class,
@@ -23,7 +19,9 @@ public class TestEvolveAds extends NewTestTemplate {
 		dataProvider = "evolveTestPage"
 	)
 	public void testEvolveCall_CA(String wikiName, String article) {
-		testEvolveCall(wikiName, article);
+		String testedPage = urlBuilder.getUrlForPath(wikiName, article);
+		AdsEvolveObject wikiPage = new AdsEvolveObject(driver, testedPage);
+		wikiPage.verifyEvolveCall();
 	}
 
 	@GeoEdgeProxy(country = "AU")
@@ -33,7 +31,9 @@ public class TestEvolveAds extends NewTestTemplate {
 		dataProvider = "evolveTestPage"
 	)
 	public void testEvolveCall_AU(String wikiName, String article) {
-		testEvolveCall(wikiName, article);
+		String testedPage = urlBuilder.getUrlForPath(wikiName, article);
+		AdsEvolveObject wikiPage = new AdsEvolveObject(driver, testedPage);
+		wikiPage.verifyEvolveCall();
 	}
 
 	@GeoEdgeProxy(country = "NZ")
@@ -43,23 +43,8 @@ public class TestEvolveAds extends NewTestTemplate {
 		dataProvider = "evolveTestPage"
 	)
 	public void testEvolveCall_NZ(String wikiName, String article) {
-		testEvolveCall(wikiName, article);
-	}
-
-	private void testEvolveCall(String wikiName, String article) {
 		String testedPage = urlBuilder.getUrlForPath(wikiName, article);
 		AdsEvolveObject wikiPage = new AdsEvolveObject(driver, testedPage);
-		Assertion.assertTrue(wikiPage.evolveCalledInSlot(AdsContent.TOP_LB),
-			EVOLVE_ERROR_MESSAGE + AdsContent.TOP_LB);
-		Assertion.assertTrue(wikiPage.evolveCalledInSlot(AdsContent.MEDREC),
-			EVOLVE_ERROR_MESSAGE + AdsContent.MEDREC);
-		Assertion.assertTrue(wikiPage.evolveCalledInSlot(AdsContent.LEFT_SKYSCRAPPER_2),
-			EVOLVE_ERROR_MESSAGE + AdsContent.LEFT_SKYSCRAPPER_2);
-		Assertion.assertFalse(wikiPage.evolveCalledInSlot(AdsContent.FLOATING_MEDREC),
-			EVOLVE_ERROR_MESSAGE + AdsContent.FLOATING_MEDREC);
-		Assertion.assertFalse(wikiPage.evolveCalledInSlot(AdsContent.PREFOOTER_LEFT),
-			EVOLVE_ERROR_MESSAGE + AdsContent.PREFOOTER_LEFT);
-		Assertion.assertFalse(wikiPage.evolveCalledInSlot(AdsContent.PREFOOTER_RIGHT),
-			EVOLVE_ERROR_MESSAGE + AdsContent.PREFOOTER_RIGHT);
+		wikiPage.verifyEvolveCall();
 	}
 }
