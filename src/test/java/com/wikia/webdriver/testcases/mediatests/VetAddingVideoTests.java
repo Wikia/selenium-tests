0ddfba1cff945b684d47cbbdd6601fd5621968e3
Mateusz Rybarski <rybarski.mateusz@gmail.com>
2015-01-13 16:08:28 +0100

    Update VetAddingVideoTests.java

--- src/test/java/com/wikia/webdriver/testcases/mediatests/VetAddingVideoTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mediatests/VetAddingVideoTests.java
@@ -53,7 +53,7 @@ public class VetAddingVideoTests extends NewTestTemplate {
 		mini.switchAndWrite(message);
 		wall.writeTitle(title);
 		VetAddVideoComponentObject vetAddingVideo = mini.clickAddVideo();
-		VetOptionsComponentObject vetOptions = vetAddingVideo.addVideoByQuery(VideoContent.WIKIA_VIDEO_QUERY, 2);
+		VetOptionsComponentObject vetOptions = vetAddingVideo.addVideoByQuery(VideoContent.WIKIA_VIDEO_QUERY, 0);
 		vetOptions.setCaption(PageContent.CAPTION);
 		vetOptions.submit();
 		mini.verifyVideoMiniEditor();
