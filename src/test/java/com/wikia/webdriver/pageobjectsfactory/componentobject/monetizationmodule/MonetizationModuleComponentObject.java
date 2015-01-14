9025c5548f445ee8198a069840996e6cc5e1c4bb
saipetch <saipetch@wikia-inc.com>
2015-01-14 13:49:00 -0800

    Monetization (MON-269): MonetizationModule - updated functions in MonetizationModuleComponentObject

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/monetizationmodule/MonetizationModuleComponentObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/monetizationmodule/MonetizationModuleComponentObject.java
@@ -41,19 +41,22 @@ public class MonetizationModuleComponentObject extends WikiBasePageObject {
 	private WebElement adsenseInsInContent;
 	@FindBy(css=".adsbygoogle.ad-responsive")
 	private WebElement adsenseInsOthers;
-	@FindBy(css="#monetization-adunit-above_title")
-	private WebElement slotAboveTitleAdsense;
-	@FindBy(css="#monetization-adunit-below_title")
-	private WebElement slotBelowTitleAdsense;
-	@FindBy(css="#monetization-adunit-in_content")
-	private WebElement slotInContentAdsense;
-	@FindBy(css="#monetization-adunit-below_category")
-	private WebElement slotBelowCategoryAdsense;
-	@FindBy(css="#monetization-adunit-above_footer")
-	private WebElement slotAboveFooterAdsense;
 	@FindBy(css=".ad-header")
 	private	WebElement adHeader;
 
+	private By monetizationModuleContainer = By.cssSelector(".monetization-module");
+	private By slotAboveTitle = By.cssSelector(".monetization-module[data-mon-slot='above_title']");
+	private By slotBelowTitle = By.cssSelector(".monetization-module[data-mon-slot='below_title']");
+	private By slotInContent = By.cssSelector(".monetization-module[data-mon-slot='in_content']");
+	private By slotBelowCategory = By.cssSelector(".monetization-module[data-mon-slot='below_category']");
+	private By slotAboveFooter = By.cssSelector(".monetization-module[data-mon-slot='above_footer']");
+	private By adsenseContainer = By.cssSelector(".monetization-module[data-mon-type='adunit']");
+	private By adsenseIns = By.cssSelector(".adsbygoogle");
+	private By slotAboveTitleAdsense = By.cssSelector("#monetization-adunit-above_title");
+	private By slotBelowTitleAdsense = By.cssSelector("#monetization-adunit-below_title");
+	private By slotInContentAdsense = By.cssSelector("#monetization-adunit-in_content");
+	private By slotBelowCategoryAdsense = By.cssSelector("#monetization-adunit-below_category");
+	private By slotAboveFooterAdsense = By.cssSelector("#monetization-adunit-above_footer");
 	private By MonetizationModuleListBy = By.cssSelector(".monetization-module");
 	private By MonetizationModuleAdsenseListBy = By.cssSelector(".monetization-module[data-mon-type='adunit']");
 
@@ -70,26 +73,26 @@ public class MonetizationModuleComponentObject extends WikiBasePageObject {
 	}
 
 	public void verifyMonetizationModuleShown() {
-		waitForElementByElement(monetizationModuleContainer);
+		waitForElementByElementLocatedBy(monetizationModuleContainer);
 		Assertion.assertTrue(checkIfElementOnPage(monetizationModuleContainer));
 		PageObjectLogging.log("verifyMonetizationModuleShown", "Monetization module is visible", true);
 	}
 
 	public void verifyMonetizationModuleNotShown() {
-		Assertion.assertFalse(checkIfElementOnPage(monetizationModuleContainer));
+		waitForElementNotPresent(monetizationModuleContainer);
 		PageObjectLogging.log("verifyMonetizationModuleNotShown", "Monetization module is not shown", true);
 	}
 
 	public void verifyAdsenseUnitShown() {
-		waitForElementByElement(adsenseContainer);
+		waitForElementByElementLocatedBy(adsenseContainer);
 		Assertion.assertTrue(checkIfElementOnPage(adsenseContainer));
 		Assertion.assertTrue(checkIfElementOnPage(adsenseIns));
 		PageObjectLogging.log("verifyAdsenseUnitShown", "Adsense unit is visible", true);
 	}
 
 	public void verifyAdsenseUnitNotShown() {
-		Assertion.assertFalse(checkIfElementOnPage(adsenseContainer));
-		Assertion.assertFalse(checkIfElementOnPage(adsenseIns));
+		waitForElementNotPresent(adsenseContainer);
+		waitForElementNotPresent(adsenseIns);
 		PageObjectLogging.log("verifyAdsenseUnitNotShown", "Adsense unit is not shown", true);
 	}
 
@@ -121,62 +124,62 @@ public class MonetizationModuleComponentObject extends WikiBasePageObject {
 	}
 
 	public void verifyAdsenseUnitShownAboveTitle() {
-		waitForElementByElement(slotAboveTitleAdsense);
+		waitForElementByElementLocatedBy(slotAboveTitleAdsense);
 		Assertion.assertTrue(checkIfElementOnPage(slotAboveTitleAdsense));
 		Assertion.assertTrue(checkIfElementOnPage(adsenseInsOthers));
 		PageObjectLogging.log("verifyAdsenseUnitShownAboveTitle", "Adsense unit is visible above the title", true);
 	}
 
 	public void verifyAdsenseUnitNotShownAboveTitle() {
-		Assertion.assertFalse(checkIfElementOnPage(slotAboveTitleAdsense));
+		waitForElementNotPresent(slotAboveTitleAdsense);
 		PageObjectLogging.log("verifyAdsenseUnitNotShownAboveTitle", "Adsense unit is not shown above the title", true);
 	}
 
 	public void verifyAdsenseUnitShownBelowTitle() {
-		waitForElementByElement(slotBelowTitleAdsense);
+		waitForElementByElementLocatedBy(slotBelowTitleAdsense);
 		Assertion.assertTrue(checkIfElementOnPage(slotBelowTitleAdsense));
 		Assertion.assertTrue(checkIfElementOnPage(adsenseInsOthers));
 		PageObjectLogging.log("verifyAdsenseUnitShownBelowTitle", "Adsense unit is visible below the title", true);
 	}
 
 	public void verifyAdsenseUnitNotShownBelowTitle() {
-		Assertion.assertFalse(checkIfElementOnPage(slotBelowTitleAdsense));
+		waitForElementNotPresent(slotBelowTitleAdsense);
 		PageObjectLogging.log("verifyAdsenseUnitNotShownBelowTitle", "Adsense unit is not shown below the title", true);
 	}
 
 	public void verifyAdsenseUnitShownInContent() {
-		waitForElementByElement(slotInContentAdsense);
+		waitForElementByElementLocatedBy(slotInContentAdsense);
 		Assertion.assertTrue(checkIfElementOnPage(slotInContentAdsense));
 		Assertion.assertTrue(checkIfElementOnPage(adsenseInsInContent));
 		PageObjectLogging.log("verifyAdsenseUnitShownInContent", "Adsense unit is visible in content", true);
 	}
 
 	public void verifyAdsenseUnitNotShownInContent() {
-		Assertion.assertFalse(checkIfElementOnPage(slotInContentAdsense));
+		waitForElementNotPresent(slotInContentAdsense);
 		PageObjectLogging.log("verifyAdsenseUnitNotShownInContent", "Adsense unit is not shown in content", true);
 	}
 
 	public void verifyAdsenseUnitShownBelowCategory() {
-		waitForElementByElement(slotBelowCategoryAdsense);
+		waitForElementByElementLocatedBy(slotBelowCategoryAdsense);
 		Assertion.assertTrue(checkIfElementOnPage(slotBelowCategoryAdsense));
 		Assertion.assertTrue(checkIfElementOnPage(adsenseInsOthers));
 		PageObjectLogging.log("verifyAdsenseUnitShownBelowCategory", "Adsense unit is visible below category", true);
 	}
 
 	public void verifyAdsenseUnitNotShownBelowCategory() {
-		Assertion.assertFalse(checkIfElementOnPage(slotBelowCategoryAdsense));
+		waitForElementNotPresent(slotBelowCategoryAdsense);
 		PageObjectLogging.log("verifyAdsenseUnitNotShownBelowCategory", "Adsense unit is not shown below category", true);
 	}
 
 	public void verifyAdsenseUnitShownAboveFooter() {
-		waitForElementByElement(slotAboveFooterAdsense);
+		waitForElementByElementLocatedBy(slotAboveFooterAdsense);
 		Assertion.assertTrue(checkIfElementOnPage(slotAboveFooterAdsense));
 		Assertion.assertTrue(checkIfElementOnPage(adsenseInsOthers));
 		PageObjectLogging.log("verifyAdsenseUnitShownAboveFooter", "Adsense unit is visible above footer", true);
 	}
 
 	public void verifyAdsenseUnitNotShownAboveFooter() {
-		Assertion.assertFalse(checkIfElementOnPage(slotAboveFooterAdsense));
+		waitForElementNotPresent(slotAboveFooterAdsense);
 		PageObjectLogging.log("verifyAdsenseUnitNotShownAboveFooter", "Adsense unit is not shown above footer", true);
 	}
 
