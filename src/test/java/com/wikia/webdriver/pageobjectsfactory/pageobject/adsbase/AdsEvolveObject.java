a6366aaa38e61cbe75bf9ccdc2e1355312304d23
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-01-22 11:47:11 +0100

    Fixes afer mercury and ADS tests wrong capsulation

---
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsEvolveObject.java
@@ -0,0 +1,42 @@
+package com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase;
+
+import com.wikia.webdriver.common.contentpatterns.AdsContent;
+import com.wikia.webdriver.common.logging.PageObjectLogging;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author Dmytro Rets
+ * @ownership AdEngineering
+ */
+public class AdsEvolveObject extends AdsBaseObject {
+	private static final String EVOLVE_SELECTOR = " script[src*=\"4403ad\"]";
+
+	public AdsEvolveObject(WebDriver driver, String page) {
+		// INVISIBLE_SKIN works only with big resolution.
+		super(driver, page, new Dimension(1366, 768));
+	}
+
+	public void verifyEvolveInSlot(String slotName) {
+		String slotSelector = AdsContent.getSlotSelector(slotName);
+		waitForElementPresenceByBy(By.cssSelector(slotSelector + EVOLVE_SELECTOR));
+		PageObjectLogging.log("Evolve", slotSelector + " slot has Evolve.", true, driver);
+	}
+
+	public void verifyNoEvolveInSlot(String slotName) {
+		String slotSelector = AdsContent.getSlotSelector(slotName);
+		waitForElementNotPresent(slotSelector + EVOLVE_SELECTOR);
+		PageObjectLogging.log("Evolve", slotSelector + " slot doesn't have Evolve.", true, driver);
+	}
+
+	public void verifyEvolveCall() {
+		verifyEvolveInSlot(AdsContent.TOP_LB);
+		verifyEvolveInSlot(AdsContent.MEDREC);
+		verifyEvolveInSlot(AdsContent.LEFT_SKYSCRAPPER_2);
+		verifyEvolveInSlot(AdsContent.INVISIBLE_SKIN);
+		verifyNoEvolveInSlot(AdsContent.FLOATING_MEDREC);
+		verifyNoEvolveInSlot(AdsContent.PREFOOTER_LEFT);
+		verifyNoEvolveInSlot(AdsContent.PREFOOTER_RIGHT);
+	}
+}
