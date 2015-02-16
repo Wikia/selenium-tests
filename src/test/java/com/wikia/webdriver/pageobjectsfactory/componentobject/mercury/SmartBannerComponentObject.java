f23e684c8840eb8924f5ea019b05c75b9204e929
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-02-16 13:36:39 +0100

    SmartBanner logic fix

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/mercury/SmartBannerComponentObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/mercury/SmartBannerComponentObject.java
@@ -4,12 +4,8 @@ package com.wikia.webdriver.pageobjectsfactory.componentobject.mercury;
  * @authors: Łukasz Nowak, Tomasz Napieralski
  */
 
-import com.wikia.webdriver.common.core.Assertion;
-import com.wikia.webdriver.common.logging.PageObjectLogging;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.article.ArticlePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.MercuryBasePageObject;
-import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.PerformTouchAction;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -41,48 +37,46 @@ public class SmartBannerComponentObject extends MercuryBasePageObject {
   public void clickCloseButton() {
     waitForElementVisibleByElement(closeButton);
     closeButton.click();
-    PageObjectLogging.log("clickCloseButton", "Close button was clicked", true, driver);
   }
 
-  public void clickInstallButton() {
+  public boolean isSmartBannerVisible() {
+    if (smartBanner.getCssValue("display").equals("none")) {
+      return false;
+    }
+    return true;
+  }
+
+  public String getButtonName() {
     waitForElementVisibleByElement(bannerButton);
-    Assertion.assertTrue(bannerButton.getText().contains("Install"),
-        "Install button was not visible");
-    bannerButton.click();
+    return bannerButton.getText();
   }
 
-  public void verifySmartBannerWasClosed() {
-    Assertion.assertTrue(smartBanner.getCssValue("display").contains("none"),
-        "Smart banner is visible");
+  public int getSmartBannerPosition() {
+    return smartBanner.getLocation().getY();
   }
 
-  public void verifyButtonName(String name) {
-    waitForElementVisibleByElement(bannerButton);
-    Assertion.assertTrue(bannerButton.getText().contains(name), "Smart banner has different name");
-  }
-
-  public void verifyFixPositionOfSmartBanner(PerformTouchAction touchAction) {
-    int lastPosition = smartBanner.getLocation().getY();
-    touchAction.swipeFromPointToPoint(50, 90, 50, 40, 500, 3000);
-    Assertion.assertTrue(lastPosition == smartBanner.getLocation().getY(),
-        "Smart banner is floating");
-  }
-
-  public void verifyThemeColorOnHub(String color) {
+  public boolean isSmartBannerColorCorrect(String color) {
+    Color properColor = Color.decode(color);
     int[] smartBannerColor =
         ArticlePageObject.convertRGBAFunctiontoIntTable(smartBanner
-            .getCssValue("border-bottom-color"));
+                                                            .getCssValue("border-bottom-color"));
+    if ((smartBannerColor[0] == properColor.getRed()) && (smartBannerColor[1] == properColor
+        .getGreen()) && (smartBannerColor[2] == properColor.getBlue())) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean isSmartBannerButtonColorCorrect(String color) {
+    Color properColor = Color.decode(color);
     int[] smartBannerButtonColor =
         ArticlePageObject.convertRGBAFunctiontoIntTable(bannerButton
-            .getCssValue("background-color"));
-    Color properColor = Color.decode(color);
-    Assertion.assertTrue((smartBannerButtonColor[0] == smartBannerColor[0])
-        && (smartBannerButtonColor[1] == smartBannerColor[1])
-        && (smartBannerButtonColor[2] == smartBannerColor[2]),
-        "Smart banner color is different than smart banner button");
-    Assertion.assertTrue((smartBannerButtonColor[0] == properColor.getRed())
-        && (smartBannerButtonColor[1] == properColor.getGreen())
-        && (smartBannerButtonColor[2] == properColor.getBlue()),
-        "Smart banner color theme is improper for that HUB");
+                                                            .getCssValue("background-color"));
+    if ((smartBannerButtonColor[0] == properColor.getRed()) && (smartBannerButtonColor[1]
+                                                                == properColor.getGreen()) && (
+            smartBannerButtonColor[2] == properColor.getBlue())) {
+      return true;
+    }
+    return false;
   }
 }
--- src/test/java/com/wikia/webdriver/testcases/mercurytests/SmartBannerTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mercurytests/SmartBannerTests.java
@@ -1,6 +1,7 @@
 package com.wikia.webdriver.testcases.mercurytests;
 
 import com.wikia.webdriver.common.contentpatterns.MercuryContent;
+import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.properties.Credentials;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.componentobject.mercury.SmartBannerComponentObject;
@@ -19,9 +20,10 @@ public class SmartBannerTests extends NewTestTemplate {
   Credentials credentials = config.getCredentials();
 
   private static final String[] DIFFERENT_HUBS_WIKIS = {"destiny", "cocktails", "thehungergames",
-      "marvel", "tardis", "starwars", "lego"};
+                                                        "marvel", "tardis", "starwars", "lego"};
   private static final String[] DIFFERENT_HUBS_COLORS = {"#94d11f", "#8ca038", "#ff7f26",
-      "#ff5400", "#00b7e0", "#09d3bf", "#ffd000"};
+                                                         "#ff5400", "#00b7e0", "#09d3bf",
+                                                         "#ffd000"};
 
   // SBT01
   @Test(groups = {"MercurySmartBannerTest_001", "MercurySmartBannerTests", "Mercury"})
@@ -31,7 +33,7 @@ public class SmartBannerTests extends NewTestTemplate {
     base.openMercuryWiki(DIFFERENT_HUBS_WIKIS[0]);
     SmartBannerComponentObject banner = new SmartBannerComponentObject(driver);
     banner.clickCloseButton();
-    banner.verifySmartBannerWasClosed();
+    Assertion.assertFalse(banner.isSmartBannerVisible(), "Smart banner is visible");
   }
 
   // SBT02
@@ -42,7 +44,10 @@ public class SmartBannerTests extends NewTestTemplate {
     base.openMercuryWiki(DIFFERENT_HUBS_WIKIS[0]);
     SmartBannerComponentObject banner = new SmartBannerComponentObject(driver);
     PerformTouchAction touchAction = new PerformTouchAction(driver);
-    banner.verifyFixPositionOfSmartBanner(touchAction);
+    int lastSmartBannerPosition = banner.getSmartBannerPosition();
+    touchAction.swipeFromPointToPoint(50, 90, 50, 40, 500, 3000);
+    Assertion.assertTrue(lastSmartBannerPosition == banner.getSmartBannerPosition(),
+                         "Smart banner is floating");
   }
 
   // SBT03
@@ -52,7 +57,9 @@ public class SmartBannerTests extends NewTestTemplate {
     MercuryBasePageObject base = new MercuryBasePageObject(driver);
     base.openMercuryWiki(DIFFERENT_HUBS_WIKIS[0]);
     SmartBannerComponentObject banner = new SmartBannerComponentObject(driver);
-    banner.verifyButtonName(SmartBannerComponentObject.BUTTON_NAME_FOR_ANDROID);
+    Assertion.assertTrue(
+        banner.getButtonName().equals(SmartBannerComponentObject.BUTTON_NAME_FOR_ANDROID),
+        "Smart banner has different name");
   }
 
   // SBT04
@@ -62,7 +69,9 @@ public class SmartBannerTests extends NewTestTemplate {
     MercuryBasePageObject base = new MercuryBasePageObject(driver);
     base.openMercuryWiki(DIFFERENT_HUBS_WIKIS[0]);
     SmartBannerComponentObject banner = new SmartBannerComponentObject(driver);
-    banner.verifyButtonName(SmartBannerComponentObject.BUTTON_NAME_FOR_IOS);
+    Assertion
+        .assertTrue(banner.getButtonName().equals(SmartBannerComponentObject.BUTTON_NAME_FOR_IOS),
+                    "Smart banner has different name");
   }
 
   // SBT05
@@ -79,7 +88,10 @@ public class SmartBannerTests extends NewTestTemplate {
         base.openMercuryWiki(DIFFERENT_HUBS_WIKIS[i]);
       }
       banner = new SmartBannerComponentObject(driver);
-      banner.verifyThemeColorOnHub(DIFFERENT_HUBS_COLORS[i]);
+      Assertion.assertTrue(banner.isSmartBannerColorCorrect(DIFFERENT_HUBS_COLORS[i]),
+                           "Smart banner color is wrong");
+      Assertion.assertTrue(banner.isSmartBannerButtonColorCorrect(DIFFERENT_HUBS_COLORS[i]),
+                           "Smart banner button color is wrong");
     }
   }
 }
