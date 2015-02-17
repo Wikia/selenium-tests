7385277d8544eec29b332d993dcc48fc9be0d22c
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-02-17 17:21:14 +0100

    Refactored char replace in title for youtube videos

--- src/test/java/com/wikia/webdriver/common/core/YoutubeVideo.java
+++ src/test/java/com/wikia/webdriver/common/core/YoutubeVideo.java
@@ -1,21 +1,42 @@
 package com.wikia.webdriver.common.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Ludwik on 2015-02-12.
  */
 public class YoutubeVideo implements Video {
 
+  static final Map<String, String> TITLE_SPECIAL_CHARS_TO_REPLACE = new HashMap<String, String>() {
+    {
+      put("| ", "");
+      put("|", "");
+      put("{", " ");
+      put("}", " ");
+      put("[", " ");
+      put("]", " ");
+      put("/", " ");
+    }
+  };
   private String url;
   private String title;
   private String fileName;
 
   public YoutubeVideo(String title, String url) {
     this.url = url;
-    this.title =
-        title.replace("| ", "").replace("|", "").replace("{", " ").replace("}", " ")
-            .replace("[", " ").replace("]", " ").replace("/", " ");
+    this.title = escapeSpecialCharactersFromTitle(title);
 
     this.fileName = transformTitleToFileName(this.title);
+  }
+
+  private static final String escapeSpecialCharactersFromTitle(String title) {
+    String titleAfterEscape = title;
+    for (Map.Entry<String, String> entry : TITLE_SPECIAL_CHARS_TO_REPLACE.entrySet()) {
+      titleAfterEscape = titleAfterEscape.replace(entry.getKey(), entry.getValue());
+    }
+
+    return titleAfterEscape;
   }
 
   @Override
