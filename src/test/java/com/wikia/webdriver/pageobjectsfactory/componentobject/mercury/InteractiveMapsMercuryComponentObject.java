c38ac83390f82cf51d7313e79d848c3197504b19
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-02-19 12:24:34 +0100

    Interactive Maps logic fix

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/mercury/InteractiveMapsMercuryComponentObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/mercury/InteractiveMapsMercuryComponentObject.java
@@ -2,14 +2,12 @@ package com.wikia.webdriver.pageobjectsfactory.componentobject.mercury;
 
 import java.io.File;
 
-import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.imageutilities.ImageComparison;
 import com.wikia.webdriver.common.core.imageutilities.Shooter;
-import com.wikia.webdriver.common.logging.PageObjectLogging;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.MercuryBasePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.PerformTouchAction;
 
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,8 +16,6 @@ public class InteractiveMapsMercuryComponentObject extends MercuryBasePageObject
 
   @FindBy(css = ".current")
   private WebElement mapFrame;
-  @FindBy(css = "div[id='map']")
-  private WebElement mapDiv;
   @FindBy(css = ".lightbox-close-wrapper")
   private WebElement closeMapLightbox;
   @FindBy(css = ".lightbox-header-title")
@@ -32,14 +28,14 @@ public class InteractiveMapsMercuryComponentObject extends MercuryBasePageObject
   private WebElement filterBoxHeader;
   @FindBy(css = ".point-types")
   private WebElement filterBoxPoints;
-  @FindBy(css = ".leaflet-tile")
-  private WebElement leafletTile;
   @FindBy(css = ".leaflet-marker-icon")
   private WebElement poiPin;
   @FindBy(css = ".leaflet-popup")
   private WebElement poiPopUp;
   @FindBy(css = ".lightbox-content")
   private WebElement lightbox;
+  @FindBy(css = ".view-map")
+  private WebElement viewMapButton;
 
   private int waitTime = 5000;
 
@@ -47,147 +43,120 @@ public class InteractiveMapsMercuryComponentObject extends MercuryBasePageObject
     super(driver);
   }
 
+  public void clickViewMapButton() {
+    waitForElementVisibleByElement(viewMapButton);
+    viewMapButton.click();
+  }
+
   public void clickCloseButton() {
     waitForElementVisibleByElement(closeMapLightbox);
     closeMapLightbox.click();
-    PageObjectLogging.log("clickCloseButton", "Close button was clicked", true);
   }
 
   public void clickFilterBox() {
-    driver.switchTo().frame(mapFrame);
     waitForElementVisibleByElement(filterBoxHeader);
     tapOnElement(filterBoxHeader);
-    PageObjectLogging.log("clickFilterBox", "Filter box caption was clicked", true);
   }
 
   public void clickZoomIn() {
     waitForElementVisibleByElement(zoomInButton);
     tapOnElement(zoomInButton);
-    PageObjectLogging.log("clickZoomIn", "Zoom in button was clicked", true);
-  }
-
-  public void clickZoomIn(int amountOfClicks) {
-    while (amountOfClicks != 0) {
-      waitForElementClickableByElement(zoomInButton);
-      tapOnElement(zoomInButton);
-      amountOfClicks--;
-    }
-    PageObjectLogging.log("clickZoomIn", "Zoom in button was clicked", true);
   }
 
   public void clickZoomOut() {
     waitForElementVisibleByElement(zoomOutButton);
     tapOnElement(zoomOutButton);
-    PageObjectLogging.log("clickZoomOut", "Zoom out button was clicked", true);
-  }
-
-
-  public void clickZoomOut(int amountOfClicks) {
-    while (amountOfClicks != 0) {
-      waitForElementClickableByElement(zoomOutButton);
-      tapOnElement(zoomOutButton);
-      amountOfClicks--;
-    }
-    PageObjectLogging.log("clickZoomOut", "Zoom out button was clicked", true);
   }
 
   public void clickPin() {
-    String methodName = "clickPin";
-    try {
-      driver.switchTo().frame(mapFrame);
-      waitForElementVisibleByElement(poiPin);
-      tapOnElement(poiPin);
-    } catch (NoSuchElementException e) {
-      PageObjectLogging.log(methodName, "There is no pins, add them manualy", false);
-      PageObjectLogging.log(methodName, e.getMessage(), false);
-    }
-    PageObjectLogging.log(methodName, "Pin was clicked", true);
-  }
-
-  public String getMapLeafletSrc() {
-    return leafletTile.getAttribute("src");
-  }
-
-  public void verifyFilterBoxWasExpanded() {
-    waitForElementVisibleByElement(filterBoxPoints);
-    Assertion.assertTrue(checkIfElementOnPage(filterBoxPoints));
-  }
-
-  public void verifyMapModalIsNotVisible() {
-    Assertion.assertFalse(checkIfElementOnPage(lightbox));
-  }
-
-  public void verifyMapModalIsVisible() {
     waitForElementByElement(lightbox);
-    Assertion.assertTrue(checkIfElementOnPage(lightbox));
-  }
-
-  public void verifyMapTitleInHeader() {
-    waitForElementVisibleByElement(mapTitle);
-    Assertion.assertFalse(mapTitle.getText().isEmpty());
-  }
-
-  public void verifyMapIdInUrl() {
-    Assertion.assertTrue(driver.getCurrentUrl().toString().contains("?map="));
-  }
-
-  public void verifyMapZoomChangedView(String leaflet, String newLeaflet) {
-    Assertion.assertFalse(leaflet.contains(newLeaflet));
-    PageObjectLogging.log("verifyMapZoomChangedView", "Map view was changed", true, driver);
-  }
-
-  public void verifyPinPopUpAppeared() {
-    Assertion.assertTrue(checkIfElementOnPage(poiPopUp));
-    PageObjectLogging.log("verifyPinPopUpAppeared", "Pin popup appeared after click", true, driver);
-  }
-
-  public void verifyZoomButtons() {
     driver.switchTo().frame(mapFrame);
-    String methodName = "verifyZoomButtons";
+    waitForElementVisibleByElement(poiPin);
+    tapOnElement(poiPin);
+  }
+
+  public boolean isFilterBoxWasExpanded() {
+    waitForElementVisibleByElement(filterBoxPoints);
+    if (checkIfElementOnPage(filterBoxPoints)) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean isMapModalVisible() {
+    try {
+      waitForElementByElement(lightbox);
+      if (checkIfElementOnPage(lightbox)) {
+        return true;
+      }
+      return false;
+    } catch (TimeoutException e) {}
+    return false;
+  }
+
+  public boolean isTextInMapTitleHeader() {
+    waitForElementVisibleByElement(mapTitle);
+    if (mapTitle.getText().isEmpty()) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isMapIdInUrl() {
+    if (driver.getCurrentUrl().toString().contains("?map=")) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean isPinPopUp() {
+    if (checkIfElementOnPage(poiPopUp)) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean isZoomButtonWorking(String zoomWay) {
     Shooter shooter = new Shooter();
     ImageComparison ic = new ImageComparison();
     File beforeZooming = shooter.capturePage(driver);
-    clickZoomOut();
-    waitMilliseconds(waitTime, methodName);
-    File afterZoomOut = shooter.capturePage(driver);
-    clickZoomIn();
-    waitMilliseconds(waitTime, methodName);
-    File afterZoomIn = shooter.capturePage(driver);
-    Assertion.assertFalse(ic.areFilesTheSame(beforeZooming, afterZoomOut), "Zoom out doesn't work");
-    Assertion.assertTrue(ic.areFilesTheSame(beforeZooming, afterZoomIn), "Zoom in doesn't work");
-  }
-
-  public void verifyZoomByGesture(PerformTouchAction touchAction) {
-    driver.switchTo().frame(mapFrame);
-    String methodName = "verifyZoomByGesture";
-    try {
-      Assertion.assertTrue(zoomInButton.getAttribute("class").contains("disabled"),
-          "Zoom in button is enabled");
-      touchAction.zoomInOutPointXY(50, 50, 50, 100, PerformTouchAction.ZOOM_WAY_OUT, waitTime);
-      Assertion.assertFalse(zoomInButton.getAttribute("class").contains("disabled"),
-          "Zoom in button is disabled");
-      touchAction.zoomInOutPointXY(50, 50, 50, 100, PerformTouchAction.ZOOM_WAY_IN, waitTime);
-      Assertion.assertTrue(zoomInButton.getAttribute("class").contains("disabled"),
-          "Zoom in button is enabled");
-    } catch (NoSuchElementException e) {
-      PageObjectLogging.log(methodName, e.getMessage(), false);
+    if (zoomWay.equals("in")) {
+      clickZoomIn();
+    } else {
+      clickZoomOut();
     }
+    waitMilliseconds(waitTime, "waitMilliseconds");
+    File afterZooming = shooter.capturePage(driver);
+    if (ic.areFilesTheSame(beforeZooming, afterZooming)) {
+      return false;
+    }
+    return true;
   }
 
-  public void verifyScrollableFilterList(PerformTouchAction touchAction) {
-    String methodName = "verifyScrollableFilterList";
+  public boolean isZoomInButtonEnabled() {
+    if (zoomInButton.getAttribute("class").contains("disabled")) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isFilterListScrollable(PerformTouchAction touchAction) {
+    String methodName = "isFilterListScrollable";
     Shooter shooter = new Shooter();
     ImageComparison ic = new ImageComparison();
     File beforeScrolling = shooter.capturePage(driver);
-    try {
-      clickFilterBox();
-      waitMilliseconds(waitTime, methodName);
-      touchAction.swipeFromPointToPoint(40, 80, 40, 40, 500, waitTime);
-      File afterScrolling = shooter.capturePage(driver);
-      Assertion.assertFalse(ic.areFilesTheSame(beforeScrolling, afterScrolling),
-          "Scrolling in filter box doesn't work");
-    } catch (NoSuchElementException e) {
-      PageObjectLogging.log(methodName, e.getMessage(), false);
+    clickFilterBox();
+    waitMilliseconds(waitTime, methodName);
+    touchAction.swipeFromPointToPoint(40, 80, 40, 40, 500, waitTime);
+    File afterScrolling = shooter.capturePage(driver);
+    if (ic.areFilesTheSame(beforeScrolling, afterScrolling)) {
+      return false;
     }
+    return true;
+  }
+
+  public void switchToMapFrame() {
+    waitForElementByElement(lightbox);
+    driver.switchTo().frame(mapFrame);
   }
 }
