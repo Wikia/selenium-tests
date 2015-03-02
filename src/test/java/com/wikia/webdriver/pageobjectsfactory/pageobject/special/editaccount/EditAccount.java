55b8cc1b6b5a86454d0cdd070993a9894848640e
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-03-02 11:05:36 +0100

    Edit account test fix

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/special/editaccount/EditAccount.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/special/editaccount/EditAccount.java
@@ -1,18 +1,21 @@
 package com.wikia.webdriver.pageobjectsfactory.pageobject.special.editaccount;
 
-import com.wikia.webdriver.common.contentpatterns.URLsContent;
-import com.wikia.webdriver.common.logging.PageObjectLogging;
-import com.wikia.webdriver.pageobjectsfactory.pageobject.BasePageObject;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import com.wikia.webdriver.common.contentpatterns.URLsContent;
+import com.wikia.webdriver.common.logging.PageObjectLogging;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.BasePageObject;
 
 /**
  * @author Karol 'kkarolk' Kujawiak
  */
 public class EditAccount extends BasePageObject {
 
+  private static final String USER_ACCOUNT_REOPEN_MESSAGE =
+      "Successfully removed disabled bit for account";
+  private static final String USER_ACCOUNT_CLOSED_MESSAGE = "Successfully disabled account";
   @FindBy(css = "[name=wpUserName]")
   private WebElement userNameField;
   @FindBy(css = "[value='Close account']")
@@ -28,47 +31,34 @@ public class EditAccount extends BasePageObject {
   @FindBy(css = "fieldset > span")
   private WebElement statusMessage;
 
-  private static final String
-      USER_ACCOUNT_REOPEN_MESSAGE =
-      "Successfully removed disabled bit for account";
-  private static final String USER_ACCOUNT_CLOSED_MESSAGE = "Successfully disabled account";
-
-  public EditAccount(WebDriver driver, String communityWikiURL, String userName) {
+  public EditAccount(WebDriver driver) {
     super(driver);
-    driver.get(
-        communityWikiURL +
-        URLsContent.SPECIAL_EDIT_ACCOUNT
-    );
+  }
+
+  public EditAccount navigateToSpecialEditAccount(String communityWikiURL) {
+    driver.get(communityWikiURL + URLsContent.SPECIAL_EDIT_ACCOUNT);
+
+    return this;
+  }
+
+  public EditAccount goToAccountManagement(String userName) {
     userNameField.sendKeys(userName);
     userNameField.submit();
-    PageObjectLogging.log(
-        "editAccount",
-        URLsContent.SPECIAL_EDIT_ACCOUNT + " page opened",
-        true
-    );
+    PageObjectLogging.log("editAccount", URLsContent.SPECIAL_EDIT_ACCOUNT + " page opened", true);
+
+    return this;
   }
 
   public void closeAccount(String reason) {
     scrollAndClick(closeAccountButton);
     closeResonField.sendKeys(reason);
     closeResonField.submit();
-    PageObjectLogging.log(
-        "closeAccount",
-        "account closed",
-        true
-    );
+    PageObjectLogging.log("closeAccount", "account closed", true);
   }
 
   public void verifyAccountClosedMessage() {
-    waitForTextToBePresentInElementByElement(
-        statusMessage,
-        USER_ACCOUNT_CLOSED_MESSAGE
-    );
-    PageObjectLogging.log(
-        "verifyAccountClosedMessage",
-        "verified account closed",
-        true
-    );
+    waitForTextToBePresentInElementByElement(statusMessage, USER_ACCOUNT_CLOSED_MESSAGE);
+    PageObjectLogging.log("verifyAccountClosedMessage", "verified account closed", true);
   }
 
   public void reopenAccount(String newPassword) {
@@ -76,22 +66,11 @@ public class EditAccount extends BasePageObject {
     newPasswordField.sendKeys(newPassword);
     newPasswordField.submit();
     scrollAndClick(clearDisableFlagButton);
-    PageObjectLogging.log(
-        "reopenAccount",
-        "account reopened",
-        true
-    );
+    PageObjectLogging.log("reopenAccount", "account reopened", true);
   }
 
   public void verifyAccountReopenedMessage() {
-    waitForTextToBePresentInElementByElement(
-        statusMessage,
-        USER_ACCOUNT_REOPEN_MESSAGE
-    );
-    PageObjectLogging.log(
-        "verifyAccountReopenedMessage",
-        "verified account reopened",
-        true
-    );
+    waitForTextToBePresentInElementByElement(statusMessage, USER_ACCOUNT_REOPEN_MESSAGE);
+    PageObjectLogging.log("verifyAccountReopenedMessage", "verified account reopened", true);
   }
 }
