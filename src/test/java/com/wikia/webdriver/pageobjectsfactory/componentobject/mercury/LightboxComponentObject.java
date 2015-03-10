44d389262812882c09eed181590a27f837ee0166
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-03-10 16:45:35 +0100

    mercury-tests-fix

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/mercury/LightboxComponentObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/mercury/LightboxComponentObject.java
@@ -6,6 +6,7 @@ import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.BasePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.PerformTouchAction;
 
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
@@ -59,9 +60,9 @@ public class LightboxComponentObject extends BasePageObject {
     try {
       waitForElementByElement(lightboxContent);
       return checkIfElementOnPage(lightboxContent);
-    } catch (NoSuchElementException e) {
+    } catch (NoSuchElementException|TimeoutException e) {
+      return false;
     }
-    return false;
   }
 
   public boolean isCurrentImageVisible() {
