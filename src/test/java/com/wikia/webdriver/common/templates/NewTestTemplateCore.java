75f02f4033564d5c3dee6637c56d3f5dd2a74922
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-03-13 12:37:41 +0100

    appium session kill

--- src/test/java/com/wikia/webdriver/common/templates/NewTestTemplateCore.java
+++ src/test/java/com/wikia/webdriver/common/templates/NewTestTemplateCore.java
@@ -93,6 +93,9 @@ public class NewTestTemplateCore {
   }
 
   protected void stopBrowser() {
+    if(NewDriverProvider.getMobileDriver().getSessionId()!=null){
+      NewDriverProvider.getMobileDriver().quit();
+    }
     if (driver != null) {
       driver.quit();
     }
