de2e6fb0204a8b341ab496544b34c8df5b61d58a
justnpT <Michal.Nowierski@wikia-inc.com>
2015-03-23 14:35:33 +0100

    removing trash from first test

--- src/test/java/com/wikia/webdriver/testcases/articlecrudtests/ClicktrackingArticleEditModeTests.java
+++ src/test/java/com/wikia/webdriver/testcases/articlecrudtests/ClicktrackingArticleEditModeTests.java
@@ -10,24 +10,16 @@ import org.testng.annotations.Test;
 import com.wikia.webdriver.common.clicktracking.ClickTrackingScriptsProvider;
 import com.wikia.webdriver.common.clicktracking.events.EventsArticleEditMode;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
-import com.wikia.webdriver.pageobjectsfactory.componentobject.global_navitagtion.NavigationBar;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.WikiBasePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.article.ArticlePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.article.editmode.VisualEditModePageObject;
 
 public class ClicktrackingArticleEditModeTests extends NewTestTemplate {
 
-    private static final String script = "window.onbeforeunload = function () "
-            + "{return 'blocking redirect - performing clicktracking test';}";
-
     @Test(groups = { "ClickTracking", "ClickTrackingArticleEditModeTests",
             "ClickTrackingArticleEditMode_001" })
     public void ClickTrackingArticleEditMode_001_verifyPreviewModalEvents() {
         WikiBasePageObject base = new WikiBasePageObject(driver);
-        base.openMainPage(wikiURL);
-        NavigationBar navbar = new NavigationBar(driver);
-        navbar.executeScript(script);
-
         ArticlePageObject article = base.openRandomArticle(wikiURL);
         VisualEditModePageObject visualEditMode = article
                 .editArticleInRTEUsingDropdown();
