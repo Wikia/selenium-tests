ac010d4cb6f5fe7637feb913001cd3f18bbcdb7b
Dyktus <lukasz.nowak@wikia-inc.com>
2015-03-23 15:27:34 +0100

    Removed QAART-544 tag

--- src/test/java/com/wikia/webdriver/testcases/mediatests/addvideo/VetArticlePlaceholderTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mediatests/addvideo/VetArticlePlaceholderTests.java
@@ -24,7 +24,7 @@ public class VetArticlePlaceholderTests extends NewTestTemplate {
   Credentials credentials = config.getCredentials();
 
   @Test(groups = {"VideoArticlePlacehoder_001", "VideoArticlePlacehoder", "Media"})
-  public void Placeholders_001_PublishedProvider_QAART_544() {
+  public void Placeholders_001_PublishedProvider() {
     WikiBasePageObject base = new WikiBasePageObject(driver);
     base.logInCookie(credentials.userName, credentials.password, wikiURL);
     base.openRandomArticle(wikiURL);
