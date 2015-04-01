c8cea52105d26818b89719c96e1bab619aa69739
bekcunning <rcunningham@wikia-inc.com>
2015-04-01 11:52:15 -0700

    added to javadoc

--- src/test/java/com/wikia/webdriver/common/core/annotations/DontRun.java
+++ src/test/java/com/wikia/webdriver/common/core/annotations/DontRun.java
@@ -8,6 +8,7 @@ import java.lang.annotation.Target;
 /**
  * This annotation allows a test to be skipped based testing environment
  * env is either "preview", "prod", or "dev-name"
+ * Tests in unsupported env will be marked as skipped with success result
  */
 @Retention(value = RetentionPolicy.RUNTIME)
 @Target(value = {ElementType.METHOD})
