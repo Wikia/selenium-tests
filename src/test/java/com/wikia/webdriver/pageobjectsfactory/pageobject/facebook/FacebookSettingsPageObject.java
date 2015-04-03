17c23641ef16c0d0a0424efcabfdc12ccbab8d51
bekcunning <rcunningham@wikia-inc.com>
2015-04-03 13:22:14 -0700

    fix incorrect selector for logOutFB

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/facebook/FacebookSettingsPageObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/facebook/FacebookSettingsPageObject.java
@@ -32,7 +32,7 @@ public class FacebookSettingsPageObject extends WikiBasePageObject {
   private WebElement settingsContent;
   @FindBy(css = "#userNavigationLabel")
   private WebElement fbDropDown;
-  @FindBy(css = "#uiLinkButtonInput")
+  @FindBy(css = ".uiLinkButtonInput")
   private WebElement fbLogOut;
 
   public FacebookSettingsPageObject(WebDriver driver) {
