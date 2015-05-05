8d9ce3ad5b14b0b271cb4ee0711d3f9b647ff6a4
justnpT <Michal.Nowierski@wikia-inc.com>
2015-05-05 15:23:27 +0200

    adding social button tests

---
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/social_buttons/SocialButtonsComponentObject.java
@@ -0,0 +1,49 @@
+package com.wikia.webdriver.pageobjectsfactory.componentobject.social_buttons;
+
+import com.wikia.webdriver.common.core.Assertion;
+import com.wikia.webdriver.common.logging.PageObjectLogging;
+import com.wikia.webdriver.pageobjectsfactory.componentobject.media.VideoComponentObject;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.WikiBasePageObject;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.special.filepage.FilePagePageObject;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+
+import java.util.List;
+
+/**
+ * @author Karol 'kkarolk' Kujawiak
+ * @author Saipetch Kongkatong
+ */
+public class SocialButtonsComponentObject extends WikiBasePageObject {
+
+  public SocialButtonsComponentObject(WebDriver driver) {
+      super(driver);
+  }
+
+    @FindBys(@FindBy(css = "#PageShareToolbar svg"))
+    protected List<WebElement> shareButtonImages;
+
+    @FindBys(@FindBy(css = "#PageShareToolbar a"))
+    protected List<WebElement> shareButtonLinks;
+
+    public void verifyShareButtonsPresent() {
+        for (int i = 0; i < shareButtonImages.size(); i++) {
+            WebElement image = shareButtonImages.get(i);
+            waitForElementByElement(image);
+        }
+        PageObjectLogging.log("verifyShareButtonsPresent", "social buttons present", true);
+    }
+
+    public String[] getShareButtonTitles() {
+        String[] titles = null;
+        for (int i = 0; i < shareButtonLinks.size(); i++) {
+            WebElement link = shareButtonLinks.get(i);
+            waitForElementByElement(link);
+            titles[i] = link.getAttribute("title");
+        }
+        return titles;
+    }
+
+}
---
+++ src/test/java/com/wikia/webdriver/testcases/social_buttons_tests/SocialButtonsForUserLanguage.java
@@ -0,0 +1,100 @@
+package com.wikia.webdriver.testcases.social_buttons_tests;
+
+import com.wikia.webdriver.common.contentpatterns.PageContent;
+import com.wikia.webdriver.common.core.Assertion;
+import com.wikia.webdriver.common.dataprovider.ArticleDataProvider;
+import com.wikia.webdriver.common.properties.Credentials;
+import com.wikia.webdriver.common.templates.NewTestTemplate;
+import com.wikia.webdriver.pageobjectsfactory.componentobject.social_buttons.SocialButtonsComponentObject;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.WikiBasePageObject;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.article.ArticlePageObject;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.article.editmode.VisualEditModePageObject;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.special.SpecialCreatePagePageObject;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * @author: Michal 'justnpT' Nowierski
+ */
+public class SocialButtonsForUserLanguage extends NewTestTemplate {
+
+    private Credentials credentials = config.getCredentials();
+
+
+  @Test(
+      groups = {"SocialButtons", "SocialButtons_001"},
+      dataProvider = "SocialButtonsDataProvider"
+  )
+
+    public void ArticleCRUDUser_001_specialPage(String[] credentials, String[] expectedSocialNetworks) {
+        WikiBasePageObject base = new WikiBasePageObject(driver);
+        base.logInCookie(credentials[0], credentials[1], wikiURL);
+        ArticlePageObject article = base.openRandomArticle(wikiURL);
+        SocialButtonsComponentObject buttons = new SocialButtonsComponentObject(driver);
+        buttons.verifyShareButtonsPresent();
+        String[] currentSocialNetworks = buttons.getShareButtonTitles();
+        for(int i = 0; i < expectedSocialNetworks.length; i++) {
+            String currentSocialNetwork = currentSocialNetworks[i];
+            String expectedSocialNetwork = expectedSocialNetworks[i];
+            Assertion.assertEquals(currentSocialNetwork, expectedSocialNetwork,
+                    "expected network not found on its position. " +
+                    "Note that the order of social buttons is also important, " +
+                    "as defined in requirements. Missing network:" + expectedSocialNetwork);
+        }
+  }
+
+    /**
+     * Requirements set in https://wikia-inc.atlassian.net/browse/CONCF-511
+     *
+     * {
+     *     {userName, userPassword},
+     *     {expected buttons}
+     * }
+     */
+    public final String[][][] SocialButtonsDataProvider() {
+        return new String[][][]{
+                {
+                        {credentials.userNameEnglish, credentials.passwordEnglish},
+                        {"Facebook", "Twitter", "Reddit", "Tumblr"}
+                },
+                {
+                        {credentials.userNameJapanese, credentials.passwordJapanese},
+                        {"Facebook", "Twitter", "GooglePlus"}
+                },
+                {
+                        {credentials.userNameBrazilianPortuguese, credentials.passwordBrazilianPortuguese},
+                        {"Facebook", "Twitter", "Reddit", "Pinterest", "Tumblr"}
+                },
+                {
+                        {credentials.userNameChinese, credentials.passwordChinese},
+                        {"Facebook", "Weibo", "WeChat"}
+                },
+                {
+                        {credentials.userNameGerman, credentials.passwordGerman},
+                        {"Facebook", "Twitter", "Pinterest", "Tumblr"}
+                },
+                {
+                        {credentials.userNameFrench, credentials.passwordFrench},
+                        {"Facebook", "Twitter"}
+                },
+                {
+                        {credentials.userNameSpanish, credentials.passwordSpanish},
+                        {"Facebook", "Twitter", "Meneame", "Tumblr"}
+                },
+                {
+                        {credentials.userNameRussian, credentials.passwordRussian},
+                        {"VK", "Facebook", "OK", "Twitter"}
+                },
+                {
+                        {credentials.userNamePolish, credentials.passwordPolish},
+                        {"Facebook", "Twitter", "NK"}
+                },
+                {
+                        {credentials.userNameItalian, credentials.passwordItalian},
+                        {"Facebook", "Twitter", "Reddit", "Tumblr"}
+                }
+        };
+    }
+
+}
+
