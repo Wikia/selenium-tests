4bdd205932051fdfd191d7e41ff16c40e7115474
archbold <patrick@wikia-inc.com>
2015-05-06 14:12:51 +0200

    Removed Jira ID as ticket resolved

--- src/test/java/com/wikia/webdriver/testcases/specialpagestests/FilePageTests.java
+++ src/test/java/com/wikia/webdriver/testcases/specialpagestests/FilePageTests.java
@@ -83,7 +83,7 @@ public class FilePageTests extends NewTestTemplate {
    *
    * @author garth
    */
-  @RelatedIssue(issueID = "MAIN-4294")
+  
   @Test(groups = {"FilePage", "filePage004_delete", "Media"})
   public void filePage004_delete() {
     // Go to Special:Videos to add a video
