e9867bc6bc5aff5671c080d76d2fd33fc0c404b8
archbold <patrick@wikia-inc.com>
2015-05-06 19:24:39 +0200

    Added Jira ID SOC-762

--- src/test/java/com/wikia/webdriver/testcases/logintests/ForgottenPasswordTests.java
+++ src/test/java/com/wikia/webdriver/testcases/logintests/ForgottenPasswordTests.java
@@ -17,8 +17,9 @@ import org.testng.annotations.Test;
 public class ForgottenPasswordTests extends NewTestTemplate {
 
   Credentials credentials = config.getCredentials();
-    
-  
+
+
+  @RelatedIssue(issueID = "SOC-762")
   @Test(groups = {"ForgottenPassword_001", "ForgottenPassword"})
   public void ForgottenPassword_001_dropdown() {
     String userName = credentials.userNameForgottenPassword;
