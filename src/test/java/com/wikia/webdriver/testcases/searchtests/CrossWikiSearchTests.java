d61423fcd2e1b7b448ae3d6bc83420ed3dee26c9
archbold <patrick@wikia-inc.com>
2015-05-25 16:01:09 +0200

    Added JIRA ID and comment to Related isse comment

--- src/test/java/com/wikia/webdriver/testcases/searchtests/CrossWikiSearchTests.java
+++ src/test/java/com/wikia/webdriver/testcases/searchtests/CrossWikiSearchTests.java
@@ -138,7 +138,7 @@ public class CrossWikiSearchTests extends NewTestTemplate {
    * Navigate to http://www.wikia.com/index.php?title=Special:Search type: GTA V verify that GTA V
    * wikia was found type: GTA 5 verify that GTA V wikia was found
    */
-  @RelatedIssue(issueID = "MAIN-4498")
+  @RelatedIssue(issueID = "MAIN-4498", comment = "Wikia code defect which will not be fixed. No need test manually")
   @Test( groups = {"CrossWikiSearchTests_009", "Search", "CrossWikiSearch"})
   public void crossWikiSearch_009_romanNumbersMatch() {
     CrossWikiSearchPageObject search = new CrossWikiSearchPageObject(driver);
