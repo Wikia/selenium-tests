0a2bd202511f37eaa3648d86fc24bbe1e41ad2aa
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-06-12 16:53:36 +0200

    Added missing property setter

--- src/test/java/com/wikia/webdriver/common/properties/Properties.java
+++ src/test/java/com/wikia/webdriver/common/properties/Properties.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import com.wikia.webdriver.common.core.Global;
 import com.wikia.webdriver.common.core.XMLFunctions;
-import com.wikia.webdriver.common.properties.PropertiesSetter;
 
 public class Properties {
 
