b40c169815af55ac542b6620f129d306cb910ff0
ludwikkazmierczak <ludwik.kazmierczak@gmail.com>
2015-06-17 13:52:42 +0200

    mover logging start before browser start

--- src/test/java/com/wikia/webdriver/common/templates/NewTestTemplate.java
+++ src/test/java/com/wikia/webdriver/common/templates/NewTestTemplate.java
@@ -20,19 +20,19 @@ public class NewTestTemplate extends NewTestTemplateCore {
 
   @BeforeMethod(alwaysRun = true)
   public void start(Method method, Object[] data) {
-    runProxyServerIfNeeded(method);
-    if (method.isAnnotationPresent(UserAgent.class)) {
-      setBrowserUserAgent(
+      PageObjectLogging.start();
+      runProxyServerIfNeeded(method);
+      if (method.isAnnotationPresent(UserAgent.class)) {
+        setBrowserUserAgent(
           method.getAnnotation(UserAgent.class).userAgent()
       );
     }
 
-    if (method.isAnnotationPresent(UseUnstablePageLoadStrategy.class)) {
-      NewDriverProvider.setUnstablePageLoadStrategy(true);
+      if (method.isAnnotationPresent(UseUnstablePageLoadStrategy.class)) {
+        NewDriverProvider.setUnstablePageLoadStrategy(true);
     }
 
-    startBrowser();
-    PageObjectLogging.start();
+      startBrowser();
     //Reset unstable page load strategy to default 'false' value
     NewDriverProvider.setUnstablePageLoadStrategy(false);
     logOut();
