e5f488c7a8be6f7800ee8c7b1c66bdcd145d3e7c
Piotr Gabryjeluk <rychu@wikia-inc.com>
2015-06-19 13:43:05 +0200

    ADEN-2083 Cleanup after Mercury lightbox refactor

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/helpers/ModalSelectorsHelper.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/helpers/ModalSelectorsHelper.java
@@ -4,8 +4,7 @@ public class ModalSelectorsHelper {
     private final String OASIS_MODAL_CSS = "#blackout_ext-wikia-adEngine-template-modal";
     private final String OASIS_MODAL_CLOSE_CSS = "#ext-wikia-adEngine-template-modal .close";
 
-    // TODO: remove old class (ads-lightbox) after release of CONCF-512
-    private final String MERCURY_MODAL_CSS = ".ads-lightbox, .lightbox-ads";
+    private final String MERCURY_MODAL_CSS = ".lightbox-ads";
     private final String MERCURY_MODAL_CLOSE_CSS = ".lightbox-close-wrapper";
 
     private String modalSelector;
