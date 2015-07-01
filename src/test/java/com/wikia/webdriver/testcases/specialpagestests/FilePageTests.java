4b1610930e40f618b17d55e4cba806400641d295
Rodrigo Molinero Gomez <rodrigo@wikia-inc.com>
2015-07-01 10:53:26 +0200

    Added related issue anotation to signup 005

--- src/test/java/com/wikia/webdriver/testcases/specialpagestests/FilePageTests.java
+++ src/test/java/com/wikia/webdriver/testcases/specialpagestests/FilePageTests.java
@@ -1,7 +1,5 @@
 package com.wikia.webdriver.testcases.specialpagestests;
 
-import org.testng.annotations.Test;
-
 import com.wikia.webdriver.common.contentpatterns.URLsContent;
 import com.wikia.webdriver.common.contentpatterns.VideoContent;
 import com.wikia.webdriver.common.core.video.YoutubeVideo;
@@ -15,6 +13,7 @@ import com.wikia.webdriver.pageobjectsfactory.pageobject.diffpage.DiffPagePageOb
 import com.wikia.webdriver.pageobjectsfactory.pageobject.historypage.HistoryPagePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.special.SpecialVideosPageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.special.filepage.FilePagePageObject;
+import org.testng.annotations.Test;
 
 /**
  * @ownership Content X-Wing
