baa1c6cee2277a1bcb29ebdc1b23d15cdfc1fd8f
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-07-02 14:33:40 +0200

    Applied formating changes and minor CR fixes

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/mobile/MobileSignupPageObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/mobile/MobileSignupPageObject.java
@@ -1,87 +1,85 @@
 package com.wikia.webdriver.pageobjectsfactory.pageobject.mobile;
 
-import com.wikia.webdriver.common.contentpatterns.PageContent;
-import com.wikia.webdriver.common.core.Assertion;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.nio.channels.ByteChannel;
+import com.wikia.webdriver.common.core.Assertion;
 
 /**
  * Created by rcunningham on 6/30/15.
  */
 public class MobileSignupPageObject extends MobileBasePageObject {
 
-    public MobileSignupPageObject(WebDriver driver) { super(driver); }
+  public MobileSignupPageObject(WebDriver driver) {
+    super(driver);
+  }
 
-    // UI Mapping
-    @FindBy(css = "#signupEmail")
-    private WebElement signupEmail;
-    @FindBy(css = "#signupUsername")
-    private WebElement signupUsername;
-    @FindBy(css = "#signupPassword")
-    private WebElement signupPassword;
-    @FindBy(css = "#signupBirthDate")
-    private WebElement signupBirthdate;
-    @FindBy(css = ".birth-month")
-    private WebElement signupBirthMonth;
-    @FindBy(css = ".birth-day")
-    private WebElement signupBirthDay;
-    @FindBy(css = ".birth-year")
-    private WebElement signupBirthYear;
-    @FindBy(css = "#signupSubmit")
-    private WebElement signupButton;
-    @FindBy(css = ".avatar")
-    private WebElement avatar;
+  @FindBy(css = "#signupEmail")
+  private WebElement signupEmail;
+  @FindBy(css = "#signupUsername")
+  private WebElement signupUsername;
+  @FindBy(css = "#signupPassword")
+  private WebElement signupPassword;
+  @FindBy(css = "#signupBirthDate")
+  private WebElement signupBirthdate;
+  @FindBy(css = ".birth-month")
+  private WebElement signupBirthMonth;
+  @FindBy(css = ".birth-day")
+  private WebElement signupBirthDay;
+  @FindBy(css = ".birth-year")
+  private WebElement signupBirthYear;
+  @FindBy(css = "#signupSubmit")
+  private WebElement signupButton;
+  @FindBy(css = ".avatar")
+  private WebElement avatar;
 
 
-    public MobileSignupPageObject typeEmailAddress(String email) {
-        waitForElementByElement(signupEmail);
-        signupEmail.sendKeys(email);
-        return this;
-    }
+  public MobileSignupPageObject typeEmailAddress(String email) {
+    waitForElementByElement(signupEmail);
+    signupEmail.sendKeys(email);
+    return this;
+  }
 
-    public MobileSignupPageObject typeUsername(String username) {
-        waitForElementByElement(signupUsername);
-        signupUsername.sendKeys(username);
-        return this;
-    }
+  public MobileSignupPageObject typeUsername(String username) {
+    waitForElementByElement(signupUsername);
+    signupUsername.sendKeys(username);
+    return this;
+  }
 
-    public MobileSignupPageObject typePassword(String password) {
-        waitForElementByElement(signupPassword);
-        signupPassword.sendKeys(password);
-        return this;
-    }
+  public MobileSignupPageObject typePassword(String password) {
+    waitForElementByElement(signupPassword);
+    signupPassword.sendKeys(password);
+    return this;
+  }
 
-    public MobileSignupPageObject typeBirthdate(String month, String day, String year) {
-        waitForElementByElement(signupBirthdate);
-        scrollAndClick((signupBirthdate));
+  public MobileSignupPageObject typeBirthdate(String month, String day, String year) {
+    waitForElementByElement(signupBirthdate);
+    scrollAndClick((signupBirthdate));
 
-        waitForElementByElement(signupBirthMonth);
-        scrollAndClick((signupBirthMonth));
-        signupBirthMonth.sendKeys(month);
+    waitForElementByElement(signupBirthMonth);
+    scrollAndClick((signupBirthMonth));
+    signupBirthMonth.sendKeys(month);
 
-        waitForElementByElement(signupBirthDay);
-        signupBirthDay.click();
-        signupBirthDay.sendKeys(day);
+    waitForElementByElement(signupBirthDay);
+    signupBirthDay.click();
+    signupBirthDay.sendKeys(day);
 
-        waitForElementByElement(signupBirthYear);
-        signupBirthYear.click();
-        signupBirthYear.sendKeys(year);
+    waitForElementByElement(signupBirthYear);
+    signupBirthYear.click();
+    signupBirthYear.sendKeys(year);
 
-        return this;
-    }
+    return this;
+  }
 
-    public void register() {
-        waitForElementByElement(signupButton);
-        scrollAndClick(signupButton);
-    }
+  public void register() {
+    waitForElementByElement(signupButton);
+    scrollAndClick(signupButton);
+  }
 
-    public void verifyAvatarAfterSignup() {
-        waitForElementByElement(avatar);
-        Assertion.assertTrue(avatar.isDisplayed());
-    }
+  public void verifyAvatarAfterSignup() {
+    waitForElementByElement(avatar);
+    Assertion.assertTrue(avatar.isDisplayed());
+  }
 
 }
--- src/test/java/com/wikia/webdriver/testcases/mobile/MobileSignupTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mobile/MobileSignupTests.java
@@ -1,27 +1,27 @@
 package com.wikia.webdriver.testcases.mobile;
 
-import com.wikia.webdriver.common.properties.Credentials;
+import org.testng.annotations.Test;
+
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.mobile.MobileBasePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.mobile.MobileSignupPageObject;
-import org.testng.annotations.Test;
 
 /**
  * Created by rcunningham on 6/30/15.
  */
-public class MobileSignupTests extends NewTestTemplate{
+@Test(groups = {"MobileSignup", "Mobile"})
+public class MobileSignupTests extends NewTestTemplate {
 
-    Credentials credentials = config.getCredentials();
+  @Test(groups = {"MobileSignup_001"})
+  public void successfulSignup() {
 
-    @Test(groups = {"MobileSignup_001", "MobileSignup", "Mobile"})
-    public void successfulSignup() {
-
-        MobileSignupPageObject mobileSignup = new MobileBasePageObject(driver).openMobileSignupPage(wikiURL);
-        String userName = "User" + mobileSignup.getTimeStamp();
-        String password = "Pass" + mobileSignup.getTimeStamp();
-        String email = "qaart001+" + mobileSignup.getTimeStamp() + "@gmail.com";
-        mobileSignup.typeEmailAddress(email).typeUsername(userName).typePassword(password).typeBirthdate("12", "12", "1952").register();
-        mobileSignup.verifyAvatarAfterSignup();
-
-    }
+    MobileSignupPageObject mobileSignup =
+        new MobileBasePageObject(driver).openMobileSignupPage(wikiURL);
+    String userName = "User" + mobileSignup.getTimeStamp();
+    String password = "Pass" + mobileSignup.getTimeStamp();
+    String email = "qaart001+" + mobileSignup.getTimeStamp() + "@gmail.com";
+    mobileSignup.typeEmailAddress(email).typeUsername(userName).typePassword(password)
+        .typeBirthdate("12", "12", "1952").register();
+    mobileSignup.verifyAvatarAfterSignup();
+  }
 }
