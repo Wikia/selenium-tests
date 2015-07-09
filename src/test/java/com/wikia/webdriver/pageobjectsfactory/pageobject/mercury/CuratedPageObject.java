a11394333279692e74d0de8cfb4464efffaa32c9
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-07-09 12:14:47 +0200

    add mercury cct01

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/mercury/CuratedPageObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/mercury/CuratedPageObject.java
@@ -1,15 +1,129 @@
 package com.wikia.webdriver.pageobjectsfactory.pageobject.mercury;
 
+import com.wikia.webdriver.common.core.urlbuilder.UrlBuilder;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * @ownership: Content X-Wing
  */
 public class CuratedPageObject extends BasePageObject {
 
-  //Place for FindBy
+  @FindBy(css = ".mobile-top-leaderboard")
+  private WebElement mobileTopLeaderboard;
+  @FindBy(css = ".article-title")
+  private WebElement articleTitle;
+  @FindBy(css = ".wikia-stats-container")
+  private WebElement wikiaStatsContainer;
+  @FindBy(css = ".featured-content")
+  private WebElement featuredContent;
+  @FindBy(css = ".curated-content")
+  private WebElement curatedContent;
+  @FindBy(css = ".mobile-in-content")
+  private WebElement mobileInContent;
+  @FindBy(css = ".trending-articles")
+  private WebElement trendingArticles;
+  @FindBy(css = ".trending-videos")
+  private WebElement trendingVideos;
+  @FindBy(css = ".mobile-prefooter")
+  private WebElement mobilePrefooter;
 
   public CuratedPageObject(WebDriver driver) {
     super(driver);
+  }
+
+  public boolean isMobileTopLeaderboardVisible() {
+    try {
+      waitForElementVisibleByElement(mobileTopLeaderboard, 5, 1000);
+    } catch (TimeoutException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isArticleTitleVisible() {
+    try {
+      waitForElementVisibleByElement(articleTitle, 5, 1000);
+    } catch (TimeoutException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isWikiaStatsContainerVisible() {
+    try {
+      waitForElementVisibleByElement(wikiaStatsContainer, 5, 1000);
+    } catch (TimeoutException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isFeaturedContentVisible() {
+    try {
+      waitForElementVisibleByElement(featuredContent, 5, 1000);
+    } catch (TimeoutException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isCuratedContentVisible() {
+    try {
+      waitForElementVisibleByElement(curatedContent, 5, 1000);
+    } catch (TimeoutException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isMobileInContentVisible() {
+    try {
+      waitForElementVisibleByElement(mobileInContent, 5, 1000);
+    } catch (TimeoutException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isTrendingArticlesVisible() {
+    try {
+      waitForElementVisibleByElement(trendingArticles, 5, 1000);
+    } catch (TimeoutException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isTrendingVideosVisible() {
+    try {
+      waitForElementVisibleByElement(trendingVideos, 5, 1000);
+    } catch (TimeoutException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isMobilePrefooterVisible() {
+    try {
+      waitForElementVisibleByElement(mobilePrefooter, 5, 1000);
+    } catch (TimeoutException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isUrlPathEqualTo(String path) {
+    String currentPath = new UrlBuilder().getUrlPath(driver);
+    return currentPath.equals(path);
+  }
+
+  public int getElementHeight(String element) {
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    return Integer.parseInt(js.executeScript("return $('" + element + "').offset().top").toString());
   }
 }
--- src/test/java/com/wikia/webdriver/testcases/mercurytests/CuratedMainPageTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mercurytests/CuratedMainPageTests.java
@@ -1,6 +1,7 @@
 package com.wikia.webdriver.testcases.mercurytests;
 
 import com.wikia.webdriver.common.contentpatterns.MercuryArticles;
+import com.wikia.webdriver.common.logging.PageObjectLogging;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.BasePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.CuratedPageObject;
@@ -20,13 +21,57 @@ public class CuratedMainPageTests extends NewTestTemplate {
     driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
   }
 
+  private static final String ROOT_PATH = "/";
+  private static final String SECTION_PATH = "/main/section/";
+  private static final String CATEGORY_PATH = "/main/category/";
+
+  private static final String[]
+      ELEMENTS_ORDER =
+      {".mobile-top-leaderboard", ".article-title", ".wikia-stats-container", ".featured-content",
+       ".curated-content", ".mobile-in-content", ".trending-articles", ".trending-videos",
+       ".mobile-prefooter"};
+
+
   // CCT01
   @Test(groups = {"MercuryCuratedMainPageTests_001", "MercuryCuratedMainPageTests", "Mercury"})
   public void MercuryCuratedMainPageTests_001_Description() {
     BasePageObject base = new BasePageObject(driver);
     base.openMercuryArticleByName(wikiURL, MercuryArticles.CC_MAIN_PAGE);
     CuratedPageObject cc = new CuratedPageObject(driver);
+    PageObjectLogging
+        .log("Current URL", "is set on " + ROOT_PATH, "is not set on " + ROOT_PATH,
+             cc.isUrlPathEqualTo(ROOT_PATH));
+    PageObjectLogging
+        .log("AD Mobile Top Leaderboard", "is visible", "is not visible",
+             cc.isMobileTopLeaderboardVisible());
+    PageObjectLogging
+        .log("Article Title", "is visible", "is not visible", cc.isArticleTitleVisible());
+    PageObjectLogging
+        .log("Wikia Stats Container", "is visible", "is not visible",
+             cc.isWikiaStatsContainerVisible());
+    PageObjectLogging
+        .log("Featured Content", "is visible", "is not visible", cc.isFeaturedContentVisible());
+    PageObjectLogging
+        .log("Curated Content", "is visible", "is not visible", cc.isCuratedContentVisible());
+    PageObjectLogging
+        .log("AD Mobile In Content", "is visible", "is not visible", cc.isMobileInContentVisible());
+    PageObjectLogging
+        .log("Trending Articles", "is visible", "is not visible", cc.isTrendingArticlesVisible());
+    PageObjectLogging
+        .log("Trending Videos", "is visible", "is not visible", cc.isTrendingVideosVisible());
+    PageObjectLogging
+        .log("AD Mobile Prefooter", "is visible", "is not visible", cc.isMobilePrefooterVisible());
+
+    int lastPosition = 0;
+    String lastElement = "top";
+
+    for (String element : ELEMENTS_ORDER) {
+      int newPosition = cc.getElementHeight(element);
+      PageObjectLogging
+          .log("Element " + element, "is after " + lastElement, "is not after " + lastElement,
+               lastPosition <= newPosition);
+      lastPosition = newPosition;
+      lastElement = element;
+    }
   }
-
-
 }
