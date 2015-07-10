ddb4db569cb37266a6d3f10de9800ef536c498e6
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-07-10 18:58:48 +0200

    code refactor

--- src/test/java/com/wikia/webdriver/testcases/mercurytests/SmartBannerTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mercurytests/SmartBannerTests.java
@@ -5,7 +5,6 @@ import com.wikia.webdriver.common.core.configuration.Configuration;
 import com.wikia.webdriver.common.logging.PageObjectLogging;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.componentobject.mercury.SmartBannerComponentObject;
-import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.BasePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.PerformTouchAction;
 
 import org.testng.annotations.BeforeMethod;
@@ -46,10 +45,10 @@ public class SmartBannerTests extends NewTestTemplate {
    */
   private static final String[][] WIKIS = {
       {"destiny", "Destiny_Wiki", Colors.LIGHT_GREEN.hex},
+      {"starwars", "Main_Page", Colors.CYAN.hex},
       {"thehungergames", "The_Hunger_Games_Wiki", Colors.LIGHT_ORANGE.hex},
       {"dc", "Main_Page", Colors.DARK_ORANGE.hex},
       {"tardis", "Doctor_Who_Wiki", Colors.LIGHT_BLUE.hex},
-      {"starwars", "Main_Page", Colors.CYAN.hex},
       {"lego", "LEGO_Wiki", Colors.YELLOW.hex},
       {"ladygaga", "Gagapedia", Colors.MAGENTA.hex}
   };
@@ -60,44 +59,74 @@ public class SmartBannerTests extends NewTestTemplate {
   // SBT01
   @Test(groups = {"MercurySmartBannerTest_001", "MercurySmartBannerTests", "Mercury"})
   public void MercurySmartBannerTest_001_ButtonName_FixPosition_Close() {
-    wikiURL = urlBuilder.getUrlForWiki(WIKIS[0][0]);
-    BasePageObject base = new BasePageObject(driver);
-    base.openMercuryArticleByName(wikiURL, WIKIS[0][1]);
     SmartBannerComponentObject banner = new SmartBannerComponentObject(driver);
+    wikiURL = urlBuilder.getUrlForWiki(WIKIS[0][0]);
+    banner.openMercuryArticleByName(wikiURL, WIKIS[0][1]);
     PerformTouchAction touchAction = new PerformTouchAction(driver);
+
     String buttonName;
-    Assertion.assertTrue(banner.isSmartBannerVisible(), "Smart banner is closed");
+
+    Assertion.assertTrue(
+        banner.isSmartBannerVisible(),
+        "Smart banner is closed"
+    );
+
     if (Configuration.getPlatform().equals("ANDROID")) {
       buttonName = BUTTON_NAME_FOR_ANDROID;
     } else {
       buttonName = BUTTON_NAME_FOR_IOS;
     }
-    PageObjectLogging.log("Button name", "is correct", "is incorrect",
-                          banner.getButtonName().equals(buttonName));
+
+    PageObjectLogging.log(
+        "Button name",
+        "is correct",
+        "is incorrect",
+        banner.getButtonName().equals(buttonName)
+    );
+
     int lastSmartBannerPosition = banner.getSmartBannerPosition();
     touchAction.swipeFromPointToPoint(50, 90, 50, 40, 500, 3000);
-    PageObjectLogging.log("Position", "is fixed", "is floated",
-                          lastSmartBannerPosition == banner.getSmartBannerPosition());
+
+    PageObjectLogging.log(
+        "Position",
+        "is fixed",
+        "is floated",
+        lastSmartBannerPosition == banner.getSmartBannerPosition()
+    );
+
     banner.scrollToTopAndWaitForShareBarToBeHidden();
     banner.clickCloseButton();
-    PageObjectLogging
-        .log("Smart banner", "is closed", "is visible", !banner.isSmartBannerVisible());
+
+    PageObjectLogging.log(
+        "Smart banner",
+        "is closed",
+        "is visible",
+        !banner.isSmartBannerVisible()
+    );
   }
 
   // SBT02
   @Test(groups = {"MercurySmartBannerTest_002", "MercurySmartBannerTests", "Mercury"})
   public void MercurySmartBannerTest_002_ThemeColorOnDifferentHubs() {
-    BasePageObject base = new BasePageObject(driver);
-    SmartBannerComponentObject banner;
+    SmartBannerComponentObject banner = new SmartBannerComponentObject(driver);;
+
     for (String[] WIKI : WIKIS) {
       wikiURL = urlBuilder.getUrlForWiki(WIKI[0]);
-      base.openMercuryArticleByName(wikiURL, WIKI[1]);
-      banner = new SmartBannerComponentObject(driver);
-      PageObjectLogging
-          .log("Smart banner color", "is correct", "is wrong", banner.isSmartBannerColorCorrect(
-              WIKI[2]));
-      PageObjectLogging.log("Smart banner button color", "is correct", "is wrong",
-                            banner.isSmartBannerButtonColorCorrect(WIKI[2]));
+      banner.openMercuryArticleByName(wikiURL, WIKI[1]);
+
+      PageObjectLogging.log(
+          "Smart banner color",
+          "is correct",
+          "is wrong",
+          banner.isSmartBannerColorCorrect(WIKI[2])
+      );
+
+      PageObjectLogging.log(
+          "Smart banner button color",
+          "is correct",
+          "is wrong",
+          banner.isSmartBannerButtonColorCorrect(WIKI[2])
+      );
     }
   }
 }
