2116e9d818fe8e64d2ddba288a0dc1394f9d8925
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-07-13 14:27:30 +0200

    Sonar fixes for MailFunctions

--- src/test/java/com/wikia/webdriver/common/core/CommonUtils.java
+++ src/test/java/com/wikia/webdriver/common/core/CommonUtils.java
@@ -2,10 +2,8 @@ package com.wikia.webdriver.common.core;
 
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -28,30 +26,16 @@ import com.wikia.webdriver.common.logging.PageObjectLogging;
 
 public class CommonUtils {
 
-  private CommonUtils() {}
+  private CommonUtils() {
+
+  }
 
   /**
    * appends given text to specified file
-   *
-   * @author Karol Kujawiak
    */
   public static void setClipboardContents(String content) {
     StringSelection ss = new StringSelection(content);
     Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
-  }
-
-  public static List<String> getLinesInFile(String pathToFile) {
-    List<String> list = new ArrayList<String>();
-    try {
-      BufferedReader br = new BufferedReader(new FileReader(pathToFile));
-      String line;
-      while ((line = br.readLine()) != null) {
-        list.add(line);
-      }
-    } catch (IOException e) {
-
-    }
-    return list;
   }
 
   public static void appendTextToFile(String filePath, String textToWrite) {
@@ -62,29 +46,24 @@ public class CommonUtils {
       out.newLine();
       out.flush();
       out.close();
-    } catch (Exception e) {
-      System.out.println("ERROR in saveTextToFile(2 args) in CommonUtils.java \n" + e.getMessage());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 
   /**
    * delete directory by path
-   *
-   * @author Karol Kujawiak
    */
   public static void deleteDirectory(String dirName) {
     try {
       FileUtils.deleteDirectory(new File(dirName));
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       PageObjectLogging.log("deleteDirectory", e.getMessage(), false);
     }
   }
 
   /**
    * creates directory based on given path
-   *
-   * @author Karol Kujawiak
    */
   public static void createDirectory(String fileName) {
     try {
