62911473f1da3767550ec1ffaf76f815a1612d74
archbold <patrick@wikia-inc.com>
2015-07-17 12:45:39 +0200

    removed related issue ID

--- src/test/java/com/wikia/webdriver/testcases/visualeditor/VEAddVideoTests.java
+++ src/test/java/com/wikia/webdriver/testcases/visualeditor/VEAddVideoTests.java
@@ -62,7 +62,6 @@ public class VEAddVideoTests extends NewTestTemplateBeforeClass {
   @Test(
       groups = {"VEAddVideo", "VEAddExternalVideoTests_002", "VEAddExternalVideo"}
   )
-  @RelatedIssue(issueID = "OPS-6181", comment = "The related OPS issue is purely sandbox related. If the test fails on production it needs verification")
   public void VEAddExternalVideoTests_002_AddPremiumVid() {
     String randomArticleName = PageContent.ARTICLE_NAME_PREFIX + base.getTimeStamp();
     VisualEditorPageObject ve = base.openVEOnArticle(wikiURL, randomArticleName);
