c68206c60d77d60767f0c5ab63d57aad1df52276
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-07-20 17:56:13 +0200

    move tests to proper groups

--- src/test/java/com/wikia/webdriver/testcases/wikiamobiletests/GameGuidesPreview.java
+++ src/test/java/com/wikia/webdriver/testcases/wikiamobiletests/GameGuidesPreview.java
@@ -1,4 +1,4 @@
-package com.wikia.webdriver.testcases.mobile;
+package com.wikia.webdriver.testcases.wikiamobiletests;
 
 import com.wikia.webdriver.common.contentpatterns.URLsContent;
 import com.wikia.webdriver.common.core.Assertion;
