169b456545d54ef3bb8665c6e992bcd889c81165
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-08-19 16:21:16 +0200

    Category tests fixes

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/article/ArticlePageObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/article/ArticlePageObject.java
@@ -605,7 +605,7 @@ public class ArticlePageObject extends WikiBasePageObject {
     wait.forElementVisible(categorySuggestionsList);
     WebElement desiredCategory = categorySuggestionsListItems.get(categoryNumber);
     String desiredCategoryText = desiredCategory.getText();
-    scrollAndClick(desiredCategory);
+    scrollAndClick(categorySuggestionsListItems.get(categoryNumber));
     waitForElementNotVisibleByElement(categorySuggestionsList);
     PageObjectLogging.log("addCategorySuggestions", "category " + category
         + " added from suggestions", true);
