815158b45b8cb6104c85a20a3de11e6bbf6571ac
justnpT <Michal.Nowierski@wikia-inc.com>
2015-08-20 14:55:01 +0200

    organizing imports

--- src/test/java/com/wikia/webdriver/testcases/oasismainpage/CuratedContentTool.java
+++ src/test/java/com/wikia/webdriver/testcases/oasismainpage/CuratedContentTool.java
@@ -5,8 +5,6 @@ import com.wikia.webdriver.common.contentpatterns.URLsContent;
 import com.wikia.webdriver.common.core.annotations.CreationTicket;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.User;
-import com.wikia.webdriver.common.core.configuration.Configuration;
-import com.wikia.webdriver.common.properties.Credentials;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.componentobject.modalwindows.curatedContentToolModal;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.oasis.MainPage;
