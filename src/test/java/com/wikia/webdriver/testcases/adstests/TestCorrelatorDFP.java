1d73b70a1498fcf6bdbbc098ca007878b83062a2
PiotrGackowski <piotr.gackowski@wikia-inc.com>
2015-08-25 15:38:55 +0200

    ADEN-2364 mobile version of correlator

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestCorrelatorDFP.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestCorrelatorDFP.java
@@ -12,12 +12,24 @@ import com.wikia.webdriver.pageobjectsfactory.pageobject.HomePageObject;
  */
 public class TestCorrelatorDFP extends TemplateNoFirstLoad {
 
-  @Test(groups = "DFPCorrelator")
+  String correlatorDesktop = "_adtest,home,gpt";
+  String correlatorMobile = "_adtest,home,mobile";
+
+  @Test(groups = "DFPCorrelatorOasis")
   @NetworkTrafficDump
   @Execute(onWikia = "adtest")
-  public void correlatorIDInQueryStringShouldBeTheSame() {
+  public void correlatorIDInQueryStringShouldBeTheSameOasis() {
     networkTrafficInterceptor.startIntercepting("DFPCorrelator");
     new HomePageObject(driver).open();
-    networkTrafficInterceptor.logDFP();
+    networkTrafficInterceptor.logDFP(correlatorDesktop);
+  }
+
+  @Test(groups = "DFPCorrelatorMercury")
+  @NetworkTrafficDump
+  @Execute(onWikia = "adtest")
+  public void correlatorIDInQueryStringShouldBeTheSameMercury() {
+    networkTrafficInterceptor.startIntercepting("DFPCorrelator");
+    new HomePageObject(driver).open();
+    networkTrafficInterceptor.logDFP(correlatorMobile);
   }
 }
