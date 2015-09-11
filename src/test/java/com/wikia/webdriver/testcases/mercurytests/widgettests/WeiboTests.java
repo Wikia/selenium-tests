c5307a27ddcdb6d11c4b7ea788e10f3f9ebeaa22
justnpT <Michal.Nowierski@wikia-inc.com>
2015-09-11 16:28:34 +0200

    adding ownership

--- src/test/java/com/wikia/webdriver/testcases/mercurytests/widgettests/WeiboTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mercurytests/widgettests/WeiboTests.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by wikia on 2015-09-11.
+ * @ownership: Content X-Wing
  */
 public class WeiboTests extends NewTestTemplate {
 
