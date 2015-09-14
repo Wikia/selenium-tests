dc75f876064480437ab48ec12ab123fb69de60e7
justnpT <Michal.Nowierski@wikia-inc.com>
2015-09-14 19:29:22 +0200

    adding tests for pollsnack

---
+++ src/test/java/com/wikia/webdriver/testcases/widgettests/PollsnackTests.java
@@ -0,0 +1,24 @@
+package com.wikia.webdriver.testcases.widgettests;
+
+import com.wikia.webdriver.common.contentpatterns.MercuryMessages;
+import com.wikia.webdriver.common.core.Assertion;
+import com.wikia.webdriver.common.core.annotations.Execute;
+import com.wikia.webdriver.common.templates.NewTestTemplate;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.PollsnackWidgetPageObject;
+
+import org.testng.annotations.Test;
+
+/**
+ * @ownership: Content X-Wing
+ */
+public class PollsnackTests extends NewTestTemplate {
+
+  @Test
+  @Execute(onWikia = "mercuryautomationtesting")
+  public void PollsnackWidgetTest_001_isLoaded() {
+    PollsnackWidgetPageObject pollsnackWidget = new PollsnackWidgetPageObject(driver);
+
+    pollsnackWidget.createAndNavigate(wikiURL);
+    Assertion.assertTrue(pollsnackWidget.isLoadedOnOasis(), MercuryMessages.INVISIBLE_MSG);
+  }
+}
