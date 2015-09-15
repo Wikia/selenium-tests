d7c33105024ed04b8a455bf82d1b4f944d64b075
Warkot <mateusz.warkocki@wikia-inc.com>
2015-09-15 18:02:08 +0200

    XW-306 | remove unused import

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/widget/WidgetPageObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/widget/WidgetPageObject.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.List;
-
 /**
  * @ownership: Content X-Wing
  */
