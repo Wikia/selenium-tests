f6aa448f3d927418c9a59a326d9b57325def5600
V <bartosz.bentkowski@gmail.com>
2015-09-16 13:34:16 +0200

    XW-206 | Add GoogleForm to AllTests

--- src/test/java/com/wikia/webdriver/testcases/widgettests/AllTagsTests.java
+++ src/test/java/com/wikia/webdriver/testcases/widgettests/AllTagsTests.java
@@ -6,13 +6,7 @@ import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.api.ArticleContent;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.mercury.ArticlePageObject;
-import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.PollsnackWidgetPageObject;
-import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.SoundCloudWidgetPageObject;
-import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.SpotifyWidgetPageObject;
-import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.TwitterWidgetPageObject;
-import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.VKWidgetPageObject;
-import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.WeiboWidgetPageObject;
-import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.WidgetPageObject;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.*;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -39,6 +33,7 @@ public class AllTagsTests extends NewTestTemplate {
     widgets.add(new TwitterWidgetPageObject(driver));
     widgets.add(new VKWidgetPageObject(driver));
     widgets.add(new WeiboWidgetPageObject(driver));
+    widgets.add(new GoogleFormWidgetPageObject(driver));
 
     String content = "";
     for (WidgetPageObject widget : widgets) {
