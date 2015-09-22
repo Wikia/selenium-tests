e66ca0cdb7ff2974d6aac56a78e660c6906043da
drets <dmitryrets@gmail.com>
2015-09-22 15:51:05 +0300

    ADEN-2449 Use right iframe name

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsBaseObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsBaseObject.java
@@ -618,7 +618,8 @@ public class AdsBaseObject extends WikiBasePageObject {
   }
 
   private boolean checkIfGptSlotHasCreativeContent(WebElement element, String hopAdType) {
-    String iframeSelector = "iframe[id*='" + element.getAttribute("id") + "_0']";
+    String slotName = element.getAttribute("id").replace("wikia_gpt","google_ads_iframe_");
+    String iframeSelector = "iframe[id*='" + slotName + "_0']";
     String adTypeScriptXpath = String.format("//script[contains(text(), '%s')]", hopAdType);
     WebElement iframe = element.findElement(By.cssSelector(iframeSelector));
     driver.switchTo().frame(iframe);
