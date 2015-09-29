32d0d8f602c14a6015a90d7a747e0b501787aa4f
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2015-09-29 18:00:28 +0200

    reformat

--- src/test/java/com/wikia/webdriver/common/contentpatterns/MercuryMessages.java
+++ src/test/java/com/wikia/webdriver/common/contentpatterns/MercuryMessages.java
@@ -1,6 +1,7 @@
 package com.wikia.webdriver.common.contentpatterns;
 
 public class MercuryMessages {
+
   public static final String VISIBLE_MSG = "element is visible";
   public static final String INVISIBLE_MSG = "element is not visible";
 
@@ -8,7 +9,9 @@ public class MercuryMessages {
   public static final String EXPANDED_MSG = "element is not expanded";
 
   public static final String LIST_ITEMS_ARE_UNIQUE_MSG = "all list elements are unique";
-  public static final String LIST_ITEMS_ARE_NOT_UNIQUE_MSG = "there are at least two elements with the same name";
+  public static final String
+      LIST_ITEMS_ARE_NOT_UNIQUE_MSG =
+      "there are at least two elements with the same name";
 
   private MercuryMessages() {
   }
