09e304656211cd8e08a7f8b977d84be468e59cad
drets <dmitryrets@gmail.com>
2015-09-30 14:18:27 +0300

    ADEN-2494 Check browser errors for scroll handler feature

---
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsBrowserError.java
@@ -0,0 +1,47 @@
+package com.wikia.webdriver.testcases.adstests;
+
+import com.wikia.webdriver.common.core.Assertion;
+import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+/**
+ * @ownership AdEng
+ */
+public class TestAdsBrowserError extends TemplateNoFirstLoad {
+
+  @Test(groups = "AdsScrollHandlerBrowserErrorMercury")
+  public void adsScrollHandlerBrowserErrorMercury() throws InterruptedException {
+    String testPage = urlBuilder.getUrlForPath("adtest", "Wikia_Ad_Testing");
+    testPage = urlBuilder.appendQueryStringToURL(testPage, "scrollhandler=1");
+    AdsBaseObject adsBaseObject = new AdsBaseObject(driver);
+    adsBaseObject.getUrl(testPage);
+
+    executeScript(addErrorListenerScript());
+    executeScript(scroll());
+
+    adsBaseObject.waitForJavaScriptTruthy("window.scrollY != 0");
+    Assertion.assertEquals(executeScript(getErrors()), "");
+  }
+
+  private String addErrorListenerScript() {
+    return "var script = document.createElement('script'); "
+           + "script.innerHTML = 'window.onerror = "
+           + "function (e, u, l, c, errorObj) { window.errors = errorObj.stack }';"
+           + "document.querySelector('body').appendChild(script);";
+  }
+
+  private String scroll() {
+    return "setTimeout(function() { window.scroll(0, 5001); }, 2000);";
+  }
+
+  private String getErrors() {
+    return "return window.errors || ''";
+  }
+
+  private String executeScript(String script) {
+    return (String) ((JavascriptExecutor) driver).executeScript(script);
+  }
+}
