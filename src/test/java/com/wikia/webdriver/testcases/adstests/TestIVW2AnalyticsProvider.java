826171bda6b20bb3cb0c560960189b9ee944e510
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-10-01 19:15:01 +0200

    changed IVW2 tests to use a proper browser

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestIVW2AnalyticsProvider.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestIVW2AnalyticsProvider.java
@@ -5,7 +5,6 @@ import com.wikia.webdriver.common.dataprovider.ads.GermanAdsDataProvider;
 import com.wikia.webdriver.common.logging.PageObjectLogging;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 
-import org.jsoup.Jsoup;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -15,8 +14,6 @@ import java.io.IOException;
  */
 public class TestIVW2AnalyticsProvider extends TemplateNoFirstLoad {
 
-  private static final int TIMEOUT_SEC = 60;
-
   @Test(groups = "TestIVW2AnalyticsProvider")
   public void testIVW2AnalyticsProvider() throws IOException {
     for (Object[] data : GermanAdsDataProvider.IVW2_TEST_DATA) {
@@ -24,7 +21,8 @@ public class TestIVW2AnalyticsProvider extends TemplateNoFirstLoad {
       String article = (String) data[1];
       String ivw2Param = (String) data[2];
       String testedPage = urlBuilder.getUrlForPath(wikiName, article);
-      String htmlSource = Jsoup.connect(testedPage).timeout(TIMEOUT_SEC * 1000).get().html();
+      driver.get(testedPage);
+      String htmlSource = driver.getPageSource();
       Assertion.assertTrue(htmlSource.contains(ivw2Param));
       PageObjectLogging.log("IVW2", ivw2Param + " param is on the " + testedPage, true);
     }
