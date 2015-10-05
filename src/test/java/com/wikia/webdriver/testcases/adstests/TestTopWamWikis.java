f702344793880bceab187b2f0a099a7574a4a7a3
drets <dmitryrets@gmail.com>
2015-10-05 12:18:43 +0300

    ADEN-2195 Remove TestTopWamWikis constructor

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestTopWamWikis.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestTopWamWikis.java
@@ -2,7 +2,6 @@ package com.wikia.webdriver.testcases.adstests;
 
 import com.wikia.webdriver.common.contentpatterns.URLsContent;
 import com.wikia.webdriver.common.core.configuration.Configuration;
-import com.wikia.webdriver.common.core.url.UrlBuilder;
 import com.wikia.webdriver.common.properties.Credentials;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.WikiBasePageObject;
@@ -14,11 +13,6 @@ public class TestTopWamWikis extends TemplateNoFirstLoad {
   Credentials credentials = Configuration.getCredentials();
   Integer numberOfTop1kWikis = 998;
   String extensionURL = "var=1429&searchType=bool&val=2&likeValue=true";
-
-  public TestTopWamWikis() {
-    super();
-    urlBuilder = new UrlBuilder(Configuration.getEnv());
-  }
 
   @Test(groups = {"TopWamWikisWhereIsMyExtension"})
   public void TopWamWikisWhereIsMyExtension_GeoEdgeFree() {
