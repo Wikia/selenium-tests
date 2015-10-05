88e85c8ed94378c65e90048f79074c576ebe29e4
drets <dmitryrets@gmail.com>
2015-10-05 13:14:06 +0300

    ADEN-2195 Remove Factory method from Test71MediaAds

--- src/test/java/com/wikia/webdriver/testcases/adstests/Test71MediaAds.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/Test71MediaAds.java
@@ -5,7 +5,6 @@ import com.wikia.webdriver.common.dataprovider.ads.GermanAdsDataProvider;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsGermanObject;
 
-import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
 
 /**
@@ -14,33 +13,38 @@ import org.testng.annotations.Test;
 public class Test71MediaAds extends TemplateNoFirstLoad {
 
   private static final String MEDIA_71_FORCE_RESPONSE = "showroom=billboard&subsite=ingrid";
-  private String testedPage;
-
-  @Factory(
-      dataProviderClass = GermanAdsDataProvider.class,
-      dataProvider = "popularGermanArticles"
-  )
-  public Test71MediaAds(String wikiName, String path) {
-    super();
-    testedPage = urlBuilder.getUrlForPath(wikiName, path);
-  }
 
   @GeoEdgeBrowserMobProxy(country = "DE")
-  @Test(groups = {"Ads", "Test71MediaAds_DE", "Ads71Media"})
-  public void Test71MediaAds_DE() {
+  @Test(
+      dataProviderClass = GermanAdsDataProvider.class,
+      dataProvider = "popularGermanArticles",
+      groups = {"Ads", "Test71MediaAds_DE", "Ads71Media"}
+  )
+  public void Test71MediaAds_DE(String wikiName, String path) {
+    String testedPage = urlBuilder.getUrlForPath(wikiName, path);
     AdsGermanObject ads71Media = new AdsGermanObject(driver, testedPage);
     ads71Media.verify71MediaAdsPresent();
   }
 
   @GeoEdgeBrowserMobProxy(country = "AU")
-  @Test(groups = {"Ads", "Test71MediaAds_AU", "Ads71Media"})
-  public void Test71MediaAds_AU() {
+  @Test(
+      dataProviderClass = GermanAdsDataProvider.class,
+      dataProvider = "popularGermanArticles",
+      groups = {"Ads", "Test71MediaAds_AU", "Ads71Media"}
+  )
+  public void Test71MediaAds_AU(String wikiName, String path) {
+    String testedPage = urlBuilder.getUrlForPath(wikiName, path);
     AdsGermanObject ads71Media = new AdsGermanObject(driver, testedPage);
     ads71Media.verify71MediaAdsPresent();
   }
 
-  @Test(groups = {"Ads", "Test71MediaAds_GeoEdgeFree", "Ads71Media"})
-  public void Test71MediaAds_GeoEdgeFree() {
+  @Test(
+      dataProviderClass = GermanAdsDataProvider.class,
+      dataProvider = "popularGermanArticles",
+      groups = {"Ads", "Test71MediaAds_GeoEdgeFree", "Ads71Media"}
+  )
+  public void Test71MediaAds_GeoEdgeFree(String wikiName, String path) {
+    String testedPage = urlBuilder.getUrlForPath(wikiName, path);
     String
         testedPage71Media =
         urlBuilder.appendQueryStringToURL(testedPage, MEDIA_71_FORCE_RESPONSE);
