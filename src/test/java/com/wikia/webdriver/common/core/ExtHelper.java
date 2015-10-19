a736c8a39613c3896fb346b746f320abe8fc654b
drets <dmitryrets@gmail.com>
2015-10-19 12:59:03 +0300

    aden-2538 CR fix: wrap for

--- src/test/java/com/wikia/webdriver/common/core/ExtHelper.java
+++ src/test/java/com/wikia/webdriver/common/core/ExtHelper.java
@@ -52,11 +52,13 @@ public class ExtHelper {
                                "extensions");
     String fullName = name + "." + suffix;
     Collection<File> extFiles = FileUtils.listFiles(extensions, new String[]{suffix}, true);
+
     for (File extFile : extFiles) {
       if (extFile.getName().equals(fullName)) {
         return extFile;
       }
     }
+
     throw new WebDriverException(
         String.format("Can't find '%s' extension in '%s'", fullName, extensions.getPath()));
   }
