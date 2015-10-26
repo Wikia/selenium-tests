4d954fe7fb468fee5bfb137ddb6261af4d6a0fe8
drets <dmitryrets@gmail.com>
2015-10-26 17:44:55 +0200

    ADEN-2566 Log on failure

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestIVW2AnalyticsProvider.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestIVW2AnalyticsProvider.java
@@ -1,6 +1,5 @@
 package com.wikia.webdriver.testcases.adstests;
 
-import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.dataprovider.ads.GermanAdsDataProvider;
 import com.wikia.webdriver.common.logging.PageObjectLogging;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
@@ -20,11 +19,17 @@ public class TestIVW2AnalyticsProvider extends TemplateNoFirstLoad {
       String wikiName = (String) data[0];
       String article = (String) data[1];
       String ivw2Param = (String) data[2];
+
       String testedPage = urlBuilder.getUrlForPath(wikiName, article);
       driver.get(testedPage);
       String htmlSource = driver.getPageSource();
-      Assertion.assertTrue(htmlSource.contains(ivw2Param));
-      PageObjectLogging.log("IVW2", ivw2Param + " param is on the " + testedPage, true);
+
+      Boolean isParamOnPage = htmlSource.contains(ivw2Param);
+      PageObjectLogging.log(
+          "IVW2",
+          ivw2Param + " param on the page: " + String.valueOf(isParamOnPage),
+          isParamOnPage
+      );
     }
   }
 }
