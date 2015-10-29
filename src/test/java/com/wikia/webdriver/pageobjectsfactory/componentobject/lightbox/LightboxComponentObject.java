60dfccc64c97e6a5a5f8d2c899b1be9498947dd9
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-10-29 17:39:20 +0100

    Fixes for lightbox video autoplay

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/lightbox/LightboxComponentObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/lightbox/LightboxComponentObject.java
@@ -44,7 +44,7 @@ public class LightboxComponentObject extends WikiBasePageObject {
   private WebElement plusoneShareLink;
   @FindBy(css = ".video-media")
   private WebElement videoContainer;
-  @FindBy(css = ".WikiaLightbox")
+  @FindBy(css = ".LightboxHeader h1 a")
   private WebElement titleLink;
   @FindBy(css = ".more-info-button")
   private WebElement moreInfoLink;
