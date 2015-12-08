5bfffa419a50dde505b043de8bed57c08d546771
archbold <patrick@wikia-inc.com>
2015-12-08 12:50:34 +0100

    Added related issue ID

--- src/test/java/com/wikia/webdriver/testcases/categoriestests/CategoriesTestsEditMode.java
+++ src/test/java/com/wikia/webdriver/testcases/categoriestests/CategoriesTestsEditMode.java
@@ -27,7 +27,7 @@ public class CategoriesTestsEditMode extends NewTestTemplate {
    */
 
   @Test(groups = {"CategoriesTestsArticleEdit_001", "CategoriesTestsArticleEditMode"})
-  @RelatedIssue(issueID = "MAIN-6028", comment = "Test manually: Test may fail until the ticket is fixed as" +
+  @RelatedIssue(issueID = "CE-3160", comment = "Test manually: Test may fail until the ticket is fixed as" +
           " notification is obscuring the feature being tested")
   public void CategoriesTestsArticleEdit_001_anonEdit() {
     WikiBasePageObject base = new WikiBasePageObject(driver);
@@ -44,7 +44,7 @@ public class CategoriesTestsEditMode extends NewTestTemplate {
   }
 
   @Test(groups = {"CategoriesTestsArticleEdit_002", "CategoriesTestsArticleEditMode"})
-  @RelatedIssue(issueID = "MAIN-6028", comment = "Test manually: Test may fail until the ticket is fixed as" +
+  @RelatedIssue(issueID = "CE-3160", comment = "Test manually: Test may fail until the ticket is fixed as" +
           " notification is obscuring the feature being tested")
   public void CategoriesTestsArticleEdit_002_anonDelete() {
     new ArticleContent().clear();
