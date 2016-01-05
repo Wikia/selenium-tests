9d3d171177ad3323fc5bdc602f2bc3e82e83c9ac
justnpT <Michal.Nowierski@wikia-inc.com>
2016-01-05 16:22:00 +0100

    [SOC-1677] adding tests for post details page

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/discussions/PostDetailsPage.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/discussions/PostDetailsPage.java
@@ -12,6 +12,14 @@ public class PostDetailsPage extends WikiBasePageObject{
 
   @FindBy(css = ".replies-list")
   private List<WebElement> repliesList;
+  @FindBy(css = "svg.upvote")
+  private WebElement upvoteButton;
+  @FindBy(css = ".upvote-reply")
+  private List<WebElement> replyUpvoteButton;
+  @FindBy(css = "a.upvote-area")
+  private WebElement upvoteArea;
+  @FindBy(css = ".replies-list small")
+  private List<WebElement> replyVoteCount;
 
   private static final String PATH = "d/p/%s";
   private static final String DEFAULT_POST_ID = "2621148372316194193";
@@ -32,4 +40,47 @@ public class PostDetailsPage extends WikiBasePageObject{
   public boolean isPostDetailsListEmpty() {
     return repliesList.isEmpty();
   }
+
+  public boolean isUpvoteButtonVisible() {
+    wait.forElementVisible(upvoteButton);
+    return upvoteButton.isDisplayed();
+  }
+
+  public boolean isReplyUpvoteButtonVisible(int index) {
+    WebElement button = replyUpvoteButton.get(index);
+    wait.forElementVisible(button);
+    return button.isDisplayed();
+  }
+
+  public void clickPostDetailsUpvoteButton() {
+    wait.forElementClickable(upvoteButton);
+    upvoteButton.click();
+  }
+
+  public void clickReplyUpvoteButton(int replyIndex) {
+    WebElement button = replyUpvoteButton.get(replyIndex);
+    wait.forElementClickable(button);
+    button.click();
+  }
+
+  public String getPostDetailsVoteCount() {
+    wait.forElementVisible(upvoteArea);
+    return upvoteArea.getText();
+  }
+
+  public String getReplyVoteCount(int index) {
+    WebElement voteCountArea = replyVoteCount.get(index);
+    wait.forElementVisible(voteCountArea);
+    return voteCountArea.getText();
+  }
+
+  public void waitForPostDetailsVoteCountToChange(String voteCount) {
+    wait.forTextNotInElement(upvoteArea, voteCount);
+  }
+
+  public void waitForReplyVoteCountToChange(int replyIndex, String voteCount) {
+    WebElement voteCountArea = replyVoteCount.get(replyIndex);
+    wait.forTextNotInElement(voteCountArea, voteCount);
+  }
+
 }
