a58144e2305c562491bf178eb5746a09a24c5c1a
jacek <jacek@wikia-inc.com>
2016-01-27 15:30:01 +0100

    geo

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestIVWAnalyticsProvider.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestIVWAnalyticsProvider.java
@@ -2,12 +2,18 @@ package com.wikia.webdriver.testcases.adstests;
 
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.NetworkTrafficDump;
+import com.wikia.webdriver.common.core.configuration.Configuration;
 import com.wikia.webdriver.common.core.geoedge.CountryCode;
 import com.wikia.webdriver.common.core.geoedge.GeoEdgeBrowserMobProxy;
+import com.wikia.webdriver.common.core.geoedge.GeoEdgeProxy;
 import com.wikia.webdriver.common.dataprovider.ads.GermanAdsDataProvider;
 import com.wikia.webdriver.common.logging.PageObjectLogging;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
+
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 import java.io.IOException;
 
@@ -15,6 +21,26 @@ public class TestIVWAnalyticsProvider extends TemplateNoFirstLoad {
 
   public static final String URL_BASE_SCRIPT = "script.ioam.de/iam.js";
   public static final String URL_TRACKING_SCRIPT = "de.ioam.de/tx.io?st=wikia";
+
+  private String countryCode = Configuration.getCountryCode();
+
+  public TestIVWAnalyticsProvider() {
+    super();
+    if (countryCode != null) {
+      // We need to set the proxy in constructor
+      setProxy(countryCode);
+    }
+  }
+
+  private void setProxy(String countryCode) {
+    String proxyAddress = GeoEdgeProxy.getProxyAddress(countryCode);
+    Proxy proxy = new Proxy();
+    proxy.setHttpProxy(proxyAddress);
+    proxy.setSslProxy(proxyAddress);
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability(CapabilityType.PROXY, proxy);
+    setDriverCapabilities(capabilities);
+  }
 
   @Test(groups = "TestIVWAnalyticsProvider")
   public void testIVW2AnalyticsProvider() throws IOException {
@@ -47,6 +73,13 @@ public class TestIVWAnalyticsProvider extends TemplateNoFirstLoad {
       networkTrafficInterceptor.startIntercepting();
       String testedPage = urlBuilder.getUrlForPath(wikiName, path);
       AdsBaseObject adsBaseObject = new AdsBaseObject(driver, testedPage);
+
+      if (countryCode == null) {
+        PageObjectLogging.log("Geo (no proxy)", adsBaseObject.getCountry(), true);
+      } else {
+        Assertion.assertEquals(adsBaseObject.getCountry(), countryCode);
+      }
+
       assertTrackingPixels(adsBaseObject,
                            URL_BASE_SCRIPT,
                            URL_TRACKING_SCRIPT);
