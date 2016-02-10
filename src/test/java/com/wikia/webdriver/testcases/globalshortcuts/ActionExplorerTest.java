61ce29f24f434725fd2cd334820c3919e7d1dbed
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2016-02-10 21:41:06 +0100

    Deleted init and added a proper open method

--- src/test/java/com/wikia/webdriver/testcases/globalshortcuts/ActionExplorerTest.java
+++ src/test/java/com/wikia/webdriver/testcases/globalshortcuts/ActionExplorerTest.java
@@ -1,51 +1,46 @@
 package com.wikia.webdriver.testcases.globalshortcuts;
 
+import org.testng.annotations.Test;
+
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
 import com.wikia.webdriver.common.core.annotations.RelatedIssue;
 import com.wikia.webdriver.common.core.helpers.Browser;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
-import com.wikia.webdriver.elements.common.Navigate;
 import com.wikia.webdriver.elements.oasis.components.globalshortcuts.ActionExplorerModal;
-
-import org.testng.annotations.Test;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.HomePageObject;
 
 @Execute(onWikia = "globalshortcuts-en")
 @InBrowser(browser = Browser.CHROME)
 public class ActionExplorerTest extends NewTestTemplate {
 
-  private ActionExplorerModal actionExplorerModal;
-
-  private void init() {
-    this.actionExplorerModal = new ActionExplorerModal(driver);
-
-    new Navigate(driver).toPage("/wiki/Globalshortcuts-en_Wikia");
-  }
-
   @Test(groups = "globalShortcuts_actionExplorer_openAndCloseModalByShortcuts")
   @RelatedIssue(issueID = "CE-3356")
   public void globalShortcuts_actionExplorer_openAndCloseModalByShortcuts() {
-    init();
+    new HomePageObject(driver).open();
 
-    actionExplorerModal.useShortcut(".");
-    actionExplorerModal.useShortcut("ESC");
+    new ActionExplorerModal(driver)
+        .useShortcut(".")
+        .useShortcut("ESC");
   }
 
   @Test(groups = "globalShortcuts_actionExplorer_openKeyboardShortcutsBySearch")
   public void globalShortcuts_actionExplorer_openKeyboardShortcutsBySearch() {
-    init();
+    new HomePageObject(driver).open();
 
-    actionExplorerModal.useShortcut(".");
-    actionExplorerModal.searchFor("Keyboard");
-    actionExplorerModal.selectKeyboardShortcutsFromSearchSuggestions();
+    new ActionExplorerModal(driver)
+        .useShortcut(".")
+        .searchFor("Keyboard")
+        .selectKeyboardShortcutsFromSearchSuggestions();
   }
 
   @Test(groups = "globalShortcuts_actionExplorer_openSpecialAllPagesFromAutocompleteSuggestions")
   public void globalShortcuts_actionExplorer_openSpecialAllPagesFromAutocompleteSuggestions() {
-    init();
+    new HomePageObject(driver).open();
 
-    actionExplorerModal.useShortcut(".");
-    actionExplorerModal.scrollToAllPagesLink();
-    actionExplorerModal.openAllPagesLink();
+    new ActionExplorerModal(driver)
+        .useShortcut(".")
+        .scrollToAllPagesLink()
+        .openAllPagesLink();
   }
 }
