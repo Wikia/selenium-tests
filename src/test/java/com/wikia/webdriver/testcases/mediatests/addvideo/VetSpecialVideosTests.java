2eabd4ca17498508fc593649f608cb5f56f9101a
archbold <patrick@wikia-inc.com>
2016-02-17 18:08:45 +0100

    Fixing MAIN-6406

--- src/test/java/com/wikia/webdriver/testcases/mediatests/addvideo/VetSpecialVideosTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mediatests/addvideo/VetSpecialVideosTests.java
@@ -27,7 +27,7 @@ public class VetSpecialVideosTests extends NewTestTemplate {
   @RelatedIssue(issueID = "MAIN-6406", comment = "Please, test manually by clicking the delete (bin) icon")
   @Execute(asUser = User.USER)
   public void SpecialVideos_001_Provider() {
-    String wikiURL = urlBuilder.getUrlForWiki("mobileregressiontesting");
+ //   String wikiURL = urlBuilder.getUrlForWiki("mobileregressiontesting");
     YoutubeVideo video = YoutubeVideoProvider.getLatestVideoForQuery("cats");
 
     SpecialVideosPageObject specialVideos = new SpecialVideosPageObject(driver);
