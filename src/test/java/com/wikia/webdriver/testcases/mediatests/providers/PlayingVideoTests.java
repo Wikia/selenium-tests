1f2e3bb3eaea3b40a6c4f4dc9b50f300aac113a4
ljedrzejczak <ukaka89@gmail.com>
2016-03-02 15:56:11 +0100

    Removed @RelatedIssue annotations from fixed tests

--- src/test/java/com/wikia/webdriver/testcases/mediatests/providers/PlayingVideoTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mediatests/providers/PlayingVideoTests.java
@@ -2,14 +2,12 @@ package com.wikia.webdriver.testcases.mediatests.providers;
 
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
-import com.wikia.webdriver.common.core.annotations.RelatedIssue;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.componentobject.lightbox.LightboxComponentObject;
 import com.wikia.webdriver.pageobjectsfactory.componentobject.media.VideoComponentObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.article.ArticlePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.special.SpecialVideosPageObject;
-
 import org.testng.annotations.Test;
 
 public class PlayingVideoTests extends NewTestTemplate {
@@ -17,7 +15,6 @@ public class PlayingVideoTests extends NewTestTemplate {
   private static final String BROWSER_SIZE = "1400x720";
 
   @Test(groups = {"Media", "ProviderTests", "PlayingVideoTests", "PlayingVideoTests_001"})
-  @RelatedIssue(issueID = "MAIN-6038", comment = "Test manually")
   @Execute(onWikia = "sktest123", disableFlash = "false")
   @InBrowser(browser = Browser.FIREFOX, browserSize = BROWSER_SIZE)
   public void PlayingVideoTests_001_ooyala() {
@@ -38,7 +35,6 @@ public class PlayingVideoTests extends NewTestTemplate {
   }
 
   @Test(groups = {"Media", "ProviderTests", "PlayingVideoTests", "PlayingVideoTests_002"})
-  @RelatedIssue(issueID = "SUS-64", comment = "Test manually")
   @Execute(disableFlash = "false", onWikia = "sktest123")
   @InBrowser(browser = Browser.FIREFOX, browserSize = BROWSER_SIZE)
   public void PlayingVideoTests_002_ooyala() {
@@ -77,8 +73,6 @@ public class PlayingVideoTests extends NewTestTemplate {
   }
 
   @Test(groups = {"Media", "ProviderTests", "PlayingVideoTests", "PlayingVideoTests_005"})
-  @RelatedIssue(issueID = "SUS-64", comment = "Make sure that anyclip video "
-                                                 + "autoplays in the lightbox")
   @Execute(disableFlash = "false", onWikia = "sktest123")
   @InBrowser(browser = Browser.FIREFOX, browserSize = BROWSER_SIZE)
   public void PlayingVideoTests_005_anyclip() {
