40d5e80350009b7720dbff415c3b906253a4cc90
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-03-15 11:13:55 +0100

    ADEN-3015 Check all collapsed slots and verify whether TOP_BUTTON_WIDE is not filled

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdType.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdType.java
@@ -8,16 +8,21 @@ import org.testng.annotations.Test;
 
 public class TestAdType extends TemplateNoFirstLoad {
 
+  private static final String TOP_BUTTON_WIDE_SELECTOR = "#TOP_BUTTON_WIDE";
+
   @Test(
       dataProviderClass = AdTypeDataProvider.class,
       dataProvider = "collapse",
       groups = {"Ads", "TestAdTypeCollapse"}
   )
-  public void TestAdTypeCollapse(String wikiName, String article, String adUnit, String slotName) {
+  public void TestAdTypeCollapse(String wikiName, String article, String adUnit, String[] slots) {
     String testedPage = urlBuilder.getUrlForPath(wikiName, article);
     AdsBaseObject ads = new AdsBaseObject(driver, testedPage);
 
-    ads.verifyGptIframe(adUnit, slotName, "gpt");
-    ads.verifyIframeSize(slotName, "gpt", 0, 0);
+    for (String slotName : slots) {
+      ads.verifyGptIframe(adUnit, slotName, "gpt");
+      ads.verifyIframeSize(slotName, "gpt", 0, 0);
+    }
+    ads.verifyNoAd(TOP_BUTTON_WIDE_SELECTOR);
   }
 }
