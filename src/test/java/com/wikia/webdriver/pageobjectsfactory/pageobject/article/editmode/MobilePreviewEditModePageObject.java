5d5e2080335b928aba550a7611ef65203d757666
nikodamn <nikodem.hynek@wikia-inc.com>
2016-03-22 15:50:54 +0100

    Add infobox test

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/article/editmode/MobilePreviewEditModePageObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/article/editmode/MobilePreviewEditModePageObject.java
@@ -1,17 +1,33 @@
 package com.wikia.webdriver.pageobjectsfactory.pageobject.article.editmode;
 
+import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.BasePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.article.ArticlePageObject;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-/**
- * @ownshership: Content West-Wing
- */
 public class MobilePreviewEditModePageObject extends BasePageObject {
 
-  @FindBy(css = ".modalcontent .ArticlePreview")
+  @FindBy(css = ".mobile-preview")
   private WebElement previewModal;
+
+  @FindBy(css = ".mobile-preview iframe")
+  private WebElement mobilePreviewIframe;
+
+  @FindBy(css = ".portable-infobox")
+  private WebElement infobox;
+
+  @FindBy(css = ".portable-infobox .pi-title")
+  private WebElement infoboxTitle;
+
+  @FindBy(css = ".portable-infobox .pi-hero")
+  private WebElement infoboxHeroImage;
+
+  @FindBy(css = ".portable-infobox .pi-data")
+  private WebElement infoboxData;
+
+  @FindBy(css = ".ember-container .article-body")
+  private WebElement previewArticleBody;
 
   public MobilePreviewEditModePageObject() {
     super();
@@ -21,9 +37,36 @@ public class MobilePreviewEditModePageObject extends BasePageObject {
     EditMode editMode = new ArticlePageObject().open(articleName).navigateToArticleEditPage();
     editMode.mobilePreviewArticle();
     wait.forElementVisible(previewModal);
-
+    driver.switchTo().frame(mobilePreviewIframe);
 
     return this;
   }
 
+  public MobilePreviewEditModePageObject infoboxIsDisplayed() {
+    wait.forElementVisible(infobox);
+    Assertion.assertTrue(infobox.isDisplayed());
+
+    return this;
+  }
+
+  public MobilePreviewEditModePageObject infoboxHeroImageIsDisplayed() {
+    wait.forElementVisible(infoboxHeroImage);
+    Assertion.assertTrue(infoboxHeroImage.isDisplayed());
+
+    return this;
+  }
+
+  public MobilePreviewEditModePageObject infoboxData() {
+    wait.forElementVisible(infoboxData);
+    Assertion.assertTrue(infoboxData.isDisplayed());
+
+    return this;
+  }
+
+  public MobilePreviewEditModePageObject infoboxTitle() {
+    wait.forElementVisible(infoboxTitle);
+    Assertion.assertTrue(infoboxTitle.isDisplayed());
+
+    return this;
+  }
 }
--- src/test/java/com/wikia/webdriver/testcases/articlecrudtests/ArticleMobilePreviewTests.java
+++ src/test/java/com/wikia/webdriver/testcases/articlecrudtests/ArticleMobilePreviewTests.java
@@ -1,26 +1,23 @@
 package com.wikia.webdriver.testcases.articlecrudtests;
 
+import com.wikia.webdriver.common.contentpatterns.PageContent;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.article.editmode.MobilePreviewEditModePageObject;
-
 import org.testng.annotations.Test;
 
-/**
- * @ownshership: Content West-Wing
- */
 @Test(groups = "ArticleMobilePreviewTests")
 @Execute(onWikia = "mediawiki119")
 public class ArticleMobilePreviewTests extends NewTestTemplate{
 
   @Execute(asUser = User.USER)
-  public void openMobilePreview() {
+  public void verifyInfoboxDisplayed() {
     MobilePreviewEditModePageObject preview = new MobilePreviewEditModePageObject();
-    preview.openMobilePreview("test4324242");
-
+    preview.openMobilePreview(PageContent.PORTABLE_INFOBOX_01)
+        .infoboxIsDisplayed()
+        .infoboxHeroImageIsDisplayed()
+        .infoboxTitle()
+        .infoboxData();
   }
-
-
-
 }
