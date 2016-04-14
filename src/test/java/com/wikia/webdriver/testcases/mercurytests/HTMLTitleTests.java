13cab3c58b8047a9910f80755a298ae61ac9ec44
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-04-14 16:14:44 -0700

    speed up tests

--- src/test/java/com/wikia/webdriver/testcases/mercurytests/HTMLTitleTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mercurytests/HTMLTitleTests.java
@@ -6,7 +6,6 @@ import com.wikia.webdriver.common.core.configuration.Configuration;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.url.Page;
-import com.wikia.webdriver.common.dataprovider.MercurySEODataProvider;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.elements.common.Navigate;
 import com.wikia.webdriver.elements.mercury.components.Head;
@@ -17,6 +16,99 @@ import org.testng.annotations.Test;
 @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
 public class HTMLTitleTests extends NewTestTemplate {
 
+  /**
+   * [0] wikiName
+   * [1] path
+   * [2] expected title
+   */
+  private String[][] testCases = {
+    {
+      "sktest123",
+      "/wiki/Sktest123_Wiki",
+      "Sktest123 Wiki - Wikia"
+    },
+    {
+      "sktest123",
+      "/wiki/Style-5H2",
+      "Style-5H2 - Sktest123 Wiki - Wikia"
+    },
+    {
+      "sktest123",
+      "/wiki/TestDisplayTitle",
+      "testing abc - Sktest123 Wiki - Wikia"
+    },
+    {
+      "sktest123",
+      "/wiki/Category:Premium_Videos",
+      "Category:Premium Videos - Sktest123 Wiki - Wikia"
+    },
+    {
+      "sktest123",
+      "/wiki/Category:Non-premium_Videos",
+      "Category:Non-premium Videos - Sktest123 Wiki - Wikia"
+    },
+    {
+      "sktest123",
+      "/wiki/Category:Premium",
+      "PremiumVideos - Sktest123 Wiki - Wikia"
+    },
+    {
+      "es.pokemon",
+      "/wiki/WikiDex",
+      "WikiDex - Wikia"
+    },
+    {
+      "es.pokemon",
+      "/wiki/Lista_de_Pokémon",
+      "Lista de Pokémon - WikiDex - Wikia"
+    },
+    {
+      "es.pokemon",
+      "/wiki/Categoría:Regiones",
+      "Categoría:Regiones - WikiDex - Wikia"
+    },
+    {
+      "starwars",
+      "/wiki/Main_Page",
+      "Wookieepedia - Wikia"
+    },
+    {
+      "starwars",
+      "/wiki/Droid_starfighter",
+      "Droid starfighter - Wookieepedia - Wikia"
+    },
+    {
+      "starwars",
+      "/main/category/Future_films",
+      "Future_films"
+    },
+    {
+      "starwars",
+      "/main/section/Films",
+      "Films"
+    },
+    {
+      "mediawiki119",
+      "/d/f/203236/latest",
+      ""
+    },
+    {
+      "mediawiki119",
+      "/d/p/2706859396041803285",
+      ""
+    },
+    {
+      "mediawiki119",
+      "/d/u/27334045",
+      ""
+    },
+    {
+      "dnd4",
+      "/wiki/Dungeons_&_Dragons",
+      "Dungeons & Dragons - D&D4 Wiki - Wikia"
+    }
+  };
+  
   private Head head;
   private Navigate navigate;
 
@@ -26,16 +118,14 @@ public class HTMLTitleTests extends NewTestTemplate {
     this.navigate = new Navigate(driver);
   }
 
-  @Test(
-          groups = "mercury_htmlTitleSet",
-          dataProviderClass = MercurySEODataProvider.class,
-          dataProvider = "htmlTitles"
-  )
-  public void mercury_htmlTitleSet(Page page, String expectedTitle) {
-    navigate.toUrl(page.getUrl());
-    String actualTitle = head.getDocumentTitle();
+  @Test(groups = "mercury_htmlTitleSet")
+  public void mercury_htmlTitleSet() {
+    for (String[] testCase : testCases) {
+      navigate.toUrl(new Page(testCase[0], testCase[1]).getUrl());
+      String actualTitle = head.getDocumentTitle();
 
-    Assertion.assertEquals(actualTitle, getFullTitle(expectedTitle));
+      Assertion.assertEquals(actualTitle, getFullTitle(testCase[2]));
+    }
   }
 
   private String getFullTitle(String title) {
