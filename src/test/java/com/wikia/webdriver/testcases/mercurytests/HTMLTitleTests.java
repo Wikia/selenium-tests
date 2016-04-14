6c7ba86849793dea318355c4bd6b1de9efaf120a
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-04-14 15:57:24 -0700

    fix test

--- src/test/java/com/wikia/webdriver/testcases/mercurytests/HTMLTitleTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mercurytests/HTMLTitleTests.java
@@ -1,20 +1,21 @@
 package com.wikia.webdriver.testcases.mercurytests;
 
-
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
+import com.wikia.webdriver.common.core.configuration.Configuration;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.url.Page;
 import com.wikia.webdriver.common.dataprovider.MercurySEODataProvider;
-import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
+import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.elements.common.Navigate;
 import com.wikia.webdriver.elements.mercury.components.Head;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-public class HTMLTitleTests extends TemplateNoFirstLoad {
+public class HTMLTitleTests extends NewTestTemplate {
 
   private Head head;
   private Navigate navigate;
@@ -34,6 +35,23 @@ public class HTMLTitleTests extends TemplateNoFirstLoad {
     navigate.toUrl(page.getUrl());
     String actualTitle = head.getDocumentTitle();
 
-    Assertion.assertEquals(expectedTitle, actualTitle);
+    Assertion.assertEquals(actualTitle, getFullTitle(expectedTitle));
+  }
+
+  private String getFullTitle(String title) {
+    String env = Configuration.getEnv();
+
+    switch (env) {
+      case "prod":
+        return title;
+      case "stable":
+        return "stable-s1 - " + title;
+      case "preview":
+        return "staging-s1 - " + title;
+      case "verify":
+        return "staging-s2 - " + title;
+      default:
+        return env + " - " + title;
+    }
   }
 }
