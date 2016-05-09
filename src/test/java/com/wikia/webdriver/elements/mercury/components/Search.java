4b6244d888d5c7cb714c6c849d77d818c482ef7e
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-05-09 13:13:36 +0200

    unused import

--- src/test/java/com/wikia/webdriver/elements/mercury/components/Search.java
+++ src/test/java/com/wikia/webdriver/elements/mercury/components/Search.java
@@ -5,7 +5,6 @@ import com.wikia.webdriver.common.logging.PageObjectLogging;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.BasePageObject;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
