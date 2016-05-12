82ba6dbdedb691f3a830465685c6a38504588ece
Michal Nowierski <neptunooo@NeptunoooBook.local>
2016-05-12 17:30:55 +0200

    removing related issue

--- src/test/java/com/wikia/webdriver/testcases/imageservingtests/ImageServingTests.java
+++ src/test/java/com/wikia/webdriver/testcases/imageservingtests/ImageServingTests.java
@@ -22,7 +22,6 @@ public class ImageServingTests extends NewTestTemplate {
 
   @Test(groups = {"ImageServing_001", "ImageServing", "Smoke3"})
   @Execute(asUser = User.USER)
-  @RelatedIssue(issueID = "MAIN-7102", comment = "The functionality is testable manually and should pass")
   public void ImageServing_001_SpecialNewFilesTest() {
     WikiBasePageObject base = new WikiBasePageObject();
     SpecialNewFilesPageObject newFiles = base.openSpecialNewFiles(wikiURL);
