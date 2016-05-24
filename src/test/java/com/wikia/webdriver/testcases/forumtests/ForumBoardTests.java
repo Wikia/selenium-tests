d5d852dbb2a0c3b3aad6a8c8b902e1093f9e8f7f
Michal Nowierski <neptunooo@NeptunoooBook.local>
2016-05-24 10:12:12 +0200

    adding related issue for a test failure

--- src/test/java/com/wikia/webdriver/testcases/forumtests/ForumBoardTests.java
+++ src/test/java/com/wikia/webdriver/testcases/forumtests/ForumBoardTests.java
@@ -3,6 +3,7 @@ package com.wikia.webdriver.testcases.forumtests;
 import com.wikia.webdriver.common.contentpatterns.PageContent;
 import com.wikia.webdriver.common.contentpatterns.VideoContent;
 import com.wikia.webdriver.common.core.annotations.Execute;
+import com.wikia.webdriver.common.core.annotations.RelatedIssue;
 import com.wikia.webdriver.common.core.configuration.Configuration;
 import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.common.properties.Credentials;
@@ -85,6 +86,8 @@ public class ForumBoardTests extends NewTestTemplate {
   }
 
   @Test(groups = {"ForumBoardTests_006", "ForumBoardTests", "Forum"})
+  @RelatedIssue(issueID = "MAIN-7213", comment = "the failure is not fully repetitive, it derivies from " +
+          "the test environment instability")
   public void anonymousUserCanFollowDiscussionOnForum() {
     ForumPageObject forumMainPage = new ForumPageObject(driver);
     forumMainPage.loginAs(credentials.userNameStaff, credentials.passwordStaff, wikiURL);
