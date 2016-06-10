5128b39d4b205ffea20d94725beea3ca9879d1df
Michal Nowierski <neptunooo@NeptunoooBook.local>
2016-06-10 18:33:13 +0200

    disabling tests

--- src/test/java/com/wikia/webdriver/testcases/discussions/CreatingPostTests.java
+++ src/test/java/com/wikia/webdriver/testcases/discussions/CreatingPostTests.java
@@ -5,6 +5,7 @@ import com.wikia.webdriver.common.contentpatterns.MercuryWikis;
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
+import com.wikia.webdriver.common.core.annotations.RelatedIssue;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.helpers.User;
@@ -25,10 +26,10 @@ public class CreatingPostTests extends NewTestTemplate {
    * ANONS ON MOBILE SECTION
    */
 
-  @Test(groups = "discussions-anonUserOnMobileCanNotWriteNewPost")
+  @Test(groups = "discussions-anonUserOnMobileCanNotWriteNewPost", enabled = false)
+  @RelatedIssue(issueID = "SOC-2372")
   @Execute(asUser = User.ANONYMOUS)
   @InBrowser(emulator = Emulator.GOOGLE_NEXUS_5)
-
   public void anonUserOnMobileCanNotWriteNewPost() {
     userOnMobileMustBeLoggedInToUsePostCreator();
   }
