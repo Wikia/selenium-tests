67e2927fdee49a6203a23d11793130740873e43b
PiotrGackowski <piotr.gackowski@wikia-inc.com>
2016-06-22 19:43:18 +0200

    ADEN-3466 sonar

--- src/test/java/com/wikia/webdriver/common/core/url/FandomUrlBuilder.java
+++ src/test/java/com/wikia/webdriver/common/core/url/FandomUrlBuilder.java
@@ -2,15 +2,13 @@ package com.wikia.webdriver.common.core.url;
 
 import com.wikia.webdriver.common.core.configuration.Configuration;
 
-import org.apache.commons.lang.StringUtils;
-
 public class FandomUrlBuilder {
 
-  final static String FANDOM_URL = "fandom.wikia.com";
-  final static String ARTICLE_PATH = "articles";
-  public final static String ENV_PROD = "prod";
-  public final static String ENV_ADENG = "adeng";
-  public final static String ENV_QA = "qa";
+  static final String FANDOM_URL = "fandom.wikia.com";
+  static final  String ARTICLE_PATH = "articles";
+  public static final  String ENV_PROD = "prod";
+  public static final String ENV_ADENG = "adeng";
+  public static final String ENV_QA = "qa";
 
   private String browser;
   private String env;
--- src/test/java/com/wikia/webdriver/common/templates/fandom/AdsFandomTestTemplate.java
+++ src/test/java/com/wikia/webdriver/common/templates/fandom/AdsFandomTestTemplate.java
@@ -1,11 +1,5 @@
 package com.wikia.webdriver.common.templates.fandom;
 
-import com.wikia.webdriver.common.core.configuration.Configuration;
-import com.wikia.webdriver.common.core.url.FandomUrlBuilder;
-import com.wikia.webdriver.common.templates.core.CoreTestTemplate;
-
-import org.openqa.selenium.JavascriptExecutor;
-
 public class AdsFandomTestTemplate extends FandomTestTemplate {
 
   @Override
