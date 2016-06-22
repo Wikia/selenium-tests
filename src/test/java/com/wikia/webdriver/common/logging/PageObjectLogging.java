3dec754369b064f194ff2f939f7dfde917b75f42
lukaszkonieczny <lukaszk@wikia-inc.com>
2016-06-22 16:12:53 +0200

    CE-3769: Disable Sales pitch dialog

--- src/test/java/com/wikia/webdriver/common/logging/PageObjectLogging.java
+++ src/test/java/com/wikia/webdriver/common/logging/PageObjectLogging.java
@@ -283,6 +283,14 @@ public class PageObjectLogging extends AbstractWebDriverEventListener implements
 
       }
 
+      /**
+       * We want to disable sales pitch dialog for new potential contributors
+       * to avoid hiding other UI elements.
+       * see https://wikia-inc.atlassian.net/browse/CE-3768
+       */
+      driver.manage().addCookie(
+          new Cookie("cpBenefitsModalShown", "1", Configuration.getWikiaDomain(), null, null));
+
       if (TestContext.isFirstLoad() && "true".equals(Configuration.getMockAds())) {
         driver.manage().addCookie(
             new Cookie("mock-ads", XMLReader.getValue("mock.ads_token"),
