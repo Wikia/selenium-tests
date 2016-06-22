79315a09008e07042dc6be0c115194940ae62123
Michal Nowierski <neptunooo@NeptunoooBook.local>
2016-06-22 12:17:24 +0200

    removing unimportant javadoc

--- src/test/java/com/wikia/webdriver/elements/mercury/pages/login/SignInPage.java
+++ src/test/java/com/wikia/webdriver/elements/mercury/pages/login/SignInPage.java
@@ -10,9 +10,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-/**
- * Created by neptunooo on 06/06/16.
- */
 public class SignInPage extends WikiBasePageObject{
 
     @Getter(lazy = true)
