af4cb4878f3466b2b5d77a3939f07ea828b4954a
PiotrGackowski <piotr.gackowski@wikia-inc.com>
2016-06-24 16:09:54 +0200

    ADEN-3466 Sonar fix

--- src/test/java/com/wikia/webdriver/common/templates/fandom/AdsFandomTestTemplate.java
+++ src/test/java/com/wikia/webdriver/common/templates/fandom/AdsFandomTestTemplate.java
@@ -4,6 +4,6 @@ public class AdsFandomTestTemplate extends FandomTestTemplate {
 
   @Override
   protected void loadFirstPage() {
-
+    // we want to avoid going to qa.fandom.com as logged in user
   }
 }
