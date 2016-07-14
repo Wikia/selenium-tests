877d27674ac6eb78550a5caa835d045aa4de5452
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-07-14 19:51:48 +0200

    XW-1658 | review article preview tests

---
+++ src/test/java/com/wikia/webdriver/testcases/articlepreviewtests/MobilePreviewTests.java
@@ -0,0 +1,26 @@
+package com.wikia.webdriver.testcases.articlepreviewtests;
+
+import com.wikia.webdriver.common.core.annotations.Execute;
+import com.wikia.webdriver.common.templates.NewTestTemplate;
+import com.wikia.webdriver.elements.oasis.pages.ArticlePreviewPage;
+
+import org.testng.annotations.Test;
+
+@Execute(onWikia = "mercuryautomationtesting")
+public class MobilePreviewTests extends NewTestTemplate {
+
+  @Test(groups = "oasis-articlePreview-mobilePreviewIsRenderedCorrectlyInModal")
+  public void mobilePreviewIsRenderedCorrectlyInModal() {
+    new ArticlePreviewPage()
+        .navigateToArticlePreviewPageInEditMode()
+        .clickOnMobilePreviewButton()
+        .heroImageIsPresent()
+        .infoboxIsPresent()
+        .tableOfContentsIsPresent()
+        .articleTableIsPresent()
+        .mediaGalleryIsPresent()
+        .linkedMediaGalleryIsPresent()
+        .singleImageIsPresent()
+        .singleVideoIsPresent();
+  }
+}
