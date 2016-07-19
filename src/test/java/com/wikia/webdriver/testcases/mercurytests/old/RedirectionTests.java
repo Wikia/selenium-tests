9e12e608cc4374ba2778b072a4185292a57f0f08
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-07-19 11:24:18 +0200

    XW-1719 | remove redirection tests

--- src/test/java/com/wikia/webdriver/testcases/mercurytests/old/RedirectionTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mercurytests/old/RedirectionTests.java
@@ -5,17 +5,10 @@ import com.wikia.webdriver.common.contentpatterns.MercuryWikis;
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
-import com.wikia.webdriver.common.core.annotations.RelatedIssue;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.url.UrlBuilder;
-import com.wikia.webdriver.common.skin.Skin;
-import com.wikia.webdriver.common.skin.SkinHelper;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
-import com.wikia.webdriver.elements.common.Navigate;
-import com.wikia.webdriver.elements.mercury.old.MercuryFooterComponentObject;
-import com.wikia.webdriver.pageobjectsfactory.componentobject.article.ArticleNavigationComponentObject;
-import com.wikia.webdriver.pageobjectsfactory.componentobject.article.OasisFooterComponentObject;
 
 import org.testng.annotations.Test;
 
@@ -26,12 +19,6 @@ import org.testng.annotations.Test;
 )
 public class RedirectionTests extends NewTestTemplate {
 
-  private Navigate navigate;
-
-  private void init() {
-    this.navigate = new Navigate();
-  }
-
   @Test(groups = "mercury_redirection_navigateToPageWithWWWAndBeRedirectedToPageWithoutWWW")
   public void mercury_redirection_navigateToPageWithWWWAndBeRedirectedToPageWithoutWWW() {
     String navigateUrl = UrlBuilder.getUrlForPageWithWWW(MercurySubpages.MAIN_PAGE);
@@ -41,32 +28,5 @@ public class RedirectionTests extends NewTestTemplate {
 
     String currentUrl = driver.getCurrentUrl();
     Assertion.assertTrue(currentUrl.contains(expectedUrl) && !currentUrl.contains("www."));
-  }
-
-  @Test(groups = "mercury_redirection_redirectFromMercuryToOasisAndStayInOasis")
-  public void mercury_redirection_redirectFromMercuryToOasisAndStayInOasis() {
-    init();
-    navigate.toPage(MercurySubpages.MAIN_PAGE);
-
-    new MercuryFooterComponentObject(driver).clickFullSiteLink();
-    new ArticleNavigationComponentObject(driver).clickRandomArticle();
-
-    Assertion.assertTrue(new SkinHelper(driver).isSkin(Skin.OASIS));
-  }
-
-  @Test(groups = "mercury_redirection_redirectFromMercuryToOasisAndThenBackToMercury", enabled = false)
-  @RelatedIssue(
-      issueID = "QAART-773",
-      comment = "Wikia.isTouchScreen() returns false for mobile emulator, "
-                + "which prevents script from adding the mobile page link."
-  )
-  public void mercury_redirection_redirectFromMercuryToOasisAndThenBackToMercury() {
-    init();
-    navigate.toPage(MercurySubpages.MAIN_PAGE);
-
-    new MercuryFooterComponentObject(driver).clickFullSiteLink();
-    new OasisFooterComponentObject(driver).clickMobileSiteLink();
-
-    Assertion.assertTrue(new SkinHelper(driver).isSkin(Skin.MERCURY));
   }
 }
