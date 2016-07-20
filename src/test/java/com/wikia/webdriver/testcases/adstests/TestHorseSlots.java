134615988b38f5153871dc2de3d5cbebf1588dcc
Bogna Knychala <bogna.ka@gmail.com>
2016-07-20 11:54:33 +0200

    ADEN-3574 add timout at the end of the test script

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestHorseSlots.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestHorseSlots.java
@@ -6,19 +6,29 @@ import org.testng.annotations.Test;
 
 public class TestHorseSlots extends NewTestTemplate {
 
-  @Test(groups = {"TestHorseSlots_Interstital", "TestHorseSlots"}, invocationCount = 10)
+  @Test(groups = {"TestHorseSlots_Interstital", "TestHorseSlots"}, invocationCount = 5)
   public void TestHorseSlots_Interstital() {
     driver.manage().window().maximize();
-    driver.get("http://sandbox-adeng02.project43.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/Interstitial");
+    driver.get("http://sandbox-adeng01.project43.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/Interstitial");
     AdsBaseObject ads = new AdsBaseObject(driver);
     ads.waitForElementPresent("wikia_gpt/5441/wka.life/_project43//article/gpt/INVISIBLE_HIGH_IMPACT_2");
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 
-  @Test(groups = {"TestHorseSlots_FloorAdhesion", "TestHorseSlots"}, invocationCount = 10)
+  @Test(groups = {"TestHorseSlots_FloorAdhesion", "TestHorseSlots"}, invocationCount = 5)
   public void TestHorseSlots_FloorAdhesion() {
     driver.manage().window().maximize();
-    driver.get("http://sandbox-adeng02.project43.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/FloorAdhesion");
+    driver.get("http://sandbox-adeng01.project43.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/FloorAdhesion");
     AdsBaseObject ads = new AdsBaseObject(driver);
     ads.waitForElementPresent("wikia_gpt/5441/wka.life/_project43//article/gpt/INVISIBLE_HIGH_IMPACT_2");
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
