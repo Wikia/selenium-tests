205b32b0db951236598a5e91db30aae76e70f5bb
Bogna Knychala <bogna.ka@gmail.com>
2016-07-21 14:02:30 +0200

    ADEN-3574 | run on sandbox-adeng01

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestHorseSlots.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestHorseSlots.java
@@ -9,7 +9,7 @@ public class TestHorseSlots extends NewTestTemplate {
   @Test(groups = {"TestHorseSlots_Interstital", "TestHorseSlots"}, invocationCount = 5)
   public void TestHorseSlots_Interstital() {
     driver.manage().window().maximize();
-    driver.get("http://sandbox-adeng02.project43.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/Interstitial");
+    driver.get("http://sandbox-adeng01.project43.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/Interstitial");
     AdsBaseObject ads = new AdsBaseObject(driver);
     ads.waitForElementPresent("wikia_gpt/5441/wka.life/_project43//article/gpt/INVISIBLE_HIGH_IMPACT_2");
     try {
@@ -22,7 +22,7 @@ public class TestHorseSlots extends NewTestTemplate {
   @Test(groups = {"TestHorseSlots_FloorAdhesion", "TestHorseSlots"}, invocationCount = 5)
   public void TestHorseSlots_FloorAdhesion() {
     driver.manage().window().maximize();
-    driver.get("http://sandbox-adeng02.project43.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/FloorAdhesion");
+    driver.get("http://sandbox-adeng01.project43.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/FloorAdhesion");
     AdsBaseObject ads = new AdsBaseObject(driver);
     ads.waitForElementPresent("wikia_gpt/5441/wka.life/_project43//article/gpt/INVISIBLE_HIGH_IMPACT_2");
     try {
