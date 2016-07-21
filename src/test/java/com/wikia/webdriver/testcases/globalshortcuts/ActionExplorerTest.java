ef1a0218c9b95d80ef4cbe32f4d2697013bf8618
nikodamn <nikodem.hynek@wikia-inc.com>
2016-07-21 13:09:33 +0200

    Change related issue ID

--- src/test/java/com/wikia/webdriver/testcases/globalshortcuts/ActionExplorerTest.java
+++ src/test/java/com/wikia/webdriver/testcases/globalshortcuts/ActionExplorerTest.java
@@ -16,7 +16,7 @@ import org.testng.annotations.Test;
 public class ActionExplorerTest extends NewTestTemplate {
 
   @Test(enabled = false, groups = "globalShortcuts_actionExplorer_openAndCloseModalByShortcuts")
-  @RelatedIssue(issueID = "CE-3356")
+  @RelatedIssue(issueID = "WW-82")
   public void globalShortcuts_actionExplorer_openAndCloseModalByShortcuts() {
     new HomePage()
         .open()
