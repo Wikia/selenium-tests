c5e6213f9afe309270b1978562b4cbf03ee0a5c0
Bogna Knychala <bogna.ka@gmail.com>
2016-08-03 18:01:15 +0200

    ADEN-3674 | mobile test on sandbox-adeng01

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestHorseSlots.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestHorseSlots.java
@@ -6,27 +6,27 @@ import org.testng.annotations.Test;
 
 public class TestHorseSlots extends NewTestTemplate {
 
-  @Test(groups = {"TestHorseSlots_FloorAdhesion", "TestHorseSlots"}, invocationCount = 5)
-  public void TestHorseSlots_FloorAdhesion() {
-    driver.manage().window().maximize();
-    driver.get("http://aga.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/FloorAdhesion");
-    AdsBaseObject ads = new AdsBaseObject(driver);
-    ads.waitForElementPresent("google_ads_iframe_/5441/wka.gaming/_aga//article/gpt/INVISIBLE_HIGH_IMPACT_2_0__container__");
-    try {
-      Thread.sleep(15000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-  }
-
+//  @Test(groups = {"TestHorseSlots_FloorAdhesion", "TestHorseSlots"}, invocationCount = 5)
+//  public void TestHorseSlots_FloorAdhesion() {
+//    driver.manage().window().maximize();
+//    driver.get("http://project43.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/FloorAdhesion");
+//    AdsBaseObject ads = new AdsBaseObject(driver);
+//    ads.waitForElementPresent("google_ads_iframe_/5441/wka.gaming/_aga//article/gpt/INVISIBLE_HIGH_IMPACT_2_0__container__");
+//    try {
+//      Thread.sleep(10000);
+//    } catch (InterruptedException e) {
+//      e.printStackTrace();
+//    }
+//  }
+//
   @Test(groups = {"TestHorseSlots_Interstitial", "TestHorseSlots"}, invocationCount = 5)
   public void TestHorseSlots_Interstitial() {
     driver.manage().window().maximize();
-    driver.get("http://aga.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/Interstitial");
+    driver.get("http://sandbox-adeng01.project43.wikia.com/wiki/SyntheticTests/Slots/InvisibleHighImpact/Interstitial");
     AdsBaseObject ads = new AdsBaseObject(driver);
-    ads.waitForElementPresent("google_ads_iframe_/5441/wka.gaming/_aga//article/gpt/INVISIBLE_HIGH_IMPACT_2_0__container__");
+    ads.waitForElementPresent("wikia_gpt/5441/wka.life/_project43//article/mobile/INVISIBLE_HIGH_IMPACT_2");
     try {
-      Thread.sleep(15000);
+      Thread.sleep(10000);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
