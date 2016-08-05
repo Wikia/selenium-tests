aa9dad61757aeb08946b57effa344403959dcc08
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2016-08-05 14:05:30 +0200

    ADEN-3592 CR cleanup#2

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsPageFairObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsPageFairObject.java
@@ -3,41 +3,35 @@ package com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase;
 import com.wikia.webdriver.common.core.networktrafficinterceptor.NetworkTrafficInterceptor;
 import com.wikia.webdriver.common.core.url.Page;
 import com.wikia.webdriver.common.core.url.UrlBuilder;
-import com.wikia.webdriver.common.logging.PageObjectLogging;
-import org.junit.Assert;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 
 public class AdsPageFairObject extends AdsBaseObject {
-    private static final String PATTERN_PAGEFAIR =
-            "http://stats.pagefair.com/stats/page_view_event/[0-9A-Z]{16}/a\\.js.*";
-    private static final String PAGEFAIR_URL_PARAM = "pagefairdetection=1";
-    private static final String PATTERN_PAGEFAIR_ADBLOCK_DETECTED = PATTERN_PAGEFAIR + "is_ab=1.*";
-    private static final String PATTERN_PAGEFAIR_ADBLOCK_NOT_DETECTED = PATTERN_PAGEFAIR + "is_ab=0.*";
 
-    public AdsPageFairObject(WebDriver driver, Page page) {
-        super(driver, AdsPageFairObject.addPageFairDetectionParam(page));
-    }
+  private static final String PATTERN_PAGEFAIR =
+      "http://stats.pagefair.com/stats/page_view_event/[0-9A-Z]{16}/a\\.js.*";
+  private static final String PAGEFAIR_URL_PARAM = "pagefairdetection=1";
+  private static final String PATTERN_PAGEFAIR_ADBLOCK_DETECTED =
+      PATTERN_PAGEFAIR + "is_ab=1.*";
+  private static final String PATTERN_PAGEFAIR_ADBLOCK_NOT_DETECTED =
+      PATTERN_PAGEFAIR + "is_ab=0.*";
 
-    private static String addPageFairDetectionParam(Page page) {
-        UrlBuilder urlBuilder = new UrlBuilder();
+  public AdsPageFairObject(WebDriver driver, Page page) {
+    super(driver, AdsPageFairObject.addPageFairDetectionParam(page));
+  }
 
-        String url = urlBuilder.getUrlForPage(page);
-        return urlBuilder.appendQueryStringToURL(url, PAGEFAIR_URL_PARAM);
-    }
+  private static String addPageFairDetectionParam(Page page) {
+    UrlBuilder urlBuilder = new UrlBuilder();
 
-    public void assertPageFairSendCorrectRequest(boolean expectedResult, NetworkTrafficInterceptor networkTrafficInterceptor) {
-        String pattern = expectedResult ? PATTERN_PAGEFAIR_ADBLOCK_DETECTED : PATTERN_PAGEFAIR_ADBLOCK_NOT_DETECTED;
-        try {
-            this.wait.forSuccessfulResponseByUrlPattern(networkTrafficInterceptor, pattern);
-        } catch (TimeoutException exception) {
-            PageObjectLogging.log("TIMEOUT", exception, true);
-            Assert.fail(getAssertionFailMessage(expectedResult));
-        }
-    }
+    String url = urlBuilder.getUrlForPage(page);
+    return urlBuilder.appendQueryStringToURL(url, PAGEFAIR_URL_PARAM);
+  }
 
-    private String getAssertionFailMessage(boolean expectedResult) {
-        String expectedValue = expectedResult ? "blocking detected" : "not detected";
-        return String.format("Detection result not correct, expected: %s", expectedValue);
-    }
+  public void assertPageFairSendCorrectRequest(
+      boolean expectedResult,
+      NetworkTrafficInterceptor networkTrafficInterceptor
+  ) {
+    String pattern = expectedResult ?
+                     PATTERN_PAGEFAIR_ADBLOCK_DETECTED : PATTERN_PAGEFAIR_ADBLOCK_NOT_DETECTED;
+    this.wait.forSuccessfulResponseByUrlPattern(networkTrafficInterceptor, pattern);
+  }
 }
--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsDetectionPageFair.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsDetectionPageFair.java
@@ -9,27 +9,27 @@ import org.testng.annotations.Test;
 
 public class TestAdsDetectionPageFair extends TemplateNoFirstLoad {
 
-    @NetworkTrafficDump
-    @Test(
-        groups = "AdsDetectAdBlockPageFair",
-        dataProviderClass = AdsDataProvider.class,
-        dataProvider = "adsDetectionPageFair"
-    )
-    public void adsDetectAdBlockPageFair(Page page) {
-        networkTrafficInterceptor.startIntercepting();
-        AdsPageFairObject adsBaseObject = new AdsPageFairObject(driver, page);
-        adsBaseObject.assertPageFairSendCorrectRequest(true, networkTrafficInterceptor);
-    }
+  @NetworkTrafficDump
+  @Test(
+      groups = "AdsDetectAdBlockPageFair",
+      dataProviderClass = AdsDataProvider.class,
+      dataProvider = "adsDetectionPageFair"
+  )
+  public void adsDetectAdBlockPageFair(Page page) {
+    networkTrafficInterceptor.startIntercepting();
+    AdsPageFairObject adsBaseObject = new AdsPageFairObject(driver, page);
+    adsBaseObject.assertPageFairSendCorrectRequest(true, networkTrafficInterceptor);
+  }
 
-    @NetworkTrafficDump
-    @Test(
-            groups = "AdsDetectNoAdBlockPageFair",
-            dataProviderClass = AdsDataProvider.class,
-            dataProvider = "adsDetectionPageFair"
-    )
-    public void adsDetectNoAdBlockPageFair(Page page) {
-        networkTrafficInterceptor.startIntercepting();
-        AdsPageFairObject adsBaseObject = new AdsPageFairObject(driver, page);
-        adsBaseObject.assertPageFairSendCorrectRequest(false, networkTrafficInterceptor);
-    }
+  @NetworkTrafficDump
+  @Test(
+      groups = "AdsDetectNoAdBlockPageFair",
+      dataProviderClass = AdsDataProvider.class,
+      dataProvider = "adsDetectionPageFair"
+  )
+  public void adsDetectNoAdBlockPageFair(Page page) {
+    networkTrafficInterceptor.startIntercepting();
+    AdsPageFairObject adsBaseObject = new AdsPageFairObject(driver, page);
+    adsBaseObject.assertPageFairSendCorrectRequest(false, networkTrafficInterceptor);
+  }
 }
