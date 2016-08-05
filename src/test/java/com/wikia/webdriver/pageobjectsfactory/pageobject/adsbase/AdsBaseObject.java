e571611470c0f8fdf1ee92c9985f0c0ba9f92778
nAndy <nandy@wikia-inc.com>
2016-08-05 17:24:48 +0200

    ADEN-3723 CR changes: removed unused imports

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsBaseObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsBaseObject.java
@@ -28,8 +28,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class AdsBaseObject extends WikiBasePageObject {
 
