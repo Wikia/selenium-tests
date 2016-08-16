b8872200626506773d40d89ca877ca6059b65b4a
Bogna Knychała <bogna@wikia-inc.com>
2016-08-16 15:51:47 +0200

    remove redundant blank line

--- src/test/java/com/wikia/webdriver/elements/mercury/components/discussions/common/Post.java
+++ src/test/java/com/wikia/webdriver/elements/mercury/components/discussions/common/Post.java
@@ -5,7 +5,6 @@ import com.wikia.webdriver.pageobjectsfactory.pageobject.BasePageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import java.util.List;
 
 
