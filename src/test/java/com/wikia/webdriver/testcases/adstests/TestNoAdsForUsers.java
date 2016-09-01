19634ae4bc515f119821a346d2bc3e34514c15e9
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-09-01 19:07:31 +0200

    ADEN-3891 Move no ads for users test to p43

---
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestNoAdsForUsers.java
@@ -0,0 +1,26 @@
+package com.wikia.webdriver.testcases.adstests;
+
+import com.wikia.webdriver.common.core.annotations.Execute;
+import com.wikia.webdriver.common.core.geoedge.CountryCode;
+import com.wikia.webdriver.common.core.geoedge.GeoEdgeBrowserMobProxy;
+import com.wikia.webdriver.common.core.helpers.User;
+import com.wikia.webdriver.common.dataprovider.ads.AdsDataProvider;
+import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
+
+import org.testng.annotations.Test;
+
+public class TestNoAdsForUsers extends TemplateNoFirstLoad {
+
+  @Test(
+      dataProviderClass = AdsDataProvider.class,
+      dataProvider = "noAdsForUsers",
+      groups = "NoAdsForUsers"
+  )
+  @Execute(asUser = User.USER)
+  public void noAdsForUsers(String wikiName, String path) throws Exception {
+    String testedPage = urlBuilder.getUrlForPath(wikiName, path);
+    AdsBaseObject wikiPage = new AdsBaseObject(driver, testedPage);
+    wikiPage.verifyNoAdsOnPage();
+  }
+}
