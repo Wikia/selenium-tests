6c55939b460b3c8a1268c72d51a311af6a3ab5d1
Ludwik <ludwik.kazmierczak@gmail.com>
2016-09-01 12:10:15 +0200

    Updated name

--- src/test/java/com/wikia/webdriver/testcases/articlepreviewtests/MobilePreviewTests.java
+++ src/test/java/com/wikia/webdriver/testcases/articlepreviewtests/MobilePreviewTests.java
@@ -6,7 +6,7 @@ import com.wikia.webdriver.elements.oasis.pages.ArticlePreviewPage;
 
 import org.testng.annotations.Test;
 
-@Test(groups = "Mercury_MobilePreview")
+@Test(groups = "MobilePreview")
 @Execute(onWikia = "mercuryautomationtesting")
 public class MobilePreviewTests extends NewTestTemplate {
 
