faaed7ff2c4283ca976554e9e3e1e071173409cd
mleczey <mleczey@gmail.com>
2016-09-05 18:02:35 +0200

    SOC-2374

--- src/test/java/com/wikia/webdriver/testcases/discussions/ReportingPostTests.java
+++ src/test/java/com/wikia/webdriver/testcases/discussions/ReportingPostTests.java
@@ -8,6 +8,8 @@ import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
+import com.wikia.webdriver.elements.mercury.components.discussions.common.Post;
+import com.wikia.webdriver.elements.mercury.pages.discussions.PostDetailsPage;
 import com.wikia.webdriver.elements.mercury.pages.discussions.PostsListPage;
 import org.testng.annotations.Test;
 
@@ -20,8 +22,19 @@ public class ReportingPostTests extends NewTestTemplate {
   @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
   public void anonUserOnMobileCanNotReportPostOnPostListPage() {
     PostsListPage postsListPage = new PostsListPage().open();
-    boolean actual = postsListPage.getPost()
-        .getTheNewestPost()
+    assertThatReportPostOptionIsNotAvailable(postsListPage.getPost());
+  }
+
+  @Test(groups = "discussions-anonUserOnMobileCanNotReportPost")
+  @Execute(asUser = User.ANONYMOUS)
+  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
+  public void anonUserOnMobileCanNotReportPostOnPostDetailsPage() {
+    PostDetailsPage postDetailsPage = new PostDetailsPage().openDefaultPost();
+    assertThatReportPostOptionIsNotAvailable(postDetailsPage.getPost());
+  }
+
+  public void assertThatReportPostOptionIsNotAvailable(Post post) {
+    boolean actual = post.getTheNewestPost()
         .clickMoreOptions()
         .hasReportPostOption();
 
