15ff8b3003e61fbe9ff96ce6299b1144c94a95ab
ljedrzejczak <ukaka89@gmail.com>
2016-09-07 10:59:34 +0200

    SUS-700 Remove checking date picker dialog in wam page

--- src/test/java/com/wikia/webdriver/testcases/wampagetests/WamPageTests.java
+++ src/test/java/com/wikia/webdriver/testcases/wampagetests/WamPageTests.java
@@ -63,8 +63,6 @@ public class WamPageTests extends NewTestTemplate {
   @RelatedIssue(issueID = "MAIN-7392", comment = "test manually. Unable to reproduce defect")
   public void wam_005_testDatePicker() {
     wam.verifyLatestDateInDatePicker();
-    String lastMonthDate = wam.changeDateToLastMonth();
-    wam.verifyDateInDatePicker(lastMonthDate);
     String date = "July 12, 2014";
     wam.typeDateInDatePicker(date);
     wam.verifyDateInDatePicker(date);
