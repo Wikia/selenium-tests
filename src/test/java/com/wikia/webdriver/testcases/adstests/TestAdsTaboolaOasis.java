1bfc8e2f795b1d0dca8550a436ff0e976ee022c0
jacek <jacek@wikia-inc.com>
2016-09-12 13:56:07 +0200

    remove unused import

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsTaboolaOasis.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsTaboolaOasis.java
@@ -1,6 +1,5 @@
 package com.wikia.webdriver.testcases.adstests;
 
-import com.wikia.webdriver.common.core.annotations.RelatedIssue;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsTaboolaObject;
 
