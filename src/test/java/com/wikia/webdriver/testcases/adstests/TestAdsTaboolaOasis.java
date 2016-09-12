427ca9b7ca94287ec80f3e2b2fd7b055bac8cb34
jacek <jacek@wikia-inc.com>
2016-09-12 13:28:03 +0200

    check above article

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsTaboolaOasis.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsTaboolaOasis.java
@@ -12,18 +12,13 @@ public class TestAdsTaboolaOasis extends TemplateNoFirstLoad {
   @Test(groups = "AdsTaboolaOasis")
   public void adsTaboolaOasis() {
 
-    String testedPage = urlBuilder.getUrlForPath("adtest", "SyntheticTests/Taboola");
+    String testedPage = urlBuilder.getUrlForPath("project43", "SyntheticTests/Taboola");
     testedPage = urlBuilder.appendQueryStringToURL(testedPage, AdsTaboolaObject.URL_PARAM_TRIGGER);
 
     AdsTaboolaObject adsTaboolaObject = new AdsTaboolaObject(driver);
     adsTaboolaObject.getUrl(testedPage);
 
-    adsTaboolaObject.verifyTaboolaContainer(AdsTaboolaObject.BELOW_ARTICLE_CSS_SELECTOR);
-    adsTaboolaObject.verifyTaboolaAdsPresent(AdsTaboolaObject.BELOW_ARTICLE_CSS_SELECTOR);
-
-//    @TODO Assertions based on configuration: ADEN-2729
-//    adsTaboolaObject.verifyTaboolaContainer(AdsTaboolaObject.RIGHT_RAIL_CSS_SELECTOR);
-//    adsTaboolaObject.verifyTaboolaAdsPresent(AdsTaboolaObject.RIGHT_RAIL_CSS_SELECTOR);
+    adsTaboolaObject.verifyTaboolaContainer(AdsTaboolaObject.ABOVE_ARTICLE_CSS_SELECTOR);
   }
 
 }
