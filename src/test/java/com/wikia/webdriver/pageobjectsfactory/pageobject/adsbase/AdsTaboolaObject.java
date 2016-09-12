fb6704436dc9b2121c4f807df2157fc594fa81ed
jacek <jacek@wikia-inc.com>
2016-09-12 13:31:16 +0200

    check above article

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsTaboolaObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsTaboolaObject.java
@@ -11,6 +11,7 @@ public class AdsTaboolaObject extends AdsBaseObject {
   public static final String URL_PARAM_TRIGGER = "AbTest.NATIVE_ADS_TABOOLA=YES";
   public static final String RIGHT_RAIL_CSS_SELECTOR = "#NATIVE_TABOOLA_RAIL";
   public static final String BELOW_ARTICLE_CSS_SELECTOR = "#NATIVE_TABOOLA_ARTICLE";
+  public static final String ABOVE_ARTICLE_CSS_SELECTOR = "#taboola-above-article-thumbnails";
 
   public AdsTaboolaObject(WebDriver driver) {
     super(driver);
