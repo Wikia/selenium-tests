11e2f7a69c81e39912b2d5f60acf4560ff4749f0
nikodamn <nikodem.hynek@wikia-inc.com>
2016-09-13 15:21:51 +0200

    Remove unused fields and methods from WikiArticleEditMode

--- src/test/java/com/wikia/webdriver/testcases/mediatests/suggestions/VideoSuggestionsTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mediatests/suggestions/VideoSuggestionsTests.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 
 public class VideoSuggestionsTests extends NewTestTemplate {
 
-  @Test(groups = {"VideoSuggestions_001", "VideoSuggestions", "VetTests", "Media"}, invocationCount = 30)
+  @Test(groups = {"VideoSuggestions_001", "VideoSuggestions", "VetTests", "Media"})
   @Execute(onWikia = "callofduty", asUser = User.USER)
   public void Vet_Tests_001_VerifyVideoSuggestionsIsDisplayed() {
     ArticlePageObject article = new ArticlePageObject().open("Frank_Woods");
