87ec36ee7b44a3ca5f80637bbda0540724222239
nAndy <nandy@wikia-inc.com>
2016-09-23 17:16:28 +0200

    ADEN-4006 change the ids to the one we controll

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsTaboolaObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsTaboolaObject.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 public class AdsTaboolaObject extends AdsBaseObject {
 
   public static final String BELOW_ARTICLE_CSS_SELECTOR = "#NATIVE_TABOOLA_ARTICLE";
-  public static final String ABOVE_ARTICLE_CSS_SELECTOR = "#taboola-above-article-thumbnails";
+  public static final String ABOVE_ARTICLE_CSS_SELECTOR = "#TOP_LEADERBOARD_AB";
 
   public AdsTaboolaObject(WebDriver driver) {
     super(driver);
