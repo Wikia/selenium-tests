1445616315efbdd3f5189d2271a0c91fc42e729b
nAndy <nandy@wikia-inc.com>
2016-09-26 17:32:04 +0200

    ADEN-4006 add check for Taboola loader.js

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsTaboolaObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsTaboolaObject.java
@@ -9,6 +9,8 @@ public class AdsTaboolaObject extends AdsBaseObject {
 
   public static final String BELOW_ARTICLE_CSS_SELECTOR = "#NATIVE_TABOOLA_ARTICLE";
   public static final String ABOVE_ARTICLE_CSS_SELECTOR = "#TOP_LEADERBOARD_AB";
+  public static final String TABOOLA_LOADER_REQUEST
+      = "http://cdn.taboola.com/libtrc/wikia-network/loader.js";
 
   public AdsTaboolaObject(WebDriver driver) {
     super(driver);
--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsTaboolaOasis.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsTaboolaOasis.java
@@ -1,5 +1,6 @@
 package com.wikia.webdriver.testcases.adstests;
 
+import com.wikia.webdriver.common.core.annotations.NetworkTrafficDump;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsTaboolaObject;
 
@@ -7,16 +8,23 @@ import org.testng.annotations.Test;
 
 public class TestAdsTaboolaOasis extends TemplateNoFirstLoad {
 
+  @NetworkTrafficDump
   @Test(groups = "AdsTaboolaOasis")
   public void adsTaboolaOasis() {
-
     String testedPage = urlBuilder.getUrlForPath("project43", "SyntheticTests/Taboola");
+
+    networkTrafficInterceptor.startIntercepting();
 
     AdsTaboolaObject adsTaboolaObject = new AdsTaboolaObject(driver);
     adsTaboolaObject.getUrl(testedPage);
 
     adsTaboolaObject.verifyTaboolaContainer(AdsTaboolaObject.ABOVE_ARTICLE_CSS_SELECTOR);
     adsTaboolaObject.verifyTaboolaContainer(AdsTaboolaObject.BELOW_ARTICLE_CSS_SELECTOR);
+
+    adsTaboolaObject.wait.forSuccessfulResponse(
+        networkTrafficInterceptor,
+        AdsTaboolaObject.TABOOLA_LOADER_REQUEST
+    );
   }
 
 }
