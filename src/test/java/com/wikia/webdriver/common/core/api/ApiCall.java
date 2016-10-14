101b170e3592626ffd4e5f6dd13932574dea6492
Sebastian Marzjan <sebastianm@wikia-inc.com>
2016-10-14 14:41:32 +0200

    Fix call logging

--- src/test/java/com/wikia/webdriver/common/core/api/ApiCall.java
+++ src/test/java/com/wikia/webdriver/common/core/api/ApiCall.java
@@ -24,8 +24,6 @@ public abstract class ApiCall {
 
   private static String ERROR_MESSAGE = "Problem with API call";
 
-  protected static String URL_STRING = null;
-
   protected ApiCall() {
   }
 
@@ -61,7 +59,7 @@ public abstract class ApiCall {
 
       httpClient.execute(httpPost);
 
-      PageObjectLogging.log("CONTENT PUSH", "Content posted to: " + URL_STRING, true);
+      PageObjectLogging.log("CONTENT PUSH", "Content posted to: " + getURL(), true);
     } catch (ClientProtocolException e) {
       PageObjectLogging.log("EXCEPTION", ExceptionUtils.getStackTrace(e), false);
       throw new WebDriverException(ERROR_MESSAGE);
