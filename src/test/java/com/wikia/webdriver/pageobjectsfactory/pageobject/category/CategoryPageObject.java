98111eda6e72daa9675f4c3bfd08732ed6518ad7
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2016-10-20 16:39:17 +0200

    CR fixes

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/category/CategoryPageObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/category/CategoryPageObject.java
@@ -3,6 +3,7 @@ package com.wikia.webdriver.pageobjectsfactory.pageobject.category;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -28,7 +29,7 @@ public class CategoryPageObject extends WikiBasePageObject {
    * Get image URL for a specific article from gallery
    * 
    * @param articleName - name of the article
-   * @return null if there is no such article name displayed
+   * @return
    */
   public String getImageURLFromGallery(String articleName) {
     for (WebElement page : categoryGalleryItems) {
@@ -36,7 +37,7 @@ public class CategoryPageObject extends WikiBasePageObject {
         return page.findElement(By.cssSelector("img")).getAttribute("src");
       }
     }
-
-    return null;
+    throw new NoSuchElementException(
+        String.format("Could not find article with name %s", articleName));
   }
 }
