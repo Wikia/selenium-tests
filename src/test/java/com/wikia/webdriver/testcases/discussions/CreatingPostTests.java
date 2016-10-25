d8aa2608a4bc774b47356664fd9bc6f2f9f06913
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-10-25 14:36:40 +0200

    Update CreatingPostTests.java

--- src/test/java/com/wikia/webdriver/testcases/discussions/CreatingPostTests.java
+++ src/test/java/com/wikia/webdriver/testcases/discussions/CreatingPostTests.java
@@ -72,6 +72,7 @@ public class CreatingPostTests extends NewTestTemplate {
   @Test(groups = "discussions-loggedInUsersMobilePosting")
   @Execute(asUser = User.USER)
   @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
+  @RelatedIssue(issueID = "SOC-3267")
   public void userOnMobileCanAddPostWithoutTitle() {
     final String description = TextGenerator.createUniqueText();
 
@@ -124,6 +125,7 @@ public class CreatingPostTests extends NewTestTemplate {
   @Test(groups = "discussions-loggedInUsersDesktopPosting")
   @Execute(asUser = User.USER)
   @InBrowser(browser = Browser.FIREFOX, browserSize = DESKTOP_RESOLUTION)
+  @RelatedIssue(issueID = "SOC-3267")
   public void userOnDesktopCanAddPostWithoutTitle() {
     final String description = TextGenerator.createUniqueText();
 
