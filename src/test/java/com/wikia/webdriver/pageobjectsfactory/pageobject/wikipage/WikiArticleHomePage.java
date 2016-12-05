9404cd4bbba67224d2afa6eec5f3689b21bd4ef8
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2016-12-05 16:36:30 +0100

    Fixed Home page check

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/wikipage/WikiArticleHomePage.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/wikipage/WikiArticleHomePage.java
@@ -1,22 +1,17 @@
 package com.wikia.webdriver.pageobjectsfactory.pageobject.wikipage;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.By;
 
 import com.wikia.webdriver.pageobjectsfactory.pageobject.WikiBasePageObject;
 
-/**
- * Author: Artur Dwornik Date: 02.04.13 Time: 17:31
- */
 public class WikiArticleHomePage extends WikiBasePageObject {
 
-  @FindBy(css = "#HOME_TOP_RIGHT_BOXAD")
-  private WebElement wikiHomePageSpecificElement;
+  private By wikiHomePageSpecificElement = By.cssSelector("#HOME_TOP_LEADERBOARD");
 
   /**
    * Check if current page is in fact home page of wiki
    */
   public void verifyThisIsWikiHomePage() {
-    wait.forElementVisible(wikiHomePageSpecificElement);
+    wait.forElementPresent(wikiHomePageSpecificElement);
   }
 }
