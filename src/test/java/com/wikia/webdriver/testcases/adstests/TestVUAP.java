12e38b553c02f4bd7845ff41d9c9445cb9fa3ba2
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2016-12-07 15:11:03 +0100

    ADEN-4330 Extract data provider

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestVUAP.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestVUAP.java
@@ -2,6 +2,7 @@ package com.wikia.webdriver.testcases.adstests;
 
 import com.wikia.webdriver.common.core.annotations.NetworkTrafficDump;
 import com.wikia.webdriver.common.core.url.Page;
+import com.wikia.webdriver.common.dataprovider.ads.AdsDataProvider;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.componentobject.ad.VUAP;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
@@ -10,16 +11,16 @@ import org.testng.annotations.Test;
 
 public class TestVUAP extends TemplateNoFirstLoad {
   private static final Dimension DESKTOP_SIZE = new Dimension(1920, 1080);
-  public static final String slotName = "TOP_LEADERBOARD";
 
   @Test(
-          groups = "AdsUapDesktop"
+          dataProviderClass = AdsDataProvider.class,
+          dataProvider = "adsVUAPDesktop",
+          groups = "AdsVuapDesktop"
   )
-  public void testVideoPlayFlow() {
-    Page page = new Page("project43", "SyntheticTests/VUAP");
+  public void testVideoPlayFlow(String slotName, Page page) {
     AdsBaseObject ads = new AdsBaseObject(driver, urlBuilder.getUrlForPage(page), DESKTOP_SIZE);
 
-    VUAP vuap = new VUAP(driver, "TOP_LEADERBOARD");
+    VUAP vuap = new VUAP(driver, slotName);
     vuap.play();
 
     vuap.waitForVideoPlayerVisible();
@@ -28,9 +29,11 @@ public class TestVUAP extends TemplateNoFirstLoad {
 
   @NetworkTrafficDump
   @Test(
-          groups = "AdsUapDesktop"
+          dataProviderClass = AdsDataProvider.class,
+          dataProvider = "adsVUAPDesktop",
+          groups = "AdsVuapDesktop"
   )
-  public void testSlotSizes() {
+  public void testSlotSizes(String slotName, Page page) {
     final int IMAGE_WIDTH = 1830;
     final int IMAGE_HEIGHT = 744;
 
@@ -39,7 +42,6 @@ public class TestVUAP extends TemplateNoFirstLoad {
 
     networkTrafficInterceptor.startIntercepting();
 
-    Page page = new Page("project43", "SyntheticTests/VUAP");
     AdsBaseObject ads = new AdsBaseObject(driver, urlBuilder.getUrlForPage(page), DESKTOP_SIZE);
 
     VUAP vuap = new VUAP(driver, slotName);
