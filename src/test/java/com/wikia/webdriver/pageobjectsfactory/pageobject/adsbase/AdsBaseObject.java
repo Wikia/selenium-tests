a4358bfe55bb530283ab9447f5d4932e75d48e21
nAndy <nandy@wikia-inc.com>
2016-12-15 14:57:05 +0100

    ADEN-4383 refactored verifyGptIframe()

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsBaseObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsBaseObject.java
@@ -328,6 +328,25 @@ public class AdsBaseObject extends WikiBasePageObject {
   }
 
   /**
+   * Builds GPT iframe id
+   *
+   * @param dfpClientId in most cases it's Wikia id but we have other partners like Evolve or
+   *                    Turtle
+   * @param adUnit      the ad unit passed to GPT, like wka.wikia/_wikiaglobal//home
+   * @param slotName    the name of the slot an ad is going to be inserted into
+   * @param src         the source of an ad, for example gpt, remnant or empty
+   */
+  public String buildGptIframeId(int dfpClientId, String adUnit, String slotName, String... src) {
+    return Joiner.on("/").skipNulls().join(
+        "google_ads_iframe_",
+        String.valueOf(dfpClientId),
+        adUnit,
+        src.length > 0 ? src[0] : null,
+        slotName + "_0"
+    );
+  }
+
+  /**
    * Test whether the correct GPT ad unit is called
    *
    * @param dfpClientId in most cases it's Wikia id but we have other partners like Evolve or
@@ -337,15 +356,11 @@ public class AdsBaseObject extends WikiBasePageObject {
    * @param src         the source of an ad, for example gpt, remnant or empty
    */
   public void verifyGptIframe(int dfpClientId, String adUnit, String slotName, String... src) {
-    String iframeId = Joiner.on("/").skipNulls().join(
-        "google_ads_iframe_",
-        String.valueOf(dfpClientId),
+    By cssSelector = By.cssSelector("iframe[id^='" + buildGptIframeId(
+        dfpClientId,
         adUnit,
-        src.length > 0 ? src[0] : null,
-        slotName + "_0"
-    );
-
-    By cssSelector = By.cssSelector("iframe[id^='" + iframeId + "']");
+        slotName,
+        src) + "']");
 
     wait.forElementPresent(cssSelector);
 
