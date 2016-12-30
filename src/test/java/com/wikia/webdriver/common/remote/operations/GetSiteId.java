1c774ffd89a63c458892ef04bd3e7edbb8e78ca3
Oliwia Golec <ogolec@wikia-inc.com>
2016-12-30 19:48:04 +0100

    NPE because someone decided that it's a good idea to set environment in @BeforeMethod. This means that one cannot possibly run any fixtures in @BeforeSuite. How come I can run fixtures otherwise?!

---
+++ src/test/java/com/wikia/webdriver/common/remote/operations/GetSiteId.java
@@ -0,0 +1,27 @@
+package com.wikia.webdriver.common.remote.operations;
+
+
+import com.wikia.webdriver.common.contentpatterns.URLsContent;
+import com.wikia.webdriver.common.remote.operations.http.NoAuthOperation;
+import lombok.Getter;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.json.JSONObject;
+
+public class GetSiteId {
+
+  public GetSiteId(String wikiUrl) {
+    this.wikiUrl = wikiUrl;
+    this.setSiteId();
+  }
+
+  @Getter
+  private String siteId;
+  private String wikiUrl;
+
+  private void setSiteId() {
+    NoAuthOperation request = new NoAuthOperation();
+    String response = request.execute(this.wikiUrl + URLsContent.SPECIAL_VERSION, new JSONObject());
+    this.siteId = response;
+  }
+}
---
+++ src/test/java/com/wikia/webdriver/common/remote/operations/http/NoAuthOperation.java
@@ -0,0 +1,7 @@
+package com.wikia.webdriver.common.remote.operations.http;
+
+/**
+ * Created by ogolec on 30.12.16.
+ */
+public class NoAuthOperation {
+}
