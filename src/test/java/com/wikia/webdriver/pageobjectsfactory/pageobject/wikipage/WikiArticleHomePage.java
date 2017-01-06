6f69598e472c6820ec9c0e0ed2e0e798fb9f154c
dianafa <diana.falkowska@gmail.com>
2017-01-06 12:09:39 +0100

    ADEN-4427
    change cssSelector to className

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/wikipage/WikiArticleHomePage.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/wikipage/WikiArticleHomePage.java
@@ -6,7 +6,7 @@ import com.wikia.webdriver.pageobjectsfactory.pageobject.WikiBasePageObject;
 
 public class WikiArticleHomePage extends WikiBasePageObject {
 
-  private By wikiHomePageSpecificElement = By.cssSelector(".mainpage");
+  private By wikiHomePageSpecificElement = By.className("mainpage");
 
   /**
    * Check if current page is in fact home page of wiki
