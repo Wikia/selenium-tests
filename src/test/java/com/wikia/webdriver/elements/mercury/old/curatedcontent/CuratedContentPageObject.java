01bebd2587d5a32c8f6e2a6b8c52f97b3f51f08c
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2017-01-11 09:48:37 +0100

    XW-2577 | Fix imports

--- src/test/java/com/wikia/webdriver/elements/mercury/old/curatedcontent/CuratedContentPageObject.java
+++ src/test/java/com/wikia/webdriver/elements/mercury/old/curatedcontent/CuratedContentPageObject.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 /**
  * This class represents all the levels below Curated Main Page
  */
