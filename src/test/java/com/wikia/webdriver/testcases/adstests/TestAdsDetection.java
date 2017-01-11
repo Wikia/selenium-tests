22cf5187ec2c1e8b171665727afdd53cb8330770
Bogna Knychala <bogna.ka@gmail.com>
2017-01-11 11:30:07 +0100

    ADEN | remove unused import

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsDetection.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsDetection.java
@@ -3,7 +3,6 @@ package com.wikia.webdriver.testcases.adstests;
 import com.wikia.webdriver.common.core.annotations.NetworkTrafficDump;
 import com.wikia.webdriver.common.dataprovider.ads.AdsDataProvider;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
-import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
 
 import org.testng.annotations.Test;
