1f0493ba12dce21f0c454f8d9dc4098434755e60
Oliwia Golec <ogolec@wikia-inc.com>
2017-04-21 16:31:37 +0200

    IRIS-3966 | added top-level action for marking all OSN as read

--- src/test/java/com/wikia/webdriver/common/remote/notifications/NotificationsOperations.java
+++ src/test/java/com/wikia/webdriver/common/remote/notifications/NotificationsOperations.java
@@ -1,13 +1,21 @@
 package com.wikia.webdriver.common.remote.notifications;
 
+import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.common.remote.Utils;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor(staticName = "using")
 public class NotificationsOperations {
 
   private final static String NOTIFICATIONS_SERVICE = "notifications/";
+  private final User user;
 
   public static String service(String url) {
     return Utils.buildServicesUrl() + NOTIFICATIONS_SERVICE + url;
+  }
+
+  public void markAllAsRead() {
+    new MarkAsRead(user).execute();
   }
 
 }
