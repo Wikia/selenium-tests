e4a126c7c45d5dadf932fbccbc8cfb8e69902a6a
Oliwia Golec <ogolec@wikia-inc.com>
2017-04-26 17:47:23 +0200

    IRIS-3966 | added tests stub

---
+++ src/test/java/com/wikia/webdriver/testcases/onsitenotifications/OnSiteNotificationsTests.java
@@ -0,0 +1,34 @@
+package com.wikia.webdriver.testcases.onsitenotifications;
+
+import com.wikia.webdriver.common.contentpatterns.MercuryWikis;
+import com.wikia.webdriver.common.core.annotations.Execute;
+import com.wikia.webdriver.common.core.helpers.User;
+import com.wikia.webdriver.common.remote.Utils;
+import com.wikia.webdriver.common.remote.discussions.DiscussionsOperations;
+import com.wikia.webdriver.common.templates.NewTestTemplate;
+import com.wikia.webdriver.elements.mercury.components.discussions.common.PostEntity;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Test(groups = "on-site-notifications")
+public class OnSiteNotificationsTests extends NewTestTemplate {
+
+  private String siteId;
+  private static final String wikiName = MercuryWikis.DISCUSSIONS_3;
+
+  @BeforeClass
+  public void setUp() {
+    siteId = Utils.excractSiteIdFromWikiName(wikiName);
+  }
+
+  @Execute(asUser = User.USER)
+  public void userOnDesktopReceivesPostReplyNotification() {
+
+  }
+
+  private PostEntity.Data createPost() {
+    return DiscussionsOperations.using(User.USER, driver).createPostWithUniqueData(siteId);
+  }
+
+  private PostEntity.Data
+}
