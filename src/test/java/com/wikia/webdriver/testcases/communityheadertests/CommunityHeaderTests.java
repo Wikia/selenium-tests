493645799afae492a3b3a48ac53715435b659485
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-05-30 10:49:55 +0200

    XW-3300: test class stub, listed test cases

---
+++ src/test/java/com/wikia/webdriver/testcases/communityheadertests/CommunityHeaderTests.java
@@ -0,0 +1,22 @@
+package com.wikia.webdriver.testcases.communityheadertests;
+
+import com.wikia.webdriver.common.templates.NewTestTemplate;
+
+
+public class CommunityHeaderTests extends NewTestTemplate {
+  // TODO: Test cases:
+  // - wordmark link links to main page
+  // - wiki name links to main page
+  // - buttons for anon links to proper places
+  // - buttons for logged in links to proper places
+  // - buttons for admin links to proper places
+  // - links in explore links to proper places
+  // - disscuss link logic
+  //    - for wiki with discussions enabled
+  //    - for wiki with forum and discussions enabled
+  //    - for wiki with only forum enabled
+  //    - for wiki without forum or discussions
+  // - visibility of elements for bigger breakpoint
+  // - visibility of elements for lower breakpoint
+
+}
