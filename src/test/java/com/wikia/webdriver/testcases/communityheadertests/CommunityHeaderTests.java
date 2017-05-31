28e3d858ce0e6516e728788ab0395b12c207f2aa
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-05-31 10:35:24 +0200

    XW-3300: Test annotations, testSuite.xml entry

--- src/test/java/com/wikia/webdriver/testcases/communityheadertests/CommunityHeaderTests.java
+++ src/test/java/com/wikia/webdriver/testcases/communityheadertests/CommunityHeaderTests.java
@@ -9,20 +9,23 @@ import com.wikia.webdriver.pageobjectsfactory.pageobject.oasis.MainPage;
 import junit.framework.Assert;
 import org.testng.annotations.Test;
 
-@Test(groups = {"CommunityHeaderTests"})
 public class CommunityHeaderTests extends NewTestTemplate {
+
+  @Test(groups = {"CommunityHeaderTests"})
   public void wordmarkShouldLinkToMainPage() {
     MainPage mainPage = new CommunityHeader().clickWordmark();
 
     Assert.assertTrue(mainPage.isMainPage());
   }
 
+  @Test(groups = {"CommunityHeaderTests"})
   public void wikiNameShouldLinkToMainPage() {
     MainPage mainPage = new CommunityHeader().clickWikiName();
 
     Assert.assertTrue(mainPage.isMainPage());
   }
 
+  @Test(groups = {"CommunityHeaderTests"})
   @Execute(asUser = User.ANONYMOUS)
   public void testAnonWikiButtons() {
     new CommunityHeader().clickAddNewPage();
@@ -30,6 +33,7 @@ public class CommunityHeaderTests extends NewTestTemplate {
     Assert.assertTrue(driver.getCurrentUrl().contains("Special:CreatePage"));
   }
 
+  @Test(groups = {"CommunityHeaderTests"})
   @Execute(asUser = User.USER)
   public void testLoggedInWikiButtons() {
     CommunityHeader communityHeader = new CommunityHeader();
@@ -41,6 +45,7 @@ public class CommunityHeaderTests extends NewTestTemplate {
     Assert.assertTrue(driver.getCurrentUrl().contains("Special:CreatePage"));
   }
 
+  @Test(groups = {"CommunityHeaderTests"})
   @Execute(asUser = User.MW119_ADMINISTRATOR)
   public void testAdminWikiButtons() {
     CommunityHeader communityHeader = new CommunityHeader();
@@ -55,6 +60,7 @@ public class CommunityHeaderTests extends NewTestTemplate {
     Assert.assertTrue(driver.getCurrentUrl().contains("Special:CreatePage"));
   }
 
+  @Test(groups = {"CommunityHeaderTests"})
   public void testExploreMenuLinks() {
     CommunityHeader communityHeader = new CommunityHeader();
 
@@ -90,6 +96,7 @@ public class CommunityHeaderTests extends NewTestTemplate {
 
   }
 
+  @Test(groups = {"CommunityHeaderTests"})
   @Execute(onWikia = "qatestdiscussionsnoforum")
   public void testDiscussLinkOnWikiWithDiscussionsWithoutForum() {
     new CommunityHeader().clickDiscussLink();
@@ -97,6 +104,7 @@ public class CommunityHeaderTests extends NewTestTemplate {
     Assert.assertTrue(driver.getCurrentUrl().contains("wikia.com/d/f"));
   }
 
+  @Test(groups = {"CommunityHeaderTests"})
   @Execute(onWikia = "qatestdiscussiosandforum")
   public void testDiscussLinkOnWikiWithDiscussionsAndForum() {
     CommunityHeader communityHeader = new CommunityHeader();
@@ -110,6 +118,7 @@ public class CommunityHeaderTests extends NewTestTemplate {
     Assert.assertTrue(driver.getCurrentUrl().contains("wikia.com/d/f"));
   }
 
+  @Test(groups = {"CommunityHeaderTests"})
   @Execute(onWikia = "qatestforumnodiscussions")
   public void testDiscussLinkOnWikiWithNoDiscussionsAndWithForum() {
     new CommunityHeader().clickDiscussLink();
@@ -117,6 +126,7 @@ public class CommunityHeaderTests extends NewTestTemplate {
     Assert.assertTrue(driver.getCurrentUrl().contains("Special:Forum"));
   }
 
+  @Test(groups = {"CommunityHeaderTests"})
   @Execute(onWikia = "qatestnodiscussionsnoforum")
   public void testNoDiscussLinkOnWikiWithNoDiscussionsAndWithNoForum() {
     CommunityHeader communityHeader = new CommunityHeader();
