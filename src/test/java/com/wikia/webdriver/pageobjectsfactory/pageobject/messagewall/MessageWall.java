372e0654ee73aaa0a71e5c94819da9d4a2e9eb86
Szymon Czerwinski <sczerwinski@wikia-inc.com>
2017-06-08 14:58:31 +0200

    Added sleep before grabbing attribute

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/messagewall/MessageWall.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/messagewall/MessageWall.java
@@ -95,6 +95,11 @@ public class MessageWall extends WikiBasePageObject {
 //    PageObjectLogging.log("class before wait", classes, true, driver);
     //wait.forElementPresent(By.cssSelector("#wall-new-message.focused"));
 //    wait.forElementPresent(By.xpath("//div[@id='wall-new-message' and contains(@class,'focused')]"));
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     wait.forAttributeToContain(newWallMessageTextBox, "class", "focused");
 //    String classes2 = driver.findElement(By.cssSelector("#wall-new-message")).getAttribute("class");
 //    PageObjectLogging.log("class after wait", classes2, true, driver);
