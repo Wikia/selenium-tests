3dbd9c0b489c580fae5cc00136f360eb34a9fa94
nikodamn <nikodem.hynek@wikia-inc.com>
2017-06-21 17:51:29 +0200

    Update Spotify tests

--- src/test/java/com/wikia/webdriver/testcases/mercurytests/old/widgettests/SpotifyTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mercurytests/old/widgettests/SpotifyTests.java
@@ -6,6 +6,7 @@ import com.wikia.webdriver.common.contentpatterns.MercuryWikis;
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
+import com.wikia.webdriver.common.core.api.ArticleContent;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.helpers.User;
@@ -29,11 +30,13 @@ public class SpotifyTests extends NewTestTemplate {
   private Navigate navigate;
   private SpotifyWidgetPageObject widget;
 
-  private static final String SPOTIFY_ONE_WIDGET_ARTICLE_NAME = "/wiki/SpotifyMercury/OneWidget";
-  private static final String SPOTIFY_MULTIPLE_WIDGETS_ARTICLE_NAME = "/wiki/SpotifyMercury/MultipleWidgets";
-  private static final String SPOTIFY_INCORRECT_WIDGET_ARTICLE_NAME = "/wiki/SpotifyMercury/IncorrectWidget";
+  private static final String SPOTIFY_ONE_WIDGET_ARTICLE_NAME = "/SpotifyMercury/OneWidget";
+  private static final String SPOTIFY_MULTIPLE_WIDGETS_ARTICLE_NAME = "/SpotifyMercury/MultipleWidgets";
+  private static final String SPOTIFY_INCORRECT_WIDGET_ARTICLE_NAME = "/SpotifyMercury/IncorrectWidget";
   private static final String QUERY_1 = MercurySubpages.MAP.substring(6);
-  private static final String QUERY_2 = SPOTIFY_ONE_WIDGET_ARTICLE_NAME.substring(6);
+  private static final String QUERY_2 = SPOTIFY_ONE_WIDGET_ARTICLE_NAME;
+  private static final String VALID_SPOTIFY_TAG = "<spotify uri=\"spotify:track:5JunxkcjfCYcY7xJ29tLai\" />\n";
+  private static final String INVALID_SPOTIFY_TAG = "<spotify />";
 
   private void init() {
     this.topBar = new TopBar();
@@ -45,9 +48,9 @@ public class SpotifyTests extends NewTestTemplate {
   @Test(groups = "MercurySpotifyWidgetTest_001")
   @Execute(asUser = User.USER)
   public void MercurySpotifyWidgetTest_001_isLoadedOnFirstVisitDirectlyFromUrl() {
+    new ArticleContent().push(VALID_SPOTIFY_TAG, SPOTIFY_ONE_WIDGET_ARTICLE_NAME);
     init();
 
-    widget.create(SPOTIFY_ONE_WIDGET_ARTICLE_NAME);
     navigate.toPage(SPOTIFY_ONE_WIDGET_ARTICLE_NAME);
 
     Assertion.assertTrue(widget.isLoaded(), MercuryMessages.INVISIBLE_MSG);
@@ -56,9 +59,9 @@ public class SpotifyTests extends NewTestTemplate {
   @Test(groups = "MercurySpotifyWidgetTest_002")
   @Execute(asUser = User.USER)
   public void MercurySpotifyWidgetTest_002_isLoadedOnFirstVisitFromDifferentArticle() {
+    new ArticleContent().push(VALID_SPOTIFY_TAG, SPOTIFY_ONE_WIDGET_ARTICLE_NAME);
     init();
 
-    widget.create(SPOTIFY_ONE_WIDGET_ARTICLE_NAME);
     navigate.toPage(MercurySubpages.MAIN_PAGE);
     topBar.openSearch().navigateToPage(QUERY_2);
 
@@ -68,9 +71,10 @@ public class SpotifyTests extends NewTestTemplate {
   @Test(groups = "MercurySpotifyWidgetTest_003")
   @Execute(asUser = User.USER)
   public void MercurySpotifyWidgetTest_003_isLoadedOnSecondVisitFromDifferentArticle() {
+    new ArticleContent().push(VALID_SPOTIFY_TAG, SPOTIFY_ONE_WIDGET_ARTICLE_NAME);
+    new ArticleContent().push("Spotify test 003", "Map");
     init();
 
-    widget.create(SPOTIFY_ONE_WIDGET_ARTICLE_NAME);
     navigate.toPage(SPOTIFY_ONE_WIDGET_ARTICLE_NAME);
     topBar.openSearch().navigateToPage(QUERY_1);
     topBar.openSearch().navigateToPage(QUERY_2);
@@ -81,9 +85,10 @@ public class SpotifyTests extends NewTestTemplate {
   @Test(groups = "MercurySpotifyWidgetTest_004")
   @Execute(asUser = User.USER)
   public void MercurySpotifyWidgetTest_004_areLoadedOnFirstVisitDirectlyFromUrl() {
+    new ArticleContent().push(VALID_SPOTIFY_TAG + " " + VALID_SPOTIFY_TAG,
+            SPOTIFY_MULTIPLE_WIDGETS_ARTICLE_NAME);
     init();
 
-    widget.createMultiple(SPOTIFY_MULTIPLE_WIDGETS_ARTICLE_NAME);
     navigate.toPage(SPOTIFY_MULTIPLE_WIDGETS_ARTICLE_NAME);
 
     Assertion.assertTrue(widget.areLoaded(), MercuryMessages.INVISIBLE_MSG);
@@ -92,9 +97,9 @@ public class SpotifyTests extends NewTestTemplate {
   @Test(groups = "MercurySpotifyWidgetTest_005")
   @Execute(asUser = User.USER)
   public void MercurySpotifyWidgetTest_005_isErrorPresent() {
+    new ArticleContent().push(INVALID_SPOTIFY_TAG, SPOTIFY_INCORRECT_WIDGET_ARTICLE_NAME);
     init();
 
-    widget.createIncorrect(SPOTIFY_INCORRECT_WIDGET_ARTICLE_NAME);
     navigate.toPage(SPOTIFY_INCORRECT_WIDGET_ARTICLE_NAME);
 
     Assertion.assertTrue(widget.isErrorPresent(), MercuryMessages.INVISIBLE_MSG);
