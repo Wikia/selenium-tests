d59eeeae81377e81a2297d3d8d1bd88e0e5e7bc9
nikodamn <nikodem.hynek@wikia-inc.com>
2017-06-21 17:48:59 +0200

    Refactor soundcloud test

--- src/test/java/com/wikia/webdriver/testcases/mercurytests/old/widgettests/SoundCloudTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mercurytests/old/widgettests/SoundCloudTests.java
@@ -6,6 +6,7 @@ import com.wikia.webdriver.common.contentpatterns.MercuryWikis;
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
+import com.wikia.webdriver.common.core.api.ArticleContent;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
@@ -28,10 +29,12 @@ public class SoundCloudTests extends NewTestTemplate {
   private SoundCloudWidgetPageObject widget;
   private Navigate navigate;
 
-  private static final String SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME = "/wiki/SoundCloudMercury/OneWidget";
-  private static final String SOUND_CLOUD_MULTIPLE_WIDGETS_ARTICLE_NAME = "/wiki/SoundCloudMercury/MultipleWidgets";
+  private static final String SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME = "/SoundCloudMercury/OneWidget";
+  private static final String SOUND_CLOUD_MULTIPLE_WIDGETS_ARTICLE_NAME = "/SoundCloudMercury/MultipleWidgets";
   private static final String QUERY_1 = MercurySubpages.MAP.substring(6);
-  private static final String QUERY_2 = SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME.substring(6);
+  private static final String QUERY_2 = SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME;
+  private static final String
+          VALID_SOUNDCLOUD_TAG = "<soundcloud width=\"100%\" height=\"166\" scrolling=\"no\" frameborder=\"no\" url=\"https://api.soundcloud.com/tracks/34019569\" color=\"0066cc\" />";
 
   private void init() {
     this.topBar = new TopBar();
@@ -42,9 +45,9 @@ public class SoundCloudTests extends NewTestTemplate {
 
   @Test(groups = "MercurySoundCloudWidgetTest_001")
   public void MercurySoundCloudWidgetTest_001_isLoadedOnFirstVisitDirectlyFromUrl() {
+    new ArticleContent().push(VALID_SOUNDCLOUD_TAG, SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME);
     init();
 
-    widget.create(SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME);
     navigate.toPage(SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME);
 
     Assertion.assertTrue(widget.isLoaded(), MercuryMessages.INVISIBLE_MSG);
@@ -52,9 +55,9 @@ public class SoundCloudTests extends NewTestTemplate {
 
   @Test(groups = "MercurySoundCloudWidgetTest_002")
   public void MercurySoundCloudWidgetTest_002_isLoadedOnFirstVisitFromDifferentArticle() {
+    new ArticleContent().push(VALID_SOUNDCLOUD_TAG, SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME);
     init();
 
-    widget.create(SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME);
     navigate.toPage(SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME);
     topBar.openSearch().navigateToPage(QUERY_2);
 
@@ -63,9 +66,10 @@ public class SoundCloudTests extends NewTestTemplate {
 
   @Test(groups = "MercurySoundCloudWidgetTest_003")
   public void MercurySoundCloudWidgetTest_003_isLoadedOnSecondVisitFromDifferentArticle() {
+    new ArticleContent().push(VALID_SOUNDCLOUD_TAG, SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME);
+    new ArticleContent().push("SoundCloud test 003", "Map");
     init();
 
-    widget.create(SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME);
     navigate.toPage(SOUND_CLOUD_ONE_WIDGET_ARTICLE_NAME);
     topBar.openSearch().navigateToPage(QUERY_1);
     topBar.openSearch().navigateToPage(QUERY_2);
@@ -75,9 +79,10 @@ public class SoundCloudTests extends NewTestTemplate {
 
   @Test(groups = "MercurySoundcloudWidgetTest_004")
   public void MercurySoundcloudWidgetTest_004_areLoadedOnFirstVisitDirectlyFromUrl() {
+    new ArticleContent().push(VALID_SOUNDCLOUD_TAG + " " + VALID_SOUNDCLOUD_TAG,
+            SOUND_CLOUD_MULTIPLE_WIDGETS_ARTICLE_NAME);
     init();
 
-    widget.createMultiple(SOUND_CLOUD_MULTIPLE_WIDGETS_ARTICLE_NAME);
     navigate.toPage(SOUND_CLOUD_MULTIPLE_WIDGETS_ARTICLE_NAME);
 
     Assertion.assertTrue(widget.areLoaded(), MercuryMessages.INVISIBLE_MSG);
