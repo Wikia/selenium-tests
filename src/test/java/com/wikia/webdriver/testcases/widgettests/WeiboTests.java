257306e831873d6c7f4dc73accf30e4948c4524b
nikodamn <nikodem.hynek@wikia-inc.com>
2017-06-26 16:30:41 +0200

    Fix Weibo widget tests

--- src/test/java/com/wikia/webdriver/testcases/widgettests/WeiboTests.java
+++ src/test/java/com/wikia/webdriver/testcases/widgettests/WeiboTests.java
@@ -1,41 +1,32 @@
 package com.wikia.webdriver.testcases.widgettests;
 
-import org.testng.annotations.Test;
-
 import com.wikia.webdriver.common.contentpatterns.MercuryMessages;
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
-import com.wikia.webdriver.elements.common.Navigate;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.article.ArticlePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.WeiboWidgetPageObject;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.widget.WidgetPageObject;
+import org.testng.annotations.Test;
 
 @Test(groups = "WeiboWidget")
 @InBrowser(browser = Browser.CHROME)
 public class WeiboTests extends NewTestTemplate {
 
-  private static final String WEIBO_ONE_WIDGET_ARTICLE_NAME = "/wiki/WeiboOasis/OneWidget";
+  private static final String WEIBO_ONE_WIDGET_ARTICLE_NAME = "WeiboOasis_OneWidget";
   private static final String WEIBO_MULTIPLE_WIDGETS_ARTICLE_NAME =
-      "/wiki/WeiboOasis/MultipleWidgets";
+      "WeiboOasis_MultipleWidgets";
   private static final String WEIBO_INCORRECT_WIDGET_ARTICLE_NAME =
-      "/wiki/WeiboOasis/IncorrectWidget";
-
-  private WeiboWidgetPageObject widget;
-  private Navigate navigate;
-
-  private void init() {
-    this.widget = new WeiboWidgetPageObject();
-    this.navigate = new Navigate();
-  }
+      "WeiboOasis_IncorrectWidget";
 
   @Test(groups = "WeiboWidgetTest_001")
   @Execute(onWikia = "mercuryautomationtesting")
   public void WeiboWidgetTest_001_isLoaded() {
-    init();
-
-    widget.create(WEIBO_ONE_WIDGET_ARTICLE_NAME);
-    navigate.toPage(WEIBO_ONE_WIDGET_ARTICLE_NAME);
+    WidgetPageObject widget =
+            new WeiboWidgetPageObject().create(WEIBO_ONE_WIDGET_ARTICLE_NAME);
+    new ArticlePageObject().open(WEIBO_ONE_WIDGET_ARTICLE_NAME);
 
     Assertion.assertTrue(widget.isLoaded(), MercuryMessages.INVISIBLE_MSG);
   }
@@ -43,10 +34,9 @@ public class WeiboTests extends NewTestTemplate {
   @Test(groups = "WeiboWidgetTest_002")
   @Execute(onWikia = "mercuryautomationtesting")
   public void WeiboWidgetTest_002_areLoaded() {
-    init();
-
-    widget.createMultiple(WEIBO_MULTIPLE_WIDGETS_ARTICLE_NAME);
-    navigate.toPage(WEIBO_MULTIPLE_WIDGETS_ARTICLE_NAME);
+    WidgetPageObject widget =
+            new WeiboWidgetPageObject().createMultiple(WEIBO_MULTIPLE_WIDGETS_ARTICLE_NAME);
+    new ArticlePageObject().open(WEIBO_MULTIPLE_WIDGETS_ARTICLE_NAME);
 
     Assertion.assertTrue(widget.areLoaded(), MercuryMessages.INVISIBLE_MSG);
   }
@@ -54,10 +44,9 @@ public class WeiboTests extends NewTestTemplate {
   @Test(groups = "WeiboWidgetTest_003")
   @Execute(onWikia = "mercuryautomationtesting")
   public void WeiboWidgetTest_003_isErrorPresent() {
-    init();
-
-    widget.createIncorrect(WEIBO_INCORRECT_WIDGET_ARTICLE_NAME);
-    navigate.toPage(WEIBO_INCORRECT_WIDGET_ARTICLE_NAME);
+    WidgetPageObject widget =
+            new WeiboWidgetPageObject().createIncorrect(WEIBO_INCORRECT_WIDGET_ARTICLE_NAME);
+    new ArticlePageObject().open(WEIBO_INCORRECT_WIDGET_ARTICLE_NAME);
 
     Assertion.assertTrue(widget.isErrorPresent(), MercuryMessages.INVISIBLE_MSG);
   }
