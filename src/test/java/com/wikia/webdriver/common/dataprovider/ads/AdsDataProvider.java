ab7a3f1c38115bd47a02102e64eb8b10836598c4
nAndy <nandy@wikia-inc.com>
2017-06-29 18:20:21 +0200

    Revert "Revert "Revert "ADEN-5452 | Remove SourcePoint tests"""
    This reverts commit 717bc788ffef43275a3df43849250ecc8f8f4906.

--- src/test/java/com/wikia/webdriver/common/dataprovider/ads/AdsDataProvider.java
+++ src/test/java/com/wikia/webdriver/common/dataprovider/ads/AdsDataProvider.java
@@ -1,13 +1,12 @@
 package com.wikia.webdriver.common.dataprovider.ads;
 
+import com.google.common.collect.ImmutableMap;
 import com.wikia.webdriver.common.WindowSize;
 import com.wikia.webdriver.common.contentpatterns.AdsContent;
 import com.wikia.webdriver.common.contentpatterns.MercuryWikis;
 import com.wikia.webdriver.common.core.url.Page;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.helpers.AdsVeles;
 import com.wikia.webdriver.testcases.adstests.TestAdsTrackingPixels;
-
-import com.google.common.collect.ImmutableMap;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
@@ -32,6 +31,9 @@ public class AdsDataProvider {
 
   private static final String SKIN_LEFT = "src/test/resources/adsResources/wikia_skin_left.png";
   private static final String SKIN_RIGHT = "src/test/resources/adsResources/wikia_skin_right.png";
+
+  private static final String SOURCE_POINT_INSTANT_GLOBAL = "?InstantGlobals.wgAdDriverSourcePointRecoveryCountries=[XX]" +
+      "&?InstantGlobals.wgAdDriverPageFairRecoveryCountries=[]";
 
   private static final String PORVATA_OVERRIDE_VAST_QUERY_STRING = "?porvata_override_vast=1";
 
@@ -791,6 +793,22 @@ public class AdsDataProvider {
   }
 
   @DataProvider
+  public static Object[][] adsDetection() {
+    return new Object[][]{
+        {
+            new Page("project43", "Project43_Wikia"),
+            "InstantGlobals.wgAdDriverSourcePointRecoveryCountries=[]&InstantGlobals.wgAdDriverPageFairRecoveryCountries=[]",
+            false
+        },
+        {
+            new Page("arecovery", "ARecovery_Wikia"),
+            "InstantGlobals.wgAdDriverSourcePointRecoveryCountries=[XX]&InstantGlobals.wgAdDriverPageFairRecoveryCountries=[]",
+            true
+        }
+    };
+  }
+
+  @DataProvider
   public static Object[][] adsSlotSizeOasis() {
     return new Object[][]{
         {
@@ -927,12 +945,60 @@ public class AdsDataProvider {
   }
 
   @DataProvider
+  public static Object[][] adsRecoverySourcePointOasis() {
+    return new Object[][]{
+        {
+            new Page("arecovery", "SyntheticTests/Static_image" + SOURCE_POINT_INSTANT_GLOBAL),
+            ImmutableMap.<String, Object>builder()
+                .put("adUnitId", "wikia_gpt/5441/wka.life/_arecovery//article/gpt/TOP_LEADERBOARD")
+                .put("slotName", AdsContent.TOP_LB)
+                .put("lineItemId", 277592292)
+                .put("src", "gpt")
+                .build()
+        },
+        {
+            new Page("arecovery", "SyntheticTests/Static_image" + SOURCE_POINT_INSTANT_GLOBAL),
+            ImmutableMap.<String, Object>builder()
+                .put("adUnitId", "wikia_gpt/5441/wka.life/_arecovery//article/gpt/TOP_RIGHT_BOXAD")
+                .put("slotName", AdsContent.MEDREC)
+                .put("lineItemId", 277592292)
+                .put("src", "gpt")
+                .build()
+        }
+    };
+  }
+
+  @DataProvider
   public static Object[][] adsRecoveryPageFairOasis() {
     return new Object[][]{
         {
             new Page("arecovery", "SyntheticTests/Static_image?InstantGlobals.wgAdDriverPageFairRecoveryCountries=[XX]"),
         }
     };
+  }
+
+  @DataProvider
+  public static Object[][] adsRecoverySourcePointOasisProject43() {
+      return new Object[][]{
+          {
+              new Page("project43", "SourcePoint/Static_image" + SOURCE_POINT_INSTANT_GLOBAL),
+              ImmutableMap.<String, Object>builder()
+                  .put("adUnitId", "wikia_gpt/5441/wka.life/_project43//article/gpt/TOP_LEADERBOARD")
+                  .put("slotName", AdsContent.TOP_LB)
+                  .put("lineItemId", 257545212)
+                  .put("src", "gpt")
+                  .build()
+          },
+          {
+              new Page("project43", "SourcePoint/Static_image" + SOURCE_POINT_INSTANT_GLOBAL),
+              ImmutableMap.<String, Object>builder()
+                  .put("adUnitId", "wikia_gpt/5441/wka.life/_project43//article/gpt/TOP_RIGHT_BOXAD")
+                  .put("slotName", AdsContent.MEDREC)
+                  .put("lineItemId", 257545212)
+                  .put("src", "gpt")
+                  .build()
+          }
+      };
   }
 
   @DataProvider
---
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsDetection.java
@@ -0,0 +1,61 @@
+package com.wikia.webdriver.testcases.adstests;
+
+import com.wikia.webdriver.common.core.annotations.NetworkTrafficDump;
+import com.wikia.webdriver.common.core.url.Page;
+import com.wikia.webdriver.common.dataprovider.ads.AdsDataProvider;
+import com.wikia.webdriver.common.templates.NewTestTemplate;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
+
+import org.testng.annotations.Test;
+
+public class TestAdsDetection extends NewTestTemplate {
+
+  public static final String PIXEL_PATTERN_WITH_ADBLOCK =
+      "http://www\\..*\\.com/bcn.*deo=1.*";
+  public static final String PIXEL_PATTERN_WITHOUT_ADBLOCK =
+      "http://www\\..*\\.com/bcn.*deo=0.*";
+
+  public static final String PIXEL_PATTERN_WITH_ADBLOCK_AND_RECOVERY =
+      ".*\\.wikia\\.com/__bre\\?.*deo=1.*";
+  public static final String PIXEL_PATTERN_WITHOUT_ADBLOCK_AND_RECOVERY =
+      ".*\\.wikia\\.com/__bre\\?.*deo=0.*";
+
+  @NetworkTrafficDump
+  @Test(
+      groups = "AdsDetectNoAdBlock",
+      dataProviderClass = AdsDataProvider.class,
+      dataProvider = "adsDetection"
+  )
+  public void adsDetectNoAdBlock(Page wiki, String urlParam, boolean isRecoveryEnabled) {
+    String pattern = PIXEL_PATTERN_WITHOUT_ADBLOCK;
+    if (isRecoveryEnabled) {
+      pattern = PIXEL_PATTERN_WITHOUT_ADBLOCK_AND_RECOVERY;
+    }
+    assertPixelWithDetectionStatus(wiki, urlParam, pattern);
+  }
+
+  @NetworkTrafficDump
+  @Test(
+      groups = "AdsDetectAdBlock",
+      dataProviderClass = AdsDataProvider.class,
+      dataProvider = "adsDetection"
+  )
+  public void adsDetectAdBlock(Page wiki, String urlParam, boolean isRecoveryEnabled) {
+    String pattern = PIXEL_PATTERN_WITH_ADBLOCK;
+    if (isRecoveryEnabled) {
+      pattern = PIXEL_PATTERN_WITH_ADBLOCK_AND_RECOVERY;
+    }
+    assertPixelWithDetectionStatus(wiki, urlParam, pattern);
+  }
+
+  private void assertPixelWithDetectionStatus(Page wiki, String urlParam, String pixelPattern) {
+    networkTrafficInterceptor.startIntercepting();
+
+    String url = urlBuilder.getUrlForPage(wiki);
+    url = urlBuilder.appendQueryStringToURL(url, urlParam);
+
+    AdsBaseObject adsBaseObject = new AdsBaseObject(driver, url);
+
+    adsBaseObject.wait.forSuccessfulResponseByUrlPattern(networkTrafficInterceptor, pixelPattern);
+  }
+}
