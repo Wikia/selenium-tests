4fee8814b027d508028b88f3f0ca8fbc8914f355
nAndy <nandy@wikia-inc.com>
2017-07-04 19:20:06 +0200

    ADEN-5469 fixed regular expresion

--- src/test/java/com/wikia/webdriver/common/templates/fandom/AdsFandomTestTemplate.java
+++ src/test/java/com/wikia/webdriver/common/templates/fandom/AdsFandomTestTemplate.java
@@ -18,7 +18,7 @@ public class AdsFandomTestTemplate extends FandomTestTemplate {
   protected AdsFandomObject loadPage(String pageName, String pageType) {
     String pageUrl;
     String queryString = Configuration.getQS();
-    Boolean isF2 = queryString.matches("/.*f2(?=\\=|&|$|\\n).*/");
+    Boolean isF2 = queryString.matches(".*f2(?=\\=|&|$|\\n).*");
 
     PageObjectLogging.log("queryString", queryString, true);
     PageObjectLogging.log("isF2", isF2.toString(), true);
