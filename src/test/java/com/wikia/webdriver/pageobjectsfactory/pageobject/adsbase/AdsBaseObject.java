7350de4a2d87e73c5713748f31a5309a596805f5
Wojtek Bulaszenko <wojciech.bulaszenko@wikia-inc.com>
2017-07-07 16:06:10 +0200

    ADEN-jenkins-test3

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsBaseObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsBaseObject.java
@@ -544,6 +544,7 @@ public class AdsBaseObject extends WikiBasePageObject {
 
     try {
       triggerComments();
+      scrollToPosition("#Section_2.mw-headline");
       wait.forElementPresent(By.cssSelector(adSlotSelector));
       new WebDriverWait(driver, SLOT_TRIGGER_TIMEOUT_SEC).until(new ExpectedCondition<Object>() {
         @Override
