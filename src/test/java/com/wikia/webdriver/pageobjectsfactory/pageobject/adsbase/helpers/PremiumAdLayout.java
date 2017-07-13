54c1c37d62c913e71fdd5b4adf1f6d6e1d6e1f1c
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2017-07-13 11:47:55 +0200

    ADEN-5508 Eliminate duplication in Featured Video tests

---
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/helpers/PremiumAdLayout.java
@@ -0,0 +1,12 @@
+package com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.helpers;
+
+import com.wikia.webdriver.common.core.url.Page;
+import com.wikia.webdriver.common.core.url.UrlBuilder;
+
+public class PremiumAdLayout {
+  private static final String TURN_ON_PREMIUM_LAYOUT = "InstantGlobals.wgAdDriverPremiumAdLayoutCountries=[XX]";
+
+  public static String addTurnOnParams(UrlBuilder urlBuilder, Page page) {
+    return urlBuilder.appendQueryStringToURL(page.getUrl(), TURN_ON_PREMIUM_LAYOUT);
+  }
+}
