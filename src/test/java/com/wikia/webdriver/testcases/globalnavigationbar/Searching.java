60c1b03804d604cfeafc3a0ed833665ae6b534ec
nikodamn <nikodem.hynek@wikia-inc.com>
2017-07-13 14:16:00 +0200

    Change wikis to selenium one's, create article at the beginning of the test execution

--- src/test/java/com/wikia/webdriver/testcases/globalnavigationbar/Searching.java
+++ src/test/java/com/wikia/webdriver/testcases/globalnavigationbar/Searching.java
@@ -1,12 +1,13 @@
 package com.wikia.webdriver.testcases.globalnavigationbar;
 
+import com.wikia.webdriver.common.contentpatterns.PageContent;
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.Execute;
+import com.wikia.webdriver.common.core.api.ArticleContent;
 import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.HomePage;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.SearchPageObject;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -16,18 +17,19 @@ public class Searching extends NewTestTemplate {
   @DataProvider
   public Object[][] getDataForGlobalSearch() {
     return new Object[][]{
-        {"muppet", "kermit", "Special:Search"},
-        {"de.gta", "san fierro", "Spezial:Suche"},
-        {"zh.pad", "pad", "Special:%E6%90%9C%E7%B4%A2"}
+        {"mediawiki119", "globalNavigation", "Special:Search"},
+        {"de.selenium", "globalNavigation", "Spezial:Suche"},
+        {"zh.selenium", "globalNavigation", "Special:%E6%90%9C%E7%B4%A2"}
     };
   }
 
   @Test(groups = {"serachGlobalNavigationBarAsAnon"},
       dataProvider = "getDataForGlobalSearch"
   )
-  public void serachGlobalNavigationBarAsAnon(
+  public void searchGlobalNavigationBarAsAnon(
       String wikiName, String query, String expectedSpecialPage
   ) {
+    new ArticleContent().push(PageContent.LOREM_IPSUM_SHORT, query);
     HomePage homePage = new HomePage();
     homePage.getUrl(urlBuilder.getUrlForWiki(wikiName));
     SearchPageObject search = homePage.getGlobalNavigation()
@@ -42,9 +44,10 @@ public class Searching extends NewTestTemplate {
       dataProvider = "getDataForGlobalSearch"
   )
   @Execute(asUser = User.USER)
-  public void serachGlobalNavigationBarAsLoggedIn(
+  public void searchGlobalNavigationBarAsLoggedIn(
       String wikiName, String query, String expectedSpecialPage
   ) {
+    new ArticleContent().push(PageContent.LOREM_IPSUM_SHORT, query);
     HomePage homePage = new HomePage();
     homePage.getUrl(urlBuilder.getUrlForWiki(wikiName));
     SearchPageObject search = homePage.getGlobalNavigation()
