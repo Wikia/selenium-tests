857c5c5c302b18f548593923f4d0bcc969562735
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-07-18 15:00:51 +0200

    ADEN-5551 Remove btf blocker tests from adtest-pluto

--- src/test/java/com/wikia/webdriver/common/dataprovider/ads/AdsDataProvider.java
+++ src/test/java/com/wikia/webdriver/common/dataprovider/ads/AdsDataProvider.java
@@ -663,20 +663,6 @@ public class AdsDataProvider {
   }
 
   @DataProvider
-  public static Object[][] delayBtfPluto() {
-    return new Object[][]{
-        {"adtest-pluto", "SyntheticTests/ATF_DELAY_BTF", false}
-    };
-  }
-
-  @DataProvider
-  public static Object[][] disableBtfPluto() {
-    return new Object[][]{
-        {"adtest-pluto", "SyntheticTests/ATF_DISABLE_BTF", false}
-    };
-  }
-
-  @DataProvider
   public static Object[][] disableBtfExceptHighlyViewableSlots() {
     return new Object[][]{
         {"project43", "SyntheticTests/Disable_BTF/Unblock_HIVI", true}
