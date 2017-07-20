e4256f0a83ed8bad9e713ebb95d9708e097d0325
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2017-07-20 11:48:22 +0200

    ADEN-5572 Code cleanup

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsRecoveryObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsRecoveryObject.java
@@ -1,15 +1,8 @@
 package com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase;
 
-import static org.apache.commons.io.FileUtils.readFileToString;
-
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.logging.PageObjectLogging;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.testng.Assert;
 
 import java.io.File;
@@ -17,25 +10,22 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.apache.commons.io.FileUtils.readFileToString;
+
 public class AdsRecoveryObject extends AdsBaseObject {
+  private static final Dimension MEDREC_SIZE = new Dimension(300, 250);
+  private static final Dimension TOP_LEADERBOARD_SIZE = new Dimension(728, 90);
   private static final int RECOVERABLE_ADS_COUNT = 2;
   private static final String EXPECTED_TOP_LEADERBOARD_PATH = "src/test/resources/adsResources/recovered_top_leaderboard";
   private static final String EXPECTED_MEDREC_PATH = "src/test/resources/adsResources/recovered_medrec";
+  private static final By RECOVERABLE_SLOT_SELECTOR = By.cssSelector("[adonis-marker]");
+  private static final By PF_RECOVERED_ADS_SELECTOR = By.cssSelector("body>span");
 
   public AdsRecoveryObject(WebDriver driver, String page, Dimension resolution) {
     super(driver, page, resolution);
   }
 
-  public String getRecoveredAdUnitId(String adUnitId) {
-    String getIdScript = String.format("window._sp_.getElementId('%s')", adUnitId);
-    jsActions.waitForJavaScriptTruthy(String.format("window._sp_ && window._sp_.getElementId && %s", getIdScript));
-    return jsActions.execute(getIdScript).toString();
-  }
-
   public void verifyPageFairRecoveryWithAdBlock() {
-    By spansBodyChildrenSelector = By.cssSelector("body>span");
-    Dimension topLeaderboardSize = new Dimension(728, 90);
-    Dimension medrecSize = new Dimension(300, 250);
     String expectedRecoveredLB;
     String expectedRecoveredMR;
 
@@ -48,27 +38,21 @@ public class AdsRecoveryObject extends AdsBaseObject {
     }
 
     // when PF recovered ad is on page, inserts span elements as a direct children of body
-    wait.forElementPresent(spansBodyChildrenSelector);
+    wait.forElementPresent(PF_RECOVERED_ADS_SELECTOR);
 
     // verify that adblock is turned on on that page
     verifyNoAdsOnPage();
 
-    String firstSpanClass = driver.findElement(spansBodyChildrenSelector).getAttribute("class");
-    List<WebElement> recoveredAds = driver
-        .findElements(By.cssSelector("body>span." + firstSpanClass))
-        .stream()
-        .filter(WebElement::isDisplayed)
-        .filter(e -> e.getCssValue("background").contains("data:image/jpeg"))
-        .collect(Collectors.toList());
+    List<WebElement> recoveredAds = getRecoveredAds(PF_RECOVERED_ADS_SELECTOR);
 
     Assert.assertEquals(recoveredAds.size(), RECOVERABLE_ADS_COUNT);
 
     for (WebElement ad : recoveredAds) {
       Dimension adSize = ad.getSize();
 
-      if (adSize.equals(topLeaderboardSize)) {
+      if (adSize.equals(TOP_LEADERBOARD_SIZE)) {
         Assertion.assertTrue(ad.getCssValue("background").contains(expectedRecoveredLB), "TOP_LEADERBOARD is not correctly recovered!");
-      } else if (adSize.equals(medrecSize)) {
+      } else if (adSize.equals(MEDREC_SIZE)) {
         Assertion.assertTrue(ad.getCssValue("background").contains(expectedRecoveredMR), "MEDREC is not correctly recovered!");
       } else {
         Assertion.fail("Not supported PageFair recovery ad size encountered: " + adSize);
@@ -76,18 +60,23 @@ public class AdsRecoveryObject extends AdsBaseObject {
     }
   }
 
+  private List<WebElement> getRecoveredAds(By spansBodyChildrenSelector) {
+    String firstSpanClass = driver.findElement(spansBodyChildrenSelector).getAttribute("class");
+    return driver
+        .findElements(By.cssSelector("body>span." + firstSpanClass))
+        .stream()
+        .filter(WebElement::isDisplayed)
+        .filter(e -> e.getCssValue("background").contains("data:image/jpeg"))
+        .collect(Collectors.toList());
+  }
+
   public void verifyPageFairRecoveryWithNoAdBlock() {
-    List<WebElement> markedSlots = driver.findElements(By.cssSelector("[adonis-marker]"));
+    List<WebElement> markedSlots = driver.findElements(RECOVERABLE_SLOT_SELECTOR);
     Assertion.assertEquals(markedSlots.size(), RECOVERABLE_ADS_COUNT);
   }
 
   public void verifyPageFairRecoveryNoMarkersOnPage() {
-    List<WebElement> markedSlots = driver.findElements(By.cssSelector("[adonis-marker]"));
+    List<WebElement> markedSlots = driver.findElements(RECOVERABLE_SLOT_SELECTOR);
     Assertion.assertEquals(markedSlots.size(), 0);
-  }
-
-  public void waitForRecoveredSlot(String slotName) {
-    // We are waitnig for Source Point recovered slot (second div appears in slot)
-    wait.forElementPresent(By.cssSelector("#" + slotName + " .provider-container > div:nth-child(2)"));
   }
 }
