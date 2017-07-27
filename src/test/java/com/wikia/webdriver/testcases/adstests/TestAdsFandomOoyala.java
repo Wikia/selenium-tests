2b5fc686d86fc9d8d31e9d2a0afec6ca6f8c0f27
nAndy <nandy@wikia-inc.com>
2017-07-27 18:27:08 +0200

    ADEN-5530 refactored autoplay methods

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsFandomOoyala.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsFandomOoyala.java
@@ -9,7 +9,6 @@ import com.wikia.webdriver.common.templates.fandom.AdsFandomTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.componentobject.ad.OoyalaPrerollAd;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -22,6 +21,7 @@ public class TestAdsFandomOoyala extends AdsFandomTestTemplate {
       ".ooyala-video[data-autoplay] iframe[src*=imasdk]";
 
   private static final String CLICK_TO_PLAY_PAGE = "the-best-movies-of-2017-so-far";
+  private static final String AUTOPLAY_PAGE = "orphan-black-clones-names";
 
   private static final Color BLUE = new Color(0, 1, 253);
 
@@ -43,7 +43,11 @@ public class TestAdsFandomOoyala extends AdsFandomTestTemplate {
       groups = {"AdsFandomOoyalaAutoplayPrerollDesktop"}
   )
   public void adsFandomOoyalaAutoplayPrerollDesktop() {
-    testOoyalaAutoplayPreroll(WindowSize.DESKTOP);
+    loadPage(AUTOPLAY_PAGE,
+             AdsFandomTestTemplate.PAGE_TYPE_ARTICLE,
+             WindowSize.DESKTOP
+    );
+    testOoyalaAutoplayPreroll();
   }
 
   @InBrowser(
@@ -70,7 +74,11 @@ public class TestAdsFandomOoyala extends AdsFandomTestTemplate {
       groups = {"AdsFandomOoyalaAutoplayPrerollMobile"}
   )
   public void adsFandomOoyalaAutoplayPrerollMobile() {
-    testOoyalaAutoplayPreroll(WindowSize.PHONE);
+    loadPage(AUTOPLAY_PAGE,
+             AdsFandomTestTemplate.PAGE_TYPE_ARTICLE,
+             WindowSize.PHONE
+    );
+    testOoyalaAutoplayPreroll();
   }
 
   public void testOoyalaClickToPlayPreroll() {
@@ -86,9 +94,7 @@ public class TestAdsFandomOoyala extends AdsFandomTestTemplate {
     wait.forElementNotVisible(adLayer);
   }
 
-  public void testOoyalaAutoplayPreroll(Dimension size) {
-    loadPage("orphan-black-clones-names", AdsFandomTestTemplate.PAGE_TYPE_ARTICLE, size);
-
+  public void testOoyalaAutoplayPreroll() {
     Wait wait = new Wait(driver);
     By autoplayAdLayer = By.cssSelector(AUTOPLAY_PLAYERER_AD_SELECTOR);
 
