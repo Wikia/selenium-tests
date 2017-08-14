b9f29fc13f8b2a3fe029ad2eb87ae99df7ac7f2a
Oliwia Golec <ogolec@wikia-inc.com>
2017-08-14 12:12:19 +0200

    IRIS-4246 | Add a private constructor to hide the implicit public one

--- src/test/java/com/wikia/webdriver/common/core/helpers/ForgottenPasswordUserPool.java
+++ src/test/java/com/wikia/webdriver/common/core/helpers/ForgottenPasswordUserPool.java
@@ -6,6 +6,10 @@ import com.wikia.webdriver.common.properties.Credentials;
 
 public class ForgottenPasswordUserPool {
 
+  private ForgottenPasswordUserPool() {
+    //no-op
+  }
+
   private static final Credentials CREDENTIALS = Configuration.getCredentials();
 
   public static UserWithEmail user1 = new UserWithEmail(User.FORGOTTEN_PASSWORD,
