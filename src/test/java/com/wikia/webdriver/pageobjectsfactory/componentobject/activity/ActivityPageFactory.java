31cb190101629c43946adeb57edaef59d8432cba
Oliwia Golec <ogolec@wikia-inc.com>
2017-08-17 16:34:15 +0200

    IRIS-4574 | implement ActivityPageFactory

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/activity/ActivityPageFactory.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/activity/ActivityPageFactory.java
@@ -1,6 +1,23 @@
 package com.wikia.webdriver.pageobjectsfactory.componentobject.activity;
 
 
+import lombok.Getter;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class ActivityPageFactory {
+
+  @Getter
+  private List<Activity> activities;
+
+  public ActivityPageFactory(List<WebElement> activities) {
+    this.activities = buildActivities(activities);
+  }
+
+  private List<Activity> buildActivities(List<WebElement> activities) {
+    activities.forEach(element -> this.activities.add(new Activity(element)));
+    return this.activities;
+  }
 
 }
