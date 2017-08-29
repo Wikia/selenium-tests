9cf6151325b06663cddcc54f44ec6f51f097efae
Oliwia Golec <ogolec@wikia-inc.com>
2017-08-29 12:15:09 +0200

    IRIS-3963 | refactor LoginTests: change names of test methods, group them by test case, add desktop and mobile cases when needed

--- src/test/java/com/wikia/webdriver/testcases/auth/LoginTests.java
+++ src/test/java/com/wikia/webdriver/testcases/auth/LoginTests.java
@@ -7,22 +7,16 @@ import com.wikia.webdriver.common.core.Helios;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
 import com.wikia.webdriver.common.core.api.ArticleContent;
-import com.wikia.webdriver.common.core.configuration.Configuration;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
-import com.wikia.webdriver.elements.mercury.components.Navigation;
-import com.wikia.webdriver.elements.mercury.components.TopBar;
-import com.wikia.webdriver.elements.mercury.old.JoinPageObject;
 import com.wikia.webdriver.elements.mercury.old.LoginPageObject;
-import com.wikia.webdriver.elements.mercury.old.SignupPageObject;
 import com.wikia.webdriver.elements.mercury.pages.ArticlePage;
 import com.wikia.webdriver.pageobjectsfactory.componentobject.global_navitagtion.NavigationBar;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.WikiBasePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.article.ArticlePageObject;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.auth.signin.DetachedSignInPage;
-
 import com.wikia.webdriver.pageobjectsfactory.pageobject.auth.signin.SignInPage;
 import org.joda.time.DateTime;
 import org.testng.annotations.Test;
@@ -33,92 +27,12 @@ import static com.wikia.webdriver.common.core.Assertion.assertTrue;
 @Execute(onWikia = MercuryWikis.MERCURY_AUTOMATION_TESTING)
 public class LoginTests extends NewTestTemplate {
 
-  User user = User.LOGIN_USER;
-  User japaneseUser = User.USER_JAPAN;
-  User staff = User.LOGIN_STAFF;
-
-
-  public void userCanLoginOnAuthModalFromGlobalNavigation() {
-    WikiBasePageObject base = new WikiBasePageObject();
-    base.openWikiPage(wikiURL);
-    NavigationBar signInLink = new NavigationBar(driver);
-    DetachedSignInPage authModal = new DetachedSignInPage(signInLink.clickOnSignIn());
-
-    authModal.login(user.getUserName(), user.getPassword());
-    base.verifyUserLoggedIn(user.getUserName());
-  }
-
-
-  public void userCanLoginAsStaffOnAuthModalFromGlobalNavigation() {
-    WikiBasePageObject base = new WikiBasePageObject();
-    NavigationBar signInLink = new NavigationBar(driver);
-    base.openWikiPage(wikiURL);
-
-    DetachedSignInPage authModal = new DetachedSignInPage(signInLink.clickOnSignIn());
-
-    authModal.login(staff.getUserName(), staff.getPassword());
-    base.verifyUserLoggedIn(staff.getUserName());
-  }
-
-  public void japaneseUserCanLogInOnAuthModalFromGlobalNavigation() {
-    WikiBasePageObject base = new WikiBasePageObject();
-    NavigationBar signInLink = new NavigationBar(driver);
-    base.openWikiPage(wikiURL);
-
-    DetachedSignInPage authModal = new DetachedSignInPage(signInLink.clickOnSignIn());
-
-    authModal.login(japaneseUser.getUserName(), japaneseUser.getPassword());
-    base.verifyUserLoggedIn(japaneseUser.getUserName());
-  }
-
-  @Execute(asUser = User.USER_12)
-  public void userWithoutAValidTokenGetsLoggedOut() {
-    new ArticleContent().clear();
-
-    ArticlePageObject article = new ArticlePageObject().open();
-    Helios.deleteAllTokens(User.USER_12);
-
-    article.refreshPageAddingCacheBuster();
-    assertTrue(article.getGlobalNavigation().isUserLoggedOut());
-  }
-
-  private static final String ERROR_MESSAGE =
-    "We don't recognize these credentials. Try again or register a new account.";
+  private User user = User.LOGIN_USER;
+  private User japaneseUser = User.USER_JAPAN;
+  private User staff = User.LOGIN_STAFF;
 
   @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void userCannotLogInWithWrongPassword() {
-    LoginPageObject loginPageObject = new LoginPageObject(driver).get();
-    loginPageObject.logUserIn(user.getUserName(), "thisIsWrongPassword");
-
-    Assertion.assertEquals(loginPageObject.getErrorMessage(), ERROR_MESSAGE);
-  }
-
-  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void invalidUserCanNotLogIn() {
-    LoginPageObject loginPageObject = new LoginPageObject(driver).get();
-    loginPageObject.logUserIn("notExistingUserName", user.getPassword());
-
-    Assertion.assertEquals(loginPageObject.getErrorMessage(), ERROR_MESSAGE);
-  }
-
-  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void notPossibleToLogInWhenUsernameFieldBlank() {
-    LoginPageObject loginPageObject = new LoginPageObject(driver).get();
-    loginPageObject.logUserIn("", user.getPassword());
-
-    Assertion.assertTrue(loginPageObject.isSubmitButtonDisabled());
-  }
-
-  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void notPossibleToLogInWhenPasswordFieldBlank() {
-    LoginPageObject loginPageObject = new LoginPageObject(driver).get();
-    loginPageObject.logUserIn(user.getUserName(), "");
-
-    Assertion.assertTrue(loginPageObject.isSubmitButtonDisabled());
-  }
-
-  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void closeButtonWorksAndRedirectsProperly() {
+  public void userCanCloseJoinPage() {
     com.wikia.webdriver.elements.mercury.old.ArticlePageObject
       homePage = new com.wikia.webdriver.elements.mercury.old.ArticlePageObject(driver);
     driver.get(wikiURL);
@@ -131,17 +45,52 @@ public class LoginTests extends NewTestTemplate {
     Assertion.assertEquals(expectedHomePageTitle, homePage.getArticleTitle());
   }
 
-  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void japaneseUserLogIn() {
-    LoginPageObject loginPageObject = new LoginPageObject(driver).get();
-    loginPageObject.logUserIn(Configuration.getCredentials().userNameJapanese2,
-      Configuration.getCredentials().passwordJapanese2);
-    //    Assertion.assertTrue(loginPageObject.getNav().isUserLoggedIn(
-    //        Configuration.getCredentials().userNameJapanese2));
+  public void userCanLogInAsStaffOnDesktop() {
+    WikiBasePageObject base = new WikiBasePageObject();
+    NavigationBar signInLink = new NavigationBar(driver);
+    base.openWikiPage(wikiURL);
+
+    DetachedSignInPage authModal = new DetachedSignInPage(signInLink.clickOnSignIn());
+
+    authModal.login(staff.getUserName(), staff.getPassword());
+    base.verifyUserLoggedIn(staff.getUserName());
+  }
+
+  public void userCanLogInAsStaffOnMobile() {
+    WikiBasePageObject base = new WikiBasePageObject();
+    NavigationBar signInLink = new NavigationBar(driver);
+    base.openWikiPage(wikiURL);
+
+    DetachedSignInPage authModal = new DetachedSignInPage(signInLink.clickOnSignIn());
+
+    authModal.login(staff.getUserName(), staff.getPassword());
+    base.verifyUserLoggedIn(staff.getUserName());
+  }
+
+  public void japaneseUserCanLogInOnDesktop() {
+    WikiBasePageObject base = new WikiBasePageObject();
+    NavigationBar signInLink = new NavigationBar(driver);
+    base.openWikiPage(wikiURL);
+
+    DetachedSignInPage authModal = new DetachedSignInPage(signInLink.clickOnSignIn());
+
+    authModal.login(japaneseUser.getUserName(), japaneseUser.getPassword());
+    base.verifyUserLoggedIn(japaneseUser.getUserName());
+  }
+
+  public void japaneseUserCanLogInOnMobile() {
+    WikiBasePageObject base = new WikiBasePageObject();
+    NavigationBar signInLink = new NavigationBar(driver);
+    base.openWikiPage(wikiURL);
+
+    DetachedSignInPage authModal = new DetachedSignInPage(signInLink.clickOnSignIn());
+
+    authModal.login(japaneseUser.getUserName(), japaneseUser.getPassword());
+    base.verifyUserLoggedIn(japaneseUser.getUserName());
   }
 
   @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void passwordTogglerWorks() {
+  public void passwordTogglerChangesPasswordVisibilityOnMobile() {
     LoginPageObject loginPageObject = new LoginPageObject(driver).get();
     loginPageObject.typePassword(user.getPassword());
 
@@ -153,39 +102,54 @@ public class LoginTests extends NewTestTemplate {
     Assertion.assertTrue(loginPageObject.isPasswordTogglerEnabled(), "password should be enabled");
   }
 
-  private static final String EXPECTED_ERROR_MESSAGE =
+  public void passwordTogglerChangesPasswordVisibilityOnDesktop() {
+    LoginPageObject loginPageObject = new LoginPageObject(driver).get();
+    loginPageObject.typePassword(user.getPassword());
+
+    Assertion
+      .assertTrue(loginPageObject.isPasswordTogglerDisabled(), "password should be disabled");
+
+    loginPageObject.clickOnPasswordToggler();
+
+    Assertion.assertTrue(loginPageObject.isPasswordTogglerEnabled(), "password should be enabled");
+  }
+
+  @Execute(asUser = User.USER_12)
+  public void userWithoutAValidTokenGetsLoggedOutOnDesktop() {
+    new ArticleContent().clear();
+
+    ArticlePageObject article = new ArticlePageObject().open();
+    Helios.deleteAllTokens(User.USER_12);
+
+    article.refreshPageAddingCacheBuster();
+    assertTrue(article.getGlobalNavigation().isUserLoggedOut());
+  }
+
+  @Execute(asUser = User.USER_12)
+  public void userWithoutAValidTokenGetsLoggedOutOnMobile() {
+    new ArticleContent().clear();
+
+    ArticlePageObject article = new ArticlePageObject().open();
+    Helios.deleteAllTokens(User.USER_12);
+
+    article.refreshPageAddingCacheBuster();
+    assertTrue(article.getGlobalNavigation().isUserLoggedOut());
+  }
+
+  private static final String ERROR_MESSAGE =
     "We don't recognize these credentials. Try again or register a new account.";
 
+
+
   @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void userCanNotLogInWithInvalidPassword() {
-    ArticlePage article = new ArticlePage();
+  public void nonexistentUserCannotLogInOnMobile() {
+    LoginPageObject loginPageObject = new LoginPageObject(driver).get();
+    loginPageObject.logUserIn("notExistingUserName", user.getPassword());
 
-    SignInPage signIn = article.open(MercurySubpages.MAIN_PAGE)
-      .getTopbar()
-      .openNavigation()
-      .clickOnSignInRegisterButton()
-      .navigateToSignIn();
-
-    signIn.login(user.getUserName(), "someinvalidpassw0rd");
-    assertTrue(signIn.getError().contains(EXPECTED_ERROR_MESSAGE));
+    Assertion.assertEquals(loginPageObject.getErrorMessage(), ERROR_MESSAGE);
   }
 
-  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void userCanNotLogInWithBlankPassword() {
-    ArticlePage article = new ArticlePage();
-
-    SignInPage signIn = article.open(MercurySubpages.MAIN_PAGE)
-      .getTopbar()
-      .openNavigation()
-      .clickOnSignInRegisterButton()
-      .navigateToSignIn();
-
-    signIn.login(user.getUserName(), "someinvalidpassw0rd");
-    assertTrue(signIn.submitButtonNotClickable());
-  }
-
-  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void userCanNotLogInWithInvalidUsername() {
+  public void nonexistentUserCannotLogInOnDesktop() {
     SignInPage signIn = new ArticlePage()
       .open(MercurySubpages.MAIN_PAGE)
       .getTopbar()
@@ -197,8 +161,32 @@ public class LoginTests extends NewTestTemplate {
     assertTrue(signIn.getError().contains(EXPECTED_ERROR_MESSAGE));
   }
 
+  private static final String EXPECTED_ERROR_MESSAGE =
+    "We don't recognize these credentials. Try again or register a new account.";
+
   @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  public void userCanNotLogInWithBlankUsername() {
+  public void userCannotLogInWithInvalidPasswordOnMobile() {
+    ArticlePage article = new ArticlePage();
+
+    SignInPage signIn = article.open(MercurySubpages.MAIN_PAGE)
+      .getTopbar()
+      .openNavigation()
+      .clickOnSignInRegisterButton()
+      .navigateToSignIn();
+
+    signIn.login(user.getUserName(), "someinvalidpassw0rd");
+    assertTrue(signIn.getError().contains(EXPECTED_ERROR_MESSAGE));
+  }
+
+  public void userCannotLogInWithInvalidPasswordOnDesktop() {
+    LoginPageObject loginPageObject = new LoginPageObject(driver).get();
+    loginPageObject.logUserIn(user.getUserName(), "thisIsWrongPassword");
+
+    Assertion.assertEquals(loginPageObject.getErrorMessage(), ERROR_MESSAGE);
+  }
+
+  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
+  public void userCannotLogInWithBlankUsernameOnMobile() {
     SignInPage signIn = new ArticlePage()
       .open(MercurySubpages.MAIN_PAGE)
       .getTopbar()
@@ -206,6 +194,34 @@ public class LoginTests extends NewTestTemplate {
       .clickOnSignInRegisterButton()
       .navigateToSignIn();
     signIn.typePassword(user.getPassword());
+    assertTrue(signIn.submitButtonNotClickable());
+  }
+
+  public void userCannotLogInWithBlankUsernameOnDesktop() {
+    LoginPageObject loginPageObject = new LoginPageObject(driver).get();
+    loginPageObject.logUserIn("", user.getPassword());
+
+    Assertion.assertTrue(loginPageObject.isSubmitButtonDisabled());
+  }
+
+  public void userCannotLogInWithBlankPasswordOnDesktop() {
+    LoginPageObject loginPageObject = new LoginPageObject(driver).get();
+    loginPageObject.logUserIn(user.getUserName(), "");
+
+    Assertion.assertTrue(loginPageObject.isSubmitButtonDisabled());
+  }
+
+  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
+  public void userCannotLogInWithBlankPasswordOnMobile() {
+    ArticlePage article = new ArticlePage();
+
+    SignInPage signIn = article.open(MercurySubpages.MAIN_PAGE)
+      .getTopbar()
+      .openNavigation()
+      .clickOnSignInRegisterButton()
+      .navigateToSignIn();
+
+    signIn.login(user.getUserName(), "someinvalidpassw0rd");
     assertTrue(signIn.submitButtonNotClickable());
   }
 }
