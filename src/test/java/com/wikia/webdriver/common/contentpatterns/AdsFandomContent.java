be4f80e39545910a9daff3f0a47861f8fed42743
Piotr Gabryjeluk <rychu@fandom.com>
2017-09-21 21:00:02 -0700

    Clean up desktop/mobile slot names

--- src/test/java/com/wikia/webdriver/common/contentpatterns/AdsFandomContent.java
+++ src/test/java/com/wikia/webdriver/common/contentpatterns/AdsFandomContent.java
@@ -10,14 +10,8 @@ public class AdsFandomContent {
 
   public static final String TOP_LEADERBOARD = "TOP_LEADERBOARD";
   public static final String BOTTOM_LEADERBOARD = "BOTTOM_LEADERBOARD";
-  public static final String BOTTOM_LEADERBOARD_DESKTOP = "BOTTOM_LEADERBOARD_DESKTOP";
-  public static final String BOTTOM_LEADERBOARD_MOBILE = "BOTTOM_LEADERBOARD_MOBILE";
   public static final String TOP_BOXAD = "TOP_BOXAD";
-  public static final String TOP_BOXAD_DESKTOP = "TOP_BOXAD_DESKTOP";
-  public static final String TOP_BOXAD_MOBILE = "TOP_BOXAD_MOBILE";
   public static final String INCONTENT_BOXAD = "INCONTENT_BOXAD";
-  public static final String INCONTENT_BOXAD_DESKTOP = "INCONTENT_BOXAD_DESKTOP";
-  public static final String INCONTENT_BOXAD_MOBILE = "INCONTENT_BOXAD_MOBILE";
   public static final String BOTTOM_BOXAD = "BOTTOM_BOXAD";
   public static final String FEED_BOXAD = "FEED_BOXAD";
 
@@ -25,31 +19,20 @@ public class AdsFandomContent {
     SLOT_SELECTORS = new HashMap<>();
     SLOT_SELECTORS.put(TOP_LEADERBOARD, "div[id$='TOP_LEADERBOARD_0__container__']");
     SLOT_SELECTORS.put(BOTTOM_LEADERBOARD, "div[id$='BOTTOM_LEADERBOARD_0__container__']");
-    SLOT_SELECTORS.put(BOTTOM_LEADERBOARD_DESKTOP, "div[id$='BOTTOM_LEADERBOARD_0__container__']");
-    SLOT_SELECTORS.put(BOTTOM_LEADERBOARD_MOBILE, "div[id$='BOTTOM_LEADERBOARD_0__container__']");
     SLOT_SELECTORS.put(TOP_BOXAD, "div[id$='TOP_BOXAD_0__container__']");
-    SLOT_SELECTORS.put(TOP_BOXAD_MOBILE, "div[id$='TOP_BOXAD_0__container__']");
-    SLOT_SELECTORS.put(TOP_BOXAD_DESKTOP, "div[id$='TOP_BOXAD_0__container__']");
     SLOT_SELECTORS.put(INCONTENT_BOXAD, "div[id$='INCONTENT_BOXAD_0__container__']");
-    SLOT_SELECTORS.put(INCONTENT_BOXAD_MOBILE, "div[id$='INCONTENT_BOXAD_0__container__']");
     SLOT_SELECTORS.put(BOTTOM_BOXAD, "div[id$='BOTTOM_BOXAD_0__container__']");
     SLOT_SELECTORS.put(FEED_BOXAD, "div[id$='FEED_BOXAD_0__container__']");
   }
 
   static {
     GPT_SLOT_SELECTORS = new HashMap<>();
-    GPT_SLOT_SELECTORS.put(TOP_LEADERBOARD, "#gpt-top-leaderboard");
-    GPT_SLOT_SELECTORS.put(BOTTOM_LEADERBOARD, "#gpt-bottom-leaderboard");
-    GPT_SLOT_SELECTORS.put(BOTTOM_LEADERBOARD_DESKTOP, "#gpt-bottom-leaderboard-desktop");
-    GPT_SLOT_SELECTORS.put(BOTTOM_LEADERBOARD_MOBILE, "#gpt-bottom-leaderboard-mobile");
+    GPT_SLOT_SELECTORS.put(TOP_LEADERBOARD, "[id^=\"gpt-top-leaderboard\"]");
+    GPT_SLOT_SELECTORS.put(BOTTOM_LEADERBOARD, "[id^=\"gpt-bottom-leaderboard\"]");
     GPT_SLOT_SELECTORS.put(TOP_BOXAD, "[id^=\"gpt-top-boxad\"]");
-    GPT_SLOT_SELECTORS.put(TOP_BOXAD_DESKTOP, "#gpt-top-boxad-desktop");
-    GPT_SLOT_SELECTORS.put(TOP_BOXAD_MOBILE, "#gpt-top-boxad-mobile");
-    GPT_SLOT_SELECTORS.put(INCONTENT_BOXAD, "#gpt-incontent-boxad");
-    GPT_SLOT_SELECTORS.put(INCONTENT_BOXAD_DESKTOP, "#gpt-incontent-boxad-desktop");
-    GPT_SLOT_SELECTORS.put(INCONTENT_BOXAD_MOBILE, "#gpt-incontent-boxad-mobile");
-    GPT_SLOT_SELECTORS.put(BOTTOM_BOXAD, "#gpt-bottom-boxad");
-    GPT_SLOT_SELECTORS.put(FEED_BOXAD, "#gpt-feed-boxad");
+    GPT_SLOT_SELECTORS.put(INCONTENT_BOXAD, "[id^=\"gpt-incontent-boxad\"]");
+    GPT_SLOT_SELECTORS.put(BOTTOM_BOXAD, "[id^=\"gpt-bottom-boxad\"]");
+    GPT_SLOT_SELECTORS.put(FEED_BOXAD, "[id^=\"gpt-feed-boxad\"]");
   }
 
   private AdsFandomContent() {
