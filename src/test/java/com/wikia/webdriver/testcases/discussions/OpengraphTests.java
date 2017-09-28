d2a9516fa1cca20875116eeeb9461cdf9e4beb76
Oliwia Golec <ogolec@wikia-inc.com>
2017-09-28 18:09:43 +0200

    IRIS-4896 | stub for opengraph tests

---
+++ src/test/java/com/wikia/webdriver/testcases/discussions/OpengraphTests.java
@@ -0,0 +1,34 @@
+package com.wikia.webdriver.testcases.discussions;
+
+
+import com.wikia.webdriver.common.contentpatterns.MercuryWikis;
+import com.wikia.webdriver.common.core.Assertion;
+import com.wikia.webdriver.common.core.annotations.Execute;
+import com.wikia.webdriver.common.core.annotations.InBrowser;
+import com.wikia.webdriver.common.core.helpers.Emulator;
+import com.wikia.webdriver.common.core.helpers.User;
+import com.wikia.webdriver.common.templates.NewTestTemplate;
+import com.wikia.webdriver.elements.mercury.components.discussions.common.PostsCreator;
+import com.wikia.webdriver.elements.mercury.components.discussions.common.TextGenerator;
+import com.wikia.webdriver.elements.mercury.pages.discussions.PostsListPage;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class OpengraphTests extends NewTestTemplate {
+
+  @Test
+  @Execute(asUser = User.USER, onWikia = MercuryWikis.DISCUSSIONS_2)
+  @InBrowser(emulator = Emulator.DESKTOP_BREAKPOINT_BIG)
+  public void validLinkGeneratesOpengraphPreviewInNewPostOnDesktop() throws MalformedURLException {
+    PostsListPage page = new PostsListPage().open();
+    PostsCreator postsCreator = page.getPostsCreatorDesktop();
+    postsCreator.click().closeGuidelinesMessage();
+    postsCreator.addTitleWith(TextGenerator.defaultText()).addDescriptionWith(new URL("http://fandom.wikia.com/"));
+    Assertion.assertTrue(postsCreator.hasOpenGraph());
+    postsCreator.clickAddCategoryButton().selectFirstCategory();
+    postsCreator.clickSubmitButton();
+    Assertion.assertTrue(page.getPost().findNewestPost().hasOpenGraphAtContentEnd());
+  }
+}
