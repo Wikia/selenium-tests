2dc8f81cbe634a67442eb94e61eb49f1e9907097
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2017-09-28 14:12:48 +0200

    ADEN-5953 Fix mobile emulator error

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsOoyalaMercury.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsOoyalaMercury.java
@@ -2,14 +2,13 @@ package com.wikia.webdriver.testcases.adstests;
 
 import com.wikia.webdriver.common.core.annotations.InBrowser;
 import com.wikia.webdriver.common.core.drivers.Browser;
-import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.dataprovider.ads.AdsDataProvider;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsOoyalaObject;
-
 import org.testng.annotations.Test;
 
-@InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
+//@InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
+@InBrowser(browser = Browser.CHROME_MOBILE, browserSize = "414x736") // This is fix for unstable emulator
 public class TestAdsOoyalaMercury extends TemplateNoFirstLoad {
 
   @Test(
