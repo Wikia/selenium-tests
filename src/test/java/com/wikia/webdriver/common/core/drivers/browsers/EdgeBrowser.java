27f47df79df27545e509225c769e1cdd37f4488f
nikodamn <nikodem.hynek@wikia-inc.com>
2017-09-28 14:23:26 +0200

    Update setOptions method in EdgeDriver class

--- src/test/java/com/wikia/webdriver/common/core/drivers/browsers/EdgeBrowser.java
+++ src/test/java/com/wikia/webdriver/common/core/drivers/browsers/EdgeBrowser.java
@@ -5,13 +5,20 @@ import com.wikia.webdriver.common.core.drivers.BrowserAbstract;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
+import java.io.File;
+
 public class EdgeBrowser extends BrowserAbstract {
 
   private EdgeOptions edgeOptions = new EdgeOptions();
 
+  private static final String EDGEDRIVER_PATH = "EdgeDriver/MicrosoftEdgeDriver.exe";
+
   @Override
   public void setOptions() {
 
+    File edgedriver = new File(ClassLoader.getSystemResource(EDGEDRIVER_PATH).getPath());
+    edgedriver.setExecutable(true);
+    System.setProperty("webdriver.edge.driver", edgedriver.getPath());
   }
 
   @Override
