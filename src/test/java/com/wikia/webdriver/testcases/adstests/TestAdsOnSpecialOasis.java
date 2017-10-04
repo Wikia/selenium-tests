3691be98ea2f041cdaf9aef1f63b7498cc0a1d84
nAndy <nandy@wikia-inc.com>
2017-10-04 21:46:26 +0200

    ADEN-5995 changed class name so it maches the convention

---
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsOnSpecialOasis.java
@@ -0,0 +1,51 @@
+package com.wikia.webdriver.testcases.adstests;
+
+import com.wikia.webdriver.common.dataprovider.ads.AdsDataProvider;
+import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
+
+import org.openqa.selenium.Dimension;
+import org.testng.annotations.Test;
+
+/**
+ * https://www.google.com/dfp/5441#delivery/LineItemDetail/lineItemId=126608052
+ */
+
+public class TestAdsOnSpecialOasis extends TemplateNoFirstLoad {
+
+  @Test(
+      groups = {"TestAdsOnSpecialPages"},
+      dataProviderClass = AdsDataProvider.class,
+      dataProvider = "specialPages"
+  )
+  public void TestAdsOnSpecialPages(String wikiName, String article, String lineItemId,
+                                                String adUnit, String leaderboardSlot,
+                                                String prefooterSlot,
+                                                Dimension resolution) throws Exception {
+    String testedPage = urlBuilder.getUrlForPath(wikiName, article);
+    AdsBaseObject ads = new AdsBaseObject(driver, testedPage, resolution);
+
+    ads.verifyGptIframe(adUnit, leaderboardSlot, "gpt");
+    ads.verifyGptAdInSlot(leaderboardSlot, lineItemId, "");
+    ads.verifyGptIframe(adUnit, prefooterSlot, "gpt");
+    ads.verifyGptAdInSlot(prefooterSlot, lineItemId, "");
+  }
+
+  @Test(
+      groups = {"TestAdsOnFilePages"},
+      dataProviderClass = AdsDataProvider.class,
+      dataProvider = "filePages"
+  )
+  public void TestAdsOnFilePages(String wikiName, String article, String lineItemId,
+                                             String adUnit, String leaderboardSlot,
+                                             String medrecSlot,
+                                             Dimension resolution) throws Exception {
+    String testedPage = urlBuilder.getUrlForPath(wikiName, article);
+    AdsBaseObject ads = new AdsBaseObject(driver, testedPage, resolution);
+
+    ads.verifyGptIframe(adUnit, leaderboardSlot, "gpt");
+    ads.verifyGptAdInSlot(leaderboardSlot, lineItemId, "");
+    ads.verifyGptIframe(adUnit, medrecSlot, "gpt");
+    ads.verifyGptAdInSlot(medrecSlot, lineItemId, "");
+  }
+}
