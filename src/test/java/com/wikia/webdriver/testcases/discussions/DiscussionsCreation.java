975eb47627b60596d742fabd451464c290fb4e84
Oliwia Golec <ogolec@wikia-inc.com>
2017-10-04 11:45:44 +0200

    IRIS-4887 | temporarily add logging and screenshot capturing

--- src/test/java/com/wikia/webdriver/testcases/discussions/DiscussionsCreation.java
+++ src/test/java/com/wikia/webdriver/testcases/discussions/DiscussionsCreation.java
@@ -2,6 +2,7 @@ package com.wikia.webdriver.testcases.discussions;
 
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.helpers.User;
+import com.wikia.webdriver.common.core.imageutilities.Shooter;
 import com.wikia.webdriver.common.logging.PageObjectLogging;
 import com.wikia.webdriver.testcases.createawikitests.CreateWikiTests_lang;
 import org.testng.annotations.Test;
@@ -12,6 +13,7 @@ public class DiscussionsCreation extends CreateWikiTests_lang {
   @Execute(asUser = User.USER_CNW)
   public void testNewCommunityDiscussionsCreation() {
     CreateNewWiki_lang_TC001("en");
+    new Shooter().capturePage(driver);
     PageObjectLogging.logInfo(driver.getCurrentUrl());
   }
 }
