62f12722314ffcbf6b4ca58b641b79f33757844a
Oliwia Golec <ogolec@wikia-inc.com>
2017-10-05 13:31:16 +0200

    IRIS-4887 | add test case and refactor

--- src/test/java/com/wikia/webdriver/testcases/discussions/DiscussionsCreation.java
+++ src/test/java/com/wikia/webdriver/testcases/discussions/DiscussionsCreation.java
@@ -1,19 +1,48 @@
 package com.wikia.webdriver.testcases.discussions;
 
+import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.helpers.User;
-import com.wikia.webdriver.common.core.imageutilities.Shooter;
-import com.wikia.webdriver.common.logging.PageObjectLogging;
+import com.wikia.webdriver.elements.common.CommunityHeader;
+import com.wikia.webdriver.elements.mercury.components.discussions.common.ErrorMessages;
+import com.wikia.webdriver.elements.mercury.pages.ArticlePage;
+import com.wikia.webdriver.elements.mercury.pages.discussions.PostsListPage;
 import com.wikia.webdriver.testcases.createawikitests.CreateWikiTests_lang;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-@Test(groups = "discussions-creation")
 public class DiscussionsCreation extends CreateWikiTests_lang {
 
-  @Execute(asUser = User.USER_CNW)
-  public void testNewCommunityDiscussionsCreation() {
+  private String wikiUrl;
+
+  @BeforeSuite
+  public void setUp() {
     CreateNewWiki_lang_TC001("en");
-    new Shooter().capturePage(driver);
-    PageObjectLogging.logInfo(driver.getCurrentUrl());
+    wikiUrl = driver.getCurrentUrl();
+  }
+
+  @Execute(asUser = User.USER_CNW)
+  @Test(groups = "discussions-creation")
+  public void userCanSeeEmptyDiscussionsMessageOnNewCommunityDiscussionsPage() {
+    PostsListPage page = navigateToDiscussions(wikiUrl);
+    ErrorMessages errorMessage = page.getErrorMessages();
+    Assertion.assertTrue(errorMessage.isEmptyPostsListMessageDisplayed());
+  }
+
+  @Execute(asUser = User.USER_CNW)
+  @Test(groups = "discussions-creation")
+  public void userCanAddPostOnNewCommunityDiscussionsPage() {
+    PostsListPage page = navigateToDiscussions(wikiUrl);
+    page.getPostsCreatorDesktop().startPostCreation().clickSubmitButton();
+    page.waitForPageReload();
+    Assertion.assertFalse(page.getPost().isPostListEmpty());
+  }
+
+  private PostsListPage navigateToDiscussions(String wikiUrl) {
+    new ArticlePage().openWikiPage(wikiUrl);
+    new CommunityHeader().clickDiscussLink();
+    PostsListPage page = new PostsListPage();
+    page.waitForPageReload();
+    return page;
   }
 }
