9ed40ac10b2420c8173ca59c613de7bb1294f9ba
nAndy <nandy@wikia-inc.com>
2017-10-05 11:57:11 +0200

    ADEN-5992 trigger dynamic slots and verify they are no there

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsBaseObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsBaseObject.java
@@ -692,7 +692,7 @@ public class AdsBaseObject extends WikiBasePageObject {
       if (StringUtils.isEmpty(javaScriptTrigger)) {
         verifyNoAd(selector);
       } else {
-
+        triggerAdSlot(slotName, true);
       }
     }
   }
