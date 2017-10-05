aef5a82160159652cd2acc5117113551260d0e9e
Oliwia Golec <ogolec@wikia-inc.com>
2017-10-05 14:00:39 +0200

    IRIS-4887 | rename test, set browser size

--- src/test/java/com/wikia/webdriver/testcases/discussions/DiscussionsCreation.java
+++ src/test/java/com/wikia/webdriver/testcases/discussions/DiscussionsCreation.java
@@ -2,6 +2,8 @@ package com.wikia.webdriver.testcases.discussions;
 
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.Execute;
+import com.wikia.webdriver.common.core.annotations.InBrowser;
+import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.elements.common.CommunityHeader;
 import com.wikia.webdriver.elements.mercury.pages.discussions.PostsListPage;
@@ -12,7 +14,8 @@ import org.testng.annotations.Test;
 public class DiscussionsCreation extends CreateWikiTests_lang {
 
   @Execute(asUser = User.USER_CNW)
-  public void userCanSeeEmptyDiscussionsMessageOnNewCommunityDiscussionsPage() {
+  @InBrowser(emulator = Emulator.DESKTOP_BREAKPOINT_BIG)
+  public void userCanSeeEmptyDiscussionsMessageAndAddPostOnNewCommunityDiscussionsPage() {
     CreateNewWiki_lang_TC001("en");
     PostsListPage page = navigateToDiscussions();
     Assertion.assertTrue(page.getErrorMessages().isEmptyPostsListMessageDisplayed());
