24b21045630c080a235746bb8b95664ef65cc539
Oliwia Golec <ogolec@wikia-inc.com>
2017-10-05 13:49:19 +0200

    IRIS-4887 | move to a single test case

--- src/test/java/com/wikia/webdriver/testcases/discussions/DiscussionsCreation.java
+++ src/test/java/com/wikia/webdriver/testcases/discussions/DiscussionsCreation.java
@@ -4,40 +4,26 @@ import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.elements.common.CommunityHeader;
-import com.wikia.webdriver.elements.mercury.components.discussions.common.ErrorMessages;
 import com.wikia.webdriver.elements.mercury.pages.ArticlePage;
 import com.wikia.webdriver.elements.mercury.pages.discussions.PostsListPage;
 import com.wikia.webdriver.testcases.createawikitests.CreateWikiTests_lang;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+@Test(groups = "discussions-creation")
 public class DiscussionsCreation extends CreateWikiTests_lang {
 
-  private String wikiUrl;
-
-  @BeforeTest
-  public void setUp() {
-    CreateNewWiki_lang_TC001("en");
-    wikiUrl = driver.getCurrentUrl();
-  }
-
   @Execute(asUser = User.USER_CNW)
-  @Test(groups = "discussions-creation")
   public void userCanSeeEmptyDiscussionsMessageOnNewCommunityDiscussionsPage() {
-    PostsListPage page = navigateToDiscussions(wikiUrl);
-    ErrorMessages errorMessage = page.getErrorMessages();
-    Assertion.assertTrue(errorMessage.isEmptyPostsListMessageDisplayed());
+    CreateNewWiki_lang_TC001("en");
+    PostsListPage page = navigateToDiscussions(driver.getCurrentUrl());
+    Assertion.assertTrue(page.getErrorMessages().isEmptyPostsListMessageDisplayed());
+    createNewPost(page);
+    Assertion.assertFalse(page.getPost().isPostListEmpty());
   }
 
-  @Execute(asUser = User.USER_CNW)
-  @Test(groups = "discussions-creation")
-  public void userCanAddPostOnNewCommunityDiscussionsPage() {
-    PostsListPage page = navigateToDiscussions(wikiUrl);
+  private void createNewPost(PostsListPage page) {
     page.getPostsCreatorDesktop().startPostCreation().clickSubmitButton();
     page.waitForPageReload();
-    Assertion.assertFalse(page.getPost().isPostListEmpty());
   }
 
   private PostsListPage navigateToDiscussions(String wikiUrl) {
