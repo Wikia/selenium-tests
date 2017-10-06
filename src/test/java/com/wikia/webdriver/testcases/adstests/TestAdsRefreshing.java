c2a1eaac548472d499707641dc3f7fe836243fce
nAndy <nandy@wikia-inc.com>
2017-10-06 19:34:40 +0200

    ADEN-6002 moved refreshing slot from FMR to MR

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsRefreshing.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsRefreshing.java
@@ -4,28 +4,31 @@ import com.wikia.webdriver.common.WindowSize;
 import com.wikia.webdriver.common.contentpatterns.AdsContent;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
-import org.openqa.selenium.By;
+
 import org.testng.annotations.Test;
 
 @Test(groups = "AdsRefreshing")
 public class TestAdsRefreshing extends TemplateNoFirstLoad {
 
-  private static final String[] CREATIVE_ID_CHAIN = {"108480236412", "108480237132", "108480238572", "108480239052",
-          "108480239532"};
-  private static final String WIKI_NAME = "project43";
-  private static final String PATH = "SyntheticTests/Slots/RefreshOnView";
-  private static final String ARTICLE_MIDDLE_SECTION_SELECTOR = "#ArticleMidSection.mw-headline";
+  private static final String[] CREATIVE_ID_CHAIN = {
+      "108480236412",
+      "108480237132",
+      "108480238572",
+      "108480239052",
+      "108480239532"
+  };
 
-  private void scrollToSeeFMR(String slotName, AdsBaseObject page) {
-    page.triggerComments();
-    page.scrollToPosition(ARTICLE_MIDDLE_SECTION_SELECTOR);
-    page.waitForSlotExpanded(page.wait.forElementVisible(By.id(slotName)));
-  }
+  private static final String WIKI_NAME = "project43";
+
+  private static final String PATH = "SyntheticTests/Slots/RefreshOnView";
 
   @Test(groups = {"AdsRefreshingFMR"})
   public void floatingMR() {
-    AdsBaseObject page = new AdsBaseObject(driver, urlBuilder.getUrlForPath(WIKI_NAME, PATH), WindowSize.DESKTOP);
-    scrollToSeeFMR(AdsContent.FLOATING_MEDREC, page);
-    page.verifyAdChainForSlot(CREATIVE_ID_CHAIN, AdsContent.FLOATING_MEDREC, page);
+    AdsBaseObject page = new AdsBaseObject(
+        driver,
+        urlBuilder.getUrlForPath(WIKI_NAME, PATH),
+        WindowSize.DESKTOP
+    );
+    page.verifyAdChainForSlot(CREATIVE_ID_CHAIN, AdsContent.MEDREC, page);
   }
 }
