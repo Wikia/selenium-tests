b65e7f1e4399faeb5fbf05b65690e51506230a12
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-10-10 16:07:15 +0200

    ADEN Veles refactor

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsVelesObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsVelesObject.java
@@ -1,14 +1,12 @@
 package com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-
 public class AdsVelesObject extends AdsBaseObject {
 
+  private static final String INCONTENT_WRAPPER = "#INCONTENT_WRAPPER,.mobile-in-content";
   private static final String INCONTENT_VIDEO = ".video-display-wrapper";
-
-  public static final String SECOND_ARTICLE_HEADER_OASIS = "#Player_above";
-  public static final String FIRST_ARTICLE_HEADER_MOBILE = "#Header";
 
   public static final String BIDDER_PLAYER_EVENT_PATTERN = ".*adengplayerinfo.*event_name=in_viewport_with_fallback_bid.*";
   public static final String DIRECT_PLAYER_EVENT_PATTERN = ".*adengplayerinfo.*event_name=in_viewport_with_direct.*";
@@ -18,23 +16,13 @@ public class AdsVelesObject extends AdsBaseObject {
     super(driver, testedPage);
   }
 
-  public void verifyVelesIncontentSlotDisplayedOasis(){
-    tiggerIncontentPlayerOnOasis();
+  public void verifyVelesPlayerInIncontentSlot() {
+    triggerIncontentSlot();
     wait.forElementVisible(driver.findElement(By.cssSelector(INCONTENT_VIDEO)));
   }
 
-  public void verifyVelesIncontentSlotDisplayedMobile(){
-    tiggerIncontentPlayerOnMobile();
-    wait.forElementVisible(driver.findElement(By.cssSelector(INCONTENT_VIDEO)));
-  }
-
-  public void tiggerIncontentPlayerOnMobile() {
-    scrollToPosition(FIRST_ARTICLE_HEADER_MOBILE);
-    fixScrollPositionByNavbarOnMobile();
-  }
-
-  public void tiggerIncontentPlayerOnOasis() {
-    scrollToPosition(SECOND_ARTICLE_HEADER_OASIS);
+  public void triggerIncontentSlot() {
+    scrollToPosition(INCONTENT_WRAPPER);
     fixScrollPositionByNavbar();
   }
 }
--- src/test/java/com/wikia/webdriver/testcases/adstests/TestVideoVelesMobile.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestVideoVelesMobile.java
@@ -11,39 +11,39 @@ import org.testng.annotations.Test;
 @InBrowser(browser = Browser.CHROME_MOBILE, browserSize = "414x736")
 public class TestVideoVelesMobile extends TemplateNoFirstLoad {
 
-    private static final String WIKIA = "project43";
-    private static final String APPNEXUS_DEBUG_MODE = "appnexusast_debug_mode=1";
-    private static final Page TEST_PAGE_BIDDER = new Page(WIKIA, "/SyntheticTests/Video/Porvata/Bidder");
-    private static final Page TEST_PAGE_DIRECT = new Page(WIKIA, "/SyntheticTests/Video/Porvata/Direct");
+  private static final String WIKIA = "project43";
+  private static final String APPNEXUS_DEBUG_MODE = "appnexusast_debug_mode=1";
+  private static final Page TEST_PAGE_BIDDER = new Page(WIKIA, "/SyntheticTests/Video/Porvata/Bidder");
+  private static final Page TEST_PAGE_DIRECT = new Page(WIKIA, "/SyntheticTests/Video/Porvata/Direct");
 
-    @NetworkTrafficDump(useMITM = true)
-    @Test(groups = {"AdsVideoVelesMobile", "AdsVelesIncontentPlayerBiderYesOfferEventMobile"})
-    public void adsVelesIncontentPlayerBiderYesOfferEventMobile() {
-      networkTrafficInterceptor.startIntercepting();
-      String url = TEST_PAGE_BIDDER.getUrl();
-      AdsVelesObject velesAds = new AdsVelesObject(driver, urlBuilder.appendQueryStringToURL(url, APPNEXUS_DEBUG_MODE));
+  @NetworkTrafficDump(useMITM = true)
+  @Test(groups = {"AdsVideoVelesMobile", "AdsVelesWithDirectOfferEventMobile"})
+  public void adsVelesWithDirectOfferEvent() {
+    networkTrafficInterceptor.startIntercepting();
+    AdsVelesObject velesAds = new AdsVelesObject(driver, TEST_PAGE_DIRECT.getUrl());
 
-      velesAds.verifyVelesIncontentSlotDisplayedMobile();
-      velesAds.wait.forSuccessfulResponseByUrlPattern(networkTrafficInterceptor, velesAds.BIDDER_PLAYER_EVENT_PATTERN);
-    }
-
-    @NetworkTrafficDump(useMITM = true)
-    @Test(groups = {"AdsVideoVelesMobile", "AdsVelesIncontentPlayerDirectEventMobile"})
-    public void adsVelesIncontentPlayerDirectEventMobile() {
-      networkTrafficInterceptor.startIntercepting();
-      AdsVelesObject velesAds = new AdsVelesObject(driver, TEST_PAGE_DIRECT.getUrl());
-
-      velesAds.verifyVelesIncontentSlotDisplayedMobile();
-      velesAds.wait.forSuccessfulResponseByUrlPattern(networkTrafficInterceptor, velesAds.DIRECT_PLAYER_EVENT_PATTERN);
-    }
-
-    @NetworkTrafficDump(useMITM = true)
-    @Test(groups = {"AdsVideoVelesMobile", "AdsVelesIncontentPlayerBidderNoOfferEventMobile"})
-    public void adsVelesIncontentPlayerBidderNoOfferEventMobile() {
-      networkTrafficInterceptor.startIntercepting();
-      AdsVelesObject velesAds = new AdsVelesObject(driver, TEST_PAGE_BIDDER.getUrl());
-
-      velesAds.tiggerIncontentPlayerOnMobile();
-      velesAds.wait.forSuccessfulResponseByUrlPattern(networkTrafficInterceptor,velesAds.NO_OFFER_PLAYER_EVENT_PATTERN);
-    }
+    velesAds.verifyVelesPlayerInIncontentSlot();
+    velesAds.wait.forSuccessfulResponseByUrlPattern(networkTrafficInterceptor, AdsVelesObject.DIRECT_PLAYER_EVENT_PATTERN);
   }
+
+  @NetworkTrafficDump(useMITM = true)
+  @Test(groups = {"AdsVideoVelesMobile", "AdsVelesWithBidderOfferEventMobile"})
+  public void adsVelesWithBidderOfferEvent() {
+    networkTrafficInterceptor.startIntercepting();
+    String url = TEST_PAGE_BIDDER.getUrl();
+    AdsVelesObject velesAds = new AdsVelesObject(driver, urlBuilder.appendQueryStringToURL(url, APPNEXUS_DEBUG_MODE));
+
+    velesAds.verifyVelesPlayerInIncontentSlot();
+    velesAds.wait.forSuccessfulResponseByUrlPattern(networkTrafficInterceptor, AdsVelesObject.BIDDER_PLAYER_EVENT_PATTERN);
+  }
+
+  @NetworkTrafficDump(useMITM = true)
+  @Test(groups = {"AdsVideoVelesMobile", "AdsVelesWithoutOfferEventMobile"})
+  public void adsVelesWithoutOfferEvent() {
+    networkTrafficInterceptor.startIntercepting();
+    AdsVelesObject velesAds = new AdsVelesObject(driver, TEST_PAGE_BIDDER.getUrl());
+
+    velesAds.triggerIncontentSlot();
+    velesAds.wait.forSuccessfulResponseByUrlPattern(networkTrafficInterceptor, AdsVelesObject.NO_OFFER_PLAYER_EVENT_PATTERN);
+  }
+}
