afa38b07d978a04df061b6604502064efc874ce9
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-10-11 15:17:04 +0200

    ADEN-6008 Test Veles alien

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsVelesObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/adsbase/AdsVelesObject.java
@@ -1,7 +1,15 @@
 package com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase;
 
+import com.wikia.webdriver.common.core.imageutilities.ImageComparison;
+import com.wikia.webdriver.common.core.imageutilities.Shooter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.TimeUnit;
 
 public class AdsVelesObject extends AdsBaseObject {
 
@@ -22,7 +30,48 @@ public class AdsVelesObject extends AdsBaseObject {
   }
 
   public void triggerIncontentPlayer() {
-    scrollToPosition(INCONTENT_WRAPPER);
-    fixScrollPositionByNavbar();
+    final WebElement wrapper = driver.findElement(By.cssSelector(INCONTENT_WRAPPER));
+
+    jsActions.scrollToElement(wrapper);
+  }
+
+  public void triggerPorvataAlien() {
+    final WebElement wrapper = driver.findElement(By.cssSelector(presentLeaderboardSelector));
+
+    jsActions.scrollToElement(wrapper);
+  }
+
+  public boolean isVideoVisible() {
+    final WebElement element = driver.findElement(By.cssSelector(INCONTENT_VIDEO));
+
+    wait.forElementVisible(element);
+
+    try {
+      waitFor.until(isElementNotBlack(element));
+
+      return true;
+    } finally {
+      driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+  }
+
+  private ExpectedCondition<Boolean> isElementNotBlack(WebElement element) {
+    final Color blackColor = new Color(0, 0, 0);
+    final ImageComparison imageComparison = new ImageComparison();
+    final Shooter shooter = new Shooter();
+
+    return new ExpectedCondition<Boolean>() {
+      @Override
+      public Boolean apply(WebDriver driver) {
+        BufferedImage image = shooter.takeScreenshot(element, driver);
+        // Big Buck Bunny movie has about 33% black pixels
+        return !imageComparison.isColorImage(image, blackColor, 40);
+      }
+
+      @Override
+      public String toString() {
+        return "Element is black";
+      }
+    };
   }
 }
