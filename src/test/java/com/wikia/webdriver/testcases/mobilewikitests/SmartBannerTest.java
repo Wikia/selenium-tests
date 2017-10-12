5b44fdad96ca8a8f4dd21991c24c6b588e744c88
qaga <qaga@wikia-inc.com>
2017-10-12 10:56:37 +0200

    XW-4097 refactoring mobile-wiki and mercury classes

---
+++ src/test/java/com/wikia/webdriver/testcases/mobilewikitests/SmartBannerTest.java
@@ -0,0 +1,42 @@
+package com.wikia.webdriver.testcases.mobilewikitests;
+
+import com.wikia.webdriver.common.contentpatterns.MercurySubpages;
+import com.wikia.webdriver.common.contentpatterns.MercuryWikis;
+import com.wikia.webdriver.common.core.annotations.Execute;
+import com.wikia.webdriver.common.core.annotations.InBrowser;
+import com.wikia.webdriver.common.core.drivers.Browser;
+import com.wikia.webdriver.common.core.helpers.Emulator;
+import com.wikia.webdriver.common.templates.NewTestTemplate;
+import com.wikia.webdriver.elements.common.Navigate;
+import com.wikia.webdriver.elements.mercury.components.SmartBanner;
+
+import org.testng.annotations.Test;
+@Test(groups = "Mercury_SmartBanner")
+@Execute(onWikia = MercuryWikis.MERCURY_AUTOMATION_TESTING)
+@InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
+public class SmartBannerTest extends NewTestTemplate {
+
+  private SmartBanner smartBanner;
+
+  private void init() {
+    this.smartBanner = new SmartBanner(driver);
+
+    new Navigate().toPageByPath(MercurySubpages.GALLERY);
+  }
+
+  @Test(groups = "mercury_smartBanner_isNotVisibleOnScrollDown")
+  public void mercury_smartBanner_isNotVisibleOnScrollDown() {
+    init();
+
+    smartBanner.scrollDown();
+  }
+
+  @Test(groups = "mercury_smartBanner_canBeClosed")
+  public void mercury_smartBanner_canBeClosed() {
+    init();
+
+    smartBanner.scrollDown();
+    smartBanner.scrollUp();
+    smartBanner.close();
+  }
+}
