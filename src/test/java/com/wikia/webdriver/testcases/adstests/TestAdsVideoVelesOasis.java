4fdea87bc305562b28b95854d747ac82a8f94800
Szymon Frąszczak <fraszczak.szymon+github@gmail.com>
2017-10-13 09:37:21 +0200

    ADEN-6006 Change video visibility messages

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsVideoVelesOasis.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsVideoVelesOasis.java
@@ -15,7 +15,7 @@ public class TestAdsVideoVelesOasis extends TemplateNoFirstLoad {
 
   private static final String ASSERTION_VELES_PLAYER_RENDERED = "Veles player rendered";
   private static final String ASSERTION_VELES_PLAYER_NOT_RENDERED = "Veles player not rendered";
-  private static final String ASSERTION_VELES_VIDEO_NOT_RENDERED = "Veles video not rendered";
+  private static final String ASSERTION_VELES_VIDEO_NOT_VISIBLE = "Veles video not visible";
 
   private static final Page TEST_PAGE_BIDDER = new Page(WIKIA, "/SyntheticTests/Video/Porvata/Bidder");
   private static final Page TEST_PAGE_DIRECT = new Page(WIKIA, "/SyntheticTests/Video/Porvata/Direct");
@@ -57,13 +57,13 @@ public class TestAdsVideoVelesOasis extends TemplateNoFirstLoad {
     AdsVelesObject velesAds = new AdsVelesObject(driver, TEST_PAGE_DIRECT.getUrl());
 
     velesAds.triggerIncontentPlayer();
-    Assertion.assertTrue(velesAds.isVideoVisible(), ASSERTION_VELES_PLAYER_NOT_RENDERED);
+    Assertion.assertTrue(velesAds.isVideoVisible(), ASSERTION_VELES_VIDEO_NOT_VISIBLE);
 
     velesAds.triggerPorvataAlien();
-    Assertion.assertTrue(velesAds.isVideoVisible(), ASSERTION_VELES_VIDEO_NOT_RENDERED);
+    Assertion.assertTrue(velesAds.isVideoVisible(), ASSERTION_VELES_VIDEO_NOT_VISIBLE);
 
     velesAds.triggerIncontentPlayer();
-    Assertion.assertTrue(velesAds.isVideoVisible(), ASSERTION_VELES_PLAYER_NOT_RENDERED);
+    Assertion.assertTrue(velesAds.isVideoVisible(), ASSERTION_VELES_VIDEO_NOT_VISIBLE);
   }
 
   @Test(groups = {"AdsVideoVelesOasis", "AdsVelesWithoutOfferHopToDisplayOasis"})
