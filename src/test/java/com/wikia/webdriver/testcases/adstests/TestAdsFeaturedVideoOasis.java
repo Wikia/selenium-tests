c369bbb9a9893e00202cf633eaab1d5733de14e4
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-10-17 10:43:38 +0200

    ADEN-6045 Test JWPlayer MOAT tracking

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsFeaturedVideoOasis.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsFeaturedVideoOasis.java
@@ -1,5 +1,6 @@
 package com.wikia.webdriver.testcases.adstests;
 
+import com.wikia.webdriver.common.core.annotations.NetworkTrafficDump;
 import com.wikia.webdriver.common.core.url.Page;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsJWPlayerObject;
@@ -10,9 +11,14 @@ public class TestAdsFeaturedVideoOasis extends TemplateNoFirstLoad {
   private static final String FV_JWPLAYER_WIKI = "project43";
   private static final String FV_JWPLAYER_PAGE_URI = "SyntheticTests/Premium/FeaturedVideo/JWPlayer";
 
-  private static final Page PAGE_WITH_FV = new Page(FV_JWPLAYER_WIKI, FV_JWPLAYER_PAGE_URI);
+  private static final String IGNORE_SAMPLING = "ignored_samplers=moatTrackingForFeaturedVideo";
   private static final String INSTANT_GLOBAL_MIDROLL = "wgAdDriverFVMidrollCountries";
+  private static final String INSTANT_GLOBAL_MOAT_TRACKING = "wgAdDriverMoatTrackingForFeaturedVideoAdCountries";
   private static final String INSTANT_GLOBAL_POSTROLL = "wgAdDriverFVPostrollCountries";
+
+  private static final String MOAT_VIDEO_TRACKING_URL = "https://z.moatads.com/wikiajwint101173217941/moatvideo.js";
+
+  private static final Page PAGE_WITH_FV = new Page(FV_JWPLAYER_WIKI, FV_JWPLAYER_PAGE_URI);
 
   @Test(
       groups = {"AdsFeaturedVideoOasis"}
@@ -31,5 +37,22 @@ public class TestAdsFeaturedVideoOasis extends TemplateNoFirstLoad {
     jwPlayerObject.verifyMidroll();
     jwPlayerObject.verifyFeaturedVideo();
     jwPlayerObject.verifyPostroll();
+  }
+
+  @NetworkTrafficDump(useMITM = true)
+  @Test(
+      groups = {"AdsFeaturedVideoOasis"}
+  )
+  public void adsFeaturedVideoPrerollWithMOATTrackingOasis() {
+    networkTrafficInterceptor.startIntercepting();
+
+    String testedPage = PAGE_WITH_FV.getUrl();
+    testedPage = urlBuilder.globallyEnableGeoInstantGlobalOnPage(testedPage, INSTANT_GLOBAL_MOAT_TRACKING);
+    testedPage = urlBuilder.appendQueryStringToURL(testedPage, IGNORE_SAMPLING);
+
+    AdsJWPlayerObject jwPlayerObject = new AdsJWPlayerObject(driver, testedPage);
+
+    jwPlayerObject.verifyPlayerOnPage();
+    jwPlayerObject.wait.forSuccessfulResponse(networkTrafficInterceptor, MOAT_VIDEO_TRACKING_URL);
   }
 }
