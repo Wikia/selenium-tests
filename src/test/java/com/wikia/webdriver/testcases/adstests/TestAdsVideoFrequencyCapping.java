31b562e6a74ca4a815b1a6d79f906f67e614b6f5
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-10-18 15:13:41 +0200

    ADEN-6074 Fix Veles capping tests

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsVideoFrequencyCapping.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsVideoFrequencyCapping.java
@@ -1,41 +1,58 @@
 package com.wikia.webdriver.testcases.adstests;
 
 import com.wikia.webdriver.common.core.Assertion;
+import com.wikia.webdriver.common.core.WikiaWebDriver;
+import com.wikia.webdriver.common.core.elemnt.JavascriptActions;
 import com.wikia.webdriver.common.core.url.Page;
-import com.wikia.webdriver.common.dataprovider.ads.AdsDataProvider;
 import com.wikia.webdriver.common.logging.PageObjectLogging;
-import com.wikia.webdriver.common.templates.NewTestTemplate;
+import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+public class TestAdsVideoFrequencyCapping extends TemplateNoFirstLoad {
+  private static final String CONFIG = "InstantGlobals.wgAdDriverOutstreamVideoFrequencyCapping=[1/2min]";
 
-public class TestAdsVideoFrequencyCapping extends NewTestTemplate {
-  private static final String INSTANT_GLOBAL_VELES_BIDDER_COUNTRIES = "InstantGlobals.wgAdDriverVelesBidderCountries=[XX]";
+  private static final String DIRECT_VELES_WIKI = "project43";
+  private static final String DIRECT_VELES_PAGE_URI = "SyntheticTests/Video/Porvata/Direct";
+
+  private static final Page PAGE_WITH_VELES = new Page(DIRECT_VELES_WIKI, DIRECT_VELES_PAGE_URI);
 
   @Test(
-      dataProviderClass = AdsDataProvider.class,
-      dataProvider = "adsVideoFrequencyCapping",
       groups = "AdsVideoFrequencyCapping"
   )
-  public void adsVideoFrequencyCapping(Page page) {
-    AdsBaseObject ads = new AdsBaseObject(driver, urlBuilder.getUrlForPage(page, INSTANT_GLOBAL_VELES_BIDDER_COUNTRIES));
-    ads.scrollToPosition("#Section_2.mw-headline");
+  public void adsVideoFrequencyCapping() {
+    String testedPage = PAGE_WITH_VELES.getUrl();
+    testedPage = urlBuilder.appendQueryStringToURL(testedPage, CONFIG);
+
+    AdsBaseObject ads = new AdsBaseObject(driver, testedPage);
+
+    triggerIncontentPlayer();
     Assertion.assertTrue(isIncontentPlayerDispalyed(ads), "Video Player is not displayed");
+
     ads.refreshPage();
-    Assertion.assertFalse(isIncontentPlayerDispalyed(ads), "Video PLayer is displayed");
+    triggerIncontentPlayer();
+    Assertion.assertFalse(isIncontentPlayerDispalyed(ads), "Video Player is displayed");
   }
 
   private boolean isIncontentPlayerDispalyed(AdsBaseObject ads) {
     try {
       ads.waitForSlotExpanded(driver.findElement(By.cssSelector("#INCONTENT_PLAYER")));
       return true;
-    }catch (TimeoutException | NoSuchElementException ex) {
+    } catch (TimeoutException | NoSuchElementException ex) {
       PageObjectLogging.log("INCONTENT_PLAYER is not displayed", ex, true);
       return false;
     }
+  }
+
+  private void triggerIncontentPlayer() {
+    JavascriptActions jsActions = new JavascriptActions(driver);
+    WebElement slotWrapper = driver.findElement(By.cssSelector("#INCONTENT_WRAPPER"));
+
+    jsActions.scrollToElement(slotWrapper);
   }
 
 }
