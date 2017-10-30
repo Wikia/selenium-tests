47e9ff3d13e60979bf153d11d424109a9d9396e8
Wojtek Bulaszenko <wojciech.bulaszenko@wikia-inc.com>
2017-10-30 20:53:54 +0100

    ADEN-6050 removed added emulator

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsOoyalaMercury.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsOoyalaMercury.java
@@ -2,13 +2,14 @@ package com.wikia.webdriver.testcases.adstests;
 
 import com.wikia.webdriver.common.core.annotations.InBrowser;
 import com.wikia.webdriver.common.core.drivers.Browser;
+import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.url.Page;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsOoyalaObject;
 
 import org.testng.annotations.Test;
 
-@InBrowser(browser = Browser.CHROME_MOBILE, browserSize = "414x736")
+@InBrowser(browser = Browser.CHROME, emulator = Emulator.NEXUS_5X, browserSize = "414x736")
 public class TestAdsOoyalaMercury extends TemplateNoFirstLoad {
 
   private static final String OOYALA_VIDEO_MODAL = "file=Synthetic_video_ad_test_(all_green_video)_320x240_(ooyala-stored_video)";
