1acfe56a6aa196fa6e8be76082a3f68bb2fa180a
Wojtek Bulaszenko <wojciech.bulaszenko@wikia-inc.com>
2017-10-30 21:12:31 +0100

    ADEN-6050 change browser

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsOoyalaMercury.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsOoyalaMercury.java
@@ -2,14 +2,13 @@ package com.wikia.webdriver.testcases.adstests;
 
 import com.wikia.webdriver.common.core.annotations.InBrowser;
 import com.wikia.webdriver.common.core.drivers.Browser;
-import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.url.Page;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsOoyalaObject;
 
 import org.testng.annotations.Test;
 
-@InBrowser(browser = Browser.CHROME, emulator = Emulator.NEXUS_5X, browserSize = "414x736")
+@InBrowser(browser = Browser.CHROME_MOBILE, browserSize = "414x736")
 public class TestAdsOoyalaMercury extends TemplateNoFirstLoad {
 
   private static final String OOYALA_VIDEO_MODAL = "file=Synthetic_video_ad_test_(all_green_video)_320x240_(ooyala-stored_video)";
@@ -22,6 +21,7 @@ public class TestAdsOoyalaMercury extends TemplateNoFirstLoad {
     String url = urlBuilder.getUrlForPage(TEST_PAGE);
     url = urlBuilder.appendQueryStringToURL(url, OOYALA_VIDEO_MODAL);
     AdsOoyalaObject wikiPage = new AdsOoyalaObject(driver, url);
+    wikiPage.;
     wikiPage.verifyLightboxAd();
     wikiPage.verifyLightboxVideo();
   }
