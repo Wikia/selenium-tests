86fae4518801d701075ad215f1b87591593e9081
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-11-15 12:19:31 +0100

    XW-4274: method for disabling Featured Video Autoplay by cookie

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/featuredvideo/FeaturedVideoComponentObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/featuredvideo/FeaturedVideoComponentObject.java
@@ -1,26 +1,42 @@
 package com.wikia.webdriver.pageobjectsfactory.componentobject.featuredvideo;
 
 import com.wikia.webdriver.common.contentpatterns.URLsContent;
-import com.wikia.webdriver.common.logging.PageObjectLogging;
+import com.wikia.webdriver.common.core.configuration.Configuration;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.WikiBasePageObject;
 
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class FeaturedVideoComponentObject extends WikiBasePageObject{
+public class FeaturedVideoComponentObject extends WikiBasePageObject {
+
+  private static final String AUTOPLAY_COOKIE = "featuredVideoAutoplay";
 
   @FindBy(css = ".featured-video")
   private WebElement featuredVideo;
 
+
+  public FeaturedVideoComponentObject setAutoplayCookie(boolean autoplay) {
+    driver.manage().addCookie(new Cookie(
+        AUTOPLAY_COOKIE,
+        autoplay ? "1" : "0",
+        Configuration.getEnvType().getWikiaDomain(),
+        null,
+        null
+    ));
+
+    return this;
+  }
+
   public FeaturedVideoComponentObject openWikiArticle(String articleName) {
-    getUrl(getWikiUrl() + URLsContent.WIKI_DIR + articleName);
-    PageObjectLogging.log("WikiPageOpened", "Wiki page is opened", true);
+    this.openWikiPage(getWikiUrl() + URLsContent.WIKI_DIR + articleName);
 
     return this;
   }
 
   public boolean isFeaturedVideo() {
     wait.forElementVisible(featuredVideo);
-    return featuredVideo.isDisplayed(); }
+    return featuredVideo.isDisplayed();
+  }
 
 }
