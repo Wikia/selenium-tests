50bd1cc6b6bad709170f90ceac966c761864d1df
qaga <qaga@wikia-inc.com>
2017-11-15 12:50:03 +0100

    few new methods

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/featuredvideo/FeaturedVideoComponentObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/featuredvideo/FeaturedVideoComponentObject.java
@@ -12,6 +12,16 @@ public class FeaturedVideoComponentObject extends WikiBasePageObject{
   @FindBy(css = ".featured-video")
   private WebElement featuredVideo;
 
+  @FindBy(css = ".jw-title-primary")
+  private WebElement primaryTitle;
+
+  @FindBy(css = ".jw-title-secondary")
+  private WebElement secondaryTitle;
+
+  @FindBy(css = "#featured-video__player")
+  private WebElement playArea;
+
+
   public FeaturedVideoComponentObject openWikiArticle(String articleName) {
     getUrl(getWikiUrl() + URLsContent.WIKI_DIR + articleName);
     PageObjectLogging.log("WikiPageOpened", "Wiki page is opened", true);
@@ -22,5 +32,22 @@ public class FeaturedVideoComponentObject extends WikiBasePageObject{
   public boolean isFeaturedVideo() {
     wait.forElementVisible(featuredVideo);
     return featuredVideo.isDisplayed(); }
+
+  public String getTitle() {
+    wait.forElementVisible(primaryTitle);
+    return primaryTitle.getText();
+  }
+
+  public String getSubtitle() {
+    wait.forElementVisible(secondaryTitle);
+    return secondaryTitle.getTagName();
+  }
+
+  public void clickPlay() {
+    wait.forElementVisible(playArea);
+    playArea.click();
+  }
+
+
 
 }
