8be75c6c6729e2997fc25803f6480d64d75263f2
Oliwia Zaremba <oliwiagolec@gmail.com>
2017-11-20 16:16:24 +0100

    IRIS-5080 | register new test suite

--- src/test/java/com/wikia/webdriver/testcases/auth/ConfirmEmailTests.java
+++ src/test/java/com/wikia/webdriver/testcases/auth/ConfirmEmailTests.java
@@ -18,6 +18,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Test(groups = "auth-confirm-email")
 public class ConfirmEmailTests extends NewTestTemplate {
 
   private UserWithEmail user = UserWithEmailFactory.getEmailOnlyUserForConfirmation();
