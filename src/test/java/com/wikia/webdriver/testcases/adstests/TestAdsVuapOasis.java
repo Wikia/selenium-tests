e58f3229c638b8957f14586f814aabe7356b6b9c
Wojtek Bulaszenko <wojciech.bulaszenko@wikia-inc.com>
2017-12-13 10:33:11 +0100

    ADEN-5884 change video

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsVuapOasis.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsVuapOasis.java
@@ -248,7 +248,7 @@ public class TestAdsVuapOasis extends TemplateNoFirstLoad {
       groups = {"AdsVuapClickToPlayDesktop"}
   )
   public void vuapCheckSlotSizesOasis(Page page, String slot) {
-    AdsBaseObject ads = new AdsBaseObject(driver, page.getUrl());
+    final AdsBaseObject ads = openPageWithVideoInLocalStorage(page);
     final AutoplayVuap vuap = new AutoplayVuap(driver, slot, ads.findFirstIframeWithAd(slot), false);
     ads.scrollToSlot(slot);
 
