3b72dbb24bb351cd4efd3b649ca147f25f0083b3
Lukasz Adamczewski <adamczewski@gmail.com>
2018-02-02 16:58:47 +0100

    Update VetModalCaptionTests.java

--- src/test/java/com/wikia/webdriver/testcases/mediatests/modal/VetModalCaptionTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mediatests/modal/VetModalCaptionTests.java
@@ -4,7 +4,6 @@ import com.wikia.webdriver.common.contentpatterns.PageContent;
 import com.wikia.webdriver.common.contentpatterns.VideoContent;
 import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.core.annotations.Execute;
-import com.wikia.webdriver.common.core.annotations.RelatedIssue;
 import com.wikia.webdriver.common.core.api.ArticleContent;
 import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
