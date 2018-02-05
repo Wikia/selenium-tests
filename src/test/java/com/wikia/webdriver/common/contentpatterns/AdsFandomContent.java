137c99bd578633eb455f6484e07724c92aa3e65f
nAndy <nandy@wikia-inc.com>
2018-02-05 10:54:05 +0100

    Revert "ADEN-6668 Remove unused slot variables"
    This reverts commit c4f44c8fcf7e0716e739550a60fb71891d48536a.

--- src/test/java/com/wikia/webdriver/common/contentpatterns/AdsFandomContent.java
+++ src/test/java/com/wikia/webdriver/common/contentpatterns/AdsFandomContent.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 public class AdsFandomContent {
 
+  public static final Map<String, String> SLOT_SELECTORS;
   public static final Map<String, String> GPT_SLOT_SELECTORS;
 
   public static final String TOP_LEADERBOARD = "TOP_LEADERBOARD";
@@ -21,6 +22,21 @@ public class AdsFandomContent {
   public static final String INCONTENT_BOXAD_MOBILE = "INCONTENT_BOXAD_MOBILE";
   public static final String BOTTOM_BOXAD = "BOTTOM_BOXAD";
   public static final String FEED_BOXAD = "FEED_BOXAD";
+
+  static {
+    SLOT_SELECTORS = new HashMap<>();
+    SLOT_SELECTORS.put(TOP_LEADERBOARD, "div[id$='TOP_LEADERBOARD_0__container__']");
+    SLOT_SELECTORS.put(BOTTOM_LEADERBOARD, "div[id$='BOTTOM_LEADERBOARD_0__container__']");
+    SLOT_SELECTORS.put(BOTTOM_LEADERBOARD_DESKTOP, "div[id$='BOTTOM_LEADERBOARD_0__container__']");
+    SLOT_SELECTORS.put(BOTTOM_LEADERBOARD_MOBILE, "div[id$='BOTTOM_LEADERBOARD_0__container__']");
+    SLOT_SELECTORS.put(TOP_BOXAD, "div[id$='TOP_BOXAD_0__container__']");
+    SLOT_SELECTORS.put(TOP_BOXAD_MOBILE, "div[id$='TOP_BOXAD_0__container__']");
+    SLOT_SELECTORS.put(TOP_BOXAD_DESKTOP, "div[id$='TOP_BOXAD_0__container__']");
+    SLOT_SELECTORS.put(INCONTENT_BOXAD, "div[id$='INCONTENT_BOXAD_0__container__']");
+    SLOT_SELECTORS.put(INCONTENT_BOXAD_MOBILE, "div[id$='INCONTENT_BOXAD_0__container__']");
+    SLOT_SELECTORS.put(BOTTOM_BOXAD, "div[id$='BOTTOM_BOXAD_0__container__']");
+    SLOT_SELECTORS.put(FEED_BOXAD, "div[id$='FEED_BOXAD_0__container__']");
+  }
 
   static {
     GPT_SLOT_SELECTORS = new HashMap<>();
