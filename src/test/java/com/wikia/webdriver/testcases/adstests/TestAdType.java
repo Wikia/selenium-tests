451646cf6c20cb018a1bf28d933741a1a4fff012
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-03-26 10:30:34 +0200

    ADEN-6922 Move inspect iframe test to desktop

--- src/test/java/com/wikia/webdriver/testcases/adstests/TestAdType.java
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdType.java
@@ -1,11 +1,17 @@
 package com.wikia.webdriver.testcases.adstests;
 
+import com.wikia.webdriver.common.contentpatterns.AdsContent;
+import com.wikia.webdriver.common.core.url.Page;
 import com.wikia.webdriver.common.dataprovider.ads.AdTypeDataProvider;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.mobile.MobileAdsBaseObject;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class TestAdType extends TemplateNoFirstLoad {
+  private static final String DFP_IMAGE_URL =
+      "googlesyndication.com/pagead/imgad?id=CICAgKCNj62dEhCsAhj6ASgBMgjBw3U0lR5Thg";
 
   @Test(
       dataProviderClass = AdTypeDataProvider.class,
@@ -20,5 +26,17 @@ public class TestAdType extends TemplateNoFirstLoad {
       ads.verifyGptIframe(adUnit, slotName, "gpt");
       ads.verifyIframeSize(slotName, 0, 0);
     }
+  }
+
+  @Test(
+      groups = "AdsAdTypeOasis"
+  )
+  public void adsAdTypeInspectIframe() {
+    Page page = new Page("project43", "SyntheticTests/AdType/InspectIframe");
+    final By slotSelector = By.id(AdsContent.MEDREC);
+    MobileAdsBaseObject ads = new MobileAdsBaseObject(driver, page.getUrl());
+    ads.waitForSlotExpanded(slotSelector);
+    ads.scrollToPosition(slotSelector);
+    ads.verifyImgAdLoadedInSlot(AdsContent.MEDREC, DFP_IMAGE_URL);
   }
 }
