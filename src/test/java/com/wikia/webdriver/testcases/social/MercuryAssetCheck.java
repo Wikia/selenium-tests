bc0fb6a2eb10781d55dc357ba289a6da7f09b1d5
Szymon Czerwinski <sczerwinski@wikia-inc.com>
2018-04-06 09:26:21 +0200

    Get rid of Glee wiki

--- src/test/java/com/wikia/webdriver/testcases/social/MercuryAssetCheck.java
+++ src/test/java/com/wikia/webdriver/testcases/social/MercuryAssetCheck.java
@@ -1,18 +1,15 @@
 package com.wikia.webdriver.testcases.social;
 
-import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.NetworkTrafficDump;
 import com.wikia.webdriver.common.core.configuration.Configuration;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.WikiBasePageObject;
-
 import org.testng.annotations.Test;
 
 public class MercuryAssetCheck extends NewTestTemplate {
 
   @Test(groups = "CheckAssetsOnJoin")
   @NetworkTrafficDump
-  @Execute(onWikia = "glee")
   public void checkAssetsOnJoin() {
     WikiBasePageObject base = new WikiBasePageObject();
     networkTrafficInterceptor.startIntercepting("MercurryAssets");
