529d5e7de5f8b69db3d8b53b591610d2e9dc561b
wbulaszenko <w.bulaszenko@gmail.com>
2018-04-23 15:23:18 +0200

    Default language in missed constructor

--- src/test/java/com/wikia/webdriver/common/core/url/Page.java
+++ src/test/java/com/wikia/webdriver/common/core/url/Page.java
@@ -22,6 +22,7 @@ public class Page {
 
   public Page(String wikiName) {
     this.wikiName = wikiName;
+    this.wikiLanguage = Config0uration.DEFAULT_LANGUAGE;
   }
 
   public String getWikiName() {
