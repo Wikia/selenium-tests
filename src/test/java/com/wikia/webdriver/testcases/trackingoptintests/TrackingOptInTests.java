9a8d704284f24fb36553d4c00cd8b367ecc73b31
nikodamn <nikodem.hynek@wikia-inc.com>
2018-05-17 15:45:06 +0200

    Stop network traffic

--- src/test/java/com/wikia/webdriver/testcases/trackingoptintests/TrackingOptInTests.java
+++ src/test/java/com/wikia/webdriver/testcases/trackingoptintests/TrackingOptInTests.java
@@ -1,19 +1,20 @@
 package com.wikia.webdriver.testcases.trackingoptintests;
 
 import com.wikia.webdriver.common.core.Assertion;
-import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.NetworkTrafficDump;
-import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.elements.mercury.pages.discussions.PostsListPage;
 import com.wikia.webdriver.pageobjectsfactory.componentobject.TrackingOptInModal;
+import net.lightbody.bmp.core.har.HarEntry;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 @Test(groups = {"TrackingOptIn"})
 public class TrackingOptInTests extends NewTestTemplate {
 
-    @Execute(trackingOptIn = "false", asUser = User.ANONYMOUS)
-    @NetworkTrafficDump(useMITM = true)
+//    @Execute(trackingOptIn = "false")
+    @NetworkTrafficDump(useMITM = false)
     @Test
     public void verifyTrackingForUserOptedIn() {
         TrackingOptInModal modal = new TrackingOptInModal();
@@ -22,6 +23,9 @@ public class TrackingOptInTests extends NewTestTemplate {
         networkTrafficInterceptor.startIntercepting();
         Assertion.assertTrue(modal.isModalDisplayed());
         modal.clickAcceptButton();
+        networkTrafficInterceptor.stop();
+        List<HarEntry> entries = networkTrafficInterceptor.getHar().getLog().getEntries();
+
 
     }
 
