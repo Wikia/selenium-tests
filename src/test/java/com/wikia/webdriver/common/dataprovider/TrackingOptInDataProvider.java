4109ed8f67a79ebd7078a7230d13729508eb7c4d
wbulaszenko <w.bulaszenko@gmail.com>
2018-05-28 15:03:03 +0200

    ADEN change s3 pattern

--- src/test/java/com/wikia/webdriver/common/dataprovider/TrackingOptInDataProvider.java
+++ src/test/java/com/wikia/webdriver/common/dataprovider/TrackingOptInDataProvider.java
@@ -18,7 +18,7 @@ public class TrackingOptInDataProvider {
       "http?://.*pubmatic\\.com/AdServer.*";
   private static final String ADS_A9_PATTERN = "http?://.*amazon-adsystem\\.com/.*/apstag\\.js.*";
   private static final String ADS_COMSCORE_PATTERN = "http?://.*scorecardresearch\\.com.*";
-  private static final String ADS_QUANTCAST_PATTERN = "http?://secure\\.quantserve\\.com.*";
+  private static final String ADS_QUANTCAST_PATTERN = "https?://secure\\.quantserve\\.com/.*";
   private static final String ADS_APP_NEXUS_PATTERN = "http?://.*ib\\.adnxs\\.com/.*";
   private static final String ADS_NORDICS_PATTERN = "https?://.*doubleclick\\.net/gampad/ads.*Nordics_RoN.*";
   private static final String ADS_OPOENX_PATTERN = "http?://.*wikia-d\\.openx\\.net/.*/arj.*";
@@ -28,7 +28,7 @@ public class TrackingOptInDataProvider {
   private static final String ADS_GOOGLE_ANALYTICS_REJECTED_PATERN =
       "https?://.*google-analytics\\.com/collect.*aip=1.*";
   private static final String ADS_QUALAROO_ANALITYCS_PATERN =
-      ".*s3\\.amazonaws\\.com.*";
+      "http?://.*amazonaws\\.com.*";
   private static final String ADS_TLB_NPA_FIRST_PARAMETER_PATTERN =
       "https?://.*\\.doubleclick\\.net.*ads.*npa=1.*TOP_LEADERBOARD.*";
   private static final String ADS_TLB_NPA_SECOND_PARAMETER_PATTERN =
