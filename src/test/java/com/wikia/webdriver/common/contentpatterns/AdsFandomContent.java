60f8d982fa42d783632ff55ce71a92ceca3a83f0
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-05-29 16:18:08 +0200

    ADEN-7119 Fix f2 selectors

--- src/test/java/com/wikia/webdriver/common/contentpatterns/AdsFandomContent.java
+++ src/test/java/com/wikia/webdriver/common/contentpatterns/AdsFandomContent.java
@@ -8,50 +8,22 @@ import java.util.Map;
 public class AdsFandomContent {
 
   public static final Map<String, String> SLOT_SELECTORS;
-  public static final Map<String, String> GPT_SLOT_SELECTORS;
 
-  public static final String TOP_LEADERBOARD = "TOP_LEADERBOARD";
-  public static final String BOTTOM_LEADERBOARD = "BOTTOM_LEADERBOARD";
-  public static final String BOTTOM_LEADERBOARD_DESKTOP = "BOTTOM_LEADERBOARD_DESKTOP";
-  public static final String BOTTOM_LEADERBOARD_MOBILE = "BOTTOM_LEADERBOARD_MOBILE";
-  public static final String TOP_BOXAD = "TOP_BOXAD";
-  public static final String TOP_BOXAD_DESKTOP = "TOP_BOXAD_DESKTOP";
-  public static final String TOP_BOXAD_MOBILE = "TOP_BOXAD_MOBILE";
-  public static final String INCONTENT_BOXAD = "INCONTENT_BOXAD";
-  public static final String INCONTENT_BOXAD_DESKTOP = "INCONTENT_BOXAD_DESKTOP";
-  public static final String INCONTENT_BOXAD_MOBILE = "INCONTENT_BOXAD_MOBILE";
-  public static final String BOTTOM_BOXAD = "BOTTOM_BOXAD";
-  public static final String FEED_BOXAD = "FEED_BOXAD";
+  public static final String TOP_LEADERBOARD = "top_leaderboard";
+  public static final String BOTTOM_LEADERBOARD = "bottom_leaderboard";
+  public static final String TOP_BOXAD = "top_boxad";
+  public static final String INCONTENT_BOXAD = "incontent_boxad";
+  public static final String BOTTOM_BOXAD = "bottom_boxad";
+  public static final String FEED_BOXAD = "feed_boxad";
 
   static {
     SLOT_SELECTORS = new HashMap<>();
-    SLOT_SELECTORS.put(TOP_LEADERBOARD, "div[id$='TOP_LEADERBOARD_0__container__']");
+    SLOT_SELECTORS.put(TOP_LEADERBOARD, "div[id$='top_leaderboard_0__container__']");
     SLOT_SELECTORS.put(BOTTOM_LEADERBOARD, "div[id*='wka1a.PF/bottom_leaderboard'][id*='_0__container__']");
-    SLOT_SELECTORS.put(BOTTOM_LEADERBOARD_DESKTOP, "div[id*='wka1a.PF/bottom_leaderboard'][id*='_0__container__']");
-    SLOT_SELECTORS.put(BOTTOM_LEADERBOARD_MOBILE, "div[id*='wka1a.PF/bottom_leaderboard'][id*='_0__container__']");
-    SLOT_SELECTORS.put(TOP_BOXAD, "div[id$='TOP_BOXAD_0__container__']");
-    SLOT_SELECTORS.put(TOP_BOXAD_MOBILE, "div[id$='TOP_BOXAD_0__container__']");
-    SLOT_SELECTORS.put(TOP_BOXAD_DESKTOP, "div[id$='TOP_BOXAD_0__container__']");
-    SLOT_SELECTORS.put(INCONTENT_BOXAD, "div[id$='INCONTENT_BOXAD_0__container__']");
-    SLOT_SELECTORS.put(INCONTENT_BOXAD_MOBILE, "div[id$='INCONTENT_BOXAD_0__container__']");
-    SLOT_SELECTORS.put(BOTTOM_BOXAD, "div[id$='BOTTOM_BOXAD_0__container__']");
-    SLOT_SELECTORS.put(FEED_BOXAD, "div[id$='FEED_BOXAD_0__container__']");
-  }
-
-  static {
-    GPT_SLOT_SELECTORS = new HashMap<>();
-    GPT_SLOT_SELECTORS.put(TOP_LEADERBOARD, "gpt-top-leaderboard");
-    GPT_SLOT_SELECTORS.put(BOTTOM_LEADERBOARD, "gpt-bottom-leaderboard");
-    GPT_SLOT_SELECTORS.put(BOTTOM_LEADERBOARD_DESKTOP, "gpt-bottom-leaderboard-desktop");
-    GPT_SLOT_SELECTORS.put(BOTTOM_LEADERBOARD_MOBILE, "gpt-bottom-leaderboard-mobile");
-    GPT_SLOT_SELECTORS.put(TOP_BOXAD, "gpt-top-boxad");
-    GPT_SLOT_SELECTORS.put(TOP_BOXAD_DESKTOP, "gpt-top-boxad-desktop");
-    GPT_SLOT_SELECTORS.put(TOP_BOXAD_MOBILE, "gpt-top-boxad-mobile");
-    GPT_SLOT_SELECTORS.put(INCONTENT_BOXAD, "gpt-incontent-boxad");
-    GPT_SLOT_SELECTORS.put(INCONTENT_BOXAD_DESKTOP, "gpt-incontent-boxad-desktop");
-    GPT_SLOT_SELECTORS.put(INCONTENT_BOXAD_MOBILE, "gpt-incontent-boxad-mobile");
-    GPT_SLOT_SELECTORS.put(BOTTOM_BOXAD, "gpt-bottom-boxad");
-    GPT_SLOT_SELECTORS.put(FEED_BOXAD, "gpt-feed-boxad");
+    SLOT_SELECTORS.put(TOP_BOXAD, "div[id$='top_boxad_0__container__']");
+    SLOT_SELECTORS.put(INCONTENT_BOXAD, "div[id$='incontent_boxad_0__container__']");
+    SLOT_SELECTORS.put(BOTTOM_BOXAD, "div[id$='bottom_boxad_0__container__']");
+    SLOT_SELECTORS.put(FEED_BOXAD, "div[id$='feed_boxad_0__container__']");
   }
 
   private AdsFandomContent() {
@@ -68,10 +40,6 @@ public class AdsFandomContent {
   }
 
   public static By getSlotSelector(String slotName) {
-    return By.id(GPT_SLOT_SELECTORS.get(slotName));
-  }
-
-  public static String getGptSlotSelector(String slotName) {
-    return GPT_SLOT_SELECTORS.get(slotName);
+    return By.id(slotName);
   }
 }
