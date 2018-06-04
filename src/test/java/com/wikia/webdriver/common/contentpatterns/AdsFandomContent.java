489b3e7ab9bb841f2d988bd24386d3f0aca7de1e
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-06-04 14:24:54 +0200

    ADEN-7119 Fix iframe selectors

--- src/test/java/com/wikia/webdriver/common/contentpatterns/AdsFandomContent.java
+++ src/test/java/com/wikia/webdriver/common/contentpatterns/AdsFandomContent.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 public class AdsFandomContent {
 
+  public static final Map<String, String> IFRAME_SLOT_SELECTORS;
   public static final Map<String, String> SLOT_SELECTORS;
 
   public static final String TOP_LEADERBOARD = "top_leaderboard";
@@ -24,6 +25,9 @@ public class AdsFandomContent {
     SLOT_SELECTORS.put(INCONTENT_BOXAD, "div[id$='incontent_boxad_0__container__']");
     SLOT_SELECTORS.put(BOTTOM_BOXAD, "div[id$='bottom_boxad_0__container__']");
     SLOT_SELECTORS.put(FEED_BOXAD, "div[id$='feed_boxad_0__container__']");
+
+    IFRAME_SLOT_SELECTORS = new HashMap<>();
+    IFRAME_SLOT_SELECTORS.put(BOTTOM_LEADERBOARD, "div[id*='wka1a.PF/bottom_leaderboard'][id*='_0__container__'] iframe");
   }
 
   private AdsFandomContent() {
