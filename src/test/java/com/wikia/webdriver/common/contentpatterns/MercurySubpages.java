338a27a7398c6648fd42d99762f946e1ae157e83
ludwikkazmierczak <ludwik.kazmierczak@gmail.com>
2018-06-07 09:17:36 +0200

    Mercury subpages fix

--- src/test/java/com/wikia/webdriver/common/contentpatterns/MercurySubpages.java
+++ src/test/java/com/wikia/webdriver/common/contentpatterns/MercurySubpages.java
@@ -30,19 +30,19 @@ public class MercurySubpages {
    */
 
   // Articles prepared for mercurycc.wikia.com wiki (with FC, CC, TA and TV)
-  public static final String CC_MAIN_PAGE = "Mercury_CC_Wikia";
+  public static final String CC_MAIN_PAGE = "/Mercury_CC_Wikia";
 
   // Articles prepared for mercuryntacc.wikia.com wiki (with FC, CC and TV)
-  public static final String NTACC_MAIN_PAGE = "Mercuryntacc_Wikia";
+  public static final String NTACC_MAIN_PAGE = "/Mercuryntacc_Wikia";
 
   // Articles prepared for mercuryntvcc.wikia.com wiki (with FC, CC and TA)
-  public static final String NTVCC_MAIN_PAGE = "Mercuryntvcc_Wikia";
+  public static final String NTVCC_MAIN_PAGE = "/Mercuryntvcc_Wikia";
 
   // Articles prepared for mercuryntavcc.wikia.com wiki (with FC and CC)
-  public static final String NTAVCC_MAIN_PAGE = "Mercuryntavcc_Wikia";
+  public static final String NTAVCC_MAIN_PAGE = "/Mercuryntavcc_Wikia";
 
   // Articles prepared for mercuryemptycc.wikia.com wiki (without FC, CC, TA and TV)
-  public static final String ECC_MAIN_PAGE = "Mercury_Empty_CC_Editor_Wikia";
+  public static final String ECC_MAIN_PAGE = "/Mercury_Empty_CC_Editor_Wikia";
 
   // Articles on mlp.wikia.com
   public static final String MLP_MAIN_PAGE = "My_Little_Pony_Friendship_is_Magic_Wiki";
