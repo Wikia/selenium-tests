b6010da6bd8a782014886c00f09b716d0ea56a1b
qaga <qaga@wikia-inc.com>
2018-07-10 16:43:31 +0200

    removed error page test

--- src/test/java/com/wikia/webdriver/elements/mercury/pages/ErrorPage.java
+++ src/test/java/com/wikia/webdriver/elements/mercury/pages/ErrorPage.java
@@ -1,8 +1,8 @@
 package com.wikia.webdriver.elements.mercury.pages;
 
+import com.wikia.webdriver.common.core.Assertion;
 import com.wikia.webdriver.common.logging.Log;
 import com.wikia.webdriver.elements.common.Navigate;
-import com.wikia.webdriver.elements.mercury.components.Navigation;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.WikiBasePageObject;
 
 import org.openqa.selenium.WebElement;
@@ -10,8 +10,8 @@ import org.openqa.selenium.support.FindBy;
 
 public class ErrorPage extends WikiBasePageObject {
 
-  @FindBy(css = ".wiki-page-error a")
-  private WebElement linkToOpenNavigation;
+  @FindBy(css = ".wiki-page-header__subtitle")
+  private WebElement pageHeaderSubtitle;
 
   @FindBy(css = ".nav-menu")
   private WebElement navMenu;
@@ -24,21 +24,15 @@ public class ErrorPage extends WikiBasePageObject {
     navigate = new Navigate();
   }
 
+  private String errorPageSubtitle = "Sorry, we can't find the page you are looking for.";
+
   public ErrorPage navigateToErrorPageFromUrl() {
     navigate.toPageByPath("/wiki/404_Error_Page");
-    wait.forElementVisible(linkToOpenNavigation);
+    wait.forElementVisible(pageHeaderSubtitle);
+    Assertion.assertEquals(pageHeaderSubtitle.getText(), errorPageSubtitle);
     Log.info("Navigated to error 404 page");
 
     return this;
   }
 
-  public Navigation openNavigationByClickOnLink() {
-    wait.forElementClickable(linkToOpenNavigation);
-    linkToOpenNavigation.click();
-
-    wait.forElementVisible(navMenu);
-    Log.info("Navigation is opened");
-
-    return new Navigation();
-  }
 }
