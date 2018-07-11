fced88d3b0b4da1f460c142e03a0b26d64006441
Ludwik <ludwik.kazmierczak@gmail.com>
2018-07-11 08:24:36 +0200

    CR fixes

--- src/test/java/com/wikia/webdriver/common/core/drivers/BrowserAbstract.java
+++ src/test/java/com/wikia/webdriver/common/core/drivers/BrowserAbstract.java
@@ -91,6 +91,7 @@ public abstract class BrowserAbstract {
         server.setConnectTimeout(90, TimeUnit.SECONDS);
         server.setTrustSource(TrustSource.defaultTrustSource());
         server.setMitmDisabled(!Boolean.parseBoolean(Configuration.useMITM()));
+        server.setRequestTimeout(90, TimeUnit.SECONDS);
         server.enableHarCaptureTypes(CaptureType.REQUEST_HEADERS,
                                      CaptureType.RESPONSE_HEADERS);
         server.setUseEcc(true);
