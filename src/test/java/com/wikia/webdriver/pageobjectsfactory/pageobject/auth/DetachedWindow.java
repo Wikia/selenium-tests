8a1736afd4f8543a4da5da007ca6dc9635a75457
chessky <adamczewski@gmail.com>
2018-07-12 10:29:47 +0200

    Deleted gainFocus and loseFocus

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/auth/DetachedWindow.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/auth/DetachedWindow.java
@@ -4,20 +4,6 @@ import com.wikia.webdriver.pageobjectsfactory.pageobject.BasePageObject;
 
 public abstract class DetachedWindow extends BasePageObject {
 
-  protected void gainFocus() {
-    String title = getTitle();
-    if(!driver.getTitle().startsWith(title)) {
-      switchToWindowWithTitle(title);
-    }
-  }
-
-  protected void loseFocus() {
-    String title = getTitle();
-    if(driver.getTitle().startsWith(title)) {
-      switchAwayFromWindowWithTitle(title);
-    }
-  }
-
   protected abstract String getTitle();
 
 }
