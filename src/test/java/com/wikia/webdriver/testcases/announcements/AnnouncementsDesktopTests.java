a6456c45f397a55bbed647b99641c41c11ba6308
nikodamn <nikodem.hynek@wikia-inc.com>
2018-07-18 15:16:44 +0200

    New user on community part

--- src/test/java/com/wikia/webdriver/testcases/announcements/AnnouncementsDesktopTests.java
+++ src/test/java/com/wikia/webdriver/testcases/announcements/AnnouncementsDesktopTests.java
@@ -3,12 +3,15 @@ package com.wikia.webdriver.testcases.announcements;
 import com.wikia.webdriver.common.contentpatterns.MercuryWikis;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
+import com.wikia.webdriver.common.core.api.ArticleContent;
 import com.wikia.webdriver.common.core.api.UserRegistration;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.SignUpUser;
 import com.wikia.webdriver.common.properties.Credentials;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
-import com.wikia.webdriver.elements.mercury.pages.discussions.GuidelinesPage;
+import com.wikia.webdriver.elements.mercury.pages.ArticlePage;
+import com.wikia.webdriver.elements.mercury.pages.discussions.PostsListPage;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.globalnav.GlobalNavigation;
 import org.joda.time.DateTime;
 import org.testng.annotations.Test;
 
@@ -20,11 +23,13 @@ import java.time.LocalDate;
 @Test(groups = "Announcements_Desktop")
 public class AnnouncementsDesktopTests extends NewTestTemplate {
 
-    @Test
+    @Test()
     public void communityNewUserCanGetFreshAnnouncement() {
+        String articleName = "QA_article_" + Long.toString(DateTime.now().getMillis());
         String login = "QA" + Long.toString(DateTime.now().getMillis());
         String pass = Long.toString(DateTime.now().getMillis());
-        //create new account
+
+        //Create account and edit an article on a wiki
         SignUpUser newUser = new SignUpUser(
                 login,
                 new Credentials().emailQaart1,
@@ -32,10 +37,13 @@ public class AnnouncementsDesktopTests extends NewTestTemplate {
                 LocalDate.of(1990, 3, 19)
         );
         UserRegistration.registerUserEmailConfirmed(newUser);
-        //login to brand new account
-        new GuidelinesPage().open().getGlobalNavigation().clickOnSignIn().login(login, pass);
-        
+        GlobalNavigation nav = new PostsListPage().open().getGlobalNavigation();
+        nav.clickOnSignIn().login(login, pass);
+        new ArticleContent().push(articleName, articleName);
+        new ArticlePage().open(articleName).navigateToArticleEditPage().clearContent().clickPublishButton();
+        nav.clickUserAvatar().clickSignOut();
 
+        
 
     }
 
