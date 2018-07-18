c72d500af4bea0ddcd20e1587b1f91661387eb67
nikodamn <nikodem.hynek@wikia-inc.com>
2018-07-18 14:18:54 +0200

    Create new user with a request

--- src/test/java/com/wikia/webdriver/testcases/announcements/AnnouncementsDesktopTests.java
+++ src/test/java/com/wikia/webdriver/testcases/announcements/AnnouncementsDesktopTests.java
@@ -3,9 +3,15 @@ package com.wikia.webdriver.testcases.announcements;
 import com.wikia.webdriver.common.contentpatterns.MercuryWikis;
 import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
+import com.wikia.webdriver.common.core.api.UserRegistration;
 import com.wikia.webdriver.common.core.drivers.Browser;
+import com.wikia.webdriver.common.core.helpers.SignUpUser;
+import com.wikia.webdriver.common.properties.Credentials;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
+import org.joda.time.DateTime;
 import org.testng.annotations.Test;
+
+import java.time.LocalDate;
 
 
 @InBrowser(browser = Browser.CHROME)
@@ -15,7 +21,13 @@ public class AnnouncementsDesktopTests extends NewTestTemplate {
 
     @Test
     public void communityNewUserCanGetFreshAnnouncement() {
-
+        SignUpUser newUser = new SignUpUser(
+                "QA" + Long.toString(DateTime.now().getMillis()),
+                new Credentials().emailQaart1,
+                Long.toString(DateTime.now().getMillis()),
+                LocalDate.of(1990, 3, 19)
+        );
+        UserRegistration.registerUserEmailConfirmed(newUser);
 
 
     }
