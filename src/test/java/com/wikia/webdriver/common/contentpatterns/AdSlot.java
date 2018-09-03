041a62c1dedc4af4c13f504938978b471bf8c654
emilmadry <emadry@wikia-inc.com>
2018-09-03 16:49:32 +0200

    Ad slots selectors fixed

--- src/test/java/com/wikia/webdriver/common/contentpatterns/AdSlot.java
+++ src/test/java/com/wikia/webdriver/common/contentpatterns/AdSlot.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 public enum AdSlot {
   TOP_LEADERBOARD("top_leaderboard","div[id$='top_leaderboard_0__container__']"),
   BOTTOM_LEADERBOARD("bottom_leaderboard","div[id*='wka1a.PF/bottom_leaderboard'][id*='_0__container__']"),
-  TOP_BOXAD("top_boxad", "div[id$='top_boxad']"),
-  INCONTENT_BOXAD("incontent_boxad", "div[id$='incontent_boxad']"),
+  TOP_BOXAD("top_boxad", "div[id*='top_boxad_0__container__']"),
+  INCONTENT_BOXAD("incontent_boxad", "div[id*='HiVi/incontent_boxad']"),
   BOTTOM_BOXAD("bottom_boxad", "div[id$='bottom_boxad_0__container__']"),
   FEED_BOXAD("feed_boxad", "div[id$='google_ads_iframe_/5441/wka1a.PF/feed_boxad/desktop/ns-article/_fandom-all_0__container__']"),
   FEED_BOXAD_MOBILE("feed_boxad", "div[id$='google_ads_iframe_/5441/wka1a.PF/feed_boxad/smartphone/ns-article/_fandom-all_0__container__']"),
@@ -15,15 +15,15 @@ public enum AdSlot {
   FEED_BOXAD_MOBILE_IFRAME("feed_boxad", "div[id$='google_ads_iframe_/5441/wka1a.PF/feed_boxad/smartphone/ns-topic/_fandom-all_0__container__']"),
   BOTTOM_LEADERBOARD_IFRAME("bottom_leaderboard", "div[id*='wka1a.PF/bottom_leaderboard'][id*='_0__container__'] iframe");
 
+  @Getter
+  private final String name;
 
   @Getter
   private final String id;
 
-  @Getter
-  private final String name;
 
   AdSlot(String name, String id) {
-    this.id = id;
     this.name = name;
+    this.id = id;
   }
 }
