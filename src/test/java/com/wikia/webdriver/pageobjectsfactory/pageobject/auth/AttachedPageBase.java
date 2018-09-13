e122f0251b01bf9631b67b125336fb0eab26dea1
Szymon Czerwinski <sczerwinski@wikia-inc.com>
2018-09-13 16:35:16 +0200

    Changed indent style to spaces

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/auth/AttachedPageBase.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/auth/AttachedPageBase.java
@@ -8,33 +8,33 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import org.openqa.selenium.WebDriverException;
 
-public abstract class AttachedPageBase<T extends AttachedPageBase<T>> extends BasePageObject{
+public abstract class AttachedPageBase<T extends AttachedPageBase<T>> extends BasePageObject {
 
-	private String siteSpecificPath;
+  private String siteSpecificPath;
 
-	public AttachedPageBase(String siteSpecificPath) {
-		this.siteSpecificPath = siteSpecificPath;
-	}
+  public AttachedPageBase(String siteSpecificPath) {
+    this.siteSpecificPath = siteSpecificPath;
+  }
 
-	protected abstract T getThis();
+  protected abstract T getThis();
 
-	public T open() {
-		open(getWikiUrl() + URLsContent.USER_LOGIN);
-		return getThis();
-	}
+  public T open() {
+    open(getWikiUrl() + URLsContent.USER_LOGIN);
+    return getThis();
+  }
 
-	public T open(String redirectUrl) {
-		String signinUrl = urlBuilder.getWikiGlobalURL() + siteSpecificPath;
-		try {
-			getUrl(urlBuilder.appendQueryStringToURL(signinUrl,
-					URLEncoder.encode(redirectUrl, StandardCharsets.UTF_8.name()))
-			);
-		} catch (UnsupportedEncodingException e) {
-			Log.log("Opening attached page", "Unable to encode redirect", false);
-			throw new WebDriverException("Unable to encode redirect", e);
-		}
-		Log.log("Opening attached page", "Special:UserLogin page opened", true);
+  public T open(String redirectUrl) {
+    String signinUrl = urlBuilder.getWikiGlobalURL() + siteSpecificPath;
+    try {
+      getUrl(urlBuilder.appendQueryStringToURL(signinUrl,
+          URLEncoder.encode(redirectUrl, StandardCharsets.UTF_8.name()))
+      );
+    } catch (UnsupportedEncodingException e) {
+      Log.log("Opening attached page", "Unable to encode redirect", false);
+      throw new WebDriverException("Unable to encode redirect", e);
+    }
+    Log.log("Opening attached page", "Special:UserLogin page opened", true);
 
-		return getThis();
-	}
+    return getThis();
+  }
 }
