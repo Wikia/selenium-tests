2540ec9d095005dd78be1ae776f1dd3c1456809c
emilmadry <emadry@wikia-inc.com>
2018-09-20 13:06:07 +0200

    Tracking opt in mobile wiki fix

--- src/test/java/com/wikia/webdriver/testcases/desktop/adstests/TestAdsTrackingOptInRequestsMobileWiki.java
+++ src/test/java/com/wikia/webdriver/testcases/desktop/adstests/TestAdsTrackingOptInRequestsMobileWiki.java
@@ -327,12 +327,12 @@ public class TestAdsTrackingOptInRequestsMobileWiki extends NewTestTemplate {
     modal.setGeoCookie(driver, "NA", "US");
     modal.getUrl(ADS_ARTICLE1_PAGE);
 
-    modal.verifyTrackingRequestsSend(urlPatterns, networkTrafficInterceptor);
+    modal.verifyTrackingRequestsSendOutsideEU(urlPatterns, networkTrafficInterceptor);
     // Check tracking pixels on consecutive page views
     for (String linkName : articles) {
       ads.verifySlotExpanded(AdsContent.MOBILE_TOP_LB);
       ads.clickOnArticleLink(linkName);
-      modal.verifyTrackingRequestsSend(urlPatterns, networkTrafficInterceptor);
+      modal.verifyTrackingRequestsSendOutsideEU(urlPatterns, networkTrafficInterceptor);
     }
   }
 
@@ -346,6 +346,6 @@ public class TestAdsTrackingOptInRequestsMobileWiki extends NewTestTemplate {
     modal.setGeoCookie(driver, "NA", "US");
     modal.getUrl(ADS_HOME_PAGE);
 
-    modal.verifyTrackingRequestsSend(urlPatterns, networkTrafficInterceptor);
+    modal.verifyTrackingRequestsSendOutsideEU(urlPatterns, networkTrafficInterceptor);
   }
 }
