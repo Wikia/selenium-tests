548af6026f918ba26fd08b0b20cb3620db58210f
chessky <adamczewski@gmail.com>
2018-10-15 10:31:35 +0200

    Selector update

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/editprofile/AvatarComponentObject.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/componentobject/editprofile/AvatarComponentObject.java
@@ -10,7 +10,7 @@ import java.io.File;
 
 public class AvatarComponentObject extends WikiBasePageObject {
 
-  @FindBy(id = "avatar-upload-input")
+  @FindBy(id = "UPPLightboxAvatarUpload")
   private WebElement uploadInput;
   @FindBy(css = "#UPPLightboxWrapper [data-event=save]")
   private WebElement saveButton;
