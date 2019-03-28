767d364b9eb92dee90acbf9446cf8d822f5d98a2
emilmadry <emadry@wikia-inc.com>
2019-03-28 10:30:03 +0100

    Free wikis split to mobile and desktop

---
+++ src/test/java/com/wikia/webdriver/testcases/desktop/adstests/TestAdsNoAdsOnFreeWikisMercury.java
@@ -0,0 +1,25 @@
+package com.wikia.webdriver.testcases.desktop.adstests;
+
+import com.wikia.webdriver.common.contentpatterns.AdsContent;
+import com.wikia.webdriver.common.core.annotations.InBrowser;
+import com.wikia.webdriver.common.core.drivers.Browser;
+import com.wikia.webdriver.common.core.helpers.Emulator;
+import com.wikia.webdriver.common.core.url.UrlBuilder;
+import com.wikia.webdriver.common.dataprovider.ads.AdsDataProvider;
+import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsBaseObject;
+
+import org.testng.annotations.Test;
+
+@Test(groups = "AdsNoAdsOnAdsFreeWikisMercury")
+public class TestAdsNoAdsOnFreeWikisMercury extends TemplateNoFirstLoad {
+
+  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
+  @Test(dataProviderClass = AdsDataProvider.class, dataProvider = "adFreeWikis", groups = "AdsNoAdsOnAdsFreeWikisMercury")
+  public void testNoAdsMercury(String wikiName, String path) {
+    String testedPage = UrlBuilder.createUrlBuilderForWiki(wikiName).getUrlForPath(path);
+    AdsBaseObject wikiPage = new AdsBaseObject(testedPage);
+    wikiPage.setEnvironment(AdsContent.ENV_MOBILE);
+    wikiPage.verifyNoAdsOnPage();
+  }
+}
--- src/test/java/com/wikia/webdriver/testcases/desktop/adstests/TestAdsNoAdsOnFreeWikisOasis.java
+++ src/test/java/com/wikia/webdriver/testcases/desktop/adstests/TestAdsNoAdsOnFreeWikisOasis.java
@@ -1,9 +1,5 @@
 package com.wikia.webdriver.testcases.desktop.adstests;
 
-import com.wikia.webdriver.common.contentpatterns.AdsContent;
-import com.wikia.webdriver.common.core.annotations.InBrowser;
-import com.wikia.webdriver.common.core.drivers.Browser;
-import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.url.UrlBuilder;
 import com.wikia.webdriver.common.dataprovider.ads.AdsDataProvider;
 import com.wikia.webdriver.common.templates.TemplateNoFirstLoad;
@@ -18,15 +14,6 @@ public class TestAdsNoAdsOnFreeWikisOasis extends TemplateNoFirstLoad {
   public void testNoAdsOasis(String wikiName, String path) {
     String testedPage = UrlBuilder.createUrlBuilderForWiki(wikiName).getUrlForPath(path);
     AdsBaseObject wikiPage = new AdsBaseObject(testedPage);
-    wikiPage.verifyNoAdsOnPage();
-  }
-
-  @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
-  @Test(dataProviderClass = AdsDataProvider.class, dataProvider = "adFreeWikis", groups = "AdsNoAdsOnAdsFreeWikisMercury")
-  public void testNoAdsMercury(String wikiName, String path) {
-    String testedPage = UrlBuilder.createUrlBuilderForWiki(wikiName).getUrlForPath(path);
-    AdsBaseObject wikiPage = new AdsBaseObject(testedPage);
-    wikiPage.setEnvironment(AdsContent.ENV_MOBILE);
     wikiPage.verifyNoAdsOnPage();
   }
 }
