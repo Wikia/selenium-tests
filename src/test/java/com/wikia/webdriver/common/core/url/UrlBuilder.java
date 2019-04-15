92b720e13062bfaeaad31f49193912369388a078
swietlana <mcabaj@wikia-inc.com>
2019-04-15 15:48:30 +0200

    Add wikis excluded from '-fandom' suffix list

--- src/test/java/com/wikia/webdriver/common/core/url/UrlBuilder.java
+++ src/test/java/com/wikia/webdriver/common/core/url/UrlBuilder.java
@@ -19,7 +19,7 @@ public class UrlBuilder extends BaseUrlBuilder {
   public static final String WIKI_NAME_FANDOM_SUFFIX = "fandom";
   private static final Set<String>
       FANDOM_EXCLUDED_WIKIS =
-      new HashSet<>(Arrays.asList(new String[]{"community", "gameofthrones", "sydneybuses", "elderscrolls"}));
+      new HashSet<>(Arrays.asList(new String[]{"community", "gameofthrones", "sydneybuses", "elderscrolls", "gta", "harrypotter"}));
   private final String wikiName;
   private Boolean forceHttps;
   private Boolean forceWikiaOrg;
