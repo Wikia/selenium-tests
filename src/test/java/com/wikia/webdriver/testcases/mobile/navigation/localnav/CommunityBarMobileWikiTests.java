23eff1e708c2ea86c809b4e2f6e3114d49d5812f
swietlana <mcabaj@wikia-inc.com>
2019-05-23 12:19:18 +0200

    Unneeded imports

--- src/test/java/com/wikia/webdriver/testcases/mobile/navigation/localnav/CommunityBarMobileWikiTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mobile/navigation/localnav/CommunityBarMobileWikiTests.java
@@ -9,10 +9,8 @@ import com.wikia.webdriver.common.core.configuration.Configuration;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.Emulator;
 import com.wikia.webdriver.common.core.helpers.User;
-import com.wikia.webdriver.common.core.url.UrlBuilder;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.elements.communities.mobile.pages.ArticlePage;
-import com.wikia.webdriver.elements.communities.mobile.pages.discussions.DiscussionsPage;
 import com.wikia.webdriver.elements.communities.mobile.components.navigation.local.CommunityBarMobile;
 
 import org.testng.annotations.Test;
