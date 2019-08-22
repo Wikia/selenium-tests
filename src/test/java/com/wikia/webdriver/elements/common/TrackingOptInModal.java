ba6bca2580e4e9e405c49fc9992f30b7dc74d351
Emil Madry <40793618+emadry@users.noreply.github.com>
2019-08-22 11:31:39 +0200

    ADEN-9224 Tracking Opt Out (#2628)
    * Consent string length updated
    
    * ADEN-9224 Rejecting tracking handled
    
    * Page refresh added
    
    * Revert "Page refresh added"
    
    This reverts commit 17d110c5c46a92cac2d294f83e197c724630e139.

--- src/test/java/com/wikia/webdriver/elements/common/TrackingOptInModal.java
+++ src/test/java/com/wikia/webdriver/elements/common/TrackingOptInModal.java
@@ -5,20 +5,30 @@ import com.wikia.webdriver.pageobjectsfactory.pageobject.BasePageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class TrackingOptInModal {
+import java.util.List;
+
+public class TrackingOptInModal extends BasePageObject {
 
   private static final By MODAL_SELECTOR = By.cssSelector("div[data-tracking-opt-in-overlay]");
   private static final By
       ACCEPT_BUTTON_SELECTOR
       = By.cssSelector("div[data-tracking-opt-in-accept]");
-  private static final By
-      REJECT_BUTTON_SELECTOR
-      = By.cssSelector("div[data-tracking-opt-in-reject]");
+  private static final By LEARN_MORE_BUTTON_SELECTOR = By.cssSelector(
+      "div[data-tracking-opt-in-learn-more]");
   private final BasePageObject page;
+  private static final By
+      SAVE_CHANGES_BUTTON_SELECTOR
+      = By.cssSelector("div[data-tracking-opt-in-save]");
+  @FindBy(css = "div[data-tracking-opt-in-overlay] [for]")
+  private List<WebElement> allFeatureSwitches;
 
   public TrackingOptInModal(BasePageObject page) {
     this.page = page;
+    PageFactory.initElements(driver, this);
   }
 
   public boolean isVisible() {
@@ -41,12 +51,29 @@ public class TrackingOptInModal {
       Log.log("Accept button clicked", e, false);
     }
   }
-
-  public void clickRejectButton() {
+  public void clickSaveChangesButton() {
     try {
-      page.wait.forElementClickable(REJECT_BUTTON_SELECTOR).click();
+      page.wait.forElementClickable(SAVE_CHANGES_BUTTON_SELECTOR).click();
+    } catch (Exception e) {
+      Log.log("Save changes button clicked", e, false);
+    }
+  }
+
+  public void clickLearnMoreButton() {
+    try {
+      page.wait.forElementClickable(LEARN_MORE_BUTTON_SELECTOR).click();
     } catch (Exception e) {
       Log.log("Reject button clicked", e, false);
+    }
+  }
+
+  public void rejectAllFeatures() {
+    try {
+      for (WebElement featureSwitch : allFeatureSwitches) {
+        featureSwitch.click();
+      }
+    } catch (TimeoutException e) {
+      Log.log("Feature switches clicked", e, false);
     }
   }
 }
