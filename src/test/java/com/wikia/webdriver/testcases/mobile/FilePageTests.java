504c16ca300927d6bfde6e61e327e868f2970cef
swietlana <mcabaj@wikia-inc.com>
2019-12-04 15:51:48 +0100

    IW | run FilePage test as User

--- src/test/java/com/wikia/webdriver/testcases/mobile/FilePageTests.java
+++ src/test/java/com/wikia/webdriver/testcases/mobile/FilePageTests.java
@@ -7,6 +7,7 @@ import com.wikia.webdriver.common.core.annotations.Execute;
 import com.wikia.webdriver.common.core.annotations.InBrowser;
 import com.wikia.webdriver.common.core.drivers.Browser;
 import com.wikia.webdriver.common.core.helpers.Emulator;
+import com.wikia.webdriver.common.core.helpers.User;
 import com.wikia.webdriver.common.templates.NewTestTemplate;
 import com.wikia.webdriver.elements.communities.mobile.pages.ArticlePage;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.FilePageObject;
@@ -14,7 +15,7 @@ import com.wikia.webdriver.pageobjectsfactory.pageobject.FilePageObject;
 import org.testng.annotations.Test;
 
 @Test(groups = "MobileWiki_FilePage")
-@Execute(onWikia = MobileWikis.MERCURY_AUTOMATION_TESTING)
+@Execute(onWikia = MobileWikis.MERCURY_AUTOMATION_TESTING, asUser = User.USER)
 @InBrowser(browser = Browser.CHROME, emulator = Emulator.GOOGLE_NEXUS_5)
 public class FilePageTests extends NewTestTemplate {
 
