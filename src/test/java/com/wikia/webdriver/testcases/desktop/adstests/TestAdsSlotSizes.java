7e9a1344ad192cedb489c6f3a1b83b7573fc6bdd
emilmadry <emadry@wikia-inc.com>
2019-12-11 19:20:50 +0100

    Moblie slot sizes moved

--- src/test/java/com/wikia/webdriver/testcases/desktop/adstests/TestAdsSlotSizes.java
+++ src/test/java/com/wikia/webdriver/testcases/desktop/adstests/TestAdsSlotSizes.java
@@ -23,13 +23,6 @@ public class TestAdsSlotSizes extends TemplateNoFirstLoad {
     adsSlotSizes(page, urlParamToEnable, WindowSize.DESKTOP, slotInfo, false);
   }
 
-  @Test(dataProviderClass = MobileAdsDataProvider.class, dataProvider = "adsSlotSizeMercury", groups = "AdsSlotSizesMercury")
-  public void adsSlotSizesMercury(
-      Page page, String urlParamToEnable, Map<String, Object> slotInfo
-  ) {
-    adsSlotSizes(page, urlParamToEnable, WindowSize.PHONE, slotInfo, true);
-  }
-
   private void adsSlotSizes(
       Page page, String urlParamToEnable, Dimension pageSize, Map<String, Object> slotInfo, Boolean isMobile
   ) {
